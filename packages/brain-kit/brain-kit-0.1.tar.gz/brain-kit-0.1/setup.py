cbd7ec6c60c1990bad817fb1d3a1674950d81132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package brain-kit/0.1/brain-kit-0.1.tar.gz

---
+++ packages/brain-kit/brain-kit-0.1.tar.gz/brain-kit-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'brain-kit',
+  packages = ['brain-kit'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Brain Image Analysis Algorithms',
+  author = 'Anand A Joshi',
+  author_email = 'ajoshi@sipi.usc.edu',
+  url = 'https://github.com/ajoshiusc/brain-kit', # use the URL to the github repo
+  download_url = 'https://github.com/ajoshiusc/brain-kit/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
