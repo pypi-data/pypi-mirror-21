e4721c68f9bd8984b709eb59577b431ad724a3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package inform/1.8.0/inform-1.8.0.tar.gz

---
+++ packages/inform/inform-1.8.0.tar.gz/inform-1.8.0/inform/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '1.8.0'
+__released__ = '2017-03-31'
+
+from .inform import (
+    # inform utilities
+    indent, cull, is_str, is_iterable, is_collection,
+
+    # user utilities
+    Color, fmt, render, os_error, conjoin, plural, full_stop, ppp, ddd, vvv,
+
+    # inform classes
+    InformantGenerator, Inform, Error,
+
+    # inform functions
+    done, terminate, terminate_if_errors, errors_accrued,
+
+    # built-in informants
+    log, comment, codicil, narrate, display, output,
+    notify, debug, warn, error, fatal, panic,
+)
