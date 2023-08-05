8490e2979a6696b092730a8e1aa9759fcb5c1ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:22 +0000

    Add package singer-python/1.0.0/singer-python-1.0.0.tar.gz

---
+++ packages/singer-python/singer-python-1.0.0.tar.gz/singer-python-1.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import subprocess
+
+
+setup(name="singer-python",
+      version="1.0.0",
+      description="Singer.io utility library",
+      author="Stitch",
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      url="http://singer.io",
+      packages=find_packages(),
+      package_data = {
+          'singer': [
+              'logging.conf'
+              ]
+          },
+)
