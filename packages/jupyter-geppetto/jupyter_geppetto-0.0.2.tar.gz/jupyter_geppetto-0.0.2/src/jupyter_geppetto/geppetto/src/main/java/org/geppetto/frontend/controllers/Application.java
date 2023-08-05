660c248a189986ca4f5c2c21493cb44992f73866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package jupyter-geppetto/0.0.2/jupyter_geppetto-0.0.2.tar.gz

---
+++ packages/jupyter-geppetto/jupyter_geppetto-0.0.2.tar.gz/jupyter_geppetto-0.0.2/src/jupyter_geppetto/geppetto/src/main/java/org/geppetto/frontend/controllers/Application.java
@@ -0,0 +1,175 @@
+package org.geppetto.frontend.controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.geppetto.core.auth.IAuthService;
+import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.core.data.DefaultGeppettoDataManager;
+import org.geppetto.core.manager.IGeppettoManager;
+import org.geppetto.core.utilities.URLReader;
+import org.geppetto.frontend.tests.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+@Controller
+public class Application
+{
+
+	@Autowired
+	private IGeppettoManager geppettoManager;
+
+	private static Log logger = LogFactory.getLog(Application.class);
+
+	@RequestMapping(value = "/geppetto", method = RequestMethod.GET)
+	public String geppetto(HttpServletRequest req)
+	{
+		try
+		{
+			IAuthService authService = AuthServiceCreator.getService();
+			boolean auth = false;
+
+			if(authService.isDefault())
+			{
+				// Default no persistence, no users
+				auth = true;
+				if(geppettoManager.getUser()==null)
+				{
+					try
+					{
+						geppettoManager.setUser(DefaultGeppettoDataManager.getGuestUser());
+					}
+					catch(GeppettoExecutionException e)
+					{
+						logger.error(e);
+					}
+				}
+			}
+			else if(geppettoManager.getUser() != null)
+			{
+				// This is with Geppetto DB, for the user to not be null inside the GeppettoManager somebody must have used
+				// the Login servlet
+				Subject currentUser = SecurityUtils.getSubject();
+				auth = currentUser.isAuthenticated();
+			}
+			else if(geppettoManager.getUser() == null)
+			{
+				// This is with any other authentication system from another web application.
+				// since sharing the scope session across the different web application bundles
+				// is more complex than expected (if possible at all) we are using cookies
+				for(Cookie c : req.getCookies())
+				{
+					if(c.getName().equals(authService.getSessionId()))
+					{
+						auth = authService.isAuthenticated(c.getValue());
+						break;
+					}
+				}
+			}
+			if(auth)
+			{
+				return "dist/geppetto";
+			}
+			else
+			{
+				return "redirect:" + authService.authFailureRedirect();
+			}
+		}
+		catch(GeppettoInitializationException e)
+		{
+			logger.error("Error while retrieving an authentication service", e);
+		}
+		return "redirect:http://geppetto.org";
+	}
+
+	@RequestMapping(value = "/geppettotestingprojects", method = RequestMethod.GET)
+	public @ResponseBody Test getTestingProjects(@RequestParam String url) throws IOException
+	{
+		URL resource = URLReader.getURL(url);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream()));
+		return new Gson().fromJson(reader, Test.class);
+	}
+	
+	@RequestMapping(value = "/GeppettoNeuronalTests.html", method = RequestMethod.GET)
+	public String testNeuronal()
+	{
+		return "dist/GeppettoNeuronalTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoNeuronalCustomTests.html", method = RequestMethod.GET)
+	public String testNeuronalCustom()
+	{
+		return "dist/GeppettoNeuronalCustomTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoComponentsTests.html", method = RequestMethod.GET)
+	public String testComponents()
+	{
+		return "dist/GeppettoComponentsTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoCoreTests.html", method = RequestMethod.GET)
+	public String testCore()
+	{
+		return "dist/GeppettoCoreTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoExternalSimulatorTests.html", method = RequestMethod.GET)
+	public String testExternalSimulator()
+	{
+		return "dist/GeppettoExternalSimulatorTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoPersistenceTests.html", method = RequestMethod.GET)
+	public String testPersistence()
+	{
+		return "dist/GeppettoPersistenceTests";
+	}
+	
+	@RequestMapping(value = "/GeppettoPersistenceTests-alt.html", method = RequestMethod.GET)
+	public String testAltPersistence()
+	{
+		return "dist/GeppettoPersistenceTests-alt";
+	}
+	
+	@RequestMapping(value = "/GeppettoFluidDynamicsTests.html", method = RequestMethod.GET)
+	public String testFluidDynamics()
+	{
+		return "dist/GeppettoFluidDynamicsTests";
+	}
+
+	@RequestMapping(value = "/GeppettoAllTests.html", method = RequestMethod.GET)
+	public String runTests()
+	{
+		return "dist/GeppettoAllTests";
+	}
+	
+	@RequestMapping(value = "/tests.html", method = RequestMethod.GET)
+	public String tests()
+	{
+		return "dist/tests";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String dashboard()
+	{
+		return "dist/dashboard";
+	}
+
+}
---
+++ packages/jupyter-geppetto/jupyter_geppetto-0.0.2.tar.gz/jupyter_geppetto-0.0.2/src/jupyter_geppetto/geppetto/src/main/java/org/geppetto/frontend/controllers/Login.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+
+package org.geppetto.frontend.controllers;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.data.DataManagerHelper;
+import org.geppetto.core.data.IGeppettoDataManager;
+import org.geppetto.core.data.model.IUser;
+import org.geppetto.core.manager.IGeppettoManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Login
+{
+
+	private static Log logger = LogFactory.getLog(Login.class);
+
+	@Autowired
+	private IGeppettoManager geppettoManager;
+
+	public Login()
+	{
+	}
+
+	@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+	public @ResponseBody
+	IUser login(@RequestParam String username, @RequestParam String password, @RequestParam(defaultValue="",required=false) String url, @RequestParam(defaultValue="web",required=false) String outputFormat, HttpServletResponse response) throws IOException
+	{
+		IGeppettoDataManager dataManager = DataManagerHelper.getDataManager();
+		if(!dataManager.isDefault())
+		{
+			Subject currentUser = SecurityUtils.getSubject();
+			if(!currentUser.isAuthenticated())
+			{
+				UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+				currentUser.login(token);
+			}
+
+			try
+			{
+				IUser user = (IUser) currentUser.getPrincipal();
+				user.addLoginTimeStamp(new Date());
+				DataManagerHelper.getDataManager().saveEntity(user);
+				geppettoManager.setUser(user);
+			}
+			catch(GeppettoExecutionException e)
+			{
+				logger.error(e);
+			}
+		}
+		if (outputFormat.equals("web"))
+			response.sendRedirect("/" + url);
+		return geppettoManager.getUser();
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(@RequestParam(defaultValue="web",required=false) String outputFormat, HttpServletResponse response) throws IOException
+	{
+		IGeppettoDataManager dataManager = DataManagerHelper.getDataManager();
+		if(!dataManager.isDefault())
+		{
+			Subject currentUser = SecurityUtils.getSubject();
+			if(geppettoManager.getUser() != null && currentUser.isAuthenticated())
+			{
+				currentUser.logout();
+				try
+				{
+					geppettoManager.setUser(null);
+				}
+				catch(GeppettoExecutionException e)
+				{
+					logger.error(e);
+				}
+			}
+		}
+		if (outputFormat.equals("web"))
+			response.sendRedirect("/");
+		return null;
+		
+	}
+
+}
