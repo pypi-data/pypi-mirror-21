7fdc0394c9026f896e56fe93462c3fd4b1ca2da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package mosfit/0.4.3/mosfit-0.4.3.tar.gz

---
+++ packages/mosfit/mosfit-0.4.3.tar.gz/mosfit-0.4.3/mosfit/modules/seds/line.py
@@ -0,0 +1,35 @@
+"""Definitions for the `Line` class."""
+from mosfit.modules.seds.sed import SED
+
+# Important: Only define one ``Module`` class per file.
+
+
+class Line(SED):
+    """Line spectral energy distribution, modifies existing SED."""
+
+    def process(self, **kwargs):
+        """Process module."""
+        kwargs = self.prepare_input(self.key('luminosities'), **kwargs)
+        self._seds = kwargs[self.key('seds')]
+        self._bands = kwargs['all_bands']
+        self._band_indices = kwargs['all_band_indices']
+        self._sample_wavelengths = kwargs['sample_wavelengths']
+        self._luminosities = kwargs[self.key('luminosities')]
+        zp1 = 1.0 + kwargs[self.key('redshift')]
+
+        seds = []
+        for li, lum in enumerate(self._luminosities):
+            bi = self._band_indices[li]
+            if bi >= 0:
+                rest_freqs = self._sample_frequencies[bi] * zp1
+            else:
+                rest_freqs = [self._frequencies[li] * zp1]
+
+            # Dummy function for now, needs implementation
+            sed = [0.0 for x in rest_freqs]
+            seds.append(list(sed))
+
+        seds = self.add_to_existing_seds(seds, **kwargs)
+
+        return {'sample_wavelengths': self._sample_wavelengths,
+                self.key('seds'): seds}
