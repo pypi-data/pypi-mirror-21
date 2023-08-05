90a6a6b03e50c6383b9c933664bce871a93a6de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:17 +0000

    Add package libcnmc/0.17.5/libcnmc-0.17.5.tar.gz

---
+++ packages/libcnmc/libcnmc-0.17.5.tar.gz/libcnmc-0.17.5/libcnmc/models/__init__.py
@@ -0,0 +1,19 @@
+from cnmcmodel import CNMCModel
+
+from res_4771 import F1Res4771
+from res_4771 import F2Res4771
+from res_4771 import F3Res4771
+from res_4771 import F4Res4771
+from res_4771 import F5Res4771
+from res_4771 import F6Res4771
+from res_4771 import F7Res4771
+from res_4771 import F8Res4771
+
+from res_4131 import F1Res4131
+from res_4131 import F2Res4131
+from res_4131 import F3Res4131
+from res_4131 import F4Res4131
+from res_4131 import F5Res4131
+from res_4131 import F6Res4131
+from res_4131 import F7Res4131
+from res_4131 import F8Res4131
