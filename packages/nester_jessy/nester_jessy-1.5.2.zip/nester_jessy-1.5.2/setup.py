2854e38c01f3ab11e05418062d7efb407b9eba52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package nester_jessy/1.5.2/nester_jessy-1.5.2.zip

---
+++ packages/nester_jessy/nester_jessy-1.5.2.zip/nester_jessy-1.5.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         = 'nester_jessy',
+    version      = '1.5.2',
+    py_modules   = ['nester_jessy'],
+    author       = 'Lijing',
+    author_email = '1056851173@qq.com',
+    description  = 'A simple printer of nested lists',
+    )
