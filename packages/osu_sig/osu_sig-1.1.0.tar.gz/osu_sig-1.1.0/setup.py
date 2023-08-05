0dcae667ece9c664504a2a565a1e7ff4ea5b7c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package osu_sig/1.1.0/osu_sig-1.1.0.tar.gz

---
+++ packages/osu_sig/osu_sig-1.1.0.tar.gz/osu_sig-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='osu_sig',
+    version='1.1.0',
+    packages=['osu_sig'],
+    url='https://github.com/MaT1g3R/Osu-Signature-Generator',
+    license='MIT',
+    author='Yasen-Baka',
+    author_email='982341777@qq.com',
+    description='A Python wrapper for osu!next Signature Generator '
+)
