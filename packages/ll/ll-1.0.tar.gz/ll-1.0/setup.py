2ab6c8d68a676445ed5e31e1e08d003ef0fcf9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:00 +0000

    Add package ll/1.0/ll-1.0.tar.gz

---
+++ packages/ll/ll-1.0.tar.gz/ll-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ll',
+    version='1.0',
+    author='Amir Nasri',
+    author_email='amnasri@gmail.com',
+    packages=['ll'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/ll/',
+    description='linked list data structure in cpython.',
+)
