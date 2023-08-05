9ac6d761a5fe62f34dfb704a85be72e884d1393c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:43 +0000

    Add package impyute/0.0.1/impyute-0.0.1-py2.py3-none-any.whl

---
+++ packages/impyute/impyute-0.0.1-py2.py3-none-any.whl/impyute/diagnostics/compare.py
@@ -0,0 +1,23 @@
+"""impyute.diagnostics.compare.py"""
+
+
+def Tester:
+    """Automates testing of datasets, machine learning models and imputation
+    algorithms"""
+    def __init__(self):
+        """ Initializes the Tester object
+
+        """
+        pass
+
+    def add(self, key, value):
+        """
+        """
+        self.parameters[key].append(value)
+
+    def _load_datasets(self, name):
+        pass
+    
+    def run(self):
+        datasets = _load_datasets(self.datasets)
+
