c059d22e5ec814b37cc850fcfd5d4e82ab159852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package empymod/1.3.0/empymod-1.3.0.tar.gz

---
+++ packages/empymod/empymod-1.3.0.tar.gz/empymod-1.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='empymod',
+    version='1.3.0',
+    description='ElectroMagnetic Python forward MODeller (1D)',
+    long_description=readme,
+    author='Dieter Werthmüller',
+    author_email='dieter@werthmuller.org',
+    url='https://github.com/prisae/empymod',
+    download_url='https://github.com/prisae/empymod/tarball/v1.3.0',
+    license='Apache License V2.0',
+    packages=['empymod'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['numpy', 'scipy'],
+)
