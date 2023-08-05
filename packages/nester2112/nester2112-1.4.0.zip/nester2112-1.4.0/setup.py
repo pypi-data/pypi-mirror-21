b7a0eb7489fe0cf31a40450041148d2af6781edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package nester2112/1.4.0/nester2112-1.4.0.zip

---
+++ packages/nester2112/nester2112-1.4.0.zip/nester2112-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name		='nester2112',
+	version		='1.4.0',
+	py_modules	=['nester2112'],
+	author		='hfpython',
+	author_email	='hfpython@headfirstlabs.com',
+	url		='http://headfirstlabs.com',
+	description	='A simple printer of nested lists',
+	)
