151b2745d5d53faafe39c2e58d29dc776b2b6057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:35 +0000

    Add package tai5-uan5-gian5-gi2-hok8-bu7/0.2.12/tai5-uan5_gian5-gi2_hok8-bu7-0.2.12.zip

---
+++ packages/tai5-uan5-gian5-gi2-hok8-bu7/tai5-uan5_gian5-gi2_hok8-bu7-0.2.12.zip/tai5-uan5_gian5-gi2_hok8-bu7-0.2.12/臺灣言語服務/Moses介面.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import Pyro4
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+class Moses介面:
+
+    def __init__(self):
+        self.服務 = Pyro4.Proxy("PYRONAME:Moses服務")
+
+    def 正規化翻譯支援腔口(self, request):
+        return JsonResponse({'腔口': self.服務.支援腔口()})
+
+    @csrf_exempt
+    def 正規化翻譯(self, request):
+        if request.method == 'GET':
+            連線參數 = request.GET
+        else:
+            連線參數 = request.POST
+        try:
+            查詢腔口 = 連線參數['查詢腔口']
+            if 查詢腔口 not in self.服務.支援腔口():
+                raise RuntimeError()
+        except:
+            查詢腔口 = '閩南語'
+        try:
+            查詢語句 = 連線參數['查詢語句']
+        except:
+            查詢語句 = '你好嗎？我很好！'
+        try:
+            return JsonResponse(self.服務.正規化翻譯實作(查詢腔口, 查詢語句))
+        except ConnectionRefusedError:
+            return JsonResponse({'失敗': '服務啟動中，一分鐘後才試'})
+
+    @csrf_exempt
+    def 標漢字音標(self, request):
+        if request.method == 'GET':
+            連線參數 = request.GET
+        else:
+            連線參數 = request.POST
+        try:
+            查詢腔口 = 連線參數['查詢腔口']
+            if 查詢腔口 not in self.服務.支援腔口():
+                raise RuntimeError()
+        except:
+            查詢腔口 = '閩南語'
+        try:
+            查詢語句 = 連線參數['查詢語句']
+        except:
+            查詢語句 = '你好嗎？我很好！'
+        try:
+            return JsonResponse(self.服務.標漢字音標實作(查詢腔口, 查詢語句))
+        except ConnectionRefusedError:
+            return JsonResponse({'失敗': '服務啟動中，一分鐘後才試'})
+
+    @csrf_exempt
+    def 漢字音標對齊(self, request):
+        if request.method == 'GET':
+            連線參數 = request.GET
+        else:
+            連線參數 = request.POST
+        try:
+            查詢腔口 = 連線參數['查詢腔口']
+            if 查詢腔口 not in self.服務.支援腔口():
+                raise RuntimeError()
+        except:
+            查詢腔口 = '閩南語'
+        try:
+            漢字 = 連線參數['漢字']
+            音標 = 連線參數['音標']
+        except:
+            return JsonResponse({'失敗': '參數有三个：查詢腔口、漢字、音標'})
+        try:
+            return JsonResponse(self.服務.漢字音標對齊實作(查詢腔口, 漢字, 音標))
+        except ConnectionRefusedError:
+            return JsonResponse({'失敗': '服務啟動中，一分鐘後才試'})
---
+++ packages/tai5-uan5-gian5-gi2-hok8-bu7/tai5-uan5_gian5-gi2_hok8-bu7-0.2.12.zip/tai5-uan5_gian5-gi2_hok8-bu7-0.2.12/臺灣言語服務/網址.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from 臺灣言語服務.HTS介面 import HTS介面
+from 臺灣言語服務.Moses介面 import Moses介面
+
+_Moses = Moses介面()
+_HTS = HTS介面()
+
+urlpatterns = [
+    url(r'^正規化翻譯支援腔口$', _Moses.正規化翻譯支援腔口),
+    url(r'^正規化翻譯$', _Moses.正規化翻譯),
+    url(r'^標漢字音標$', _Moses.標漢字音標),
+    url(r'^漢字音標對齊$', _Moses.漢字音標對齊),
+    url(r'^語音合成支援腔口$', _HTS.語音合成支援腔口),
+    url(r'^語音合成$', _HTS.語音合成),
+    url(r'^文本直接合成$', _HTS.文本直接合成),
+]
