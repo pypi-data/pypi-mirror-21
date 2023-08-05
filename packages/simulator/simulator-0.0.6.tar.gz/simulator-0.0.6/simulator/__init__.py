2e42a489b2ef39cdb5f7d13ff9b94d4f3d4187b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package simulator/0.0.6/simulator-0.0.6.tar.gz

---
+++ packages/simulator/simulator-0.0.6.tar.gz/simulator-0.0.6/simulator/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# !/usr/bin/python
+
+"""
+Project: simulator
+Wed Jul 13 22:54:34 2016
+"""
+
+import simulator.models
+import simulator.miscs
+import simulator.solver
+
+# Author
+__author__ = 'Jason Xing Zhang'
+__email__ = 'jaszhang@tesla.com'
+
