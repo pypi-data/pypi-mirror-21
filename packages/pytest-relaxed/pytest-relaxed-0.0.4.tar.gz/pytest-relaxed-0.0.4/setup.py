c4d4a6eaecea6b6ee81a369c4b6efbef16fa9ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package pytest-relaxed/0.0.4/pytest-relaxed-0.0.4.tar.gz

---
+++ packages/pytest-relaxed/pytest-relaxed-0.0.4.tar.gz/pytest-relaxed-0.0.4/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest-relaxed',
+    version='0.0.4',
+    description='Relaxed test discovery/organization for pytest',
+    license='BSD',
+
+    url="https://github.com/bitprophet/pytest-relaxed",
+    author='Jeff Forcier',
+    author_email='jeff@bitprophet.org',
+    long_description="\n" + open('README.rst').read(),
+
+    packages=find_packages(),
+    entry_points={
+        # TODO: do we need to name the LHS 'pytest_relaxed' too? meh
+        'pytest11': ['relaxed = pytest_relaxed.plugin'],
+    },
+
+    # TODO: is it worth tightening/loosening this? At the moment I don't know
+    # of any specific pytest releases/bugs/features that limit me besides
+    # presumable major version API compat concerns.
+    install_requires=['pytest>=3,<4'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+)
