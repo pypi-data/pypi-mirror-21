b2503f3c91e5afcbb7bbadab4d8b30c25a920fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package vortexpy/0.7.2/vortexpy-0.7.2.tar.gz

---
+++ packages/vortexpy/vortexpy-0.7.2.tar.gz/vortexpy-0.7.2/vortex/DeferUtil.py
@@ -0,0 +1,36 @@
+import logging
+
+from twisted.internet.threads import deferToThread
+
+
+def vortexLogFailure(failure, logger, consumeError=False, successValue=True):
+    if not hasattr(failure, '_vortexLogged'):
+        if failure.getTraceback():
+            logger.error(failure.getTraceback())
+        failure._vortexFailureLogged = True
+    return successValue if consumeError else failure
+
+
+def printFailure(failure, logger):
+    logger.error(failure)
+    return failure
+
+
+def deferToThreadWrapWithLogger(logger):
+    assert isinstance(logger, logging.Logger), """Usage:
+    import logging
+    logger = logging.getLogger(__name__)
+    @deferToThreadWrapWithLogger(logger)
+    def myFunction(arg1, kw=True):
+        pass
+    """
+
+    def wrapper(funcToWrap):
+        def func(*args, **kwargs):
+            d = deferToThread(funcToWrap, *args, **kwargs)
+            d.addErrback(vortexLogFailure, logger)
+            return d
+
+        return func
+
+    return wrapper
