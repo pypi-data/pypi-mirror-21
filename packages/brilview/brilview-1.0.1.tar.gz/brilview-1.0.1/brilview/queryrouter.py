8613521701bf73281e82769d839fb714f443583f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:10 +0000

    Add package brilview/1.0.1/brilview-1.0.1.tar.gz

---
+++ packages/brilview/brilview-1.0.1.tar.gz/brilview-1.0.1/brilview/queryrouter.py
@@ -0,0 +1,46 @@
+from handlers import cmmdhandler
+
+
+def query(options):
+    """Choose appropriate handler and call it with options.
+
+    :param options: dict query options
+    :returns: chart data
+    :rtype: dict
+
+    """
+    hanlerfn = get_handler_fn(options)
+    return hanlerfn(options)
+
+
+def get_handler_fn(options):
+    """Select handler function by query type
+
+    :param options: dict query options
+    :returns: handler executor function
+    :rtype: function
+
+    """
+    if 'type' not in options:
+        raise KeyError(
+            'Query type is missing. Could not determine handlername.')
+
+    t = str(options['query_type']).lower()
+    if t == 'timelumi':
+        return _call_cmmdhandler
+    elif t == 'dummy':
+        raise NotImplementedError('No dummy handler yet.')
+
+    raise KeyError('Query type did not match any handler name.')
+
+
+def _call_cmmdhandler(options):
+    """cmmdhandler executor - call to query cmmdhandler
+
+    :param options: dict query options
+    :returns: chart data
+    :rtype: dict
+
+    """
+
+    return cmmdhandler.brilcalcLumiHandler(options)
