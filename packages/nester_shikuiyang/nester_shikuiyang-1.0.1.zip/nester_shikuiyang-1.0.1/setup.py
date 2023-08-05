24ba13958608279765304d88160656e2e52384d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package nester_shikuiyang/1.0.1/nester_shikuiyang-1.0.1.zip

---
+++ packages/nester_shikuiyang/nester_shikuiyang-1.0.1.zip/nester_shikuiyang-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_shikuiyang',
+    version='1.0.1',
+    py_modules=['nester'],
+    author='shikuiyang',
+    author_email='shikuiyang@qq.com',
+    url='https://www.headfirst.com',
+    description='A simple printrt of nested lists',
+    )
