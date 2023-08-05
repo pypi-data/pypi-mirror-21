dfde58d7a4a9945666b03e5904099ec649bdaa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package veritas/0.1.0/veritas-0.1.0.tar.gz

---
+++ packages/veritas/veritas-0.1.0.tar.gz/veritas-0.1.0/README.rst
@@ -0,0 +1,40 @@
+veritas
+======
+
+**This is an experiment. Don't use this, yet.**
+
+Teritas is a Python library that attempts to implement a throwaway variable that implements most interfaces and makes a best-faith effort to evaluate to true no matter how an implementation calls on it.
+
+:Code:          https://github.com/lonnen/veritas/
+:Issues:        https://github.com/lonnen/veritas/issues
+:License:       BSD 3-clause; See LICENSE
+:Contributors:  See AUTHORS.rst
+:Documentation: FIXME
+
+Install
+=======
+
+From PyPI
+---------
+
+Run::
+
+    $ pip install veritas
+
+FIXME: Doesn't work until we post it to PyPI.
+
+
+For hacking
+-----------
+
+Run::
+
+    $ git clone https://github.com/lonnen/veritas
+    # Create a virtualenvironment
+    $ pip install -r requirements-dev.txt
+
+
+Usage
+=====
+
+See documentation for configuration and usage.
---
+++ packages/veritas/veritas-0.1.0.tar.gz/veritas-0.1.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+readme = open('README.rst').read()
+
+def get_version():
+    VERSIONFILE = os.path.join('veritas', '__init__.py')
+    VSRE = r"""^__version__ = ['"]([^'"]*)['"]"""
+    version_file = open(VERSIONFILE, 'rt').read()
+    return re.search(VSRE, version_file, re.M).group(1)
+
+
+setup(
+    name="veritas",
+    version=get_version(),
+    description="an executable implementation of _ in function definitions",
+    long_description=readme,
+    license="BSD",
+    author="lonnen",
+    url='https://github.com/lonnen/veritas',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+)
