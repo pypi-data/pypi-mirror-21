d36ae8b78cb5851fc6ca2addd23785594dd6e61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:38 +0000

    Add package pypodcaster/2.0.0/pypodcaster-2.0.0.tar.gz

---
+++ packages/pypodcaster/pypodcaster-2.0.0.tar.gz/pypodcaster-2.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'Jinja2',
+    'mutagen',
+    'pyyaml',
+    'validators',
+],
+
+setup(
+    name='pypodcaster',
+    version='2.0.0',
+    install_requires=requirements,
+    packages=find_packages(exclude=("tests",)),
+    url='http://github.com/mantlepro/pypodcaster',
+    license='GPL-3.0',
+    author='mantlepro',
+    author_email='mantlepro@gmail.com',
+    description='Generate podcast xml feed from a directory of media files',
+    package_data={'pypodcaster': ['templates/*']},
+    entry_points = {
+        'console_scripts': ['pypodcaster = pypodcaster.__main__:main']
+    }
+)
