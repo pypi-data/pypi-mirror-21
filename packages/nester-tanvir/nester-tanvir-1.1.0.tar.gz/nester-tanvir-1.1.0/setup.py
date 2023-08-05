bc2b3bb6be72767c04c45625556075d31f5f4813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package nester-tanvir/1.1.0/nester-tanvir-1.1.0.tar.gz

---
+++ packages/nester-tanvir/nester-tanvir-1.1.0.tar.gz/nester-tanvir-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nester-tanvir',
+		version = '1.1.0',
+		py_modules = ['nester'],
+		author = 'tanvir',
+		author_email = 'tanvir.dpmiracle@gmail.com',
+		url = 'https://tanvirahmedchowdhury.com',
+		description = 'A simple printer of nested lists',
+	)
