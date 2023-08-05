9039bf6e122ea27f29cd5ac738401a5418c4569c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package russell-cli/0.1.1/russell-cli-0.1.1.tar.gz

---
+++ packages/russell-cli/russell-cli-0.1.1.tar.gz/russell-cli-0.1.1/russell/__init__.py
@@ -0,0 +1,6 @@
+# Russell session parameters
+
+# russell_host = "https://www.floydhub.com"
+russell_host = "http://www.russellcloud.cn:5000"
+russell_web_host = russell_host
+russell_proxy_host = "http://www.russellcloud.cn:8000"
