d3dbf849c77765d69de0b0d6c333a1f20211f798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package deep_list/1.2.0/deep_list-1.2.0.zip

---
+++ packages/deep_list/deep_list-1.2.0.zip/deep_list-1.2.0/set-up.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name            =   'deep_list',
+    version         =   '1.2.0',
+    py_modules      =   ['deep_list'],
+    author          =   'hfpython',
+    author_email    =   'hfpython@headfirstlabs.com',
+    url             =   'http://www.headfirstlabs.com',
+    description     =   'A simple printer of nested lists',
+    )
+
