358542ce9ff7b4afa719a9b682709ce48f36865c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package jconfigpy/0.0.8/jconfigpy-0.0.8.tar.gz

---
+++ packages/jconfigpy/jconfigpy-0.0.8.tar.gz/jconfigpy-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(fname).read()
+
+setup(
+    name="jconfigpy",
+    version="0.0.8",
+    author="fritzprix",
+    author_email="innocentevil0914@gmail.com",
+    description="configuration utility which easily integrated into project using gnu make as build system",
+    license="BSD",
+    keywords="configuration utility make",
+    url="http://github.com/fritzprix/jconfigpy",
+    download_url="http://github.com/fritzprix/jconfigpy/archive/0.0.8.tar.gz",
+    packages=['jconfigpy'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
