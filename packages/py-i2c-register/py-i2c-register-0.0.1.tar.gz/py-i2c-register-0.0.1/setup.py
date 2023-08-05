94150be53ce593fc94618db07eefbc9194cd5db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package py-i2c-register/0.0.1/py-i2c-register-0.0.1.tar.gz

---
+++ packages/py-i2c-register/py-i2c-register-0.0.1.tar.gz/py-i2c-register-0.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name="py-i2c-register",
+    version="0.0.1",
+    description="Python wrapper library around the common I2C controller register pattern.",
+    long_description=long_description,
+    url="https://github.com/Noah-Huppert/py-i2c-register",
+    author="Noah Huppert",
+    author_email="developer.noah@gmail.com",
+    license="MIT",
+
+    # List of: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Project maturity
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+
+        # Intended audience
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+
+    keywords="library i2c registers",
+
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+
+    install_requires=[],
+    package_data={},
+    data_files=[],
+    entry_points={}
+)
