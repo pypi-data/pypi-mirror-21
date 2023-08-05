9a5f244973b324ca20fdd9527912927e52da2435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package placer/0.0.4/placer-0.0.4-py2.py3-none-any.whl

---
+++ packages/placer/placer-0.0.4-py2.py3-none-any.whl/placer/__init__.py
@@ -0,0 +1,19 @@
+import sys
+
+__project__ = "placer"
+__version__ = "0.0.4"
+__repo__ = "https://github.com/kootenpv/placer"
+
+
+def print_version():
+    sv = sys.version_info
+    py_version = "{}.{}.{}".format(sv.major, sv.minor, sv.micro)
+    s = ""
+    s += "placer version: [{}], Python {}\n".format(__version__, py_version)
+    version_parts = __version__.split(".")
+    s += "major version: {}  (breaking changes)\n".format(version_parts[0])
+    s += "minor version: {}  (extra feature)\n".format(version_parts[1])
+    s += "micro version: {} (commit count)\n".format(version_parts[2])
+    s += "Find out the most recent version at {}".format(__repo__)
+    print(s)
+    return s
