f509ef916b9738aaca556ba150f7544072b39082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package aws-iot-segmentation/0.4/aws-iot-segmentation-0.4.tar.gz

---
+++ packages/aws-iot-segmentation/aws-iot-segmentation-0.4.tar.gz/aws-iot-segmentation-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='aws-iot-segmentation',
+    packages=['aws_iot_segmentation'],
+    version='0.4',
+    description='A library which segments big message to be sent on AWS IOT Broker',
+    author='Andrei Mircescu',
+    author_email='mircescu.andrei@gmail.com',
+    url='https://github.com/andreimircescu/aws-iot-segmentation/archive/0.4.tar.gz',
+    download_url='https://github.com/andreimircescu/aws-iot-segmentation/archive/0.4.tar.gz',
+    keywords=['AWS', 'IOT', 'message', 'segmentation'],
+    classifiers=[],
+)
