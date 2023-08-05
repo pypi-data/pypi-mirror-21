8908daae93bd75435c0df42d4a6a4d6615b749a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package pywords/1.0.0/pywords-1.0.0.tar.gz

---
+++ packages/pywords/pywords-1.0.0.tar.gz/pywords-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pywords',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Ashley Williams',
+    url='https://github.com/ehwhydubya/pywords'
+)
