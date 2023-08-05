b06a0fda2c8610db2d221b4011d044252344a9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package assi_lzh/1.0.0/assi_lzh-1.0.0.zip

---
+++ packages/assi_lzh/assi_lzh-1.0.0.zip/assi_lzh-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='assi_lzh',
+	version='1.0.0',
+	py_modules=['assi_lzh'],
+	author='lzh',
+	author_email='lizhihao559@hotmail.com',
+	url='https://pypi.python.org/pypi?:action=role_form&package_name=assi_lzh',
+	description='tools for rich',
+	)
