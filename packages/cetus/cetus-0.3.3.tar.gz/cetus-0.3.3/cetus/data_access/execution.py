1ae0787a9bf15d005d0e781e4bf1e1c5c298618f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package cetus/0.3.3/cetus-0.3.3.tar.gz

---
+++ packages/cetus/cetus-0.3.3.tar.gz/cetus-0.3.3/cetus/data_access/execution.py
@@ -0,0 +1,50 @@
+from functools import wraps
+from typing import (Union,
+                    Callable,
+                    Coroutine,
+                    Iterable,
+                    Tuple)
+
+from asyncpg import PostgresError
+from pymysql import Error
+
+from cetus.types import (ConnectionType,
+                         RecordType)
+
+
+def handle_exceptions(function: Callable[..., Coroutine]):
+    @wraps(function)
+    async def decorated(query: str, *args, **kwargs):
+        try:
+            res = await function(query, *args, **kwargs)
+            return res
+        except (Error, PostgresError) as err:
+            err_msg = ('Error while processing '
+                       f'query: "{query}".')
+            raise IOError(err_msg) from err
+
+    return decorated
+
+
+@handle_exceptions
+async def execute(query: str, *args: Tuple[RecordType],
+                  is_mysql: bool,
+                  connection: ConnectionType) -> Union[int, str]:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            resp = await cursor.execute(query, args=args)
+    else:
+        resp = await connection.execute(query, *args)
+    return resp
+
+
+@handle_exceptions
+async def execute_many(query: str, *,
+                       args: Iterable[RecordType],
+                       is_mysql: bool,
+                       connection: ConnectionType) -> None:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            await cursor.executemany(query, args=args)
+    else:
+        await connection.executemany(query, args=args)
