52c312eacbb0ab5f46a23b5da7eee40a9bd9621b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package ioant-mysqlhelper/0.1.1/ioant_mysqlhelper-0.1.1.tar.gz

---
+++ packages/ioant-mysqlhelper/ioant_mysqlhelper-0.1.1.tar.gz/ioant_mysqlhelper-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+
+setup(
+    name="ioant_mysqlhelper",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Adam Saxen",
+    author_email="adam@asaxen.com",
+    description="This package is used for python scripts in IOAnt solution using mysql",
+    license="MIT",
+    keywords="",
+    url="http://ioant.com",   # project home page, if any
+    install_requires=[
+        "nose>=1.3.7",
+        "MySQL-python >= 1.2.5"
+    ],
+
+)
