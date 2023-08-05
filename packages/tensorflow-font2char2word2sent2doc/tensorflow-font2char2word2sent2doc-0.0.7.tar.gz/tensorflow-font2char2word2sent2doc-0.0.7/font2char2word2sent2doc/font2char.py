c901ac120738378d8e518cffbf61ae02c22569e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:42 +0000

    Add package tensorflow-font2char2word2sent2doc/0.0.7/tensorflow-font2char2word2sent2doc-0.0.7.tar.gz

---
+++ packages/tensorflow-font2char2word2sent2doc/tensorflow-font2char2word2sent2doc-0.0.7.tar.gz/tensorflow-font2char2word2sent2doc-0.0.7/font2char2word2sent2doc/font2char.py
@@ -0,0 +1,18 @@
+import extenteten as ex
+import tensorflow as tf
+
+
+@ex.func_scope()
+def font2char(fonts, num_layers=4, num_channels=32):
+    assert ex.static_rank(fonts) == 3
+
+    h = tf.expand_dims(fonts, -1)
+    for index, num_channels in enumerate([num_channels] * num_layers):
+        h = tf.contrib.slim.conv2d(
+            h, num_channels, 3, scope='conv{}'.format(index))
+        h = tf.contrib.slim.max_pool2d(h, 2, 2, scope='pool{}'.format(index))
+    h = tf.contrib.slim.flatten(h)
+
+    ex.summary.image(tf.expand_dims(tf.expand_dims(h[:256], 0), 3))
+
+    return h
