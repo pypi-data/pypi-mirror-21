864aab825af483945cf86be3df7087b35fe468b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package rc4/0.2/rc4-0.2.tar.gz

---
+++ packages/rc4/rc4-0.2.tar.gz/rc4-0.2/setup.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2016-2017 Jurriaan Bremer.
+# See the file 'LICENSE.txt' for copying permission.
+
+from setuptools import setup
+
+setup(
+    name="rc4",
+    version="0.2",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "rc4",
+    ],
+    url="https://github.com/jbremer/rc4",
+    license="BSD 3-Clause",
+    description="Package which exposes the RC4 algorithm",
+)
