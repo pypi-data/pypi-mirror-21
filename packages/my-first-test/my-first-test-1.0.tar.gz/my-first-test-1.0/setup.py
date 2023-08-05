9af63eed155dbddf632f8f8acde6db8017946c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package my-first-test/1.0/my-first-test-1.0.tar.gz

---
+++ packages/my-first-test/my-first-test-1.0.tar.gz/my-first-test-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='my-first-test',
+      version='1.0',
+      description='The funniest joke in the world',
+      url='https://github.com/huseni/rancher',
+      author='funniest test user',
+      author_email='huseni',
+      license='MIT',
+      packages=['my-first-test'],
+      zip_safe=False)
