c71756b764a1dda55ecb12599d8167c49dd0dbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:22 +0000

    Add package product_tagger/0.103/product_tagger-0.103.tar.gz

---
+++ packages/product_tagger/product_tagger-0.103.tar.gz/product_tagger-0.103/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'product_tagger',
+	version = '0.103',
+	packages = ['product_tagger','product_tagger.resources'],
+    package_data={'': ['*.pkl', '*.txt',]},
+	url = 'https://github.com/nickyongzhang/product_tagger',
+	license = 'MIT licenced',
+	long_description = open('README.txt').read()
+	)
+
