8b10d32294f87c8f4e32f48e24c29b11f014affd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:51 +0000

    Add package ghost/0.6.1/ghost-0.6.1.tar.gz

---
+++ packages/ghost/ghost-0.6.1.tar.gz/ghost-0.6.1/README.rst
@@ -0,0 +1,4 @@
+ghost
+=====
+
+See `Official Github Repo <https://github.com/nir0s/ghost>`_.
---
+++ packages/ghost/ghost-0.6.1.tar.gz/ghost-0.6.1/setup.py
@@ -0,0 +1,53 @@
+import os
+import codecs
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+setup(
+    name='ghost',
+    version="0.6.1",
+    url='https://github.com/nir0s/ghost',
+    author='nir0s',
+    author_email='nir36g@gmail.com',
+    license='LICENSE',
+    platforms='All',
+    description='Ghost stores your secrets where no one can see',
+    long_description=read('README.rst'),
+    py_modules=['ghost'],
+    entry_points={'console_scripts': ['ghost = ghost:main']},
+    install_requires=[
+        "click>=6.7",
+        "tinydb>=3.2.2",
+        "cryptography>=1.5.0"
+    ],
+    extras_require={
+        'vault': ['hvac>=0.2.16'],
+        'consul': ['requests>=2.11.1'],
+        'sqlalchemy': ['sqlalchemy>=1.0.15', 'sqlalchemy-utils>=0.32.14'],
+        'elasticsearch': ['elasticsearch>=2.4.0']
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
