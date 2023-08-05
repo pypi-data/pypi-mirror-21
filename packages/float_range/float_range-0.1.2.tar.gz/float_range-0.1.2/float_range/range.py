dcf06fc5bba760984fc074d2d8d39dbc1fae5c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package float_range/0.1.2/float_range-0.1.2.tar.gz

---
+++ packages/float_range/float_range-0.1.2.tar.gz/float_range-0.1.2/float_range/range.py
@@ -0,0 +1,26 @@
+def range(start, stop=None, step=1):
+    if stop is None:
+        stop, start = start, 0
+
+    cache_precision = _precision(step)
+    if step < 0:
+        while start > stop:
+            yield round(start, cache_precision)
+            start += step
+    else:
+        while start < stop:
+            yield round(start, cache_precision)
+            start += step
+
+def _precision(number):
+    try:
+        number = float(number)
+        decimal_part = str(number).split('.')[1]
+    except (ValueError, TypeError, IndexError):
+        msg = 'Cannot determine precision. \"{}\" is not a valid float'
+        raise ValueError(msg.format(number))
+
+    if decimal_part == '0':
+        return 1
+    else:
+        return len(decimal_part)
