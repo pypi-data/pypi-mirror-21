ade4f052c48335075619227bf97b0523ee55c954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package litter-getter/0.1.2/litter_getter-0.1.2.tar.gz

---
+++ packages/litter-getter/litter_getter-0.1.2.tar.gz/litter_getter-0.1.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+
+def get_version():
+    regex = r"""^__version__ = '(.*)'$"""
+    with open('litter_getter/__init__.py', 'r') as f:
+        text = f.read()
+    return re.findall(regex, text, re.MULTILINE)[0]
+
+
+requirements = [
+    'requests',
+    'RISparser>=0.4.1',
+    'XlsxWriter>=0.7.3',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='litter_getter',
+    version=get_version(),
+    description="Retrieve literature from biomedical reference libraries such as PubMed, EPA's HERO, and imports from Endnote RIS exports",
+    long_description=readme + '\n\n' + history,
+    author="Andy Shapiro",
+    author_email='shapiromatron@gmail.com',
+    url='https://github.com/shapiromatron/litter_getter',
+    packages=[
+        'litter_getter',
+    ],
+    package_dir={'litter_getter': 'litter_getter'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='litter_getter',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
