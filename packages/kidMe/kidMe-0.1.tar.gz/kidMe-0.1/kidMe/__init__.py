2c2af9843fe1e5fa7256aad7bfc8c5258f378970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package kidMe/0.1/kidMe-0.1.tar.gz

---
+++ packages/kidMe/kidMe-0.1.tar.gz/kidMe-0.1/kidMe/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return ('This is my first python package! How are you world?')
---
+++ packages/kidMe/kidMe-0.1.tar.gz/kidMe-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kidMe',
+      version='0.1',
+      description='The funniest joke in the world',
+      #url='http://github.com/storborg/funniest',
+      author='Saeed Mirshekari',
+      author_email='saeed.mirshekari@gmail.com',
+      license='MIT',
+      packages=['kidMe'],
+      zip_safe=False)
