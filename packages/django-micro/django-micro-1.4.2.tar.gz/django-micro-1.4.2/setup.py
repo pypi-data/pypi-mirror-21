2b3e5205c4b43151f0fab289fe48d107342eea0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package django-micro/1.4.2/django-micro-1.4.2.tar.gz

---
+++ packages/django-micro/django-micro-1.4.2.tar.gz/django-micro-1.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django-micro',
+    description='Django as microframework',
+    keywords='django microframework',
+    py_modules=['django_micro'],
+    version='1.4.2',
+    author='Max Poletaev',
+    author_email='max.poletaev@gmail.com',
+    url='https://github.com/zenwalker/django-micro',
+    license='BSD',
+    install_requires=[
+        'django>=1.10,<1.12',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+    ],
+)
