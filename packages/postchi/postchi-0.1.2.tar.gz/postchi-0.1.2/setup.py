3bedbbfdad8788c942586984bfc4d21bb47bec88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:29 +0000

    Add package postchi/0.1.2/postchi-0.1.2.tar.gz

---
+++ packages/postchi/postchi-0.1.2.tar.gz/postchi-0.1.2/setup.py
@@ -0,0 +1,37 @@
+import sys
+from distutils.core import setup
+
+import setuptools
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name='postchi',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    version='0.1.2',
+    description='A reliable async email sender for django',
+    author='Mahdi Zareie',
+    install_requires=["easy-job"],
+    tests_require=['django-setuptest'],
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    py_modules=['tests.tests'],
+    author_email='mahdi.elf@gmail.com',
+    url='https://github.com/inb-co/postchi',
+    keywords=['postchi', 'async email sender', 'django email backend'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
