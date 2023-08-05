e35181cb734760580c5dfdb157504d1f231a7196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package SimPEG/0.5.1b0/SimPEG-0.5.1b0.tar.gz

---
+++ packages/SimPEG/SimPEG-0.5.1b0.tar.gz/SimPEG-0.5.1b0/SimPEG/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+from SimPEG import Utils
+from SimPEG.Utils import mkvc
+from SimPEG.Utils.SolverUtils import (
+    _checkAccuracy, SolverWrapD, SolverWrapI,
+    Solver, SolverCG, SolverDiag, SolverLU, SolverBiCG,
+)
+
+import discretize as Mesh
+from SimPEG import Maps
+from SimPEG import Models
+from SimPEG import Problem
+from SimPEG import Survey
+from SimPEG import Regularization
+from SimPEG import DataMisfit
+from SimPEG import InvProblem
+from SimPEG import Optimization
+from SimPEG import Directives
+from SimPEG import Inversion
+from SimPEG import Tests
+
+__version__   = '0.5.1b0'
+__author__    = 'Rowan Cockett'
+__license__   = 'MIT'
+__copyright__ = 'Copyright 2014 Rowan Cockett'
