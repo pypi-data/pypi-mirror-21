d58c48ba013d6275d6b0def358a4b4dc9fcfd1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package web-walker/3.1.1/web-walker-3.1.1.tar.gz

---
+++ packages/web-walker/web-walker-3.1.1.tar.gz/web-walker-3.1.1/walker/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+from .redis_feed import RedisFeed
+from .check_status import main
+from .start_project import start as start_project
+
+VERSION = '3.1.1'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+URL = "https://www.github.com/ShichaoMa/webWalker"
+
+
+def check():
+    main()
+
+
+def feed():
+    RedisFeed.parse_args().start()
+
