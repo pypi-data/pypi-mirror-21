297d0044ec4128e6ee4025177d0b9a7bd0900e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package edward_nester_2017/1.1.0/edward_nester_2017-1.1.0.tar.gz

---
+++ packages/edward_nester_2017/edward_nester_2017-1.1.0.tar.gz/edward_nester_2017-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'edward_nester_2017',
+	version = '1.1.0',
+	py_modules = ['edward_nester_2017'],
+	author = 'edward',
+	author_email = '241317177@qq.com',
+	url = 'http://www.baidu.com',
+	description = 'A simple printer of nested lists'
+	)
