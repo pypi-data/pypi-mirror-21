1a1e153280c9dab06e50d5f616e696a4bda6697f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package avdeev/0.1/avdeev.tar.gz

---
+++ packages/avdeev/avdeev.tar.gz/avdeev/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='avdeev',
+      version='0.1',
+      description='Test',
+      url='https://github.com/altair249',
+      author='me',
+      author_email='milhen08@gmail.com',
+      license='GPL',
+      packages=['avdeev'],
+      zip_safe=False)
