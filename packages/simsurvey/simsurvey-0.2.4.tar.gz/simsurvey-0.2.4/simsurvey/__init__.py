aa0129458791d40429238d714fc1335ea64901bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package simsurvey/0.2.4/simsurvey-0.2.4.tar.gz

---
+++ packages/simsurvey/simsurvey-0.2.4.tar.gz/simsurvey-0.2.4/simsurvey/__init__.py
@@ -0,0 +1,8 @@
+
+""" Module base on astrobject, sncosmo and astropy to prepare a future survey. """
+
+__version__ = "0.2.4"
+
+from simultarget import *
+from simulsurvey import *
+from models      import *
