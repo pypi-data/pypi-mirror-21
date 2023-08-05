e8f5726adefd6cf4821bf8502fbfb78466cd802b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package ak-vendoroo/1.9.7/ak-vendoroo-1.9.7.tar.gz

---
+++ packages/ak-vendoroo/ak-vendoroo-1.9.7.tar.gz/ak-vendoroo-1.9.7/setup.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Version: 0.1
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2015-11-24
+"""
+
+from setuptools import setup
+
+setup(
+    name='ak-vendoroo',
+    version='1.9.7',
+    description="Some vendor scripts that we use here at Appknox",
+    long_description="All the Vendor/helper files the Appknox relies on",
+    url='https://github.com/appknox/vendor',
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='appknox vendor',
+    packages=['ak_vendor'],
+    entry_points='',
+    install_requires=[
+        "python3-protobuf==2.5.0",
+    ],
+    extras_require={
+        'dev': [''],
+        'test': [''],
+    },
+    package_data={
+        'ak_vendor': ['data/*.html'],
+    },
+)
