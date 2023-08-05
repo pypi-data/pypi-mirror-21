6db1fcc51d19e23d13dd89a6a4c641cc9f703bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:13 +0000

    Add package cypari/2.2.0/cypari-2.2.0.tar.gz

---
+++ packages/cypari/cypari-2.2.0.tar.gz/cypari-2.2.0/build_pari.sh
@@ -0,0 +1,186 @@
+#! /bin/bash
+
+# On macOS this builds a fat (i386/x86_64) PARI library for OS X > 10.5.
+# On Windows it uses mingw32 or mingw64, depending on the MSys2 environment.
+# On linux the default system gcc is used to build for the host architecture.
+
+set -e
+
+if [[ $(pwd) =~ " " ]]; then
+    echo "Fatal Error: Sorry, the path:"
+    echo "             $(pwd)"
+    echo "             has a space in it, preventing GMP from building"
+    echo "             because of a limitation of libtool."
+    exit 1
+fi
+
+if [ "$#" -eq 2 ] ; then
+    PARIPREFIX=$(pwd)/libcache/$1
+    PARILIBDIR=$(pwd)/libcache/$1/lib
+    GMPPREFIX=$(pwd)/libcache/$2
+else
+    PARIPREFIX=$(pwd)/libcache/pari
+    PARILIBDIR=$(pwd)/libcache/pari/lib
+    GMPPREFIX=$(pwd)/libcache/gmp
+fi
+
+echo Building gmp ...
+
+if [ "$2" != "nogmp" ] ; then
+    if [ ! -d "build/gmp_src" ] ; then
+	echo "Untarring gmp ..."
+	if [ ! -d "build" ] ; then
+	    mkdir build ;
+	fi
+	cd build
+	tar xzf ../gmp-6.1.2.tar.gz
+	mv gmp-6.1.2 gmp_src
+	cd gmp_src
+    else
+	cd build/gmp_src
+    fi
+    if [ $(uname) = "Darwin" ] ; then
+    #macOS -- build separately for 32 and 64 bits then use lipo
+	export CFLAGS='-mmacosx-version-min=10.5 -arch i386'
+	export ABI=32
+	./configure --with-pic --build=i686-none-darwin --prefix=${GMPPREFIX}32
+	make install
+	make distclean
+	export CFLAGS='-mmacosx-version-min=10.5 -arch x86_64'
+	export ABI=64
+	./configure --with-pic --build=x86_64-none-darwin --prefix=${GMPPREFIX}64
+	make install
+        make distclean
+	if [ ! -d "${GMPPREFIX}/lib" ] ; then
+	    mkdir -p ${GMPPREFIX}/lib
+	fi
+	lipo ${GMPPREFIX}32/lib/libgmp.a ${GMPPREFIX}64/lib/libgmp.a -create -output ${GMPPREFIX}/lib/libgmp.a
+	cd ../..
+    else
+	if [ $(uname | cut -b -5) = "MINGW" ] ; then
+	# Windows -- with no CFLAGS the ABI is not needed
+	    if [ "$2" = "gmp32u" ] || [ "$2" = "gmp64u" ] ; then
+		export CFLAGS='-DUNIVERSAL_CRT'
+	    fi
+	    if [ "$2" = "gmp32" ] || [ "$2" = "gmp32u" ] ; then
+		export ABI=32
+		BUILD=i686-w32-mingw32
+	    else
+		export ABI=64
+		BUILD=x86_64-w64-mingw32
+	    fi
+	else
+	# linux
+	    if [ "$2" = "gmp32" ] ; then
+		export ABI=32
+		BUILD=i686-none-none
+	    else
+		export ABI=64
+		BUILD=x86_64-none-none
+	    fi
+	fi
+	echo ./configure --build=${BUILD} --prefix=${GMPPREFIX} --with-pic
+	./configure --build=${BUILD} --prefix=${GMPPREFIX} --with-pic
+	make install
+	make distclean
+	cd ../..
+    fi
+fi
+
+echo Building Pari ...
+
+if [ ! -d "build/pari_src" ] ; then
+    echo "Untarring Pari..."
+    if [ ! -d "build" ] ; then
+	mkdir build ;
+    fi
+    cd build
+    tar xzf ../pari-2.9.1.tar.gz
+    mv pari-2.9.1 pari_src
+    cd pari_src
+    # neuter win32_set_pdf_viewer so it won't break linking with MSVC.
+    patch -p0 < ../../Windows/mingw_c.patch
+else
+    cd build/pari_src
+fi
+
+export DESTDIR=
+if [ $(uname) = "Darwin" ] ; then
+#macOS -- build separately for 32 and 64 bits then use lipo
+    export CFLAGS='-mmacosx-version-min=10.5 -arch i386'
+    ./Configure --prefix=../pari32 --libdir=../pari32/lib --with-gmp=${GMPPREFIX}32
+    cd Odarwin-i386
+    make install-lib-sta
+    make install-include
+    cd ..
+    cp src/language/anal.h ../pari32/include/pari
+    export CFLAGS='-mmacosx-version-min=10.5 -arch x86_64'
+    ./Configure --prefix=../pari64 --libdir=../pari64/lib --with-gmp=${GMPPREFIX}64 --host=x86_64-darwin
+    cd Odarwin-x86_64
+    make install
+    make install-lib-sta
+    cd ..
+    if [ ! -d ${PARILIBDIR} ] ; then
+	mkdir -p ${PARILIBDIR}
+    fi
+    lipo ../pari32/lib/libpari.a ../pari64/lib/libpari.a -create -output ${PARIPREFIX}/lib/libpari.a
+    cp -r ../pari64/include ${PARIPREFIX}
+    cp -r ../pari64/bin ${PARIPREFIX}
+    cp -r ../pari64/share ${PARIPREFIX}
+    cp -r ../pari64/lib/pari ${PARIPREFIX}/lib
+    cp src/language/anal.h ${PARIPREFIX}/include/pari
+    # Patch paricfg.h for dual architectures
+    cd ${PARIPREFIX}
+    patch include/pari/paricfg.h < ../../macOS/mac_paricfg.patch
+    # patch gphelp, because we relocate it.
+    sed -i -e 's/build\/pari_src\/\.\.\/pari64/libcache\/pari/g' ${PARIPREFIX}/bin/gphelp
+    cd ../../build/pari_src
+    
+elif [ $(uname | cut -b -5) = "MINGW" ] ; then
+#Windows
+    # This allows using C99 format specifications in printf.
+    if [ "$1" = "pari32u" ] || [ "$1" = "pari64u" ] ; then
+	export CFLAGS='-D__USE_MINGW_ANSI_STDIO -Dprintf=__MINGW_PRINTF_FORMAT -DUNIVERSAL_CRT'
+    else
+	export CFLAGS='-D__USE_MINGW_ANSI_STDIO -Dprintf=__MINGW_PRINTF_FORMAT'
+    fi
+    ./Configure --prefix=${PARIPREFIX} --libdir=${PARILIBDIR} --with-gmp=${GMPPREFIX}
+
+    # Pari's Configure script screws up the paths if you specify
+    # an absolute prefix within the msys64 environment.
+    # At least the pattern is easy to recognize.
+    cd Omingw-*
+    sed -i -e 's/C:.*C:/C:/g' Makefile
+    sed -i -e 's/C:.*C:/C:/g' pari.cfg
+    sed -i -e 's/C:.*C:/C:/g' pari.nsi
+    sed -i -e 's/C:.*C:/C:/g' paricfg.h
+    cd ..
+    make install-lib-sta
+    
+    # We cannot build the dll for Pythons > 3.4, because mingw can't
+    # handle the Universal CRT.  So we also cannot build gp. But we
+    # need gphelp.  So do a partial install.
+    cd Omingw-*
+    make install-include
+    make install-cfg
+    make install-doc
+    cd ..
+
+    # Pari's Configure will also screw up the path in the gphelp script.
+    sed -i -e 's/C:.*C:/C:/g' ${PARIPREFIX}/bin/gphelp
+
+    # Remove the .o files, since Pari always builds in the same directory.
+    make clean
+    
+else
+# linux
+    ./Configure --prefix=${PARIPREFIX} --libdir=${PARILIBDIR} --with-gmp=${GMPPREFIX}
+    make install
+    make install-lib-sta
+fi
+
+# We need this "private" header file.
+cp src/language/anal.h $PARIPREFIX/include/pari
+
+# Fix some non-prototype function declarations (until Pari 2.9.2 is released).
+sed -i -e s/\(\)\;/\(void\)\;/ $PARIPREFIX/include/pari/paripriv.h
---
+++ packages/cypari/cypari-2.2.0.tar.gz/cypari-2.2.0/setup.py
@@ -0,0 +1,364 @@
+long_description =  """\
+The package *cypari* is a Python wrapper for the `PARI library
+<http://pari.math.u-bordeaux.fr/>`_, a computer algebra system for
+number theory computations.  It is derived from the `corresponding
+component
+<https://github.com/sagemath/sage/blob/797dd7b4c273556d9677fadffa2ef6dd7f113857/src/sage/libs/cypari2/gen.pyx>`_
+of `SageMath <http://www.sagemath.org>`_, but is independent of the rest of
+SageMath and can be used with any recent version of Python 2 or 3.
+"""
+import os, sys, re, sysconfig, subprocess, shutil, site, platform, time
+from glob import glob
+from setuptools import setup, Command
+from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+from distutils.command.sdist import sdist
+from distutils.util import get_platform
+
+cpu_width = '64bit' if sys.maxsize > 2**32 else '32bit'
+
+if sys.platform == 'win32':
+    compiler_set = False
+    ext_compiler = 'msvc'
+    for n, arg in enumerate(sys.argv):
+        if arg == '-c':
+            ext_compiler = sys.argv[n+1]
+            compiler_set = True
+            break
+        elif arg.startswith('-c'):
+            ext_compiler = arg[2:]
+            compiler_set = True
+            break
+        elif arg.startswith('--compiler'):
+            ext_compiler = arg.split('=')[1]
+            compiler_set = True
+            break
+    if not compiler_set and 'build' in sys.argv:
+        sys.argv.append('--compiler=msvc')
+else:
+    ext_compiler = ''
+
+# Path setup for building with the mingw C compiler on Windows.
+if sys.platform == 'win32':
+    # We always build the Pari library with mingw, no matter which compiler
+    # is used for the CyPari extension.
+    # Make sure that our C compiler matches our python and that we can run bash
+    # This assumes that msys2 is installed in C:\msys64.
+    if cpu_width == '64bit':   # use mingw64
+        WINPATH=r'C:\msys64\mingw64\bin;C:\msys64\usr\local\bin;C:\msys64\usr\bin'
+        BASHPATH='/c/msys64/mingw64/bin:/c/msys64/usr/local/bin:/c/msys64/usr/bin'
+    else:   # use mingw32
+        WINPATH=r'C:\msys64\mingw32\bin;C:\msys64\usr\local\bin;C:\msys64\usr\bin'
+        BASHPATH='/c/msys64/mingw32/bin:/c/msys64/usr/local/bin:/c/msys64/usr/bin'
+    os.environ['PATH'] = ';'.join([WINPATH, os.environ['PATH']])
+    BASH = r'C:\msys64\usr\bin\bash'
+else:
+    BASHPATH = os.environ['PATH']
+    BASH = '/bin/bash'
+
+# We build the 32 bit and 64 bit versions of the Pari library in separate
+# directories, but in macOS we use lipo to combine them into a fat library.
+# On Windows we also build separately for the Universal CRT on Python >= 3.5
+if sys.platform == 'darwin':
+    PARIDIR = 'pari'
+    GMPDIR = 'gmp'
+elif sys.platform == 'win32':
+    if cpu_width == '64bit':
+        if sys.version_info >= (3,5):
+            PARIDIR = 'pari64u'
+            GMPDIR = 'gmp64u'
+        else:
+            PARIDIR = 'pari64'
+            GMPDIR = 'gmp64'
+    else:
+        if sys.version_info >= (3,5):
+            PARIDIR = 'pari32u'
+            GMPDIR = 'gmp32u'
+        else:
+            PARIDIR = 'pari32'
+            GMPDIR = 'gmp32'
+else:
+    if cpu_width  == '64bit':
+        GMPDIR = 'gmp64'
+        PARIDIR = 'pari64'
+    else:
+        GMPDIR = 'gmp32'
+        PARIDIR = 'pari32'
+    
+pari_include_dir = os.path.join('libcache', PARIDIR, 'include')
+pari_library_dir = os.path.join('libcache', PARIDIR, 'lib')
+pari_static_library = os.path.join(pari_library_dir, 'libpari.a')
+gmp_library_dir = os.path.join('libcache', GMPDIR, 'lib')
+gmp_static_library = os.path.join(gmp_library_dir, 'libgmp.a')
+
+class CyPariClean(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        junkdirs = (glob('build/lib*') +
+                    glob('build/bdist*') +
+                    glob('build/temp*') +
+                    glob('cypari*.egg-info')
+        )
+        for dir in junkdirs:
+            try:
+                shutil.rmtree(dir)
+            except OSError:
+                pass
+        junkfiles = (glob('cypari_src/*.so*') +
+                     glob('cypari_src/*.pyc') +
+                     glob('cypari_src/_pari.c') +
+                     glob('cypari_src/_pari*.h') +
+                     glob('cypari_src/auto*.pxi')
+        )
+        for file in junkfiles:
+            try:
+                os.remove(file)
+            except OSError:
+                pass
+
+class CyPariTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        build_lib_dir = os.path.join(
+            'build',
+            'lib.{platform}-{version_info[0]}.{version_info[1]}'.format(
+                platform=sysconfig.get_platform(),
+                version_info=sys.version_info)
+        )
+        sys.path.insert(0, build_lib_dir)
+        from cypari.test import runtests
+        sys.exit(runtests())
+
+def check_call(args):
+    try:
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError:
+        executable = args[0]
+        command = [a for a in args if not a.startswith('-')][-1]
+        raise RuntimeError(command + ' failed for ' + executable)
+
+def python_major(python):
+    proc = subprocess.Popen([python, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, errors = proc.communicate()
+    # Python 2 writes to stderr, but Python 3 writes to stdout
+    return (output + errors).split()[1].split('.')[0]
+    
+class CyPariRelease(Command):
+    user_options = [('install', 'i', 'install the release into each Python')]
+    def initialize_options(self):
+        self.install = False
+    def finalize_options(self):
+        pass
+    def run(self):
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        if os.path.exists('dist'):
+            shutil.rmtree('dist')
+        for filename in glob('cypari_src/_pari*.c'):
+            os.remove(filename)
+
+        pythons = os.environ.get('RELEASE_PYTHONS', sys.executable).split(',')
+        print('releasing for: %s'%(', '.join(pythons)))
+        for python in pythons:
+            check_call([python, 'setup.py', 'clean'])
+            check_call([python, 'setup.py', 'build'])
+            check_call([python, 'setup.py', 'test'])
+            # Save a copy of the _pari.c file for each major version of Python.
+            _pari_c_name = '_pari_py%s.c'%python_major(python)
+            _pari_c_path = os.path.join('cypari_src', _pari_c_name)
+            if not os.path.exists(_pari_c_path):
+                os.rename(os.path.join('cypari_src', '_pari.c'), _pari_c_path)
+            if sys.platform.startswith('linux'):
+                plat = get_platform().replace('linux', 'manylinux1')
+                plat = plat.replace('-', '_')
+                check_call([python, 'setup.py', 'bdist_wheel', '-p', plat])
+                check_call([python, 'setup.py', 'bdist_egg'])
+            else:
+                check_call([python, 'setup.py', 'bdist_wheel'])
+
+            if self.install:
+                check_call([python, 'setup.py', 'install'])
+
+        # Build sdist using the *first* specified Python
+        check_call([pythons[0], 'setup.py', 'sdist'])
+
+        # Double-check the Linux wheels
+        if sys.platform.startswith('linux'):
+            for name in os.listdir('dist'):
+                if name.endswith('.whl'):
+                    subprocess.check_call(['auditwheel', 'repair',
+                                           os.path.join('dist', name)])
+
+class CyPariBuildExt(build_ext):
+        
+    def run(self):
+        building_sdist = False
+        
+        if os.path.exists('pari_src'):
+            # We are building an sdist.  Move the Pari source code into build.
+            if not os.path.exists('build'):
+                os.mkdir('build')
+            os.rename('pari_src', os.path.join('build', 'pari_src'))
+            os.rename('gmp_src', os.path.join('build', 'gmp_src'))
+            # Find the correct _pari.c for our version of Python.
+            _pari_c_name = '_pari_py%d.c'%sys.version_info.major
+            os.rename(os.path.join('cypari_src', _pari_c_name),
+                      os.path.join('cypari_src', '_pari.c'))
+            building_sdist = True
+        
+        if (not os.path.exists(os.path.join('libcache', PARIDIR))
+            or not os.path.exists(os.path.join('libcache', GMPDIR))):
+            if sys.platform == 'win32':
+                # This is meant to work even in a Windows Command Prompt
+                cmd = r'export PATH="%s" ; export MSYSTEM=MINGW32 ; bash build_pari.sh %s %s'%(BASHPATH, PARIDIR, GMPDIR)
+            elif sys.platform == 'darwin':
+                cmd = r'export PATH="%s" ; bash build_pari.sh'%BASHPATH
+            else:
+                cmd = r'export PATH="%s" ; bash build_pari.sh %s %s'%(BASHPATH, PARIDIR, GMPDIR)
+            print([BASH, '-c', cmd])
+            if subprocess.call([BASH, '-c', cmd]):
+                sys.exit("***Failed to build PARI library***")
+
+        if building_sdist:
+            build_ext.run(self)
+            return
+
+        if (not os.path.exists(os.path.join('cypari_src', 'auto_gen.pxi')) or
+            not os.path.exists(os.path.join('cypari_src', 'auto_instance.pxi'))):
+            import autogen
+            autogen.autogen_all()
+            
+        # Provide compile time constants which indicate whether we are
+        # building for 64 bit Python on Windows, and which version of
+        # Python we are using.  We need to handle 64 bit Windows
+        # differently because (a) it is the only 64 bit system with 32
+        # bit longs and (b) Pari deals with this by:
+        #  #define long long long
+        # thereby breaking lots of stuff in the Python headers.
+        ct_filename = os.path.join('cypari_src', 'ct_constants.pxi') 
+        ct_constants = b''
+        if sys.platform == 'win32' and cpu_width == '64bit':
+            ct_constants += b'DEF WIN64 = True\n'
+        else:
+            ct_constants += b'DEF WIN64 = False\n'
+        ct_constants += ('DEF PYTHON_MAJOR = %d\n'%sys.version_info.major).encode('ascii')
+        if os.path.exists(ct_filename):
+            with open(ct_filename) as input:
+                old_constants = input.read().encode('ascii')
+        else:
+            old_constants = ''
+        if old_constants != ct_constants:
+            with open(ct_filename, 'wb') as output:
+                output.write(ct_constants)
+
+        # If have Cython, check that .c files are up to date
+        try: 
+            from Cython.Build import cythonize
+            cythonize([os.path.join('cypari_src', '_pari.pyx')])
+        except ImportError:
+            if not os.path.exists(os.path.join('cypari_src', '_pari.c')):
+                sys.exit("***Cython needed to create cypari_src/_pari.c***")
+
+        build_ext.run(self)
+
+class CyPariSourceDist(sdist):
+        
+    def run(self):
+        check_call(['tar', 'xfz', 'pari-2.9.1.tar.gz'])
+        os.rename('pari-2.9.1', 'pari_src')
+        check_call(['tar', 'xfz', 'gmp-6.1.2.tar.gz'])
+        os.rename('gmp-6.1.2', 'gmp_src')
+        sdist.run(self)
+        shutil.rmtree('pari_src')
+        shutil.rmtree('gmp_src')
+
+link_args = []
+compile_args = []
+if ext_compiler == 'mingw32':
+    major, minor = sys.version_info.major, sys.version_info.minor
+    if major == 3:
+        if minor == 4:
+            link_args = [r'C:\Windows\System32\Python34.dll']
+        link_args += ['-specs=specs100']
+    else:
+        link_args = ['-specs=specs90']
+    link_args += ['-Wl,--subsystem,windows']
+    compile_args += ['-D__USE_MINGW_ANSI_STDIO',
+                     '-Dprintf=__MINGW_PRINTF_FORMAT']
+    if cpu_width == '64bit':
+        compile_args.append('-DMS_WIN64')
+elif ext_compiler == 'msvc':
+    # Ignore the assembly language inlines when building the extension.
+    compile_args += ['/DDISABLE_INLINE']
+    if False:  # Toggle for debugging symbols
+        compile_args += ['/Zi']
+        link_args += ['/DEBUG']
+    # Add the mingw crt objects needed by libpari.
+    if cpu_width == '64bit':
+        link_args += [os.path.join('Windows', 'crt', 'libparicrt64.a')]
+        if sys.version_info >= (3, 5):
+            link_args += [os.path.join('Windows', 'crt', 'get_output_format64.o')]
+    else:
+        link_args += [os.path.join('Windows', 'crt', 'libparicrt32.a')]
+        if sys.version_info >= (3, 5):
+            link_args += [os.path.join('Windows', 'crt', 'get_output_format32.o')]
+            
+link_args += [pari_static_library, gmp_static_library]
+    
+if sys.platform.startswith('linux'):
+    link_args += ['-Wl,-Bsymbolic-functions', '-Wl,-Bsymbolic']
+
+include_dirs = []
+include_dirs=[pari_include_dir]
+pari_gen = Extension('cypari._pari',
+                     sources=['cypari_src/_pari.c'],
+                     include_dirs=include_dirs,
+                     extra_link_args=link_args,
+                     extra_compile_args=compile_args)
+
+# Load the version number.
+sys.path.insert(0, 'cypari_src')
+from version import __version__
+sys.path.pop(0)
+
+setup(
+    name = 'cypari',
+    version = __version__,
+    description = "Sage's PARI extension, modified to stand alone.",
+    packages = ['cypari'],
+    package_dir = {'cypari':'cypari_src'},
+    install_requires = ['six', 'future'],
+    cmdclass = {
+        'build_ext': CyPariBuildExt,
+        'clean': CyPariClean,
+        'test': CyPariTest,
+        'release': CyPariRelease,
+        'sdist': CyPariSourceDist,
+    },
+    ext_modules = [pari_gen],
+    zip_safe = False,
+    long_description = long_description,
+    url = 'https://bitbucket.org/t3m/cypari',
+    author = 'Marc Culler and Nathan M. Dunfield',
+    author_email = 'culler@uic.edu, nathan@dunfield.info',
+    license='GPLv2+',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+    keywords = 'Pari, SageMath, SnapPy',
+)
+
