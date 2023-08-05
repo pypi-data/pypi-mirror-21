babece11f35f018f34b38c5ea6a88f7390916b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package pymap3d/1.2.0/pymap3d-1.2.0.tar.gz

---
+++ packages/pymap3d/pymap3d-1.2.0.tar.gz/pymap3d-1.2.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+req = ['python-dateutil','pytz','nose','numpy','astropy']
+
+setup(name='pymap3d',
+      packages=['pymap3d'],
+      version = '1.2.0',
+      description='Python coordinate conversions, following convention of several popular Matlab routines.',
+      author = 'Michael Hirsch, Ph.D.',
+      url = 'https://github.com/scivision/pymap3d',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: BSD License',
+      'Topic :: Scientific/Engineering :: GIS',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      ],
+      install_requires=req,
+	  )
+
