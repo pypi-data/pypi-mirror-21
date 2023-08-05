08ef24abaec5e6da0bf41812563481f14fa7466a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:55 +0000

    Add package django-request-profiler/0.12/django-request-profiler-0.12.tar.gz

---
+++ packages/django-request-profiler/django-request-profiler-0.12.tar.gz/django-request-profiler-0.12/test_app/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from . import views
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(
+        r'^test/response',
+        views.test_response,
+        name='test_response'
+    ),
+    url(
+        r'^test/view',
+        views.test_view,
+        name='test_view'
+    ),
+    url(
+        r'^test/404',
+        views.test_404,
+        name='test_404'
+    ),
+
+    url(
+        r'^admin/',
+        include(admin.site.urls)
+    ),
+]
