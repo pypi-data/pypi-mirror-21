de88b8470cc1bb7e49d7ba7b55fb1ebb23a1966c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package cassandra-migrate/0.1/cassandra-migrate-0.1.tar.gz

---
+++ packages/cassandra-migrate/cassandra-migrate-0.1.tar.gz/cassandra-migrate-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(name='cassandra-migrate',
+      packages=['cassandra_migrate'],
+      version=VERSION,
+      description='Simple Cassandra database migration program',
+      url='https://github.com/Cobliteam/cassandra-migrate',
+      download_url='https://github.com/Cobliteam/cassandra-migrate/archive/{}.tar.gz'.format(VERSION),
+      author='Daniel Miranda',
+      author_email='daniel@cobli.co',
+      license='MIT',
+      install_requires=[
+          'cassandra-driver',
+          'future',
+          'pyyaml',
+          'arrow',
+          'click'
+      ],
+      scripts=['bin/cassandra-migrate'],
+      keywords='cassandra schema migration')
