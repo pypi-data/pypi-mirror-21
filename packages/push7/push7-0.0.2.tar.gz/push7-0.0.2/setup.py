b24d5a27756345f00415ecbb67ff02c6a61efc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package push7/0.0.2/push7-0.0.2.tar.gz

---
+++ packages/push7/push7-0.0.2.tar.gz/push7-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+import os
+
+
+if os.path.exists('README.md'):
+    long_description = open('README.md').read()
+
+
+setup(
+    name='push7',
+    version='0.0.2',
+    description='Python API Client for Push7',
+    long_description=open('README.md').read(),
+    license="MIT",
+    keywords="push7",
+    author='a_r_g_v',
+    author_email='info@arg.vc',
+    url='https://github.com/a-r-g-v/push7-python',
+    test_suite='tests',
+    packages=['push7'],
+    install_requires=['requests', 'simplejson', 'six', 'enum34'])
