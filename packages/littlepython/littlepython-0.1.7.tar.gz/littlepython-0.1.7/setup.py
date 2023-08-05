cb461c65bd6547effdb7af65e0d327db8dc114db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:05 +0000

    Add package littlepython/0.1.7/littlepython-0.1.7.tar.gz

---
+++ packages/littlepython/littlepython-0.1.7.tar.gz/littlepython-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+# TODO: add better metadata https://python-packaging.readthedocs.io/en/latest/metadata.html
+
+setup(name='littlepython',
+      version='0.1.7',
+      description='A Super Simplified Python with a Little Syntactic Sugar',
+      url='https://github.com/DerPferd/little-python',
+      author='Jonathan Beaulieu',
+      author_email='123.jonathan@gmail.com',
+      license='MIT',
+      packages=['littlepython'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/littlepy'])
