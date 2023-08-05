acf8075b88db59bbf552771f303ccd674ce7bb17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package ADLES/0.7.9/ADLES-0.7.9.tar.gz

---
+++ packages/ADLES/ADLES-0.7.9.tar.gz/ADLES-0.7.9/adles/parser.py
@@ -0,0 +1,408 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from os.path import exists
+
+from netaddr import IPNetwork
+
+import adles.utils as utils
+
+
+# Reference: http://pyyaml.org/wiki/PyYAMLDocumentation
+def parse_file(filename):
+    """
+    Parses the YAML file and returns a nested dictionary containing it's contents
+
+    >>> parse_file("test.yaml")
+    { "test": "value" }
+
+    :param filename: Name of YAML file to parse
+    :return: dictionary of parsed file contents
+    """
+    from yaml import load, YAMLError
+    try:
+        from yaml import CLoader as Loader  # TODO: package Windows wheel when building for win
+        logging.debug("Using C-based YAML parser")
+    except ImportError:
+        from yaml import Loader
+        logging.debug("Using pure Python YAML parser")
+    with open(filename, 'r') as f:
+        try:
+            doc = load(f, Loader=Loader)  # Parses the YAML file into a dict
+        except YAMLError as exc:
+            logging.error("Could not parse file %s", filename)
+            if hasattr(exc, 'problem_mark'):
+                mark = exc.problem_mark
+                logging.error("Error position: (%s:%s)", mark.line + 1, mark.column + 1)
+            else:
+                logging.error("Error: %s", exc)
+            return None
+    return doc
+
+
+def _checker(value_list, source, data, flag):
+    """
+    Checks if values in the list are in data (Syntax warnings or errors)
+    :param value_list: List of values to check
+    :param source: Name of source that's being checked
+    :param data: Data being checked
+    :param flag: What to do if value not found ("warnings" | "errors")
+    :return: Number of hits (warnings/errors)
+    """
+    num_hits = 0
+    for value in value_list:
+        if value not in data:
+            if flag == "warnings":
+                logging.warning("Missing %s in %s", value, source)
+            elif flag == "errors":
+                logging.error("Missing %s in %s", value, source)
+            else:
+                logging.error("Invalid flag for _checker: %s", flag)
+            num_hits += 1
+    if num_hits > 0:
+        logging.info("Total number of %s in %s: %d", flag, source, num_hits)
+    return num_hits
+
+
+def _verify_metadata_syntax(metadata):
+    """
+    Verifies that the syntax for metadata matches the specification
+    :param metadata: Dict of metadata
+    :return: (Number of errors, Number of warnings)
+    """
+    warnings = ["description", "activity", "date-created", "folder-name",
+                "root-path", "template-path"]
+    errors = ["name", "prefix", "infrastructure-config-file"]
+
+    num_warnings = _checker(warnings, "metadata", metadata, "warnings")
+    num_errors = _checker(errors, "metadata", metadata, "errors")
+
+    if "infrastructure-config-file" in metadata:
+        infra_contents = parse_file(metadata["infrastructure-config-file"])
+        e, w = _verify_infra_syntax(infra_contents)
+        num_errors += e
+        num_warnings += w
+    return num_errors, num_warnings
+
+
+def _verify_infra_syntax(infra):
+    """
+    Verifies syntax of infrastructure-config-file
+    :param infra: Dict of infrastructure
+    :return: (Number of errors, Number of warnings)
+    """
+    warnings = ["datacenter", "datastore", "server-root"]
+    errors = ["platform", "server-hostname", "server-port",
+              "login-file", "template-folder"]
+
+    num_warnings = _checker(warnings, "infrastructure", infra, "warnings")
+    num_errors = _checker(errors, "infrastructure", infra, "errors")
+    return num_errors, num_warnings
+
+
+def _verify_groups_syntax(groups):
+    """
+    Verifies that the syntax for groups matches the specification
+    :param groups: Dict of groups
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+    
+    for key, value in groups.items():
+        if "instances" in value:  # Templates
+            if type(value["instances"]) != int:
+                logging.error("Instances must be an Integer for group %s", key)
+                num_errors += 1
+            if "ad-group" in value:
+                if type(value["ad-group"]) != str:
+                    logging.error("AD group must be a string")
+                    num_errors += 1
+            elif "filename" in value:
+                e, w = _check_group_file(value["filename"])
+                num_errors += e
+                num_warnings += w
+            else:
+                logging.error("Invalid user specification method for template group %s", key)
+                num_errors += 1
+        else:  # Non-templates
+            if "ad-group" in value:
+                if type(value["ad-group"]) != str:
+                    logging.error("AD group must be a string")
+                    num_errors += 1
+            elif "filename" in value:
+                e, w = _check_group_file(value["filename"])
+                num_errors += e
+                num_warnings += w
+            elif "user-list" in value:
+                if type(value["user-list"]) is not list:
+                    logging.error("Username specification must be a list for group %s", key)
+                    num_errors += 1
+            else:
+                logging.error("Invalid user specification method for group %s", key)
+                num_errors += 1
+    return num_errors, num_warnings
+
+
+def _check_group_file(filename):
+    """
+    Verifies user info file for a group
+    :param filename: Name of user info JSON file
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+
+    if not exists(filename):
+        logging.error("Group user file %s does not exist", filename)
+        num_errors += 1
+    return num_errors, num_warnings
+
+
+def _verify_services_syntax(services):
+    """
+    Verifies that the syntax for services matches the specification
+    :param services: Dict of services
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+    
+    for key, value in services.items():
+        if "network-interfaces" in value and not isinstance(value["network-interfaces"], list):
+            logging.error("Network interfaces must be a list for service %s", key)
+            num_errors += 1
+        if "template" in value:
+            pass
+        elif "image" in value:
+            pass
+        elif "compose-file" in value:
+            pass
+        else:
+            logging.error("Invalid service definition: %s", key)
+            num_errors += 1
+    return num_errors, num_warnings
+
+
+def _verify_resources_syntax(resources):
+    """
+    Verifies that the syntax for resources matches the specification
+    :param resources: Dict of resources
+    :return: (Number of errors, Number of warnings)
+    """
+    warnings = []
+    errors = ["lab", "resource"]
+    num_warnings = _checker(warnings, "resources", resources, "warnings")
+    num_errors = _checker(errors, "resources", resources, "errors")
+    return num_errors, num_warnings
+
+
+def _verify_networks_syntax(networks):
+    """
+    Verifies that the syntax for networks matches the specification
+    :param networks: Dict of networks
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+    
+    net_types = ["unique-networks", "generic-networks", "base-networks"]
+    if not any(net in networks for net in net_types):
+        logging.error("Network specification exists but is empty!")
+        num_errors += 1
+    else:
+        for name, network in networks.items():
+            e, w = _verify_network(name, network)
+            num_errors += e
+            num_warnings += w
+    return num_errors, num_warnings
+
+
+def _verify_network(name, network):
+    """
+    Verifies syntax of a specific network
+    :param name: Name of network
+    :param network: Dict of the network
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+    
+    for key, value in network.items():
+        if "subnet" not in value:
+            logging.warning("No subnet specified for %s %s", name, key)
+            num_warnings += 1
+        else:
+            subnet = IPNetwork(value["subnet"])
+            if subnet.is_reserved() or subnet.is_multicast() or subnet.is_loopback():
+                logging.error("%s %s is in a invalid IP address space", name, key)
+                num_errors += 1
+            elif not subnet.is_private():
+                logging.warning("Non-private subnet used for %s %s", name, key)
+                num_warnings += 1
+    return num_errors, num_warnings
+
+
+def _verify_folders_syntax(folders):
+    """
+    Verifies that the syntax for folders matches the specification
+    :param folders: Dict of folders
+    :return: (Number of errors, Number of warnings)
+    """
+    num_errors = 0
+    num_warnings = 0
+
+    for key, value in folders.items():
+        if "instances" in value:  # Check instances syntax, regardless of parent or base
+            if "number" in value["instances"]:
+                if type(value["instances"]["number"]) != int:
+                    logging.error("Number of instances for folder '%s' must be an Integer", key)
+                    num_errors += 1
+            elif "size-of" in value["instances"]:
+                pass
+            else:
+                logging.error("Must specify number of instances for folder '%s'", key)
+                num_errors += 1
+
+        # Check if parent or base
+        if "services" in value:  # It's a base folder
+            if "group" not in value:
+                logging.error("No group specified for folder '%s'", key)
+                num_errors += 1
+            for skey, svalue in value["services"].items():
+                if "service" not in svalue:
+                    logging.error("Service %s is unnamed in folder '%s'", skey, key)
+                    num_errors += 1
+                if "networks" in svalue and type(svalue["networks"]) is not list:
+                    logging.error("Network specifications must be a list for service '%s' "
+                                  "in folder '%s'", skey, key)
+                    num_errors += 1
+                if "scoring" in svalue:
+                    e, w = _verify_scoring_syntax(skey, svalue["scoring"])
+                    num_errors += e
+                    num_warnings += w
+        else:  # It's a parent folder
+            if key == "group" or key == "instances" or key == "description":
+                pass
+            elif not isinstance(value, dict):
+                logging.error("Could not verify syntax of folder '%s': '%s' is not a folder!",
+                              str(key), str(value))
+                num_errors += 1
+            else:
+                e, w = _verify_folders_syntax(value)
+                num_errors += e
+                num_warnings += w
+    return num_errors, num_warnings
+
+
+def _verify_scoring_syntax(service_name, scoring):
+    """
+    Verifies syntax for the scoring definition of a service.
+    This is functionalized because it is likely to change in the future.
+    :param service_name: Name of the service for which the scoring specification applies
+    :param scoring: Dict of scoring parameters
+    :return: (Number of errors, Number of warnings)
+    """
+    num_warnings = 0
+    num_errors = 0
+
+    if "ports" not in scoring:
+        logging.error("No scoring ports specified for service %s", service_name)
+        num_errors += 1
+    elif not isinstance(scoring["ports"], list):
+        logging.error("Scoring ports must be a list for service %s", service_name)
+        num_errors += 1
+    if "protocols" not in scoring:
+        logging.error("No protocols specified for service %s", service_name)
+        num_errors += 1
+    elif not isinstance(scoring["protocols"], list):
+        logging.error("Protocols must be a list for service %s", service_name)
+        num_errors += 1
+    if "criteria" not in scoring:
+        logging.error("No criteria file specified for service %s", service_name)
+        num_errors += 1
+    # TODO: need to pass package dir or path to where the environment file is
+    # elif not exists(scoring["criteria"]):
+    #    logging.error("Could not find criteria file %s for service %s",
+    #       scoring["criteria"], service_name)
+    #    num_errors += 1
+    return num_errors, num_warnings
+
+
+@utils.time_execution
+def verify_syntax(spec):
+    """
+    Verifies the syntax for the dictionary representation of an environment specification
+    :param spec: Dictionary of environment specification
+    :return: (Number of errors, Number of warnings)
+    """
+    num_warnings = 0
+    num_errors = 0
+    funcs = {"metadata": _verify_metadata_syntax,
+             "groups": _verify_groups_syntax,
+             "services": _verify_services_syntax,
+             "resources": _verify_resources_syntax,
+             "networks": _verify_networks_syntax,
+             "folders": _verify_folders_syntax}
+
+    required = ["metadata", "groups", "services", "networks", "folders"]
+    optional = ["resources"]
+
+    for key, func in funcs.items():
+        if key not in spec:
+            if key in required:
+                logging.error("Required definition %s was not found", key)
+                num_errors += 1
+            elif key in optional:
+                logging.debug('Optional definition "%s" was not found', key)
+            else:
+                logging.warning("Unknown definition found: %s", key)
+                num_warnings += 1
+        else:
+            e, w = func(spec[key])
+            num_errors += e
+            num_warnings += w
+
+    return num_errors, num_warnings
+
+
+@utils.time_execution
+def check_syntax(specfile_path):
+    """
+    Checks the syntax of a specification file
+    :param specfile_path: Path to the specification file
+    :return: The specification
+    """
+    from os.path import exists, basename
+
+    if not exists(specfile_path):
+        logging.error("Could not find specification file in path %s", str(specfile_path))
+        return None
+    spec = parse_file(specfile_path)
+    if spec is None:
+        logging.error("Failed to ingest specification file %s", basename(specfile_path))
+        return None
+    logging.info("Successfully ingested specification file %s", basename(specfile_path))
+    logging.info("Checking syntax...")
+    errors, warnings = verify_syntax(spec)
+    if errors == 0 and warnings == 0:
+        logging.info("Syntax check successful!")
+        return spec
+    elif errors == 0:
+        logging.info("Syntax check successful, but there were %d warnings", warnings)
+        return spec
+    else:
+        logging.error("Syntax check failed! Errors: %d\tWarnings: %d", errors, warnings)
+        return None
---
+++ packages/ADLES/ADLES-0.7.9.tar.gz/ADLES-0.7.9/vsphere_testing.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""vsphere_testing. Entrypoint used to test the vSphere class. Has examples of API usage.
+
+Usage:
+    vsphere_testing.py [-v] [options]
+    vsphere_testing.py --version
+    vsphere_testing.py (-h | --help)
+
+Options:
+    -h, --help          Shows this help
+    --version           Prints current version
+    --no-color          Do not color terminal output
+    -v, --verbose       Emit debugging logs to terminal
+    -f, --file FILE     Name of JSON file with server connection information
+
+"""
+
+import logging
+
+from docopt import docopt
+from pyVmomi import vim
+
+from adles.vsphere.vsphere_class import Vsphere
+from adles.vsphere import vm_utils
+from adles.utils import script_setup
+
+
+args = docopt(__doc__, version=Vsphere.__version__, help=True)
+server = script_setup('vsphere_testing.log', args, (__file__, Vsphere.__version__))
+
+folder = server.get_folder("monkeys")
+
+
+# bios_uuid = "42053029-1098-6ef1-da79-5e2c4103f600"
+# instance_uuid = "50056cfe-aaa0-c28d-a702-d72e68e50f3a"
+# vm = server.find_by_uuid(uuid=instance_uuid)
+# vm = server.find_by_ds_path(path="[Datastore] (MASTER) apache/(MASTER) apache.vmx")
+# logging.info("%s", vm_utils.get_vm_info(vm, uuids=True))
+
+# pool = server.get_item(vim.ResourcePool)
+# vm_spec = vm_utils.gen_vm_spec(name="test_vm", datastore_name="Datastore",
+#                                annotation="testing 1...")
+# vm_utils.create_vm(folder, vm_spec, pool)
+# attach_iso(vm, "ISO-Images/vyos-1.1.7-amd64.iso", server.get_datastore("Datastore"))
+
+# vm = server.get_vm("dummy")
+# logging.info(vm_utils.get_vm_info(vm, uuids=True, snapshot=True))
+
+# vms = server.get_all_vms()
+# for vm in vms:
+#     logging.info(vm_utils.get_vm_info(vm))
+
+# net = server.get_network("ARP-LAN")
+# logging.info(str(net))
+
+# host = server.get_host()
+# logging.info(host)
+
+# logging.info(str(server))
+# logging.info(repr(server))
+
+# from adles.vsphere.folder_utils import traverse_path
+# folder = server.get_folder("script_testing")
+# vm = traverse_path(folder, "/Templates/Routers/VyOS 1.1.7 (64-bit)")
+# logging.info(get_vm_info(vm))
+
+# datastore = server.get_datastore("Datastore")
+# logging.info(get_datastore_info(datastore))
+
+# portgroup = server.get_item(vim.Network, "test_network")
+# logging.info("Portgroup: %s", str(portgroup))
+# vm_utils.add_nic(vm, portgroup, "test_summary")
+# vm_utils.edit_nic(vm, 2, summary="lol")
+# vm_utils.delete_nic(vm, 1)
+
+# create_portgroup("test_portgroup", server.get_host(), "test_vswitch")
+# delete_portgroup("test_portgroup", server.get_host())
