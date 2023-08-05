fd1306bb47faea022441d6b0f6cb6dbc040b164f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package kibitzr/2.7.5/kibitzr-2.7.5-py2.py3-none-any.whl

---
+++ packages/kibitzr/kibitzr-2.7.5-py2.py3-none-any.whl/kibitzr/cli.py
@@ -0,0 +1,36 @@
+import logging
+
+import click
+
+from kibitzr import __version__ as VERSION
+from kibitzr.main import main
+
+
+LOG_LEVEL_CODES = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+}
+
+
+@click.command()
+@click.option("--once", is_flag=True,
+              help="Run checks once and exit")
+@click.option("-l", "--log-level", default="info",
+              type=click.Choice(LOG_LEVEL_CODES.keys()),
+              help="Logging level")
+@click.option("-v", "--version", is_flag=True,
+              help="Print version and exit")
+@click.argument('name', nargs=-1)
+def entry(once, log_level, version, name):
+    """Run kibitzr in the foreground mode"""
+    if version:
+        print(VERSION)
+    else:
+        log_level_code = LOG_LEVEL_CODES[log_level]
+        main(once=once, log_level=log_level_code, names=name)
+
+
+if __name__ == "__main__":
+    entry()
---
+++ packages/kibitzr/kibitzr-2.7.5-py2.py3-none-any.whl/kibitzr/conf.py
@@ -0,0 +1,175 @@
+import os
+import copy
+import logging.config
+
+import yaml
+
+
+logger = logging.getLogger(__name__)
+
+
+class ConfigurationError(RuntimeError):
+    pass
+
+
+class ReloadableSettings(object):
+    _instances = {}
+    CONFIG_DIRS = (
+        '',
+        '~/.config/kibitzr/',
+        '~/',
+    )
+    CONFIG_FILENAME = 'kibitzr.yml'
+    CREDENTIALS_FILENAME = 'kibitzr-creds.yml'
+
+    def __init__(self, config_dir):
+        self.filename = os.path.join(config_dir, self.CONFIG_FILENAME)
+        self.creds_filename = os.path.join(config_dir,
+                                           self.CREDENTIALS_FILENAME)
+        self.pages = None
+        self.notifiers = None
+        self.creds = {}
+        self.reread()
+
+    @classmethod
+    def detect_config_dir(cls):
+        candidates = [
+            (directory, os.path.join(directory, cls.CONFIG_FILENAME))
+            for directory in map(os.path.expanduser, cls.CONFIG_DIRS)
+        ]
+        for directory, file_path in candidates:
+            if os.path.exists(file_path):
+                return directory
+        raise ConfigurationError(
+            "kibitzr.yml not found in following locations: %s"
+            % ", ".join([x[1] for x in candidates])
+        )
+
+    @classmethod
+    def instance(cls, config_dir):
+        key = config_dir
+        if key not in cls._instances:
+            cls._instances[key] = cls(key)
+        return cls._instances[key]
+
+    def reread(self):
+        """
+        Read configuration file and substitute references into pages conf
+        """
+        logger.debug("Loading settings from %s",
+                     os.path.abspath(self.filename))
+        with open(self.filename) as fp:
+            conf = yaml.load(fp)
+        changed = self.read_creds()
+        pages = conf.get('checks', conf.get('pages', []))
+        notifiers = conf.get('notifiers', {})
+        templates = conf.get('templates', {})
+        scenarios = conf.get('scenarios', {})
+        pages = list(self.unpack_batches(pages))
+        for i, page in enumerate(pages):
+            name = page['name']
+            if 'template' in page:
+                if page['template'] in templates:
+                    templated_page = copy.deepcopy(templates[page['template']])
+                else:
+                    raise ConfigurationError(
+                        "Template %r not found. Referenced in page %r"
+                        % (page['template'], name)
+                    )
+                templated_page.update(page)
+                page = templated_page
+                pages[i] = page
+            if 'scenario' in page:
+                if page['scenario'] in scenarios:
+                    page['scenario'] = scenarios[page['scenario']]
+            if 'notify' in page:
+                for notify in page['notify']:
+                    if hasattr(notify, 'keys'):
+                        notify_type = next(iter(notify.keys()))
+                        notify_param = next(iter(notify.values()))
+                        try:
+                            notify[notify_type] = notifiers[notify_param]
+                        except (TypeError, KeyError):
+                            # notify_param is not a predefined notifier name
+                            # Save it as is:
+                            notify[notify_type] = notify_param
+        if self.pages != pages or self.notifiers != notifiers:
+            self.pages = pages
+            self.notifiers = notifiers
+            return True
+        else:
+            return changed
+
+    def unpack_batches(self, pages):
+        for page in pages:
+            if 'batch' in page:
+                base = copy.deepcopy(page)
+                batch = base.pop('batch')
+                url_pattern = base.pop('url-pattern')
+                items = base.pop('items')
+                for item in items:
+                    new_page = copy.deepcopy(base)
+                    new_page['name'] = batch.format(item)
+                    new_page['url'] = url_pattern.format(item)
+                    yield new_page
+            else:
+                yield page
+
+    def read_creds(self):
+        """
+        Read and parse credentials file.
+        If something goes wrong, log exception and continue.
+        """
+        logger.debug("Loading credentials from %s",
+                     os.path.abspath(self.creds_filename))
+        try:
+            with open(self.creds_filename, 'r') as fp:
+                creds = yaml.load(fp)
+                if creds != self.creds:
+                    self.creds = creds
+                    return True
+        except IOError:
+            logger.info("No credentials file found at %s",
+                        os.path.abspath(self.creds_filename))
+        except:
+            logger.exception("Error loading credentials file")
+        return False
+
+
+def settings():
+    """
+    Returns singleton instance of settings
+    """
+    config_dir = ReloadableSettings.detect_config_dir()
+    return ReloadableSettings.instance(config_dir)
+
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'sh': {
+            'level': 'INFO',
+        },
+        'sh.command': {
+            'level': 'WARNING',
+        },
+    }
+})
