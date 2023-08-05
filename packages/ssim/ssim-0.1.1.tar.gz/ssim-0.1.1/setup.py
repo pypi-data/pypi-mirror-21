4dd0303a714500183decd3ec80dcb6626b59c62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package ssim/0.1.1/ssim-0.1.1.tar.gz

---
+++ packages/ssim/ssim-0.1.1.tar.gz/ssim-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='ssim',
+    packages=['ssim'],
+    package_dir={'ssim': 'ssim'},
+    version='0.1.1',
+    description='A parser for ssim',
+    author='Rok Mihevc',
+    author_email='rok.mihevc@gmail.com',
+    url='https://github.com/rok/ssim',
+    keywords=['parsing', 'ssim', 'data'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
