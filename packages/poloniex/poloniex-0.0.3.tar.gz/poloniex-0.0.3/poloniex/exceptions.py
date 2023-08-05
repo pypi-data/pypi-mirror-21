8cd70db772eddb6061025699fc368634daf145f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package poloniex/0.0.3/poloniex-0.0.3.tar.gz

---
+++ packages/poloniex/poloniex-0.0.3.tar.gz/poloniex-0.0.3/poloniex/exceptions.py
@@ -0,0 +1,15 @@
+class PoloniexException(Exception):
+    """Generic Poloniex Exception."""
+    pass
+
+class PoloniexCredentialsException(PoloniexException, RuntimeError):
+    """Missing or wrong credentials while using Trading API."""
+    pass
+
+class PoloniexCommandException(PoloniexException, RuntimeError):
+    """Error in command execution."""
+    pass
+
+class PoloniexInvalidParametersException(PoloniexException, RuntimeError):
+    """Wrong parameters while using Private API."""
+    pass
