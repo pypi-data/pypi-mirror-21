9ab0e62ac7d623f19fb0597d32ba115816a714a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package nesteramy/1.0.0/nesteramy-1.0.0.zip

---
+++ packages/nesteramy/nesteramy-1.0.0.zip/nesteramy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nesteramy',
+    version     = '1.0.0',
+    py_modules  = ['nesteramy'],
+    author      = 'xuxuexia',
+    author_email= 'yesiwillbethere@126.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
