2fc6b35e17f4f84b3b14a0aad0af22a0bc8bd69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:19 +0000

    Add package pypoet/0.2.0/pypoet-0.2.0-py2.py3-none-any.whl

---
+++ packages/pypoet/pypoet-0.2.0-py2.py3-none-any.whl/poet/console/commands/update.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from ...installer import Installer
+
+from .command import Command
+
+
+class UpdateCommand(Command):
+    """
+    Update dependencies as according to the <comment>poetry.toml</> file.
+
+    update
+        { packages?* : The packages to update.}
+    """
+
+    def handle(self):
+        if not self.has_lock():
+            # We do not have a lock file
+            # Assuming installation
+            return self.call('install')
+
+        installer = Installer(self, self._repository)
+
+        installer.update(packages=self.argument('packages'))
