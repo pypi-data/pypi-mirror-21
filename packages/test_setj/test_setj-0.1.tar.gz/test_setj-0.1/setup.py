0438a37aa1b38d45081efec773729075a3dc2bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package test_setj/0.1/test_setj-0.1.tar.gz

---
+++ packages/test_setj/test_setj-0.1.tar.gz/test_setj-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_setj',
+      version='0.1',
+      description='Demo python package for testing how setuptools work',
+      url='https://github.com/apatniv/test_setj',
+      author='vivek',
+      author_email='apatniv@gmail.com',
+      license='MIT',
+      packages=['test_setj'],
+      zip_safe=False)
