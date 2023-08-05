72f74aed79c6c25012ab8798c3420082378bf89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package mogu/0.1.0/mogu-0.1.0.tar.gz

---
+++ packages/mogu/mogu-0.1.0.tar.gz/mogu-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='mogu',
+      version="0.1.0",
+      description="Collection of Simple Numerical Routines",
+      long_description="Collection of simple numerical routines, independent of each other",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Artificial Intelligence",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Programming Language :: Python :: 2.7",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords="mogu numerics computation",
+      url="https://github.com/stephenhky/MoguNumerics",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['mogu',
+                'mogu.embed',
+                'mogu.fit',
+                'mogu.util',
+                'mogu.util.derivatives',
+                'mogu.simvoltage'],
+      install_requires=[
+          'numpy', 'scipy', 'theano', 'networkx',
+      ],
+      scripts=['bin/DictConcatenator'],
+      include_package_data=True,
+      zip_safe=False)
