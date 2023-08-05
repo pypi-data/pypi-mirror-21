c189d66f883bb01530fdb2758815bd5d8ef11d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package grako/3.19.4/grako-3.19.4-py2.py3-none-any.whl

---
+++ packages/grako/grako-3.19.4-py2.py3-none-any.whl/grako/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017      by Juancarlo Añez
+# Copyright (C) 2012-2016 by Juancarlo Añez and Thomas Bragg
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from grako._config import __version__  # noqa
+from grako._config import __toolname__  # noqa
+from grako.tool import gencode, genmodel, main  # noqa
+
+
+if __name__ == '__main__':
+    main()
