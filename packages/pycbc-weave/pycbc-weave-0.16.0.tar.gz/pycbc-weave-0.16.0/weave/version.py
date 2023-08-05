90417f72c685f4b249c932c02757ce41c1ce9d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package pycbc-weave/0.16.0/pycbc-weave-0.16.0.tar.gz

---
+++ packages/pycbc-weave/pycbc-weave-0.16.0.tar.gz/pycbc-weave-0.16.0/weave/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM WEAVE SETUP.PY
+short_version = '0.16.0'
+version = '0.16.0'
+full_version = '0.16.0'
+git_revision = '4ebb6e93963e38125d8ad1fc7118853491f60d48'
+release = True
+
+if not release:
+    version = full_version
