f145d125fe9728ebb8f8e33bb667e965961f93c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package hearstPatterns/0.1.2/hearstPatterns-0.1.2.tar.gz

---
+++ packages/hearstPatterns/hearstPatterns-0.1.2.tar.gz/hearstPatterns-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hearstPatterns',
+  packages = ['hearstPatterns'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'This is a Python implementation of Hearst patterns for finding hyponyms in text.',
+  author = 'Matt Michelson',
+  author_email = 'mmichelson@inferlink.com',
+  url = 'https://github.com/mmichelsonIF/hearst_patterns_python', # use the URL to the github repo
+  download_url = 'https://github.com/mmichelsonIF/hearst_patterns_python/tarball/0.1.2', # I'll explain this in a second
+  keywords = ['Hearst patterns','hyponyms','natural language processing','relations'], # arbitrary keywords
+  classifiers = [],
+)
