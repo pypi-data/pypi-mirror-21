f8ba0e8b934df77730abf3189627cc4e874e2a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package datapack/0.0/datapack-0.0.tar.gz

---
+++ packages/datapack/datapack-0.0.tar.gz/datapack-0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'datapack',
+    packages = ['datapack'], # this must be the same as the name above
+    version = '0.0',
+    description = 'Data Packages for Science',
+    author = 'Stéfan van der Walt',
+    author_email = 'stefanv@berkeley.edu',
+    url = 'https://github.com/scikit-image/datapack',
+    download_url = 'https://github.com/scikit-image/datapack/archive/0.0.tar.gz',
+    keywords = ['data', 'packaging', 'science'],
+    classifiers = [],
+)
