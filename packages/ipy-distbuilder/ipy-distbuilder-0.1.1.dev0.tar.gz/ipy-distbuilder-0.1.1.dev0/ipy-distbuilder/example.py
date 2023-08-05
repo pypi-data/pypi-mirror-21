9a6e8afd9205377ab739c8cbcc2b7bbae50f910c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:46 +0000

    Add package ipy-distbuilder/0.1.1.dev0/ipy-distbuilder-0.1.1.dev0.tar.gz

---
+++ packages/ipy-distbuilder/ipy-distbuilder-0.1.1.dev0.tar.gz/ipy-distbuilder-0.1.1.dev0/ipy-distbuilder/example.py
@@ -0,0 +1,15 @@
+import ipywidgets as widgets
+from traitlets import Unicode
+
+
+@widgets.register('hello.Hello')
+class DistBuilder(widgets.DOMWidget):
+    """"""
+    _view_name = Unicode('DistBuilderView').tag(sync=True)
+    _model_name = Unicode('DistBuilderModel').tag(sync=True)
+    _view_module = Unicode('ipy-distbuilder').tag(sync=True)
+    _model_module = Unicode('ipy-distbuilder').tag(sync=True)
+    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    value = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
+
