853e041dc8ff968bd629ba322096b95b66c9e42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package cetus/0.5.0/cetus-0.5.0.tar.gz

---
+++ packages/cetus/cetus-0.5.0.tar.gz/cetus-0.5.0/cetus/data_access/execution.py
@@ -0,0 +1,34 @@
+from typing import (Union,
+                    Iterable,
+                    Tuple)
+
+from cetus.types import (ConnectionType,
+                         RecordType)
+from .utils import handle_exceptions
+
+
+@handle_exceptions
+async def execute(query: str,
+                  *args: Tuple[RecordType],
+                  is_mysql: bool,
+                  connection: ConnectionType
+                  ) -> Union[int, str]:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            resp = await cursor.execute(query, args=args)
+    else:
+        resp = await connection.execute(query, *args)
+    return resp
+
+
+@handle_exceptions
+async def execute_many(query: str, *,
+                       args: Iterable[RecordType],
+                       is_mysql: bool,
+                       connection: ConnectionType
+                       ) -> None:
+    if is_mysql:
+        async with connection.cursor() as cursor:
+            await cursor.executemany(query, args=args)
+    else:
+        await connection.executemany(query, args=args)
