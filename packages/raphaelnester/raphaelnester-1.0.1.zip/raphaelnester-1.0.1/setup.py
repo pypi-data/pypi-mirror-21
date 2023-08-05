e275145406810ab234697ef03ccca365dc64e871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package raphaelnester/1.0.1/raphaelnester-1.0.1.zip

---
+++ packages/raphaelnester/raphaelnester-1.0.1.zip/raphaelnester-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name ='raphaelnester',
+	version='1.0.1',
+	py_modules=['raphaelnester'],
+	author='Raphael',
+	author_email='504321123@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
