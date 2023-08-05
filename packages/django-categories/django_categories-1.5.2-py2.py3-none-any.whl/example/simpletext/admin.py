65ea4fa7665d0c2a8cd61be8394197d8662ecc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package django-categories/1.5.2/django_categories-1.5.2-py2.py3-none-any.whl

---
+++ packages/django-categories/django_categories-1.5.2-py2.py3-none-any.whl/example/simpletext/admin.py
@@ -0,0 +1,26 @@
+from .models import SimpleText, SimpleCategory
+from django.contrib import admin
+
+from categories.admin import CategoryBaseAdmin, CategoryBaseAdminForm
+
+
+class SimpleTextAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', )
+        }),
+    )
+
+
+class SimpleCategoryAdminForm(CategoryBaseAdminForm):
+    class Meta:
+        model = SimpleCategory
+        fields = '__all__'
+
+
+class SimpleCategoryAdmin(CategoryBaseAdmin):
+    form = SimpleCategoryAdminForm
+
+
+admin.site.register(SimpleText, SimpleTextAdmin)
+admin.site.register(SimpleCategory, SimpleCategoryAdmin)
