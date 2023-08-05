2d6c956dce2949f5e2d539b91856899c2a6589a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package prance/0.4.0/prance-0.4.0.tar.gz

---
+++ packages/prance/prance-0.4.0.tar.gz/prance-0.4.0/prance/util/iterators.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""This submodule contains specialty iterators over specs."""
+
+__author__ = 'Jens Finkhaeuser'
+__copyright__ = 'Copyright (c) 2016 Jens Finkhaeuser'
+__license__ = 'MIT +no-false-attribs'
+__all__ = ()
+
+
+def item_iterator(specs):
+  """
+  Return item iterator over the specs.
+
+  :param dict specs: The specifications to iterate over.
+  :return: An iterator over all items in the specs.
+  :rtype: iterator
+  """
+  import six
+  return six.viewitems(specs)
+
+
+def reference_iterator(specs, path = ()):
+  """
+  Iterate through the given specs, returning only references.
+
+  The iterator returns three values:
+    - The key, mimicking the behaviour of other iterators, although
+      it will always equal '$ref'
+    - The value
+    - The path to the item. This is a tuple of all the item's ancestors,
+      in sequence, so that you can reasonably easily find the containing
+      item.
+
+  :param dict specs: The specifications to iterate over.
+  :return: An iterator over all references in the specs.
+  :rtype: iterator
+  """
+  # We need to iterate through the nested specification dict, so let's
+  # start with an appropriate iterator. We can immediately optimize it by
+  # only returning '$ref' items.
+  import collections
+
+  for key, value in item_iterator(specs):
+    if isinstance(value, collections.Mapping):
+      for inner in reference_iterator(value, path + (key,)):
+        yield inner
+    elif key == '$ref':
+      yield key, value, path
