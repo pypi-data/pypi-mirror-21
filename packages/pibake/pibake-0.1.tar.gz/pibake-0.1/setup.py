e1a742e54a2f370b93789d0bc7f8a7ee9396b198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:32 +0000

    Add package pibake/0.1/pibake-0.1.tar.gz

---
+++ packages/pibake/pibake-0.1.tar.gz/pibake-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+long_description = '''
+Fetches and stores Raspberry PI Images to local cache.
+'''
+
+setup(
+    author='Thys Meintjes',
+    author_email='sthysel@gmail.com',
+    name='pibake',
+    version='0.1',
+    keywords='Raspberry PI Downloader',
+    long_description=long_description,
+    url='https://github.com/sthysel/pibake',
+    download_url='https://github.com/sthysel/pibake/archive/0.1.tar.gz',
+    license="GPLv2",
+    description='Fetches and Manages Raspberry PI Images',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+        'psutil',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pibake=pibake.cli:cli',
+        ]
+    }
+)
