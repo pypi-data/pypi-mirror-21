9712e38d05cc73dcb4d1e2e522c258369d737eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package hahatest/0.0.5/hahatest-0.0.5.tar.gz

---
+++ packages/hahatest/hahatest-0.0.5.tar.gz/hahatest-0.0.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+	name='hahatest',
+	version='0.0.5',
+	author='heliang',
+	author_email='heliang.whut@gmail.com',
+	url='https://abc.xyz',
+	description=u'测试测试',
+	packages=['hahatest'],
+	install_requires=[],
+	entry_points={
+		'console_scripts': [
+			'haha=hahatest:haha'
+		]
+	}
+)
