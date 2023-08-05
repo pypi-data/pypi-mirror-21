0697914fe7a717b6df9322b5a87ec5db0a4e6a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:27 +0000

    Add package connexion/1.1.6/connexion-1.1.6-py3-none-any.whl

---
+++ packages/connexion/connexion-1.1.6-py3-none-any.whl/connexion/__init__.py
@@ -0,0 +1,13 @@
+import werkzeug.exceptions as exceptions  # NOQA
+from .apps import AbstractApp, FlaskApp  # NOQA
+from .apis import AbstractAPI, FlaskApi  # NOQA
+from .exceptions import ProblemException  # NOQA
+from .problem import problem  # NOQA
+from .decorators.produces import NoContent  # NOQA
+from .resolver import Resolution, Resolver, RestyResolver  # NOQA
+
+App = FlaskApp
+Api = FlaskApi
+
+# This version is replaced during release process.
+__version__ = '1.1.6'
