f2e6366a255584dad0c6d47befb92fddff941b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package callfire-api-client-python/0.0.7/callfire-api-client-python-0.0.7.tar.gz

---
+++ packages/callfire-api-client-python/callfire-api-client-python-0.0.7.tar.gz/callfire-api-client-python-0.0.7/callfire/client.py
@@ -0,0 +1,29 @@
+import logging
+
+from bravado.requests_client import RequestsClient
+from bravado.client import SwaggerClient
+
+log = logging.getLogger(__name__)
+
+
+class CallfireClient:
+    def __init__(self, login, password, config=None):
+        self.config = {
+            'validate_responses': False
+        }
+        self.config.update({} if config is None else config)
+        print(self.config)
+        log.debug('CallfireClient.config %s', self.config)
+        self.http_client = RequestsClient()
+        self.http_client.set_basic_auth('www.callfire.com', login, password)
+        self.swagger_client = SwaggerClient.from_url(
+            spec_url=self.swagger_url(),
+            http_client=self.http_client,
+            config=self.config)
+
+    def __getattr__(self, item):
+        return getattr(self.swagger_client, item)
+
+    @staticmethod
+    def swagger_url():
+        return 'https://www.callfire.com/v2/api-docs/swagger.json'
