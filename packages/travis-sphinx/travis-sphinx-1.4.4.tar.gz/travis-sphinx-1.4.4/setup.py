837c03dd6d112bd334c117004feba716ee93bdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package travis-sphinx/1.4.4/travis-sphinx-1.4.4.tar.gz

---
+++ packages/travis-sphinx/travis-sphinx-1.4.4.tar.gz/travis-sphinx-1.4.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'travis-sphinx',
+    version = '1.4.4',
+    description=('Manages building sphinx documentation and deploying documentation to gh-pages via travis'),
+    author='Grant Mercer',
+    author_email='gmercer015@gmail.com',
+    url='https://github.com/syntaf/travis-sphinx',
+    download_url='https://github.com/Syntaf/travis-sphinx/archive/master.zip',
+    keywords=['documentation', 'travis', 'python', 'deploy'],
+    packages = find_packages(),
+    py_modules = ['travis_sphinx'],
+    entry_points = {
+        'console_scripts' : ['travis-sphinx=travis_sphinx:main']
+    },
+    install_requires=[
+        'sphinx'
+    ],
+    classifiers = ['Topic :: Software Development :: Documentation',
+                   'Programming Language :: Python'],
+)
+
