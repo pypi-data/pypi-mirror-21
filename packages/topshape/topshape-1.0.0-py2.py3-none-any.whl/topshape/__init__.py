fceca59a41b0dfd35c8c875afad5cc265c3dd5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package topshape/1.0.0/topshape-1.0.0-py2.py3-none-any.whl

---
+++ packages/topshape/topshape-1.0.0-py2.py3-none-any.whl/topshape/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Modules:
+ * topshape: main application module
+
+Sub-packages:
+ * tests: contains testing code
+"""
+from .topshape import *
+
+__author__ = 'Martin Chlumsky'
+__email__ = 'martin.chlumsky@gmail.com'
+__version__ = '1.0.0'
