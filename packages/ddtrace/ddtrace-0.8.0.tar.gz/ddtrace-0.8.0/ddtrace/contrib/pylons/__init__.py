f8071d248c2a2c57d3d25a4bd6ad6917be95fc78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:05 +0000

    Add package ddtrace/0.8.0/ddtrace-0.8.0.tar.gz

---
+++ packages/ddtrace/ddtrace-0.8.0.tar.gz/ddtrace-0.8.0/ddtrace/contrib/pylons/__init__.py
@@ -0,0 +1,22 @@
+"""
+The pylons trace middleware will track request timings. To
+install the middleware, prepare your WSGI application and do
+the following::
+
+    from pylons.wsgiapp import PylonsApp
+
+    from ddtrace import tracer
+    from ddtrace.contrib.pylons import PylonsTraceMiddleware
+
+    app = PylonsApp(...)
+
+    traced_app = PylonsTraceMiddleware(app, tracer, service="my-pylons-app")
+
+Then you can define your routes and views as usual.
+"""
+
+from .middleware import PylonsTraceMiddleware
+from .patch import patch
+
+
+__all__ = ['PylonsTraceMiddleware', 'patch']
