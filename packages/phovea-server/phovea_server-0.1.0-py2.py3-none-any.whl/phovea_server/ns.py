840b675e7d8a8c9216368da75faeacc237d8e213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:39 +0000

    Add package phovea-server/0.1.0/phovea_server-0.1.0-py2.py3-none-any.whl

---
+++ packages/phovea-server/phovea_server-0.1.0-py2.py3-none-any.whl/phovea_server/ns.py
@@ -0,0 +1,7 @@
+###############################################################################
+# Caleydo - Visualization for Molecular Biology - http://caleydo.org
+# Copyright (c) The Caleydo Team. All rights reserved.
+# Licensed under the new BSD license, available at http://caleydo.org/license
+###############################################################################
+
+from flask import Flask as Namespace, request, abort, make_response, Response, send_from_directory, safe_join, jsonify, render_template_string, send_file  # noqa
