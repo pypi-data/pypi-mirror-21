1729180519cd95a12aec3bdaf930b3954e40c088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package siftpy/0.0.5/siftpy-0.0.5.tar.gz

---
+++ packages/siftpy/siftpy-0.0.5.tar.gz/siftpy-0.0.5/siftpy/_exceptions.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+class OperatorException(Exception): pass
+class ContextPropertyException(Exception): pass
+class InvalidChoiceException(Exception): pass
+class ValidationException(Exception): pass
+class FlowException(Exception): pass
