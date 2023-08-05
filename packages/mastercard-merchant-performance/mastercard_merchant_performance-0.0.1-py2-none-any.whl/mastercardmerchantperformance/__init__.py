f884113b8c76fd1f350d3f9e2d12a340bc384af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:45 +0000

    Add package mastercard-merchant-performance/0.0.1/mastercard_merchant_performance-0.0.1-py2-none-any.whl

---
+++ packages/mastercard-merchant-performance/mastercard_merchant_performance-0.0.1-py2-none-any.whl/mastercardmerchantperformance/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmerchantperformance.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmerchantperformance.merchantperformance import *
