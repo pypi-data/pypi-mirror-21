d9db5f2502ca68c8b7f3c9b64fc438857fe45509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:16 +0000

    Add package clytnester/1.2.0/clytnester-1.2.0.zip

---
+++ packages/clytnester/clytnester-1.2.0.zip/clytnester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'clytnester',
+	version	='1.2.0',
+	py_modules=['clytnester'],
+	author='hfpython',
+	author_email = 'hfpython@headfirstlabs.com',
+	url	='http://www.headfistlabs.com',
+	description = 'A simple printer of nested list',
+     )
