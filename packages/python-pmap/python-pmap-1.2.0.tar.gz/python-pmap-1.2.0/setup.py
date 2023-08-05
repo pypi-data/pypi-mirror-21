dc55761e5c1550d1ec039c758dab28382e45ddb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package python-pmap/1.2.0/python-pmap-1.2.0.tar.gz

---
+++ packages/python-pmap/python-pmap-1.2.0.tar.gz/python-pmap-1.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="python-pmap",
+    description="Clojure's pmap implementation for Python.",
+    long_description=open("README.md").read(),
+    author="Henrique Carvalho Alves",
+    author_email="hcarvalhoalves@gmail.com",
+    url="https://github.com/hcarvalhoalves/python-pmap",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    version="1.2.0",
+    install_requires=[
+        "pytest",
+        "pytest-xdist",
+        "toolz"
+    ],
+    dependency_links=[],
+    include_package_data=True,
+    zip_safe=False)
