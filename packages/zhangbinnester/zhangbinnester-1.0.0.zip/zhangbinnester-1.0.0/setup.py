327deae142c1281d1659bdfb3e8f86fcbf94d535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package zhangbinnester/1.0.0/zhangbinnester-1.0.0.zip

---
+++ packages/zhangbinnester/zhangbinnester-1.0.0.zip/zhangbinnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name  = 'zhangbinnester',
+	version ='1.0.0',
+	py_modules =['zhangbinnester'],
+	author ='hfpython',
+	author_email ='hfpython@headfirstlabs.com',
+	url ='http://www.headfirstlabs.com',
+	description = 'A simple printer of zhangbinnester lists',
+)
