e3d7b8dd011d3e4cdfa1fdddabe2bb4efec179b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:44 +0000

    Add package foxpath/1.0.3/foxpath-1.0.3.tar.gz

---
+++ packages/foxpath/foxpath-1.0.3.tar.gz/foxpath-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+
+with open(join(dirname(__file__), 'README.rst')) as f:
+    readme_text = f.read()
+
+setup(
+    name = "foxpath",
+    version = "1.0.3",
+    packages = find_packages(),
+    author = "Andy Lulham",
+    author_email = "andy.lulham@publishwhatyoufund.org",
+    description = "Python library for running FoXPath tests against XML",
+    long_description = readme_text,
+    license = "MIT",
+    install_requires = [
+        'lxml == 3.7.1',
+        'PyYAML == 3.12',
+    ],
+)
