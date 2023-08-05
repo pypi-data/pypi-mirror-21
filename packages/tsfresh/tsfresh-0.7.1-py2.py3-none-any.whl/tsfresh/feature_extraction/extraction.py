562b911d5643a64289d25e9ce4ffffdffb8308fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package tsfresh/0.7.1/tsfresh-0.7.1-py2.py3-none-any.whl

---
+++ packages/tsfresh/tsfresh-0.7.1-py2.py3-none-any.whl/tsfresh/feature_extraction/extraction.py
@@ -0,0 +1,363 @@
+# -*- coding: utf-8 -*-
+# This file as well as the whole tsfresh package are licenced under the MIT licence (see the LICENCE.txt)
+# Maximilian Christ (maximilianchrist.com), Blue Yonder Gmbh, 2016
+"""
+This module contains the main function to interact with tsfresh: extract features
+"""
+
+from __future__ import absolute_import, division
+
+import warnings
+from builtins import str
+from multiprocessing import Pool
+from functools import partial
+from six.moves.queue import Queue
+import logging
+import pandas as pd
+import numpy as np
+
+from tqdm import tqdm
+
+from tsfresh.utilities import dataframe_functions, helper_functions, profiling
+from tsfresh.feature_extraction.settings import FeatureExtractionSettings
+
+_logger = logging.getLogger(__name__)
+
+
+def extract_features(timeseries_container, feature_extraction_settings=None,
+                     column_id=None, column_sort=None, column_kind=None, column_value=None,
+                     parallelization=None):
+    """
+    Extract features from
+
+    * a :class:`pandas.DataFrame` containing the different time series
+
+    or
+
+    * a dictionary of :class:`pandas.DataFrame` each containing one type of time series
+
+    In both cases a :class:`pandas.DataFrame` with the calculated features will be returned.
+
+    For a list of all the calculated time series features, please see the
+    :class:`~tsfresh.feature_extraction.settings.FeatureExtractionSettings` class,
+    which is used to control which features with which parameters are calculated.
+
+    For a detailed explanation of the different parameters and data formats please see :ref:`data-formats-label`.
+
+    Examples
+    ========
+
+    >>> from tsfresh.examples import load_robot_execution_failures
+    >>> from tsfresh import extract_features
+    >>> df, _ = load_robot_execution_failures()
+    >>> X = extract_features(df, column_id='id', column_sort='time')
+
+    which would give the same results as described above. In this case, the column_kind is not allowed.
+    Except that, the same rules for leaving out the columns apply as above.
+
+    :param timeseries_container: The pandas.DataFrame with the time series to compute the features for, or a
+            dictionary of pandas.DataFrames.
+    :type timeseries_container: pandas.DataFrame or dict
+
+    :param feature_extraction_settings: settings object that controls which features are calculated
+    :type feature_extraction_settings: tsfresh.feature_extraction.settings.FeatureExtractionSettings
+
+    :param column_id: The name of the id column to group by.
+    :type column_id: str
+
+    :param column_sort: The name of the sort column.
+    :type column_sort: str
+
+    :param column_kind: The name of the column keeping record on the kind of the value.
+    :type column_kind: str
+
+    :param column_value: The name for the column keeping the value itself.
+    :type column_value: str
+
+    :param parallelization: Either ``'per_sample'`` or ``'per_kind'``   , see
+                            :func:`~tsfresh.feature_extraction.extraction._extract_features_parallel_per_sample`,
+                            :func:`~tsfresh.feature_extraction.extraction._extract_features_parallel_per_kind` and
+                            :ref:`parallelization-label` for details.
+    :type parallelization: str
+
+    :return: The (maybe imputed) DataFrame containing extracted features.
+    :rtype: pandas.DataFrame
+    """
+    import logging
+    logging.basicConfig()
+    
+    # Always use the standardized way of storing the data.
+    # See the function normalize_input_to_internal_representation for more information.
+    kind_to_df_map, column_id, column_value = \
+        dataframe_functions.normalize_input_to_internal_representation(df_or_dict=timeseries_container,
+                                                                       column_id=column_id,
+                                                                       column_sort=column_sort,
+                                                                       column_kind=column_kind,
+                                                                       column_value=column_value)
+
+    # Use the standard setting if the user did not supply ones himself.
+    if feature_extraction_settings is None:
+        feature_extraction_settings = FeatureExtractionSettings()
+        for key in kind_to_df_map:
+            feature_extraction_settings.set_default_parameters(key)
+
+    # Choose the parallelization according to a rule-of-thumb
+    if parallelization is None:
+        parallelization = 'per_sample' if (feature_extraction_settings.n_processes / 2) > len(kind_to_df_map) \
+            else 'per_kind'
+
+    _logger.info('Parallelizing feature calculation {}'.format(parallelization))
+
+    # If requested, do profiling (advanced feature)
+    if feature_extraction_settings.PROFILING:
+        profiler = profiling.start_profiling()
+
+    # Calculate the result
+    if parallelization == 'per_kind':
+        result = _extract_features_per_kind(kind_to_df_map, feature_extraction_settings,
+                                            column_id, column_value)
+    elif parallelization == 'per_sample':
+        result = _extract_features_parallel_per_sample(kind_to_df_map, feature_extraction_settings,
+                                                       column_id, column_value)
+    elif parallelization == 'no_parallelization':
+        result = _extract_features_per_kind(kind_to_df_map, feature_extraction_settings,
+                                            column_id, column_value, serial=True)
+    else:
+        raise ValueError("Argument parallelization must be one of: 'per_kind', 'per_sample'")
+
+    # Turn off profiling if it was turned on
+    if feature_extraction_settings.PROFILING:
+        profiling.end_profiling(profiler, filename=feature_extraction_settings.PROFILING_FILENAME,
+                                sorting=feature_extraction_settings.PROFILING_SORTING)
+
+    return result
+
+
+def _extract_features_per_kind(kind_to_df_map, settings, column_id, column_value, serial=False):
+    """
+    Parallelize the feature extraction per kind.
+
+    :param kind_to_df_map: The time series to compute the features for in our internal format
+    :type kind_to_df_map: dict of pandas.DataFrame
+
+    :param column_id: The name of the id column to group by.
+    :type column_id: str
+    :param column_value: The name for the column keeping the value itself.
+    :type column_value: str
+
+    :param settings: settings object that controls which features are calculated
+    :type settings: tsfresh.feature_extraction.settings.FeatureExtractionSettings
+
+    :param serial: Do not parallelize the extraction. This can be handy if (1) you want to debug something
+       (2) you want to profile something or (3) your environment does not support multiprocessing
+
+    :return: The (maybe imputed) DataFrame containing extracted features.
+    :rtype: pandas.DataFrame
+    """
+    partial_extract_features_for_one_time_series = partial(_extract_features_for_one_time_series,
+                                                           column_id=column_id,
+                                                           column_value=column_value,
+                                                           settings=settings)
+    pool = Pool(settings.n_processes)
+
+    chunksize = helper_functions.calculate_best_chunksize(kind_to_df_map, settings)
+
+    total_number_of_expected_results = len(kind_to_df_map)
+
+    if serial:
+        map_function = map
+    else:
+        map_function = partial(pool.imap_unordered, chunksize=chunksize)
+
+    extracted_features = tqdm(map_function(partial_extract_features_for_one_time_series, kind_to_df_map.items()),
+                              total=total_number_of_expected_results,
+                              desc="Feature Extraction", disable=settings.disable_progressbar)
+
+    pool.close()
+
+    # Concatenate all partial results
+    result = pd.concat(extracted_features, axis=1, join='outer').astype(np.float64)
+
+    # Impute the result if requested
+    if settings.IMPUTE is not None:
+        settings.IMPUTE(result)
+
+    pool.join()
+    return result
+
+
+def _extract_features_parallel_per_sample(kind_to_df_map, settings, column_id, column_value):
+    """
+    Parallelize the feature extraction per kind and per sample.
+
+    As the splitting of the dataframes per kind along column_id is quite costly, we settled for an async map in this
+    function. The result objects are temporarily stored in a fifo queue from which they can be retrieved in order
+    of submission.
+
+    :param kind_to_df_map: The time series to compute the features for in our internal format
+    :type kind_to_df_map: dict of pandas.DataFrame
+
+    :param column_id: The name of the id column to group by.
+    :type column_id: str
+    :param column_value: The name for the column keeping the value itself.
+    :type column_value: str
+
+    :param settings: settings object that controls which features are calculated
+    :type settings: tsfresh.feature_extraction.settings.FeatureExtractionSettings
+
+    :return: The (maybe imputed) DataFrame containing extracted features.
+    :rtype: pandas.DataFrame
+    """
+    partial_extract_features_for_one_time_series = partial(_extract_features_for_one_time_series,
+                                                           column_id=column_id,
+                                                           column_value=column_value,
+                                                           settings=settings)
+    pool = Pool(settings.n_processes)
+    total_number_of_expected_results = 0
+
+    # Submit map jobs per kind per sample
+    results_fifo = Queue()
+
+    for kind, df_kind in kind_to_df_map.items():
+        df_grouped_by_id = df_kind.groupby(column_id)
+
+        total_number_of_expected_results += len(df_grouped_by_id)
+
+        chunksize = helper_functions.calculate_best_chunksize(df_grouped_by_id, settings)
+
+        results_fifo.put(
+            pool.imap_unordered(
+                partial_extract_features_for_one_time_series,
+                [(kind, df_group) for _, df_group in df_grouped_by_id],
+                chunksize=chunksize
+            )
+        )
+
+    pool.close()
+
+    # Wait for the jobs to complete and concatenate the partial results
+    dfs_per_kind = []
+
+    # Do this all with a progress bar
+    with tqdm(total=total_number_of_expected_results, desc="Feature Extraction", disable=settings.disable_progressbar) as progress_bar:
+        # We need some sort of measure, when a new result is there. So we wrap the
+        # map_results into another iterable which updates the progress bar each time
+        # a new result is there
+        def iterable_with_tqdm_update(queue, progress_bar):
+            for element in queue:
+                progress_bar.update(1)
+                yield element
+
+        result = pd.DataFrame()
+        while not results_fifo.empty():
+            map_result = results_fifo.get()
+            dfs_kind = iterable_with_tqdm_update(map_result, progress_bar)
+            df_tmp = pd.concat(dfs_kind, axis=0).astype(np.float64)
+            result = pd.concat([result, df_tmp], axis=1).astype(np.float64)
+
+    # Impute the result if requested
+    if settings.IMPUTE is not None:
+        settings.IMPUTE(result)
+
+    pool.join()
+    return result
+
+
+def _extract_features_for_one_time_series(prefix_and_dataframe, column_id, column_value, settings):
+    """
+    Extract time series features for a given data frame based on the passed settings.
+
+    This is an internal function, please use the extract_features function.
+
+    The `dataframe` is expected to have at least two columns: column_id and column_value. The data is grouped together
+    by their column_id value and the time series features are calculated independently for each of the groups.
+    As a result, the function returns a :class:`pandas.DataFrame` with the ids as an index and as many columns as there
+    were features calculated.
+
+    To distinguish the features from others, all the calculated columns are given the prefix passed in by column_prefix.
+
+    For example, if you pass in a `dataframe` of shape
+
+        +====+=======+=====+
+        | id | value | ... |
+        +====+=======+=====+
+        | A  | 1     | ... |
+        +----+-------+-----+
+        | A  | 2     | ... |
+        +----+-------+-----+
+        | A  | 3     | ... |
+        +----+-------+-----+
+        | B  | 1     | ... |
+        +----+-------+-----+
+        | B  | 2     | ... |
+        +----+-------+-----+
+        | B  | 3     | ... |
+        +----+-------+-----+
+
+    with `column_id="id"`, `column_value="value"` and `column_prefix="prefix"` the resulting :class:`pandas.DataFrame`
+    will have shape
+
+        +=======+==================+==================+=====+==================+
+        | Index | prefix_feature_1 | prefix_feature_2 | ... | prefix_feature_N |
+        +=======+==================+==================+=====+==================+
+        | A     | ...              | ...              | ... | ...              |
+        +-------+------------------+------------------+-----+------------------+
+        | B     | ...              | ...              | ... | ...              |
+        +-------+------------------+------------------+-----+------------------+
+
+    where N is the number of features that were calculated. Which features are calculated is controlled by the
+    passed settings instance (see :class:`~tsfresh.feature_extraction.settings.FeatureExtractionSettings` for a list of
+    all possible features to calculate).
+
+    The parameter `dataframe` is not allowed to have any NaN value in it. It is possible to have different numbers
+    of values for different ids.
+
+    :param prefix_and_dataframe: Tuple of column_prefix and dataframe
+        column_prefix is the string that each extracted feature will be prefixed with (for better separation)
+        dataframe with at least the columns column_id and column_value to extract the time
+        series features for.
+    :type prefix_and_dataframe: (str, DataFrame)
+    :param column_id: The name of the column with the ids.
+    :param column_value: The name of the column with the values.
+    :param settings: The settings to control, which features will be extracted.
+    :return: A dataframe with the extracted features as the columns (prefixed with column_prefix) and as many
+        rows as their are unique values in the id column.
+    """
+    column_prefix, dataframe = prefix_and_dataframe
+    column_prefix = str(column_prefix)
+
+    # Ensure features are calculated on float64
+    dataframe[column_value] = dataframe[column_value].astype(np.float64)
+
+    with warnings.catch_warnings():
+        if not settings.show_warnings:
+            warnings.simplefilter("ignore")
+        else:
+            warnings.simplefilter("default")
+
+        if settings.set_default and column_prefix not in settings.kind_to_calculation_settings_mapping:
+            settings.set_default_parameters(column_prefix)
+
+        # Calculate the aggregation functions
+        column_name_to_aggregate_function = settings.get_aggregate_functions(column_prefix)
+
+        if column_name_to_aggregate_function:
+            extracted_features = dataframe.groupby(column_id)[column_value].aggregate(column_name_to_aggregate_function)
+        else:
+            extracted_features = pd.DataFrame(index=dataframe[column_id].unique())
+
+        # Calculate the apply functions
+        apply_functions = settings.get_apply_functions(column_prefix)
+
+        if apply_functions:
+            list_of_extracted_feature_dataframes = [extracted_features]
+            for apply_function, kwargs in apply_functions:
+                current_result = dataframe.groupby(column_id)[column_value].apply(apply_function, **kwargs).unstack()
+                if len(current_result) > 0:
+                    list_of_extracted_feature_dataframes.append(current_result)
+
+            if len(list_of_extracted_feature_dataframes) > 0:
+                extracted_features = pd.concat(list_of_extracted_feature_dataframes, axis=1,
+                                               join_axes=[extracted_features.index])
+
+        return extracted_features
---
+++ packages/tsfresh/tsfresh-0.7.1-py2.py3-none-any.whl/tsfresh/feature_selection/feature_selector.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+# This file as well as the whole tsfresh package are licenced under the MIT licence (see the LICENCE.txt)
+# Maximilian Christ (maximilianchrist.com), Blue Yonder Gmbh, 2016
+"""
+Contains a feature selection method that evaluates the importance of the different extracted features. To do so,
+for every feature the influence on the target is evaluated by an univariate tests and the p-Value is calculated.
+The methods that calculate the p-values are called feature selectors.
+
+Afterwards the Benjamini Hochberg procedure which is a multiple testing procedure decides which features to keep and
+which to cut off (solely based on the p-values).
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from functools import partial
+
+from builtins import zip
+from builtins import range
+from tqdm import tqdm
+import os
+import numpy as np
+import pandas as pd
+import logging
+from multiprocessing import Pool
+from tsfresh.utilities import helper_functions
+from tsfresh.feature_selection.significance_tests import target_binary_feature_real_test, \
+    target_real_feature_binary_test, target_real_feature_real_test, target_binary_feature_binary_test
+
+from tsfresh.feature_selection.settings import FeatureSignificanceTestsSettings
+
+
+_logger = logging.getLogger(__name__)
+
+
+def check_fs_sig_bh(X, y, settings=None):
+    """
+    The wrapper function that calls the significance test functions in this package.
+    In total, for each feature from the input pandas.DataFrame a univariate feature significance test is conducted.
+    Those tests generate p values that are then evaluated by the Benjamini Hochberg procedure to decide which features
+    to keep and which to delete.
+
+    We are testing
+    
+        :math:`H_0` = the Feature is not relevant and cannot be added
+
+    against
+
+        :math:`H_1` = the Feature is relevant and should be kept
+   
+    or in other words
+ 
+        :math:`H_0` = Target and Feature are independent / the Feature has no influence on the target
+
+        :math:`H_1` = Target and Feature are associated / dependent
+
+    When the target is binary this becomes
+    
+        :math:`H_0 = \\left( F_{\\text{target}=1} = F_{\\text{target}=0} \\right)`
+
+        :math:`H_1 = \\left( F_{\\text{target}=1} \\neq F_{\\text{target}=0} \\right)`
+    
+    Where :math:`F` is the distribution of the target.
+
+    In the same way we can state the hypothesis when the feature is binary
+    
+        :math:`H_0 =  \\left( T_{\\text{feature}=1} = T_{\\text{feature}=0} \\right)`
+
+        :math:`H_1 = \\left( T_{\\text{feature}=1} \\neq T_{\\text{feature}=0} \\right)`
+
+    Here :math:`T` is the distribution of the target.
+
+    TODO: And for real valued?
+
+    :param X: The DataFrame containing all the features and the target
+    :type X: pandas.DataFrame
+
+    :param y: The target vector
+    :type y: pandas.Series
+
+    :param settings: The feature selection settings to use to perform the tests.
+    :type settings: FeatureSignificanceTestsSettings
+
+    :return: A pandas.DataFrame with each column of the input DataFrame X as index with information on the significance
+            of this particular feature. The DataFrame has the columns
+            "Feature",
+            "type" (binary, real or const),
+            "p_value" (the significance of this feature as a p-value, lower means more significant)
+            "rejected" (if the Benjamini Hochberg procedure rejected this feature)
+    :rtype: pandas.DataFrame
+
+    """
+    if settings is None:
+        settings = FeatureSignificanceTestsSettings()
+
+    target_is_binary = len(set(y)) == 2
+
+    # todo: solve the multiclassification case. for a multi classification the algorithm considers the target to be
+    # regression. Instead one could perform a binary one versus all classification.
+
+    # Only allow entries for which the target is known!
+    y = y.astype(np.float)
+    X = X.copy().loc[~(y == np.NaN), :]
+
+    # Create the DataFrame df_features containing the information about the different hypotheses
+    # Every row contains information over one feature column from X
+    df_features = pd.DataFrame()
+
+    df_features['Feature'] = list(set(X.columns))
+    df_features = df_features.set_index('Feature', drop=False)
+
+    # Add relevant columns to df_features
+    df_features["rejected"] = np.nan
+    df_features["type"] = np.nan
+    df_features["p_value"] = np.nan
+
+    # Calculate the feature significance in parallel
+    pool = Pool(settings.n_processes)
+
+    # Helper function which wraps the _calculate_p_value with many arguments already set
+    f = partial(_calculate_p_value, y=y, settings=settings, target_is_binary=target_is_binary)
+
+    chunksize = helper_functions.calculate_best_chunksize(df_features, settings)
+    total_number_of_features = len(df_features)
+    results = tqdm(pool.imap_unordered(f, [X[feature] for feature in df_features['Feature']], chunksize=chunksize),
+                   total=total_number_of_features, desc="Feature Selection")
+
+    p_values_of_features = pd.DataFrame(list(results))
+    df_features.update(p_values_of_features)
+
+    pool.close()
+    pool.join()
+
+    # Perform the real feature rejection
+    if "const" in set(df_features.type):
+        df_features_bh = benjamini_hochberg_test(df_features.loc[~(df_features.type == "const")], settings)
+        df_features = pd.concat([df_features_bh, df_features.loc[df_features.type == "const"]])
+    else:
+        df_features = benjamini_hochberg_test(df_features, settings)
+        
+    # It is very important that we have a boolean "rejected" column, so we do a cast here to be sure
+    df_features["rejected"] = df_features["rejected"].astype("bool")
+
+    if settings.write_selection_report:
+        # Write results of BH - Test to file
+        if not os.path.exists(settings.result_dir):
+            os.mkdir(settings.result_dir)
+
+        with open(os.path.join(settings.result_dir, "fs_bh_results.txt"), 'w') as file_out:
+            file_out.write(("Performed BH Test to control the false discovery rate(FDR); \n"
+                            "FDR-Level={0};Hypothesis independent={1}\n"
+                            ).format(settings.fdr_level, settings.hypotheses_independent))
+            df_features.to_csv(index=False, path_or_buf=file_out, sep=';', float_format='%.4f')
+    return df_features
+
+
+def _calculate_p_value(feature_column, y, settings, target_is_binary):
+    """
+    Internal helper function to calculate the p-value of a given feature using one of the dedicated
+    functions target_*_feature_*_test.
+
+    :param feature_column: the feature column.
+    :type feature_column: pandas.Series
+
+    :param y: the binary target vector
+    :type y: pandas.Series
+
+    :param settings: The settings object to control how the significance is calculated.
+    :type settings: FeatureSignificanceTestsSettings
+
+    :param target_is_binary: Whether the target is binary or not
+    :type target_is_binary: bool
+
+    :return: the p-value of the feature significance test and the type of the tested feature as a Series.
+             Lower p-values indicate a higher feature significance.
+    :rtype: pd.Series
+    """
+    # Do not process constant features
+    if len(pd.unique(feature_column.values)) == 1:
+        _logger.warning("[test_feature_significance] Feature {} is constant".format(feature_column.name))
+        return pd.Series({"type": "const", "rejected": False}, name=feature_column.name)
+
+    else:
+        if target_is_binary:
+            # Decide if the current feature is binary or not
+            if len(set(feature_column.values)) == 2:
+                type = "binary"
+                p_value = target_binary_feature_binary_test(feature_column, y, settings)
+            else:
+                type = "real"
+                p_value = target_binary_feature_real_test(feature_column, y, settings)
+        else:
+            # Decide if the current feature is binary or not
+            if len(set(feature_column.values)) == 2:
+                type = "binary"
+                p_value = target_real_feature_binary_test(feature_column, y, settings)
+            else:
+                type = "real"
+                p_value = target_real_feature_real_test(feature_column, y, settings)
+
+        return pd.Series({"p_value": p_value, "type": type}, name=feature_column.name)
+
+
+def benjamini_hochberg_test(df_pvalues, settings):
+    """
+    This is an implementation of the benjamini hochberg procedure that calculates which of the hypotheses belonging
+    to the different p-Values from df_p to reject. While doing so, this test controls the false discovery rate,
+    which is the ratio of false rejections by all rejections:
+
+    .. math::
+
+        FDR = \\mathbb{E} \\left [ \\frac{ |\\text{false rejections}| }{ |\\text{all rejections}|} \\right]
+
+
+    References
+    ----------
+
+    .. [1] Benjamini, Yoav and Yekutieli, Daniel (2001).
+        The control of the false discovery rate in multiple testing under dependency.
+        Annals of statistics, 1165--1188
+
+
+    :param df_pvalues: This DataFrame should contain the p_values of the different hypotheses in a column named
+                       "p_values".
+    :type df_pvalues: pandas.DataFrame
+
+    :param settings: The settings object to use for controlling the false discovery rate (FDR_level) and
+           whether to treat the hypothesis as independent or not (hypotheses_independent).
+    :type settings: FeatureSignificanceTestsSettings
+
+    :return: The same DataFrame as the input, but with an added boolean column "rejected".
+    :rtype: pandas.DataFrame
+    """
+
+    # Get auxiliary variables and vectors
+    df_pvalues = df_pvalues.sort_values(by="p_value")
+    m = len(df_pvalues)
+    K = list(range(1, m + 1))
+
+    # Calculate the weight vector C
+    if settings.hypotheses_independent:
+        # c(k) = 1
+        C = [1] * m
+    else:
+        # c(k) = \sum_{i=1}^m 1/i
+        C = [sum([1.0 / i for i in range(1, k + 1)]) for k in K]
+
+    # Calculate the vector T to compare to the p_value
+    T = [settings.fdr_level * k / m * 1.0 / c for k, c in zip(K, C)]
+
+    # Get the last rejected p_value
+    try:
+        k_max = list(df_pvalues.p_value <= T).index(False)
+    except ValueError:
+        k_max = m
+
+    # Add the column denoting if hypothesis was rejected
+    df_pvalues["rejected"] = [True] * k_max + [False] * (m - k_max)
+
+    return df_pvalues
