358901b65b0685a5ae2ae8ed84ce3a09c63ff2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package py6Nimmt/0.1.2/py6Nimmt-0.1.2-py3-none-any.whl

---
+++ packages/py6Nimmt/py6Nimmt-0.1.2-py3-none-any.whl/py6Nimmt/Table.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+
+from py6Nimmt import Card
+from py6Nimmt import Deck
+from py6Nimmt import Player
+
+
+class Table(object):
+    """ Table """
+
+    def __init__(self, numPlayers=2, namesPlayers=[]):
+
+        self.deck = Deck.Deck()
+        self.iniDeck()
+        self.shuffleDeck()
+
+        self.discards = Deck.Deck()
+
+        self.board = [Deck.Deck(0), Deck.Deck(1), Deck.Deck(2), Deck.Deck(3)]
+        for i in range(0, len(self.board)):
+            self.board[i].add(self.deck.draw())
+
+        self.players = []
+        for i in range(0, numPlayers):
+            hand = Deck.Deck(i+1)
+            for c in range(0, 10):
+                hand.add(self.deck.draw())
+            if i >= len(namesPlayers):
+                self.players.append(Player.Player(i+1, 'Player', hand))
+            else:
+                if namesPlayers[i]:
+                    self.players.append(Player.Player(i+1,
+                                                      namesPlayers[i], hand))
+                else:
+                    self.players.append(Player.Player(i+1, 'Player', hand))
+
+    def countBoards(self):
+        return len(self.board)
+
+    def countDeck(self):
+        return self.deck.count()
+
+    def countDiscard(self):
+        return self.discards.count()
+
+    def printPlayer(self, id=None):
+        s = ''
+        for p in self.players:
+            if id is None or p.id == id:
+                s = s + str(p) + ':' + '\n'
+                s = s + p.printHand() + '\n'
+        return s
+
+    def printPlayerPile(self, id=None):
+        s = ''
+        for p in self.players:
+            if id is None or p.id == id:
+                s = s + p.printPile() + '\n'
+        return s
+
+    def printBoard(self):
+        i = 1
+        s = ''.rjust(57, '-') + ' ' + str(self.countDeck()) + '\n'
+        for b in self.board:
+            s = s + '{}: {} |'.format(i, b)
+            for z in range(0, 5 - b.count()):
+                s = s + '         |'
+            s = s + '\n'
+            i = i + 1
+        s = s + ''.rjust(60, '-')
+        return s
+
+    def shuffleDeck(self):
+        self.deck.shuffle()
+
+    def player(self, id):
+        for p in self.players:
+            if p.id == id:
+                return p
+
+    def winner(self):
+        win = self.player(1)
+        for p in self.players:
+            if p > win:
+                win = p
+        return win
+
+    def boardCorrect(self, card):
+        i = 0
+        z = -1
+        anterior = Card.Card(0, 0, 'none', 0)
+        for b in self.board:
+            if card >= b.lastCard() and anterior <= b.lastCard():
+                z = i
+                anterior = b.lastCard()
+            i = i + 1
+        if z == -1:
+            return None
+        else:
+            return self.board[z]
+
+    def catchBoard(self, p, b, card):
+        b = b - 1
+        self.player(p).pile = self.player(p).pile + self.board[b]
+        self.board[b].clean()
+        self.board[b].add(card)
+
+    def turnPlayer(self, p):
+        # DEPRECATED
+        c = int(input("Choose a card to play:"))
+        card = self.player(p).playCard(c)
+        if self.boardCorrect(card) is None:
+            print("The played card is lower than all "
+                  "the latest cards present on the rows.")
+            b = int(input("Choose a row:"))
+            self.catchBoard(p, b, card)
+        else:
+            bc = self.boardCorrect(card)
+            if bc.count() >= 5:
+                self.player(p).pile = self.player(p).pile + bc
+                bc.clean()
+            bc.add(card)
+        if self.deck.count() > 0:
+            self.player(p).addHand(self.deck.draw())
+
+    def playCard(self, p, c):
+        card = self.player(p).playCard(c)
+        if self.boardCorrect(card):
+            bc = self.boardCorrect(card)
+            if bc.count() >= 5:
+                self.player(p).pile = self.player(p).pile + bc
+                bc.clean()
+            bc.add(card)
+            return None
+        else:
+            return card
+
+    def drawCard(self, p):
+        if self.deck.count() > 0:
+            self.player(p).addHand(self.deck.draw())
+
+    def endgame(self):
+        fin = True
+        if self.deck.count() != 0:
+            fin = False
+        for p in self.players:
+            if p.countHand() != 0:
+                fin = False
+        return fin
+
+    def stats(self):
+        s = ''.rjust(60, '=') + '\n'
+        for p in self.players:
+            s = s + '{}: {:03}'.format(p, p.score()) + '\n'
+            s = s + p.printPile() + '\n'
+        s = s + ''.rjust(60, '=')
+        return s
+
+    def iniDeck(self):
+        # Initialize Deck
+        # Cartas del 1 al 104
+        # Los multiplos de 5 dan 2 puntos
+        # Los multiplos de 10, 3 puntos
+        # Los multiplos de 11, 5 puntos
+        # El 55 da 7 puntos (es múltiplo de 11 y de 5)
+        # El resto dan 1 punto
+        for i in range(1, 105):
+            score = 0
+            if i % 10 == 0:
+                score = -3
+            elif i % 5 == 0:
+                score = -2
+            if i % 11 == 0:
+                score = score - 5
+            if score == 0:
+                score = -1
+            c = Card.Card(i, i, 'none', score)
+            self.deck.add(c)
+        return self.deck.count()
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.deck.count(),
+                                     self.discards.count(),
+                                     len(self.players))
+
+    def __repr__(self):
+        return '{} - {} - {}'.format(self.deck.count(),
+                                     self.discards.count(),
+                                     len(self.players))
---
+++ packages/py6Nimmt/py6Nimmt-0.1.2-py3-none-any.whl/py6Nimmt/py6Nimmt.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+
+from py6Nimmt import buildparser
+from py6Nimmt import Table
+import os
+import sys
+import gettext
+_ = gettext.gettext
+
+
+def main():
+
+    # Get Args
+    parser = buildparser.build_parser()
+    options = parser.parse_args()
+    # gettext
+    if options.lang != '':
+        os.environ['LANG'] = options.lang
+    else:
+        if sys.platform.startswith('win'):
+            import locale
+            if os.getenv('LANG') is None:
+                lang, enc = locale.getdefaultlocale()
+                os.environ['LANG'] = lang
+    gettext.textdomain('py6Nimmt')
+    gettext.bindtextdomain('py6Nimmt', './py6Nimmt/locale')
+
+    # print('Locale: ' + os.environ['LANG'])
+
+    if options.rules:
+        print(printRules())
+        exit()
+
+    ps = int(options.players)
+    if ps < 1 or ps > 10:
+        print(_('The argument value of -ps ' +
+              'must be a number between 1 and 10: ') + '{}.'.format(ps))
+        exit()
+    pn = list(options.namesPlayers)
+    if ps < len(pn):
+        ps = len(pn)
+
+    t = Table.Table(ps, pn)
+
+    p = 0
+    e = ''
+
+    while e != 'Q' and e != 'q' and not t.endgame():
+        if p < ps:
+            p = p + 1
+        else:
+            p = 1
+        e = input(_('Next Player (q to Quit):'))
+        clearscr()
+        if e != 'q' and e != 'Q':
+            print(t.printBoard())
+            print(t.printPlayer(p))
+            print(_('Pile: ') + t.printPlayerPile(p))
+            c = input_int(_('Choose a card to play (q to Quit):'),
+                          1, t.player(p).countHand())
+            if c:
+                card = t.playCard(p, c)
+                b = -1
+                if card:
+                    print(_('The played card is lower than all '
+                          'the latest cards present on the rows.'))
+                    b = input_int(_('Choose a row to catch (q to Quit):'),
+                                  1, t.countBoards())
+                    if b:
+                        t.catchBoard(p, b, card)
+                    else:
+                        e = 'q'
+                if e or e != 'q':
+                    t.drawCard(p)
+            else:
+                e = 'q'
+            print(t.printBoard())
+            print(t.printPlayer(p))
+            print(_('Pile: ') + t.printPlayerPile(p))
+        else:
+            print(t.printBoard())
+            print(t.printPlayer(p))
+            print(_('Pile: ') + t.printPlayerPile(p))
+        print(''.rjust(60, '='))
+    clearscr()
+    print(t.stats())
+    print(_('Winner: ') + str(t.winner()))
+
+
+def input_int(text='Insert a number (q to Quit):', min=0, max=10):
+    while True:
+        valor = input(text)
+        try:
+            if valor == 'q' or valor == 'Q':
+                return None
+            else:
+                valor = int(valor)
+                if valor >= min and valor <= max:
+                    return valor
+        except ValueError:
+            print(_('Value error'))
+
+
+def clearscr():
+    osname = os.name
+    if osname == 'posix':
+        os.system('clear')
+    elif osname == 'nt' or osname == 'dos':
+        os.system('cls')
+    else:
+        print('\n' * 30)
+
+
+def printRules():
+    txt = _('text_rules')
+    return txt
+
+
+if __name__ == '__main__':
+    main()
