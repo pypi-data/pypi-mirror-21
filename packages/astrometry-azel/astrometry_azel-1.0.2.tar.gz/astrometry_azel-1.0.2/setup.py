2707241ecfebeca1ea730c6356407be942d205f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package astrometry-azel/1.0.2/astrometry_azel-1.0.2.tar.gz

---
+++ packages/astrometry-azel/astrometry_azel-1.0.2.tar.gz/astrometry_azel-1.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+req = ['pymap3d',
+        'nose','pillow','python-dateutil','pytz','numpy','scipy','h5py','astropy','scikit-image','matplotlib','seaborn' ]
+
+setup(name='astrometry_azel',
+      packages=['astrometry_azel'],
+      author='Michael Hirsch, Ph.D.',
+      description='Register images to az/el using the astrometry.net program',
+      url='https://github.com/scivision/astrometry_azel',
+      version='1.0.2',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: Scientific/Engineering :: Atmospheric Science',
+      'Programming Language :: Python :: 3.6',
+      ],
+      install_requires=req,
+      extras_require={'pyfits':'pyfits'},
+	  )
+
