a431c08c7cbc7b162ff9d5784f381b4df0f73ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package ethics/0.5.1/ethics-0.5.1.tar.gz

---
+++ packages/ethics/ethics-0.5.1.tar.gz/ethics-0.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='ethics',
+      version='0.5.1',
+      description = ("A python toolbox for ethical reasoning."),
+      author='Felix Lindner',
+      author_email='info@hera-project.com',
+      url='http://www.hera-project.com',
+      py_modules=['language', 'semantics', 'principles', 'tools'],
+      packages=['cases'],
+      package_data={'cases': ['rescue-robot.json']},
+      )
