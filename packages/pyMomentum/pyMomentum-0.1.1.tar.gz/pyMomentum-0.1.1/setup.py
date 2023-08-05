702ea486d92b434683370adb15c12150b96d0a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package pyMomentum/0.1.1/pyMomentum-0.1.1.tar.gz

---
+++ packages/pyMomentum/pyMomentum-0.1.1.tar.gz/pyMomentum-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pyMomentum',
+      packages=['pyMomentum','pyMomentum.sms','pyMomentum.sms.providers'],
+      version='0.1.1',
+      description='Python libraries developed by Momentum',
+      url='https://github.com/MomentumAS/pyMomentum',
+      keywords=['momentum', 'library', 'sms'],
+      author='Momentum Teknoloji AS',
+      author_email='info@mtas.com.tr',
+      license='MIT',
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False
+      )
