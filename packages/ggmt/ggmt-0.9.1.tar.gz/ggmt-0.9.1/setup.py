bb6805beb64f7f6fbe67e64b85b2f27fabe9595e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package ggmt/0.9.1/ggmt-0.9.1.tar.gz

---
+++ packages/ggmt/ggmt-0.9.1.tar.gz/ggmt-0.9.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='ggmt',
+    version='0.9.1',
+    packages=['ggmt'],
+    url='https://github.com/Granitosaurus/ggmt',
+    license='GPLV3+',
+    author='Bernardas Ališauskas',
+    author_email='bernardas.alisauskas@gmail.com',
+    description='Good Game Match Ticker - command line application for tracking matches of various e-sport games',
+    install_requires=[
+        'click',
+        'requests',
+        'parsel',
+        'jinja2'
+    ],
+    entry_points="""
+        [console_scripts]
+        ggmt=ggmt.cli:cli
+    """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Games/Entertainment",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ]
+)
