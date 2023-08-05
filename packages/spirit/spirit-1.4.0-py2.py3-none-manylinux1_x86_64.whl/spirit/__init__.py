1950a8e5e350b2541e288e4af998e7723f2d3b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package spirit/1.4.0/spirit-1.4.0-py2.py3-none-manylinux1_x86_64.whl

---
+++ packages/spirit/spirit-1.4.0-py2.py3-none-manylinux1_x86_64.whl/spirit/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.4.0"
+__title__ = "Spirit"
+__description__ = "Optimizations and Dynamics Framework for atomistic Spin systems"
+__uri__ = "https://github.com/spirit-code/spirit"
+__license__ = "MIT"
+__author__ = "-"
+__email__ = "g.mueller@fz-juelich.de"
