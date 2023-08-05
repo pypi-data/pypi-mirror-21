45fcc6415456de4f3c658ebcff161989d7eaf433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:46 +0000

    Add package csirtg-smrt/0.0.0a38/csirtg_smrt-0.0.0a38.tar.gz

---
+++ packages/csirtg-smrt/csirtg_smrt-0.0.0a38.tar.gz/csirtg_smrt-0.0.0a38/csirtg_smrt/exceptions.py
@@ -0,0 +1,18 @@
+
+class CsirtgException(Exception):
+    def __init__(self, msg):
+        self.msg = "{}".format(msg)
+
+    def __str__(self):
+        return self.msg
+
+
+class AuthError(CsirtgException):
+    pass
+
+
+class TimeoutError(CsirtgException):
+    pass
+
+class RuleUnsupported(CsirtgException):
+    pass
---
+++ packages/csirtg-smrt/csirtg_smrt-0.0.0a38.tar.gz/csirtg_smrt-0.0.0a38/test/ufw/test_ufw.py
@@ -0,0 +1,35 @@
+from csirtg_smrt.ufw import process_events
+
+
+def test_ufw():
+    file = 'test/ufw/ufw.log'
+
+    events = []
+    with open(file) as f:
+        for l in f.read().split("\n"):
+            i = next(process_events([l]))
+            events.append(i)
+
+    assert len(events) > 0
+    assert events[0].indicator == '114.33.197.193'
+
+
+def test_ufw_ubuntu16():
+    file = 'test/ufw/ufw_ubuntu16.log'
+
+    events = []
+    with open(file) as f:
+        for l in f.read().split("\n"):
+            i = process_events([l])
+
+            try:
+                i = next(i)
+            except StopIteration:
+                pass
+
+            events.append(i)
+
+    from pprint import pprint
+    assert len(events) > 0
+    assert events[0].indicator == '10.0.2.2'
+    assert events[1].indicator == '61.7.190.140'
