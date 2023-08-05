e76b76bffc38fdbcc7e55165ed0ee5feeb2a4895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:11 +0000

    Add package globus-cli/1.0.0.0/globus_cli-1.0.0.0-py2.7.egg

---
+++ packages/globus-cli/globus_cli-1.0.0.0-py2.7.egg/globus_cli/version.py
@@ -0,0 +1,48 @@
+from distutils.version import LooseVersion
+
+# single source of truth for package version,
+# see https://packaging.python.org/en/latest/single_source_version/
+
+# The Globus CLI version *always* starts with the version of the SDK which it
+# depends upon. If necessary, we do additional point releases upon the SDK
+# version.
+# For example, if the SDK version is
+#       2.4.6
+# then the following are valid CLI versions:
+#       2.4.6.0
+#       2.4.6.1
+#       2.4.6.10
+#       2.4.6.99
+# and the following invalid CLI versions:
+#       2.4.6       -- doesn't specify point release
+#       2.4.7.0     -- should depend on SDK 2.4.7
+#       2.4.6.1.2   -- no additional point versions
+#       0.1.0.0     -- no special rules for things like this
+#       1.4.6.0     -- differing major version, obviously wrong
+__version__ = "1.0.0.0"
+
+# app name to send as part of SDK requests
+app_name = 'Globus CLI v{} - Beta'.format(__version__)
+
+
+# pull down version data from PyPi
+def get_versions():
+    """
+    Wrap in a function to ensure that we don't run this every time a CLI
+    command runs (yuck!)
+
+    Also protects import of `requests` from issues when grabbed by setuptools.
+    More on that inline
+    """
+    # import in the func (rather than top-level scope) so that at setup time,
+    # `requests` isn't required -- otherwise, setuptools will fail to run
+    # because requests isn't installed yet.
+    import requests
+    try:
+        version_data = requests.get(
+            'https://pypi.python.org/pypi/globus-cli/json').json()
+        latest = max(LooseVersion(v) for v in version_data['releases'])
+        return latest, LooseVersion(__version__)
+    # if the fetch from pypi fails
+    except requests.RequestException:
+        return None, LooseVersion(__version__)
---
+++ packages/globus-cli/globus_cli-1.0.0.0-py2.7.egg/tests/framework/constants.py
@@ -0,0 +1,15 @@
+GO_EP1_ID = "ddb59aef-6d04-11e5-ba46-22000b92c6ec"
+GO_EP2_ID = "ddb59af0-6d04-11e5-ba46-22000b92c6ec"
+
+# ------ #
+# tokens #
+# ------ #
+CLITESTER1A_TRANSFER_RT = (
+    "AQEAAAAAAASZW75F06PMlmWcsINaQOKnbY72CBk0Ig"
+    "FSgL3tO6szYUEBJOq8xTDdKtHUKD4o4Pdm9x012Owt")
+CLITESTER1A_AUTH_RT = (
+    "AQEAAAAAAASZXC-lkxvvazq0ZZMiOMOiWHmgmUXrhH"
+    "sS57LzjtA1fNjQYZgS7Mk0mRPRQ4nnpNVdR3D3ts51")
+# ---------- #
+# end tokens #
+# ---------- #
