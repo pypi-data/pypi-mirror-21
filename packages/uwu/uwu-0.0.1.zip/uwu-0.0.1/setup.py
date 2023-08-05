0eba6ac11b1381268f64804fbfead88490db3949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:21 +0000

    Add package uwu/0.0.1/uwu-0.0.1.zip

---
+++ packages/uwu/uwu-0.0.1.zip/uwu-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'uwu',
+    packages = ['uwu'],
+    version = '0.0.1',
+    description = "A placeholder for something special!",
+    author = 'silicWulf',
+    author_email = 'me@siliconwolf.pw',
+    url = 'https://github.com/silicWulf/esto',
+    download_url = 'https://github.com/silicWulf/esto/tarball/0.0.1',
+    keywords = ['uwu'],
+    classifiers = [],
+)
