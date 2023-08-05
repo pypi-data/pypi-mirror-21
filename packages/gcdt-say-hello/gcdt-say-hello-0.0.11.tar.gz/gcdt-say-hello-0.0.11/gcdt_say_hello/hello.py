b44be3e76125c2f9599a48319b19f21fdc091f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package gcdt-say-hello/0.0.11/gcdt-say-hello-0.0.11.tar.gz

---
+++ packages/gcdt-say-hello/gcdt-say-hello-0.0.11.tar.gz/gcdt-say-hello-0.0.11/gcdt_say_hello/hello.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""A gcdt-plugin which demonstrates how to implement hello world as plugin."""
+from __future__ import unicode_literals, print_function
+
+from gcdt import gcdt_signals
+
+
+def say_hello(context):
+    """say hi.
+    :param context: The _awsclient, etc.. say_hello plugin needs the 'user'
+    """
+    print('MoinMoin %s!' % context.get('user', 'to you'))
+
+
+def say_bye(context):
+    """say bye.
+    :param context: The _awsclient, etc.. say_hello plugin needs the 'user'
+    """
+    print('Bye %s. Talk to you soon!' % context.get('user', 'then'))
+
+
+def register():
+    """Please be very specific about when your plugin needs to run and why.
+    E.g. run the sample stuff after at the very beginning of the lifecycle
+    """
+    gcdt_signals.initialized.connect(say_hello)
+    gcdt_signals.finalized.connect(say_bye)
+
+
+def deregister():
+    gcdt_signals.initialized.disconnect(say_hello)
+    gcdt_signals.finalized.disconnect(say_bye)
