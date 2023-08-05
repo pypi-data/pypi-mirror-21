8786e5a9d7addc50f0f920c607837d206f1c2794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package listmatch/0.2.0/listmatch-0.2.0-py2.py3-none-any.whl

---
+++ packages/listmatch/listmatch-0.2.0-py2.py3-none-any.whl/listmatch/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from .listmatch import (
+    Possibilities,
+    atom,
+    concat,
+    maybe,
+    one_or_more,
+    options,
+    zero_or_more
+)
+
+
+__author__ = """Pokey Rule"""
+__email__ = 'pokey.rule@gmail.com'
+__version__ = '0.2.0'
+
+__all__ = [
+    Possibilities,
+    atom,
+    concat,
+    maybe,
+    one_or_more,
+    options,
+    zero_or_more,
+]
