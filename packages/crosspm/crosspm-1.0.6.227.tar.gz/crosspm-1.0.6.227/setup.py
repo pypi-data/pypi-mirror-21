b8ca8b1226eab83d0689be60e9c809564793d4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:57 +0000

    Add package crosspm/1.0.6.227/crosspm-1.0.6.227.tar.gz

---
+++ packages/crosspm/crosspm-1.0.6.227.tar.gz/crosspm-1.0.6.227/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from crosspm import config
+
+setup(
+    name='crosspm',
+    version=config.__version__,
+    description='Cross Package Manager',
+    license='MIT',
+    author='Alexander Kovalev',
+    author_email='ak@alkov.pro',
+    url='https://github.com/devopshq/crosspm.git',
+    entry_points={'console_scripts': ['crosspm=crosspm.__main__:main']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=[
+        'development',
+        'dependency',
+        'requirements',
+    ],
+    packages=[
+        'crosspm',
+        'crosspm.helpers',
+        'crosspm.adapters',
+    ],
+    setup_requires=[
+        'wheel',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-flask',
+        'pyyaml',
+    ],
+    install_requires=[
+        'requests',
+        'docopt',
+        'pyyaml',
+        'dohq-art',
+    ],
+    package_data={
+        '': [
+            '*.cmake',
+            '../LICENSE',
+        ],
+    },
+)
