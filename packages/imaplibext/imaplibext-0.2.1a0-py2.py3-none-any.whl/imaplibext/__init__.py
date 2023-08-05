749caf141a7e8ffe0fe6c539cdb405b8f2fca5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:55 +0000

    Add package imaplibext/0.2.1a0/imaplibext-0.2.1a0-py2.py3-none-any.whl

---
+++ packages/imaplibext/imaplibext-0.2.1a0-py2.py3-none-any.whl/imaplibext/__init__.py
@@ -0,0 +1,10 @@
+from ._imaplibext import (
+    IMAP4,
+    IMAP4_SSL
+)
+
+__title__ = 'imaplibext'
+__author__ = 'Thomas Ward'
+__copyright__ = '2017 Thomas Ward'
+__license__ = 'AGPLv3+'
+__all__ = ('IMAP4', 'IMAP4_SSL')
