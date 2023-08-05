59ee4a54f6338595d8d1df5eea2e1c68836880e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package asyncpg/0.10.1/asyncpg-0.10.1-cp36-cp36m-manylinux1_i686.whl

---
+++ packages/asyncpg/asyncpg-0.10.1-cp36-cp36m-manylinux1_i686.whl/asyncpg/compat.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2016-present the ayncpg authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of asyncpg and is released under
+# the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+
+
+import functools
+import sys
+
+
+if sys.version_info < (3, 5, 2):
+    def aiter_compat(func):
+        @functools.wraps(func)
+        async def wrapper(self):
+            return func(self)
+        return wrapper
+else:
+    def aiter_compat(func):
+        return func
