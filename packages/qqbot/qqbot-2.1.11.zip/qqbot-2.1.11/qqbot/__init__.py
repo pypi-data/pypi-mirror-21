fb8d256f4c8f3f16ba1277f95cbb643aa4b4dc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package qqbot/2.1.11/qqbot-2.1.11.zip

---
+++ packages/qqbot/qqbot-2.1.11.zip/qqbot-2.1.11/qqbot/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .qqbotcls import QQBot, QQBotSlot, RunBot
+from .qterm import QTerm
+from .common import CallInNewConsole, AutoTest
+from .mainloop import MainLoop, Put, PutTo, AddWorkerTo
+Main = RunBot
+qqbotslot = QQBotSlot
