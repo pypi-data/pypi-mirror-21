1c65223e7874efc5f024114ee91ad5d842f6afb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package aiosmtplib/1.0.1/aiosmtplib-1.0.1.tar.gz

---
+++ packages/aiosmtplib/aiosmtplib-1.0.1.tar.gz/aiosmtplib-1.0.1/setup.py
@@ -0,0 +1,53 @@
+import re
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+VERSION_REGEX = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+
+init = Path('src/aiosmtplib/__init__.py')
+readme = Path(__file__).with_name('README.rst')
+version_match = re.search(VERSION_REGEX, init.read_text('utf-8'), re.MULTILINE)
+
+if version_match:
+    version = version_match.group(1)
+else:
+    raise RuntimeError('Cannot find version information')
+
+setup(
+    name='aiosmtplib',
+    version=version,
+    description='asyncio SMTP client',
+    long_description=readme.read_text('utf-8'),
+    author='Cole Maclean',
+    author_email='hi@cole.io',
+    url='https://github.com/cole/aiosmtplib',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license='MIT',
+    keywords=['smtp', 'email', 'asyncio'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Communications :: Email',
+        'Topic :: System :: Networking',
+    ],
+    extras_require={
+        'testing': [
+            'pytest >= 3.0.2',
+            'pytest-asyncio ~= 0.5.0',
+            'pytest-cov ~= 2.4',
+            'wheel[signatures]'
+        ]
+    }
+)
---
+++ packages/aiosmtplib/aiosmtplib-1.0.1.tar.gz/aiosmtplib-1.0.1/src/aiosmtplib/__init__.py
@@ -0,0 +1,28 @@
+"""
+aiosmtplib
+==========
+
+An asyncio SMTP client.
+
+Roughly based (with API differences) on smtplib from the Python 3 standard
+library by: The Dragon De Monsyne <dragondm@integral.org>
+
+Author: Cole Maclean <hi@cole.io>
+"""
+
+__title__ = 'aiosmtplib'
+__version__ = '1.0.1'
+__author__ = 'Cole Maclean'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Cole Maclean'
+
+
+from .errors import *  # NOQA
+from .response import *  # NOQA
+from .smtp import *  # NOQA
+from .status import *  # NOQA
+
+
+__all__ = (
+    errors.__all__ + response.__all__ + smtp.__all__ + status.__all__  # NOQA
+)
