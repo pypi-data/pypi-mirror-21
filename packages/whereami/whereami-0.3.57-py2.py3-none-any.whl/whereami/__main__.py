1494662d62cb1bfb41d6f8cd5dd4cc775eb8f431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:32 +0000

    Add package whereami/0.3.57/whereami-0.3.57-py2.py3-none-any.whl

---
+++ packages/whereami/whereami-0.3.57-py2.py3-none-any.whl/whereami/__main__.py
@@ -0,0 +1,46 @@
+from whereami.predict import predict
+from whereami.predict import predict_proba
+from whereami.predict import crossval
+from whereami.predict import locations
+from whereami.learn import learn
+
+from whereami import print_version
+
+
+def get_args_parser():
+    import argparse
+    from argparse import RawTextHelpFormatter
+    desc = 'Uses WiFi signals and machine learning to predict where you are.'
+    desc += '\nFeel free to try out commands, if anything is missing it will print help.'
+    desc += '\n\nYou will want to start with `whereami learn`'
+    p = argparse.ArgumentParser(description=desc, formatter_class=RawTextHelpFormatter)
+    p.add_argument('--version', '-v', action='version', version=print_version())
+    subparsers = p.add_subparsers(dest="command")
+    subparsers.add_parser('predict')
+    subparsers.add_parser('predict_proba')
+    subparsers.add_parser('crossval')
+    subparsers.add_parser('locations')
+    learn_parser = subparsers.add_parser('learn')
+    learn_parser.add_argument('--location', '-l', required=True,
+                              help='A name-tag for location to learn.')
+    learn_parser.add_argument('--num_samples', '-n', type=int, default=100,
+                              help='Number of samples to take')
+    return p
+
+
+def main():
+    parser = get_args_parser()
+    args = parser.parse_args()
+    if args.command == "predict_proba":
+        predict_proba()
+    elif args.command == "predict":
+        print(predict())
+    elif args.command == "learn":
+        learn(args.location, args.num_samples)
+    elif args.command == "crossval":
+        crossval()
+    elif args.command == "locations":
+        locations()
+    else:
+        parser.print_help()
+        parser.exit(1)
---
+++ packages/whereami/whereami-0.3.57-py2.py3-none-any.whl/whereami/predict.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+from whereami.get_data import get_train_data
+from whereami.get_data import sample
+from whereami.pipeline import get_model
+from whereami.compat import cross_val_score
+
+
+def predict_proba():
+    lp = get_model()
+    print({x: y for x, y in zip(lp.classes_, lp.predict_proba(sample())[0])})
+
+
+def predict():
+    lp = get_model()
+    return lp.predict(sample())[0]
+
+
+def crossval(clf=None, X=None, y=None, folds=10, n=5):
+    if X is None or y is None:
+        X, y = get_train_data()
+    clf = clf or get_model()
+    tot = 0
+    print("KFold folds={}, running {} times".format(folds, n))
+    for i in range(n):
+        res = cross_val_score(clf, X, y, cv=folds).mean()
+        tot += res
+        print("{}/{}: {}".format(i + 1, n, res))
+    print("-------- total --------")
+    print(tot / n)
+    return tot / n
+
+
+def locations():
+    X, y = get_train_data()
+    if len(y) == 0:  # pragma: no cover
+        msg = "No location samples available. First learn a location, e.g. with `whereami learn -l kitchen`."
+        print(msg)
+    else:
+        occurrences = Counter(y)
+        for key, value in occurrences.items():
+            print("{}: {}".format(key, value))
