8b80fbfd1b68e75bb9b27d616ee8f7c4bb890afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package r_nester/1.0.0/r_nester-1.0.0.zip

---
+++ packages/r_nester/r_nester-1.0.0.zip/r_nester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='r_nester',
+version='1.0.0',
+py_modules=['r_nester'],
+author_email='504321123@qq.com',
+url='http://www.headfirstlabs.com',
+description='A simple printer of nested lists',
+)
