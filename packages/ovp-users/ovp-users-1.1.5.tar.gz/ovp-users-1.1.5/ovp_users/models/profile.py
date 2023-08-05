2feda56de4e049ab66256c5827c2fdf7c872a590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package ovp-users/1.1.5/ovp-users-1.1.5.tar.gz

---
+++ packages/ovp-users/ovp-users-1.1.5.tar.gz/ovp-users-1.1.5/ovp_users/models/profile.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from ovp_users.helpers import get_settings, import_from_string
+
+class UserProfile(models.Model):
+  user = models.OneToOneField('User', blank=True, null=True, related_name='%(app_label)s_%(class)s_profile')
+  full_name = models.CharField(_('Full name'), max_length=300, null=True, blank=True)
+  skills = models.ManyToManyField('ovp_core.Skill')
+  causes = models.ManyToManyField('ovp_core.Cause')
+  about = models.TextField(_('About me'), null=True, blank=True)
+
+def get_profile_model():
+  s = get_settings()
+  class_path = s.get('PROFILE_MODEL', None)
+  if class_path:
+    return import_from_string(class_path)
+  return UserProfile
