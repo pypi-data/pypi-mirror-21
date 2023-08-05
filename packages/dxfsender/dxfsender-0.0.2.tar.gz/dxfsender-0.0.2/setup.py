0d3c0256eafebb8998e1324c211eb1a5f2e6e076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:21 +0000

    Add package dxfsender/0.0.2/dxfsender-0.0.2.tar.gz

---
+++ packages/dxfsender/dxfsender-0.0.2.tar.gz/dxfsender-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name = 'dxfsender',
+    version = '0.0.2',
+    author = 'Mio Kato',
+    author_email = 'miokato@me.com',
+    license = 'MIT',
+    packages = ['dxfsender']
+)
+
