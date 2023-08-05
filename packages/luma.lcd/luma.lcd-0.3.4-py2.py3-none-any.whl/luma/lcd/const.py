a24b9f28bdb393a63bfb2254df6fd5167bdb0c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package luma.lcd/0.3.4/luma.lcd-0.3.4-py2.py3-none-any.whl

---
+++ packages/luma.lcd/luma.lcd-0.3.4-py2.py3-none-any.whl/luma/lcd/const.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-17 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+
+class pcd8544(object):
+    DISPLAYON = 0x0C
+    DISPLAYOFF = 0x08
+
+
+class st7735(object):
+    DISPLAYON = 0x29
+    DISPLAYOFF = 0x28
