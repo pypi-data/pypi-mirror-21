3140864c2cbf827bb2df7817390e67b3655912f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package lobar/0.1.1rc0/lobar-0.1.1rc0.tar.gz

---
+++ packages/lobar/lobar-0.1.1rc0.tar.gz/lobar-0.1.1rc0/docs/installation.rst
@@ -0,0 +1,51 @@
+.. highlight:: shell
+
+============
+Installation
+============
+
+
+Stable release
+--------------
+
+To install lobar, run this command in your terminal:
+
+.. code-block:: console
+
+    $ pip install lobar
+
+This is the preferred method to install lobar, as it will always install the most recent stable release. 
+
+If you don't have `pip`_ installed, this `Python installation guide`_ can guide
+you through the process.
+
+.. _pip: https://pip.pypa.io
+.. _Python installation guide: http://docs.python-guide.org/en/latest/starting/installation/
+
+
+From sources
+------------
+
+The sources for lobar can be downloaded from the `Github repo`_.
+
+You can either clone the public repository:
+
+.. code-block:: console
+
+    $ git clone git://github.com/mhetrerajat/lobar
+
+Or download the `tarball`_:
+
+.. code-block:: console
+
+    $ curl  -OL https://github.com/mhetrerajat/lobar/tarball/master
+
+Once you have a copy of the source, you can install it with:
+
+.. code-block:: console
+
+    $ python setup.py install
+
+
+.. _Github repo: https://github.com/mhetrerajat/lobar
+.. _tarball: https://github.com/mhetrerajat/lobar/tarball/master
---
+++ packages/lobar/lobar-0.1.1rc0.tar.gz/lobar-0.1.1rc0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='lobar',
+    version='0.1.1rc',
+    description="A python utility library",
+    long_description=readme + '\n\n' + history,
+    author="Rajat Mhetre",
+    author_email='mhetrerajat@gmail.com',
+    url='https://github.com/mhetrerajat/lobar',
+    packages=[
+        'lobar',
+    ],
+    package_dir={'lobar':
+                 'lobar'},
+    entry_points={
+        'console_scripts': [
+            'lobar=lobar.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='lobar',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
