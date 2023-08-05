8f191fe637e1b43317c526bf54cf2fc6ab7d410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:00 +0000

    Add package svg.charts/4.0/svg.charts-4.0.tar.gz

---
+++ packages/svg.charts/svg.charts-4.0.tar.gz/svg.charts-4.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+name = 'svg.charts'
+description = 'Python SVG Charting Library'
+
+params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	python_requires='>=2.7',
+	install_requires=[
+		'cssutils>=0.9.8a3',
+		'python-dateutil>=2.0',
+		'lxml>=2.0',
+		'more_itertools',
+		'six',
+		'tempora>=1.3',
+		'jaraco.itertools',
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.15.0',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**params)
