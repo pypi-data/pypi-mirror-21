42a2247d6211cc2b6f2352eaf274f086203984c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package pythonrestclient/1.0.3/pythonrestclient-1.0.3.tar.gz

---
+++ packages/pythonrestclient/pythonrestclient-1.0.3.tar.gz/pythonrestclient-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    author='Hawk73',
+    author_email='hawk.bms@gmail.com',
+    description='Python rest client',
+    license='MIT',
+    name='pythonrestclient',
+    packages=[
+        'pythonrestclient',
+        'pythonrestclient.api',
+        'pythonrestclient.models',
+    ],
+    url='https://github.com/Hawk73/python-rest-client',
+    version='1.0.3',
+    zip_safe=False
+)
