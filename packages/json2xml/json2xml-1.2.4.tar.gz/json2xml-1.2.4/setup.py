06030d7d92c4a699882b9c9a75985da1efca6deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package json2xml/1.2.4/json2xml-1.2.4.tar.gz

---
+++ packages/json2xml/json2xml-1.2.4.tar.gz/json2xml-1.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2.4'
+
+setup(
+    name='json2xml',
+    version=version,
+    description='To convert json data to xml data',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='https://github.com/vinitkumar/json2xml',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['BeautifulSoup4==4.4.1',
+                      'dict2xml==1.3',
+                      'simplejson==3.6.5',
+                      'six',
+                      'lxml',
+                      'requests',
+                      ],
+)
