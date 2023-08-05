ef4ac8d9b5e5f75729db0e3c108fdbd6067049e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package capy/1.0.3/capy-1.0.3.tar.gz

---
+++ packages/capy/capy-1.0.3.tar.gz/capy-1.0.3/capy/conf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from os import path
+import sys
+import yaml
+from util import merge, get
+from device import DeviceManager
+from device_os import OS
+from test import TestManager
+from bds import BuildManager
+
+
+################################
+# Setup
+################################
+class Config:
+    def __init__(self, file_name, private_file_name):
+        self.data = self.load_config(file_name, private_file_name)
+
+        self.build_manager = BuildManager(get(self.data, 'bds', None), OS.all)
+        self.device_manager = DeviceManager(get(self.data, 'devices', None), OS.all)
+        self.test_manager = TestManager(get(self.data, 'tests', None))
+
+    def load_yaml(self, file_name, check):
+        if not path.exists(file_name):
+            if check:
+                print "Current directory does not contain configuration file '%s'. Please create one and run again." % file_name
+                sys.exit(1)
+            else:
+                return None
+
+        with open(file_name, 'r') as stream:
+            try:
+                return yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    def load_config(self, file_name, private_file_name):
+        data = self.load_yaml(file_name, check=True)
+        private_data = self.load_yaml(private_file_name, check=False)
+
+        if private_data:
+            private_data = merge(private_data, data)
+            return private_data
+        else:
+            return data
