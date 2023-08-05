fae7c82122932718c6e581cda2db402a6656dc63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:58 +0000

    Add package collective.timedevents/1.0.4/collective.timedevents-1.0.4.zip

---
+++ packages/collective.timedevents/collective.timedevents-1.0.4.zip/collective.timedevents-1.0.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.4'
+
+setup(name='collective.timedevents',
+      version=version,
+      description="Plone/Zope time based event mechanism",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone zope event clock time subscriber date day',
+      author='Mikko Ohtamaa',
+      author_email='mikko.ohtamaa@twinapex.com',
+      url='http://plone.org/products/collective-timedevents',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'Products.PloneTestCase',
+            'zope.app.testing',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          'zope.session',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
