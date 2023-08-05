38f13e4b030e0320eb150d5ba66d9af9f988b4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package nephele/0.0.26/nephele-0.0.26-py2-none-any.whl

---
+++ packages/nephele/nephele-0.0.26-py2-none-any.whl/stdplus/__init__.py
@@ -0,0 +1,29 @@
+from _readfile import readfile
+from _sshConfig import *
+from _writefile import writefile
+from _fexecvp import fexecvp
+from _fnmatches import fnmatches
+from _run_cmd import *
+from _defaultify import *
+from _isInt import *
+
+__all__ = [
+    'defaultify',
+    'defaultify',
+    'defaultifyDict',
+    'defaultifyDict',
+    'fexecvp',
+    'fnmatches',
+    'getSshHost',
+    'isInt',
+    'keyscanHost',
+    'readSshConfig',
+    'readfile',
+    'removeKnownHosts',
+    'resetKnownHost',
+    'resetKnownHosts',
+    'run',
+    'run_cmd',
+    'writefile'
+]
+
