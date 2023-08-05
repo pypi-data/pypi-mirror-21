a31f140b913ade490b5aa5f58dbdf01555a28f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:33 +0000

    Add package futoin-cid/0.6.3/futoin-cid-0.6.3.tar.gz

---
+++ packages/futoin-cid/futoin-cid-0.6.3.tar.gz/futoin-cid-0.6.3/futoin/cid/tool/archivatool.py
@@ -0,0 +1,10 @@
+
+from ..rmstool import RmsTool
+
+class archivaTool( RmsTool ):
+    """Apache Archiva: The Build Artifact Repository Manager.
+Home: https://archiva.apache.org/
+
+NOT IMPLEMENTED YET!
+"""
+    
---
+++ packages/futoin-cid/futoin-cid-0.6.3.tar.gz/futoin-cid-0.6.3/futoin/cid/tool/nexustool.py
@@ -0,0 +1,9 @@
+
+from ..rmstool import RmsTool
+
+class nexusTool( RmsTool ):
+    """Sonatype Nexus RMS.
+Home: http://www.sonatype.org/nexus/
+
+NOT IMPLEMENTED YET.
+"""
