460964401896c2b86afb10f14b7dfda6b1b780d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package prettyoutput/1.7.42a1/prettyoutput-1.7.42a1.tar.gz

---
+++ packages/prettyoutput/prettyoutput-1.7.42a1.tar.gz/prettyoutput-1.7.42a1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='prettyoutput',
+    version='1.7.42a1',
+    description='A functional output module to pretty up your console!',
+    url='https://github.com/Aareon/prettyoutput',
+    download_url='https://github.com/Aareon/prettyoutput/archive/0.1.tar.gz',
+    author='Aareon Sullivan',
+    author_email='askully13@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    classifiers = [],
+)
