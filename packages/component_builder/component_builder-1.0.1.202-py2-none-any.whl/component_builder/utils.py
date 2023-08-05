25fac27453f18e1e14f183c5b7d0803211902058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package component_builder/1.0.1.202/component_builder-1.0.1.202-py2-none-any.whl

---
+++ packages/component_builder/component_builder-1.0.1.202-py2-none-any.whl/component_builder/utils.py
@@ -0,0 +1,36 @@
+from bash import bash as bash_graceful
+
+
+class bash(bash_graceful):
+    def bash(self, cmd, *args, **kwargs):
+        ret = super(bash, self).bash(cmd, *args, **kwargs)
+        if self.code != 0:
+
+            raise Exception('Error running command {0}:\n{1}'.format(
+                cmd, self.stderr)
+            )
+        return ret
+
+
+def convert_dict_to_env_string(envdict):
+    env_list = ["{0}={1}".format(k, v) for k, v in envdict.items()]
+    env_string = ' '.join(env_list)
+    return env_string
+
+
+def make(path, cmd, envs="", options="", output=None):
+    cmd = 'make {0} {1}'.format(options, cmd)
+    return component_script(
+        path=path, script=cmd, envs=envs, output=output
+    )
+
+
+def component_script(path, script, envs="", output=None):
+    full_cmd = 'cd {path} && {envs} {0}'.format(
+        script, path=path, envs=envs)
+    bashkw = {}
+
+    if output:
+        bashkw = output
+
+    return bash(full_cmd, **bashkw)
