5a720ba6db9fdd129b298eb59444d4ec4a7aa7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package fenixedu_django/1.0.2/fenixedu_django-1.0.2.tar.gz

---
+++ packages/fenixedu_django/fenixedu_django-1.0.2.tar.gz/fenixedu_django-1.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name='fenixedu_django',
+		version='1.0.2',
+		description='FenixEdu Django utilities',
+		author='Samuel Coelho',
+		author_email='samuelfcmc@gmail.com',
+		url='https://github.com/samfcmc/fenixedu_django',
+		setup_requires=['fenixedu'],
+		install_requires=['fenixedu'],
+		packages=['fenixedu.authentication']
+		)
