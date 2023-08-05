553fb63d9da1cad327da0adff6ed777715a4c444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package scrapy-nimbus/0.5.3/scrapy_nimbus-0.5.3-py2.py3-none-any.whl

---
+++ packages/scrapy-nimbus/scrapy_nimbus-0.5.3-py2.py3-none-any.whl/scrapy_nimbus/middlewares/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+"""
+Package Description
+"""
+import os
+import sys
+import logging
+import json
+from functools import wraps
+
+from .base import NimbusDownloadMiddleware
+from .download import DuplicationUrlDownloadMiddleware
+from .phantomjs import PhantomJSDownloadMiddleware
