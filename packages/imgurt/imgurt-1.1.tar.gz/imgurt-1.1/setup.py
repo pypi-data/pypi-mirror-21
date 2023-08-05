23eec143a6ae517fb6b51938fd734e27a66dd124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package imgurt/1.1/imgurt-1.1.tar.gz

---
+++ packages/imgurt/imgurt-1.1.tar.gz/imgurt-1.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='imgurt',
+    version='1.1',
+    packages=['imgurt'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="uses math to select wallpapers from Reddit",
+    long_description=open('README.rst').read(),
+    license="MIT",
+    keywords="imgur reddit wallpaper changer",
+    url="https://github.com/evidlo/imgurt",
+    install_requires=[
+        "PyYAML",
+        "requests",
+        "json",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
