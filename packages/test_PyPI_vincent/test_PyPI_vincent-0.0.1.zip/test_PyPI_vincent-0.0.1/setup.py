279bc1aa9ba327dab0153e270aabdbc78b499b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:24 +0000

    Add package test_PyPI_vincent/0.0.1/test_PyPI_vincent-0.0.1.zip

---
+++ packages/test_PyPI_vincent/test_PyPI_vincent-0.0.1.zip/test_PyPI_vincent-0.0.1/setup.py
@@ -0,0 +1,22 @@
+# encoding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='test_PyPI_vincent',
+    version='0.0.1',
+    author='vincent',
+    author_email='szxwpp@163.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸',
+    packages=['test_PyPI_vincent'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'test=test_PyPI_vincent:test',
+            'run=test_PyPI_vincent:run'
+        ]
+    }
+)
+
+# console_scripts提供终端命令，test执行 test_PyPI_vincent 包下（__init__ 中）的 test 函数
---
+++ packages/test_PyPI_vincent/test_PyPI_vincent-0.0.1.zip/test_PyPI_vincent-0.0.1/test_PyPI_vincent/__init__.py
@@ -0,0 +1,9 @@
+# encoding=utf-8
+
+
+def test():
+    print(u'吃枣')
+ 
+
+def run():
+    print(u'药丸)
