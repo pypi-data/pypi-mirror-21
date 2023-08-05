9b248f7bc4b7310f9a23f92eb821b0b9df527dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package poline/0.4.1/poline-0.4.1.tar.gz

---
+++ packages/poline/poline-0.4.1.tar.gz/poline-0.4.1/setup.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+from setuptools import setup, Command
+
+setup(
+    name='poline',
+    version='0.4.1',
+    description='Python one-liners: Awk-like one-liners for python',
+    long_description='pol lets you do awk-like one liners in python.',
+    url='https://github.com/riolet/pol',
+    author='Rohana Rezel',
+    author_email='poline@riolet.com',
+    maintainer='Rohana Rezel',
+    maintainer_email='poline@riolet.com',
+    packages = ['poline', '_compatibility'],
+    entry_points={
+        'console_scripts': ['pol = poline.poline:main'],
+    },
+    classifiers=[
+        'Topic :: System :: Operating System',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
