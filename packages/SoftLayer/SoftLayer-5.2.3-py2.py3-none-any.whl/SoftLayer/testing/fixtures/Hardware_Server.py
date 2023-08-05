73cace1744ee7847675b8cb776e31ce07dd34fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package SoftLayer/5.2.3/SoftLayer-5.2.3-py2.py3-none-any.whl

---
+++ packages/SoftLayer/SoftLayer-5.2.3-py2.py3-none-any.whl/SoftLayer/testing/fixtures/Hardware_Server.py
@@ -0,0 +1,107 @@
+getObject = {
+    'id': 1000,
+    'datacenter': {'id': 50, 'name': 'TEST00',
+                   'description': 'Test Data Center'},
+    'billingItem': {
+        'id': 6327,
+        'recurringFee': 1.54,
+        'orderItem': {
+            'order': {
+                'userRecord': {
+                    'username': 'chechu',
+                }
+            }
+        }
+    },
+    'primaryIpAddress': '172.16.1.100',
+    'hostname': 'hardware-test1',
+    'domain': 'test.sftlyr.ws',
+    'bareMetalInstanceFlag': True,
+    'fullyQualifiedDomainName': 'hardware-test1.test.sftlyr.ws',
+    'processorPhysicalCoreAmount': 2,
+    'memoryCapacity': 2,
+    'primaryBackendIpAddress': '10.1.0.2',
+    'networkManagementIpAddress': '10.1.0.3',
+    'hardwareStatus': {'status': 'ACTIVE'},
+    'primaryNetworkComponent': {'maxSpeed': 10, 'speed': 10},
+    'provisionDate': '2013-08-01 15:23:45',
+    'notes': 'These are test notes.',
+    'operatingSystem': {
+        'softwareLicense': {
+            'softwareDescription': {
+                'referenceCode': 'Ubuntu',
+                'name': 'Ubuntu 12.04 LTS',
+            }
+        },
+        'passwords': [
+            {'username': 'root', 'password': 'abc123'}
+        ],
+    },
+    'networkVlans': [
+        {
+            'networkSpace': 'PRIVATE',
+            'vlanNumber': 1800,
+            'id': 9653
+        },
+        {
+            'networkSpace': 'PUBLIC',
+            'vlanNumber': 3672,
+            'id': 19082
+        },
+    ],
+    'tagReferences': [
+        {'tag': {'name': 'test_tag'}}
+    ],
+    'activeTransaction': {
+        'transactionStatus': {
+            'name': 'TXN_NAME',
+            'friendlyName': 'Friendly Transaction Name',
+            'id': 6660
+        }
+    }
+}
+editObject = True
+setPrivateNetworkInterfaceSpeed = True
+setPublicNetworkInterfaceSpeed = True
+powerOff = True
+powerOn = True
+powerCycle = True
+rebootSoft = True
+rebootDefault = True
+rebootHard = True
+setUserMetadata = ['meta']
+reloadOperatingSystem = 'OK'
+getReverseDomainRecords = [
+    {'resourceRecords': [{'data': '2.0.1.10.in-addr.arpa'}]}]
+bootToRescueLayer = True
+getFrontendNetworkComponents = [
+    {'maxSpeed': 100},
+    {
+        'maxSpeed': 1000,
+        'networkComponentGroup': {
+            'groupTypeId': 2,
+            'networkComponents': [{'maxSpeed': 1000}, {'maxSpeed': 1000}]
+        }
+    },
+    {
+        'maxSpeed': 1000,
+        'networkComponentGroup': {
+            'groupTypeId': 2,
+            'networkComponents': [{'maxSpeed': 1000}, {'maxSpeed': 1000}]
+        }
+    },
+    {
+        'maxSpeed': 1000,
+        'networkComponentGroup': {
+            'groupTypeId': 2,
+            'networkComponents': [{'maxSpeed': 1000}, {'maxSpeed': 1000}]
+        }
+    },
+    {
+        'maxSpeed': 1000,
+        'networkComponentGroup': {
+            'groupTypeId': 2,
+            'networkComponents': [{'maxSpeed': 1000}, {'maxSpeed': 1000}]
+        }
+    }
+]
---
+++ packages/SoftLayer/SoftLayer-5.2.3-py2.py3-none-any.whl/SoftLayer/tests/CLI/modules/call_api_tests.py
@@ -0,0 +1,129 @@
+"""
+    SoftLayer.tests.CLI.modules.call_api_tests
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :license: MIT, see LICENSE for more details.
+"""
+from SoftLayer import testing
+
+import json
+
+
+class CallCliTests(testing.TestCase):
+
+    def test_options(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = 'test'
+
+        result = self.run_command(['call-api', 'Service', 'method',
+                                   '--mask=some.mask',
+                                   '--limit=20',
+                                   '--offset=40',
+                                   '--id=100'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(json.loads(result.output), 'test')
+        self.assert_called_with('SoftLayer_Service', 'method',
+                                mask='mask[some.mask]',
+                                limit=20,
+                                offset=40,
+                                identifier='100')
+
+    def test_object(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = {'string': 'string',
+                             'int': 10,
+                             'float': 1.0,
+                             'None': None,
+                             'Bool': True}
+
+        result = self.run_command(['call-api', 'Service', 'method'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(json.loads(result.output),
+                         {'string': 'string',
+                          'int': 10,
+                          'float': 1.0,
+                          'None': None,
+                          'Bool': True})
+
+    def test_object_table(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = {'string': 'string',
+                             'int': 10,
+                             'float': 1.0,
+                             'None': None,
+                             'Bool': True}
+
+        result = self.run_command(['call-api', 'Service', 'method'],
+                                  fmt='table')
+
+        self.assertEqual(result.exit_code, 0)
+        # NOTE(kmcdonald): Order is not guaranteed
+        self.assertIn(":........:........:", result.output)
+        self.assertIn(":   Name : Value  :", result.output)
+        self.assertIn(":    int : 10     :", result.output)
+        self.assertIn(":   None : None   :", result.output)
+        self.assertIn(":  float : 1.0    :", result.output)
+        self.assertIn(":   Bool : True   :", result.output)
+        self.assertIn(": string : string :", result.output)
+        self.assertIn(":........:........:", result.output)
+
+    def test_object_nested(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = {'this': {'is': [{'pretty': 'nested'}]}}
+
+        result = self.run_command(['call-api', 'Service', 'method'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(json.loads(result.output),
+                         {'this': {'is': [{'pretty': 'nested'}]}})
+
+    def test_list(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = [{'string': 'string',
+                              'int': 10,
+                              'float': 1.0,
+                              'None': None,
+                              'Bool': True}]
+
+        result = self.run_command(['call-api', 'Service', 'method'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(json.loads(result.output),
+                         [{'string': 'string',
+                           'int': 10,
+                           'float': 1.0,
+                           'None': None,
+                           'Bool': True}])
+
+    def test_list_table(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = [{'string': 'string',
+                              'int': 10,
+                              'float': 1.0,
+                              'None': None,
+                              'Bool': True}]
+
+        result = self.run_command(['call-api', 'Service', 'method'],
+                                  fmt='table')
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output,
+                         """:......:......:.......:.....:........:
+: Bool : None : float : int : string :
+:......:......:.......:.....:........:
+: True : None :  1.0  :  10 : string :
+:......:......:.......:.....:........:
+""")
+
+    def test_parameters(self):
+        mock = self.set_mock('SoftLayer_Service', 'method')
+        mock.return_value = {}
+
+        result = self.run_command(['call-api', 'Service', 'method',
+                                   'arg1', '1234'])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assert_called_with('SoftLayer_Service', 'method',
+                                args=('arg1', '1234'))
