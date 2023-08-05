4b0fbb89a7b6b40227bce489225b0cc45ba1ae25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:20 +0000

    Add package aumbry/0.3.0/aumbry-0.3.0.tar.gz

---
+++ packages/aumbry/aumbry-0.3.0.tar.gz/aumbry-0.3.0/aumbry/__init__.py
@@ -0,0 +1,24 @@
+"""
+Attributes:
+    FILE (str): Alias of SourceTypes.file
+    CONSUL (str): Alias of SourceTypes.consul
+"""
+from .loader import load, save # NOQA
+from .formats.js import JsonConfig # NOQA
+from .formats.yml import YamlConfig # NOQA
+from .sources import SourceTypes
+
+from alchemize.mapping import Attr # NOQA
+
+FILE = SourceTypes.file
+CONSUL = SourceTypes.consul
+
+
+__all__ = [
+    'Attr',
+    'load',
+    'save',
+    'JsonConfig',
+    'YamlConfig',
+    'SourceTypes',
+]
