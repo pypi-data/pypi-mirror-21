9623799377aa01ea32dc32366742d3f74c772cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package npcompare/0.4.0/npcompare-0.4.0.tar.gz

---
+++ packages/npcompare/npcompare-0.4.0.tar.gz/npcompare-0.4.0/npcompare/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#import get_plots
+from npcompare.compare import Compare
+from npcompare.fourierseries import fourierseries
+from npcompare.estimate import Estimate
+
+__version__ = "0.4.0"
