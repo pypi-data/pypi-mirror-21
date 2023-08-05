c4da3fd230054d49b25c439fd0f55c8a4f8caf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package pydfs-lineup-optimizer/1.1.1/pydfs-lineup-optimizer-1.1.1.tar.gz

---
+++ packages/pydfs-lineup-optimizer/pydfs-lineup-optimizer-1.1.1.tar.gz/pydfs-lineup-optimizer-1.1.1/pydfs_lineup_optimizer/__init__.py
@@ -0,0 +1,47 @@
+__version__ = "1.1.1"
+
+from .player import Player
+from .exceptions import LineupOptimizerException, LineupOptimizerIncorrectTeamName, \
+    LineupOptimizerIncorrectPositionName
+from .lineup_optimizer import LineupOptimizer
+from .lineup import Lineup
+from .settings import YahooBasketballSettings, YahooFootballSettings, YahooHockeySettings, YahooBaseballSettings, \
+    FanDuelBasketballSettings, FanDuelFootballSettings, FanDuelHockeySettings, FanDuelBaseballSettings, \
+    DraftKingsBasketballSettings, DraftKingsFootballSettings, DraftKingsHockeySettings, DraftKingBaseballSettings, \
+    FantasyDraftBasketballSettings, FantasyDraftFootballSettings, FantasyDraftHockeySettings
+from .constants import Site, Sport
+
+
+settings_mapping = {
+    Site.DRAFTKINGS: {
+        Sport.BASKETBALL: DraftKingsBasketballSettings,
+        Sport.FOOTBALL: DraftKingsFootballSettings,
+        Sport.HOCKEY: DraftKingsHockeySettings,
+        Sport.BASEBALL: DraftKingBaseballSettings,
+    },
+    Site.FANDUEL: {
+        Sport.BASKETBALL: FanDuelBasketballSettings,
+        Sport.FOOTBALL: FanDuelFootballSettings,
+        Sport.HOCKEY: FanDuelHockeySettings,
+        Sport.BASEBALL: FanDuelBasketballSettings,
+    },
+    Site.YAHOO: {
+        Sport.BASKETBALL: YahooBasketballSettings,
+        Sport.FOOTBALL: YahooFootballSettings,
+        Sport.HOCKEY: YahooHockeySettings,
+        Sport.BASEBALL: YahooBaseballSettings,
+    },
+    Site.FANTASY_DRAFT: {
+        Sport.BASKETBALL: FantasyDraftBasketballSettings,
+        Sport.FOOTBALL: FantasyDraftFootballSettings,
+        Sport.HOCKEY: FantasyDraftHockeySettings,
+    },
+}
+
+
+def get_optimizer(site, sport):
+    try:
+        return LineupOptimizer(settings_mapping[site][sport])
+    except KeyError:
+        raise NotImplementedError
+
