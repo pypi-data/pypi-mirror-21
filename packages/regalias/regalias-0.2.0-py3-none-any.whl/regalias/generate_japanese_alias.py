66e79ba8098a57409545a1595d8e4d8ab708d35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:18 +0000

    Add package regalias/0.2.0/regalias-0.2.0-py3-none-any.whl

---
+++ packages/regalias/regalias-0.2.0-py3-none-any.whl/regalias/generate_japanese_alias.py
@@ -0,0 +1,30 @@
+# Copyright 2017 Letla Fox
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from random import Random
+
+from .generate_japanese_alias_from_rng import generate_japanese_alias_from_rng
+from .generate_japanese_alias_from_str import generate_japanese_alias_from_str
+
+
+def generate_japanese_alias(str_or_rng=None):
+  if str_or_rng is None:
+    rng = Random()
+    return generate_japanese_alias_from_rng(rng)
+  elif isinstance(str_or_rng, str):
+    str_ = str_or_rng
+    return generate_japanese_alias_from_str(str_)
+  else:  # isinstance(str_or_rng, Random)
+    rng = str_or_rng
+    return generate_japanese_alias_from_rng(rng)
