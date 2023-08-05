1c14b94677b8d7961b6ab7cbb065a3a72d624fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:18 +0000

    Add package sphinxcontrib-translation-assistant/0.1.0/sphinxcontrib_translation_assistant-0.1.0-py2.py3-none-any.whl

---
+++ packages/sphinxcontrib-translation-assistant/sphinxcontrib_translation_assistant-0.1.0-py2.py3-none-any.whl/sphinxcontrib/translation_assistant.py
@@ -0,0 +1,103 @@
+from collections import deque
+from itertools import zip_longest
+from docutils import nodes
+from docutils.parsers.rst import directives
+from sphinx.util.compat import Directive as BaseDirective
+
+
+SEPARATOR = '---SEPERATOR---'
+
+
+def parse(text):
+    if text[0].encode() != b'\xef\xbb\xbf':
+        raise ValueError('Not a valid Translation Assistant format.')
+
+    return merge(*split(text=text[1:]))
+
+
+def split(text):
+    texts = text.split(SEPARATOR)
+    raws = [line.strip() for line in texts[0].strip().splitlines()]
+    translations = [line.rstrip() for line in texts[1].strip().splitlines()]
+
+    return raws, translations
+
+
+def merge(raws, translations):
+    data = deque()
+
+    for raw, translation in zip_longest(raws, translations, fillvalue=''):
+        prefix = raw[0]
+        if prefix == '$':
+            previous = len(data) - 1
+            data[previous][0] = ''.join([data[previous][0], raw[1:]])
+            data[previous][1] = ' '.join([data[previous][1], translation])
+        else:
+            data.append([raw[1:], translation])
+
+    return data
+
+
+class Directive(BaseDirective):
+
+    required_arguments = 1  # Path to yml file
+    final_argument_whitespace = True
+    has_content = False
+
+    option_spec = {
+        'no-title': directives.flag,
+        'no-raw': directives.flag,
+    }
+
+    def run(self):
+        env = self.state.document.settings.env
+
+        rel_path, path = env.relfn2path(directives.path(self.arguments[0]))
+        env.note_dependency(rel_path)
+
+        no_title = 'no-title' in self.options
+        no_raw = 'no-raw' in self.options
+
+        with open(path, encoding='utf-8') as f:
+            text = f.read()
+
+        lines = parse(text=text)
+
+        section = None
+
+        if not no_title:
+            line = lines.popleft()
+            if no_raw:
+               title = line[1]
+            else:
+                title = '{0} ー {1}'.format(line[0], line[1])
+            section = nodes.section(ids=title)
+            section.append(nodes.title(text=title))
+            lines.popleft()
+
+        contents = []
+
+        for line in lines:
+            if not line[0].strip():
+                contents.append(nodes.paragraph('　'))
+                continue
+
+            if not no_raw:
+                paragraph = nodes.paragraph(classes=['raw'], text=line[0])
+                contents.append(paragraph)
+
+            if line[1].strip():
+                paragraph = nodes.paragraph(
+                    classes=['translated'],
+                    text=line[1],
+                )
+                contents.append(paragraph)
+
+        if not no_title:
+            section.extend(contents)
+
+        return [section] if section else contents
+
+
+def setup(app):
+    app.add_directive('translation-assistant', Directive)
---
+++ packages/sphinxcontrib-translation-assistant/sphinxcontrib_translation_assistant-0.1.0-py2.py3-none-any.whl/sphinxcontrib_translation_assistant-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,39 @@
+####################################
+sphinxcontrib.transalation_assistant
+####################################
+
+Sphinx extension to view
+`Translation Assistant <http://joeglens.com/translation-assistant-tool>`__
+file.
+
+Example:
+
+::
+
+  .. translation-assistant:: /src/chapter-01.txt
+    :no-title:
+    :no-raw:
+
+Available options:
+
+no-title
+  Flag indicating that the first line of the file is not a title.
+
+  Please note that this option will skip 2 first line of the file.
+
+  Line starting with ``$`` sign in not counted.
+
+no-raw
+  flag indicating that the raw text should not be rendered.
+
+
+Changes
+=======
+
+
+0.1.0
+-----
+
+* First public release.
+
+
