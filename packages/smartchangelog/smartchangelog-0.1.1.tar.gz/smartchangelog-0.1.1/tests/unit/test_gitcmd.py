7f1860b72d1307c28a5a4972fe77f301a61b2f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package smartchangelog/0.1.1/smartchangelog-0.1.1.tar.gz

---
+++ packages/smartchangelog/smartchangelog-0.1.1.tar.gz/smartchangelog-0.1.1/tests/unit/test_gitcmd.py
@@ -0,0 +1,64 @@
+import os
+import pytest
+
+from smartchangelog.gitcmd import GitCmdError, is_inside_work_tree, get_gitdir, tag
+from tests.unit import data_dir_path
+
+
+@pytest.fixture(scope='function')
+def cmd():
+    old_cmd = os.getcwd()
+    yield None
+    os.chdir(old_cmd)
+
+
+@pytest.mark.usefixtures('cmd')
+def test_is_inside_work_tree_ok():
+    # GIVEN
+    os.chdir(data_dir_path())
+    # WHEN
+    result = is_inside_work_tree()
+    # THEN
+    assert result
+
+
+@pytest.mark.usefixtures('cmd')
+def test_is_inside_work_tree_ko():
+    # GIVEN
+    os.chdir(os.path.expanduser('~'))
+    # WHEN
+    result = is_inside_work_tree()
+    # THEN
+    assert not result
+
+
+@pytest.mark.usefixtures('cmd')
+def test_get_gitdir_ok():
+    # GIVEN
+    os.chdir(data_dir_path())
+    # WHEN
+    gitdir_path = get_gitdir()
+    # THEN
+    assert os.path.split(gitdir_path)[-1] == '.git'
+
+
+@pytest.mark.usefixtures('cmd')
+def test_get_gitdir_ko():
+    # GIVEN
+    os.chdir(os.path.expanduser('~'))
+    # WHEN
+    with pytest.raises(GitCmdError):
+        get_gitdir()
+        # THEN
+        pass
+
+
+@pytest.mark.usefixtures('cmd')
+def test_get_tag():
+    # GIVEN
+    os.chdir(data_dir_path())
+    # WHEN
+    tags = tag()
+    # THEN
+    assert len(tags) > 0
+
