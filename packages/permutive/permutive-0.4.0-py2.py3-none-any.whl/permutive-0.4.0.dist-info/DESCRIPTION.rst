090a10d81c62489bc185ed116d4ae7e9fcbf4560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package permutive/0.4.0/permutive-0.4.0-py2.py3-none-any.whl

---
+++ packages/permutive/permutive-0.4.0-py2.py3-none-any.whl/permutive-0.4.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,52 @@
+===============================
+Permutive SDK
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/permutive.svg
+        :target: https://pypi.python.org/pypi/permutive
+
+.. image:: https://img.shields.io/travis/tailsdotcom/permutive-sdk.svg
+        :target: https://travis-ci.org/tailsdotcom/permutive-sdk
+
+.. image:: https://readthedocs.org/projects/permutive-sdk/badge/?version=latest
+        :target: https://permutive-sdk.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/tailsdotcom/permutive-sdk/shield.svg
+     :target: https://pyup.io/repos/github/tailsdotcom/permutive-sdk/
+     :alt: Updates
+
+
+Python wrapper for Permutive API
+
+
+* Free software: MIT license
+* Documentation: https://permutive-sdk.readthedocs.io.
+
+
+Features
+--------
+
+This package aims to provide an easy to use python interface for interacting with Permutive API
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+
+
+=======
+History
+=======
+
+0.1.0 (2017-03-23)
+------------------
+
+* First release on PyPI.
+
+
---
+++ packages/permutive/permutive-0.4.0-py2.py3-none-any.whl/permutive/event.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import re
+
+from .util import none_default_namedtuple
+from .base import Resource
+
+Event = none_default_namedtuple('Event', 'name, user_id, id, properties, time')
+
+
+class EventResource(Resource):
+
+    def track(self, event_name, user_id, **properties):
+        """
+        Records an event in Permutive API
+        
+        https://developer.permutive.com/v1.1/reference#record-an-event
+        :param event_name: String name of the event, e.g. EmailSubscription
+        :param user_id: String - Permutive user_id
+        :param properties: keyword arguments representing event properties to be sent
+        :return: dict 
+        """
+        if re.match('^[\w-]+$', event_name) is None:
+            raise ValueError('event_name should only contain A-Za-z0-9_')
+        result = self.client.request('POST', '/events', data={
+            'user_id': user_id,
+            'name': event_name,
+            'properties': properties
+        })
+        return Event(**result)
+
+    def get(self, user_id, start=None, end=None):
+        """
+         Fetch events for a given user_id
+        :param user_id: 
+        :param start: (optional) datetime
+        :param end: (optional) datetime
+        :return: Generator that return Event objects
+        """
+        data = {'user_id': user_id}
+        if start:
+            data['start'] = start.isoformat()
+        if end:
+            data['end'] = end.isoformat()
+
+        result = self.client.request('GET', '/events', data=data)
+
+        for event in result:
+            yield Event(**event)
