4146f366c71d54fc6dbb65eeef1d70dfd4c7ffdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package panic/6.0.6/panic-6.0.6.tar.gz

---
+++ packages/panic/panic-6.0.6.tar.gz/panic-6.0.6/setup.py
@@ -0,0 +1,42 @@
+"""
+To test this script just run:
+
+mkdir /tmp/builds/
+python setup.py install --root=/tmp/builds
+/tmp/builds/usr/bin/PyAlarm -? -v4
+
+"""
+
+from setuptools import setup, find_packages
+
+
+install_requires = ['taurus',
+                    'fandango',
+                    'PyTango',]
+
+
+package_data = {'': ['VERSION']}
+
+scripts = [
+  './bin/PyAlarm',
+  './bin/panic',
+  ]
+
+entry_points = {
+        'console_scripts': [
+            #'panic-gui=panic.gui.gui:main_gui',
+        ],
+}
+
+
+setup(
+    name="panic",
+    version=open('panic/VERSION').read().strip(),
+    packages=find_packages(),
+    package_data=package_data,
+    install_requires=install_requires,
+    entry_points=entry_points,
+    scripts=scripts,
+)
+
+
