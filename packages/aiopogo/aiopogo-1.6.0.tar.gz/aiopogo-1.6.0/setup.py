2c8457391d82fb048c1254d3c6adbb7fab60afe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:50 +0000

    Add package aiopogo/1.6.0/aiopogo-1.6.0.tar.gz

---
+++ packages/aiopogo/aiopogo-1.6.0.tar.gz/aiopogo-1.6.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='aiopogo',
+      author='David Christenson',
+      author_email='mail@noctem.xyz',
+      description='Asynchronous Pokemon API lib',
+      version='1.6.0',
+      url='https://github.com/Noctem/aiopogo',
+      packages=find_packages(),
+      install_requires=[
+          'protobuf>=3.0.0',
+          'gpsoauth>=0.4.0',
+          'protobuf3-to-dict>=0.1.4',
+          'aiohttp==1.3.*',
+          'pycrypt>=0.2.0',
+          'pogeo>=0.2.0'],
+      extras_require={'ujson': ['ujson']},
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'License :: OSI Approved :: MIT License'
+      ]
+)
