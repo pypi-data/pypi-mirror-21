e62fc326c14215e08a9d59c3238925a8cc463966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package plawt/1.0.10/plawt-1.0.10.tar.gz

---
+++ packages/plawt/plawt-1.0.10.tar.gz/plawt-1.0.10/plawt.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+from __future__ import division
+import matplotlib.pyplot as plt
+
+
+def plot(plotStruct):
+	lines = []
+
+	# setup
+	if 'figsize' in plotStruct:
+		plt.figure(figsize=plotStruct['figsize'])
+
+	for key in plotStruct:
+		val = plotStruct[key]
+		if type(key) == int: # its a line
+			x = val['x']
+			y = val['y'] if 'y' in val else None
+			line = val['line'] if 'line' in val else ''
+			label = val['label'] if 'label' in val else ''
+			markersize = val['markersize'] if 'markersize' in val else 6.0
+			markersize = val['ms'] if 'ms' in val else markersize
+			linewidth = val['linewidth'] if 'linewidth' in val else 1.0
+			linewidth = val['lw'] if 'lw' in val else linewidth
+			alpha = val['alpha'] if 'alpha' in val else 1.0
+
+			if y is None:
+				plt.plot(x, line, label=label, ms=markersize, lw=linewidth, alpha=alpha)
+			else:
+				plt.plot(x, y, line, label=label, ms=markersize, lw=linewidth, alpha=alpha)
+
+			if 'markerfacecolor' in val:
+				plt.gca().get_lines()[-1].set_markerfacecolor(val['markerfacecolor'])
+			elif 'mfc' in val:
+				plt.gca().get_lines()[-1].set_markerfacecolor(val['mfc'])
+
+		elif type(key) == str: # its a property
+			if key == 'xlabel':
+				plt.xlabel(val)
+			if key == 'ylabel':
+				plt.ylabel(val)
+			if key == 'title':
+				plt.title(val)
+			if key == 'set_yscale':
+				plt.gca().set_yscale(val)
+			if key == 'set_xscale':
+				plt.gca().set_xscale(val)
+			if key == 'grid':
+				plt.grid()
+			if key == 'legend':
+				legend = val
+				loc = legend['loc'] if 'loc' in legend else '1'
+				plt.legend(loc=loc)
+			if key == 'xlim':
+				plt.xlim(val)
+			if key == 'ylim':
+				plt.ylim(val)
+
+	# Storing and cleanup of plot
+	if 'filename' in plotStruct:
+		plt.savefig(plotStruct['filename'])
+	if 'show' in plotStruct and plotStruct['show']:
+		plt.show()
+	if 'keepOpen' not in plotStruct or not plotStruct['keepOpen']:
+		plt.close()
+
+	return plt # in case people wanna do extra shit
