561ee901af9df7f55410518d39655ed010498fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package chnester/1.1.0/chnester-1.1.0.zip

---
+++ packages/chnester/chnester-1.1.0.zip/chnester-1.1.0/chnester.py
@@ -0,0 +1,13 @@
+"""
+这是我第一模块
+递归输出列表里的所有数据
+20170406
+"""
+def print_lol(the_list,level):
+    for item in the_list:
+        if isinstance(item,list):
+            print_lol(item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(item)
