5a8391b335d6c1b754b55e2182187ed23df1b03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:06 +0000

    Add package qtplot/0.1.1.dev5/qtplot-0.1.1.dev5.tar.gz

---
+++ packages/qtplot/qtplot-0.1.1.dev5.tar.gz/qtplot-0.1.1.dev5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='qtplot',
+      version='0.1.1.dev5',
+      description='Data plotting and analysis tool',
+      url='https://github.com/Rubenknex/qtplot',
+      author='Ruben van Gulik',
+      author_email='rubenvangulik@gmail.com',
+      license='MIT',
+      packages=['qtplot',
+                'tests',
+                'qtplot/ui',
+                'qtplot/colormaps',
+                'qtplot/colormaps/nanoscope',
+                'qtplot/colormaps/transform',
+                'qtplot/colormaps/wsxm'],
+      install_requires=[
+        'QtPy',
+        'pyopengl',
+        'vispy',
+      ],
+      package_data={
+        '': ['*.npy', '*.ui', '*.json']
+      },
+      entry_points={
+        'console_scripts': ['qtplot-console = qtplot.qtplot:main'],
+        'gui_scripts': ['qtplot = qtplot.qtplot:main']
+      })
