f7b22d5118e46171831bcdca7030cb19580d2013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:16 +0000

    Add package mookfist-lled-controller/0.0.10/mookfist-lled-controller-0.0.10.tar.gz

---
+++ packages/mookfist-lled-controller/mookfist-lled-controller-0.0.10.tar.gz/mookfist-lled-controller-0.0.10/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+__doc__ = """A python library for communicating with LimitlessLED/Milight/Easybulb compatible wifi bridges."""
+
+setup(
+    name="mookfist-lled-controller",
+    description=__doc__,
+    version="0.0.10",
+    author="mookfist",
+    author_email="mookfist@gmail.com",
+    url="https://github.com/mookfist/mookfist-lled-controller",
+    scripts=['lled.py'],
+    packages=['mookfist_lled_controller','mookfist_lled_controller.bridges'],
+    install_requires=[
+        'docopt',
+        'colorama'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords=['milight','limitlessled']
+)
