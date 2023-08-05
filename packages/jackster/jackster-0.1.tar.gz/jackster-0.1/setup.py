3a684e80b0b409cc84edbe19fde485247284a74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package jackster/0.1/jackster-0.1.tar.gz

---
+++ packages/jackster/jackster-0.1.tar.gz/jackster-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'jackster',
+  packages = ['jackster'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A micro web framework.',
+  author = 'Daan van der Kallen',
+  author_email = 'mail@daanvdk.com',
+  url = 'https://github.com/Daanvdk/jackster', # use the URL to the github repo
+  download_url = 'https://github.com/Daanvdk/jackster/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['web', 'framework'], # arbitrary keywords
+  classifiers = [],
+)
