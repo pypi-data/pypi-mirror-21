09f78375d5ed78e997960394ef37f028a10c49b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:34 +0000

    Add package djaodjin-pages/0.1.1/djaodjin-pages-0.1.1.tar.gz

---
+++ packages/djaodjin-pages/djaodjin-pages-0.1.1.tar.gz/djaodjin-pages-0.1.1/pages/models.py
@@ -0,0 +1,218 @@
+# Copyright (c) 2017, Djaodjin Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import unicode_literals
+
+import logging, random
+
+from django.db import IntegrityError, models, transaction
+from django.db.models import F, Q
+from django.template.defaultfilters import slugify
+from django.utils.encoding import python_2_unicode_compatible
+from rest_framework.exceptions import ValidationError
+
+from . import settings
+
+
+LOGGER = logging.getLogger(__name__)
+
+class RelationShipManager(models.Manager):
+
+    def insert_node(self, root, node, pos=0):
+        queryset = self.filter(orig_element=root)
+        queryset.filter(Q(rank__gte=pos)).update(rank=F('rank') + 1)
+        self.create(orig_element=root, dest_element=node, rank=pos)
+
+
+@python_2_unicode_compatible
+class RelationShip(models.Model):
+    """
+    Encodes a relation between two ``PageElement``.
+    """
+    objects = RelationShipManager()
+
+    orig_element = models.ForeignKey(
+        "PageElement", related_name='from_element')
+    dest_element = models.ForeignKey(
+        "PageElement", related_name='to_element')
+    tag = models.SlugField(null=True)
+    rank = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('orig_element', 'dest_element')
+
+    def __str__(self):
+        return "%s to %s" % (
+            self.orig_element.slug, self.dest_element.slug) #pylint: disable=no-member
+
+class PageElementManager(models.Manager):
+
+    def get_roots(self):
+        return self.raw('SELECT * FROM %(page_element_table)s'\
+' LEFT OUTER JOIN %(relationship_table)s ON %(page_element_table)s.id'\
+' = %(relationship_table)s.dest_element_id WHERE'\
+' %(relationship_table)s.dest_element_id IS NULL' % {
+    'page_element_table': 'pages_pageelement',
+    'relationship_table': 'pages_relationship'})
+
+
+@python_2_unicode_compatible
+class PageElement(models.Model):
+    """
+    Elements of an editable HTML page.
+    """
+    objects = PageElementManager()
+
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200, blank=True)
+    text = models.TextField(blank=True)
+    account = models.ForeignKey(
+        settings.ACCOUNT_MODEL, related_name='account_page_element', null=True)
+    relationships = models.ManyToManyField("self",
+        related_name='related_to', through='RelationShip', symmetrical=False)
+    tag = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.slug
+
+    def add_relationship(self, element, tag=None):
+        return RelationShip.objects.get_or_create(
+            orig_element=self, dest_element=element,
+            defaults={'tag': tag})
+
+    def remove_relationship(self, element):
+        RelationShip.objects.filter(
+            orig_element=self,
+            dest_element=element).delete()
+        return True
+
+    def get_relationships(self, tag=None):
+        if not tag:
+            return self.relationships.filter(
+                to_element__orig_element=self)
+        else:
+            return self.relationships.filter(
+                to_element__tag=tag,
+                to_element__orig_element=self)
+
+    def get_related_to(self, tag):
+        return self.related_to.filter(
+            from_element__tag=tag,
+            from_element__dest_element=self)
+
+    def save(self, force_insert=False, force_update=False,
+             using=None, update_fields=None):
+        if self.slug: # serializer will set created slug to '' instead of None.
+            return super(PageElement, self).save(
+                force_insert=force_insert, force_update=force_update,
+                using=using, update_fields=update_fields)
+        max_length = self._meta.get_field('slug').max_length
+        slug_base = slugify(self.title)
+        if len(slug_base) > max_length:
+            slug_base = slug_base[:max_length]
+        self.slug = slug_base
+        for _ in range(1, 10):
+            try:
+                with transaction.atomic():
+                    return super(PageElement, self).save(
+                        force_insert=force_insert, force_update=force_update,
+                        using=using, update_fields=update_fields)
+            except IntegrityError as err:
+                if 'uniq' not in str(err).lower():
+                    raise
+                suffix = '-%s' % "".join([random.choice("abcdef0123456789")
+                    for _ in range(7)])
+                if len(slug_base) + len(suffix) > max_length:
+                    self.slug = slug_base[:(max_length - len(suffix))] + suffix
+                else:
+                    self.slug = slug_base + suffix
+        raise ValidationError({'detail':
+            "Unable to create a unique URL slug from title '%s'" % self.title})
+
+
+@python_2_unicode_compatible
+class MediaTag(models.Model):
+
+    location = models.CharField(max_length=250)
+    tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tag
+
+
+@python_2_unicode_compatible
+class LessVariable(models.Model):
+    """
+    This model stores value of a variable used to generate a css file.
+    """
+    name = models.CharField(max_length=250)
+    value = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    account = models.ForeignKey(settings.ACCOUNT_MODEL,
+        related_name='account_less_variable', null=True)
+    cssfile = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ('account', 'cssfile', 'name')
+
+    def __str__(self):
+        return '%s: %s' % (self.name, self.value)
+
+
+@python_2_unicode_compatible
+class ThemePackage(models.Model):
+    """
+    This model allow to record uploaded template.
+    """
+    slug = models.SlugField(unique=True)
+    account = models.ForeignKey(
+        settings.ACCOUNT_MODEL,
+        related_name='account_template', null=True, blank=True)
+    name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.account:
+            return '%s-%s' % (self.account, self.name)
+        else:
+            return self.name
+
+
+def get_active_theme():
+    """
+    Returns the active theme from a request.
+    """
+    if settings.ACTIVE_THEME_CALLABLE:
+        from .compat import import_string  # Because AppRegistryNotReady
+        theme_slug = import_string(settings.ACTIVE_THEME_CALLABLE)()
+        LOGGER.debug("pages: get_active_theme('%s')", theme_slug)
+        try:
+            return ThemePackage.objects.get(slug=theme_slug)
+        except ThemePackage.DoesNotExist:
+            pass
+    return None
+
---
+++ packages/djaodjin-pages/djaodjin-pages-0.1.1.tar.gz/djaodjin-pages-0.1.1/pages/static/js/djaodjin-upload.js
@@ -0,0 +1,242 @@
+(function ($) {
+    "use strict";
+
+    function Djupload(el, options){
+        this.element = $(el);
+        this.options = options;
+        this.init();
+    }
+
+    /* UI element to upload files directly to S3.
+
+       <div data-complete-url="">
+       </div>
+     */
+    Djupload.prototype = {
+
+        _csrfToken: function() {
+            var self = this;
+            if( self.options.csrfToken ) { return self.options.csrfToken; }
+            return getMetaCSRFToken();
+        },
+
+        _uploadSuccess: function(file, resp) {
+            var self = this;
+            self.element.trigger("djupload.success", resp.location);
+            if( self.options.uploadSuccess ) {
+                self.options.uploadSuccess(file, resp);
+            } else {
+                if( resp.details ) {
+                    showMessages(resp.details, "success");
+                } else {
+                    showMessages(["\"" + file.name
+                        + "\" uploaded sucessfully to \"" + resp.location
+                        + "\""], "success");
+                }
+            }
+            return true;
+        },
+
+        _uploadError: function(file, resp) {
+            var self = this;
+            self.element.trigger("djupload.error", [file.name, resp]);
+            if( self.options.uploadError ) {
+                self.options.uploadError(file, resp);
+            } else {
+                if( typeof resp === "string" ) {
+                    showErrorMessages(
+                 "Error: " + resp + "(while uploading '" + file.name + "')");
+                } else {
+                    showErrorMessages(resp);
+                }
+            }
+        },
+
+        _uploadProgress: function(file, progress) {
+            var self = this;
+            self.element.trigger("djupload.progress", [file.name, progress]);
+            if( self.options.uploadProgress ) {
+                self.options.uploadProgress(file, progress);
+            }
+            return true;
+        },
+
+        init: function(){
+            var self = this;
+            var dropzoneUrl = (self.options.accessKey ? self.options.uploadUrl
+                : (self.element.attr("data-complete-url") ?
+                    self.element.attr("data-complete-url")
+                    : self.options.uploadUrl));
+
+            if( !dropzoneUrl ) {
+                showErrorMessages(
+                    "instantiated djupload() with no uploadUrl specified.");
+                throw new Error(
+                    "instantiated djupload() with no uploadUrl specified.");
+            }
+            if( self.options.mediaPrefix !== ""
+                && !self.options.mediaPrefix.match(/\/$/)){
+                self.options.mediaPrefix += "/";
+            }
+            self.element.dropzone({
+                paramName: self.options.uploadParamName,
+                url: dropzoneUrl,
+                maxFilesize: self.options.uploadMaxFileSize,
+                clickable: self.options.uploadClickableZone,
+                createImageThumbnails: false,
+                previewTemplate: "<div></div>",
+                init: function() {
+                    if( self.options.accessKey ) {
+                        // We are going to remove extra input files that AWS
+                        // would reject (ex: csrftoken).
+                        var fields = this.element.querySelectorAll(
+                            "input, textarea, select, button");
+                        for( var idx = 0; idx < fields.length; ++idx ) {
+                            if( fields[idx].getAttribute("name")
+                                && (fields[idx].getAttribute("name")
+                                    !== self.options.uploadParamName) ) {
+                                fields[idx].parentNode.removeChild(fields[idx]);
+                            }
+                        }
+                    }
+                    this.on("sending", function(file, xhr, formData){
+                        if( self.options.accessKey ) {
+                            formData.append(
+                                "key", self.options.mediaPrefix + file.name);
+                            formData.append("policy", self.options.policy);
+                            formData.append(
+                                "x-amz-algorithm", "AWS4-HMAC-SHA256");
+                            formData.append(
+                                "x-amz-credential", self.options.amzCredential);
+                            formData.append("x-amz-date", self.options.amzDate);
+                            formData.append("x-amz-security-token",
+                                self.options.securityToken);
+                            formData.append(
+                                "x-amz-signature", self.options.signature);
+                            if( self.options.acl ) {
+                                formData.append("acl", self.options.acl);
+                            }
+                            var ext = file.name.slice(
+                                file.name.lastIndexOf('.')).toLowerCase();
+                            if( ext === ".jpg" ) {
+                                formData.append("Content-Type", "image/jpeg");
+                            } else if( ext === ".png" ) {
+                                formData.append("Content-Type", "image/png");
+                            } else if( ext === ".mp4" ) {
+                                formData.append("Content-Type", "video/mp4");
+                            } else {
+                                formData.append(
+                                    "Content-Type", "binary/octet-stream");
+                            }
+                        } else {
+                            formData.append(
+                                "csrfmiddlewaretoken", self._csrfToken());
+                            var data = self.element.data();
+                            for( var key in data ) {
+                                if( data.hasOwnProperty(key)
+                                    && key != 'djupload' ) {
+                                    formData.append(key, data[key]);
+                                }
+                            }
+                        }
+                    });
+
+                    this.on("success", function(file, response){
+                        if( self.options.accessKey) {
+                            // With a direct upload to S3, we need to build
+                            // a custom response with location url ourselves.
+                            response = {
+                                location: file.xhr.responseURL + self.options.mediaPrefix + file.name
+                            };
+                            // We will also call back a completion url
+                            // on the server.
+                            var completeUrl = self.element.attr(
+                                "data-complete-url");
+                            if( completeUrl ) {
+                                $.extend(response, self.element.data());
+                                delete response.djupload;
+                                $.ajax({
+                                    type: "POST",
+                                    url: completeUrl,
+                                    beforeSend: function(xhr) {
+                                        xhr.setRequestHeader(
+                                            "X-CSRFToken", self._csrfToken());
+                                    },
+                                    data: JSON.stringify(response),
+                                    datatype: "json",
+                                    contentType: "application/json; charset=utf-8",
+                                    success: function(resp) {
+             // We used ``response`` instead of ``resp`` to have consistent API,
+             // but sometimes it is nice to be able to update the ``details``
+             // message shown to the user.
+                                      if( resp.details ) {
+                                          self._uploadSuccess(file, resp);
+                                      } else {
+                                          self._uploadSuccess(file, response);
+                                      }
+                                    },
+                                    error: function(resp) {
+                                        self._uploadError(file, resp);
+                                    }
+                                });
+                            } else {
+                                self._uploadSuccess(file, response);
+                            }
+                        } else {
+                            self._uploadSuccess(file, response);
+                        }
+                    });
+
+                    this.on("error", function(file, message){
+                        self._uploadError(file, message);
+                    });
+
+                    this.on("uploadprogress", function(file, progress){
+                        self._uploadProgress(file, progress);
+                    });
+                }
+            });
+        }
+    };
+
+    $.fn.djupload = function(options) {
+        var opts = $.extend( {}, $.fn.djupload.defaults, options );
+        return this.each(function() {
+            if (!$.data(this, "djupload")) {
+                $.data(this, "djupload", new Djupload(this, opts));
+            }
+        });
+    };
+
+    $.fn.djupload.defaults = {
+        // location
+        uploadUrl: null,
+        mediaPrefix: "",
+
+        uploadZone: "body",
+        uploadClickableZone: true,
+        uploadParamName: "file",
+        uploadMaxFileSize: 250,
+
+        // Django upload
+        csrfToken: null,
+
+        // S3 direct upload
+        accessKey: null,
+        securityToken: null,
+        acl: null, // defaults to "private".
+        policy: "",
+        signature: null,
+        amzCredential: null,
+        amzDate: null,
+
+        // callback
+        uploadSuccess: null,
+        uploadError: null,
+        uploadProgress: null
+    };
+
+    Dropzone.autoDiscover = false;
+
+})(jQuery);
+
