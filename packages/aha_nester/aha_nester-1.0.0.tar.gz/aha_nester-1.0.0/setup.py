06028475c3fea50e29ae91e90d21a4dabba28d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package aha_nester/1.0.0/aha_nester-1.0.0.tar.gz

---
+++ packages/aha_nester/aha_nester-1.0.0.tar.gz/aha_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name		= 'aha_nester',
+		version		= '1.0.0',
+		py_modules	= ['aha_nester'],
+		author		= 'aha',
+		author_email= 'aha@coordiwise.com',
+		url			= 'http://www.coordiwise.com',
+		description = 'A simple printer of nested lists',
+		)
+
