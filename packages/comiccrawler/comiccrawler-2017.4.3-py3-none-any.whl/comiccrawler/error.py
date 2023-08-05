7a66bc7348ada90f6dc7099f669903cc2ccc99dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package comiccrawler/2017.4.3/comiccrawler-2017.4.3-py3-none-any.whl

---
+++ packages/comiccrawler/comiccrawler-2017.4.3-py3-none-any.whl/comiccrawler/error.py
@@ -0,0 +1,31 @@
+from requests import HTTPError # pylint: disable=unused-import
+
+class ComicCrawlerSignal(BaseException):
+	"""Extend BaseException."""
+	pass
+
+class LastPageError(ComicCrawlerSignal):
+	"""Raise LastPageError to exit crawl pages loop."""
+	pass
+
+class SkipEpisodeError(ComicCrawlerSignal):
+	"""Raise SkipEpisodeError to exit crawl episodes loop."""
+	def __init__(self, always=True):
+		super().__init__()
+		self.always = always
+
+class PauseDownloadError(ComicCrawlerSignal):
+	"""Raise PauseDownloadError to exit crawl mission loop."""
+	pass
+
+# class ExitErrorLoop(ComicCrawlerSignal):
+	# """Raise ExitErrorLoop to exit error loop."""
+	# pass
+
+class ComicCrawlerError(Exception):
+	"""Extend Exception."""
+	pass
+
+class ModuleError(ComicCrawlerError):
+	"""Can't find module."""
+	pass
