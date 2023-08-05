1353638e26034983abb64228c7c16733ac0db82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package csv2anki/0.0.1/csv2anki-0.0.1.tar.gz

---
+++ packages/csv2anki/csv2anki-0.0.1.tar.gz/csv2anki-0.0.1/csv2anki/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright: Albertix <albertix@live.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from .collection import *
---
+++ packages/csv2anki/csv2anki-0.0.1.tar.gz/csv2anki-0.0.1/csv2anki/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright: Albertix <albertix@live.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from csv2anki.cli import cli
+
+
+if __name__ == '__main__':
+    cli()
