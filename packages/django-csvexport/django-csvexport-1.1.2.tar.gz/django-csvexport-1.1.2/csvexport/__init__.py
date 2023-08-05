b8265992f0e2aea50a7f509ebb1d1861d9a6c069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:22 +0000

    Add package django-csvexport/1.1.2/django-csvexport-1.1.2.tar.gz

---
+++ packages/django-csvexport/django-csvexport-1.1.2.tar.gz/django-csvexport-1.1.2/csvexport/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+csvexport
+----------
+
+A django extension that simplifies model to csv conversions.
+"""
+
+
+__title__ = "csvexport"
+__version__ = "1.1.2"
+__build__ = 112
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2017 Martin Sandström"
---
+++ packages/django-csvexport/django-csvexport-1.1.2.tar.gz/django-csvexport-1.1.2/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pip
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import csvexport
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+# Handle requirements
+requires = parse_requirements("requirements/install.txt",
+                              session=pip.download.PipSession())
+install_requires = [str(ir.req) for ir in requires]
+
+requires = parse_requirements("requirements/tests.txt",
+                              session=pip.download.PipSession())
+tests_require = [str(ir.req) for ir in requires]
+
+# Convert markdown to rst
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
+
+
+setup(
+    name="django-csvexport",
+    version=csvexport.__version__,
+    description=("Simple model to csv conversions for Django."),
+    long_description=long_description,
+    author="Martin Sandström",
+    author_email="martin@marteinn.se",
+    url="https://github.com/marteinn/django-csvexport",
+    packages=find_packages(exclude=('tests*',)),
+    include_package_data=True,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    license="MIT",
+    zip_safe=False,
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ),
+)
