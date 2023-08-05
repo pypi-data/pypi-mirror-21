0d64ae1a10ebda9aa5c81fccd2b7dd13112bc239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:11 +0000

    Add package globus-cli/1.0.0.0/globus-cli-1.0.0.0.tar.gz

---
+++ packages/globus-cli/globus-cli-1.0.0.0.tar.gz/globus-cli-1.0.0.0/globus_cli/commands/bookmark/rename.py
@@ -0,0 +1,24 @@
+import click
+
+from globus_cli.parsing import common_options
+from globus_cli.safeio import formatted_print
+
+from globus_cli.services.transfer import get_client
+
+
+@click.command('rename', help='Change a bookmark\'s name')
+@common_options
+@click.argument('bookmark_id')
+@click.argument('new_bookmark_name')
+def bookmark_rename(bookmark_id, new_bookmark_name):
+    """
+    Executor for `globus bookmark rename`
+    """
+    client = get_client()
+
+    submit_data = {
+        'name': new_bookmark_name
+    }
+
+    res = client.update_bookmark(bookmark_id, submit_data)
+    formatted_print(res, simple_text='Success')
