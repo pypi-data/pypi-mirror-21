4d68fefb6fef3f7e9475f0998982c810d7fef3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:24 +0000

    Add package hub-pip/0.0.2/hub-pip-0.0.2.tar.gz

---
+++ packages/hub-pip/hub-pip-0.0.2.tar.gz/hub-pip-0.0.2/hub_pip/Main.py
@@ -0,0 +1,73 @@
+
+"""
+hub-pip
+Usage:
+  hub-pip --Project-Name=<None> --Project-Version=<None>
+        [
+        ((-c | --Config) <hub_config.ini>) 
+        --Hub-Url=<huburl>
+        --Hub-Username=<username>
+        --Hub-Password=<password>
+        --Hub-Proxy-Host=<None>
+        --Hub-Proxy-Port=<None>
+        --Hub-Proxy-Username=<None>
+        --Hub-Proxy-Password=<None>
+        --Hub-Timeout=<120>
+        --Hub-ScanTimeout=<300>
+        --Hub-CodeLocationName=<None>
+        --OutputDirectory=<build/output/>
+        --RequirementsFile=<None>
+        --IgnoreFailure=<False>
+        --CreateFlatDependencyList=<True>
+        --CreateTreeDependencyList=<True>
+        --CreateHubBdio=<True>
+        --DeployHubBdio=<False>
+        --CheckPolicies=<False>
+        ]
+
+Options:
+    -h --help                            Show this screen.
+    --version                            Show version.
+    (-c <hub_config.ini>)                Path to hub configuration file
+Examples:
+  hub-pip -c config.ini --DeployHubBdio=True
+Help:
+  For help using this tool, please open an issue on the Github_pip repository:
+  https://github.com/BlackDuckSoftware/hub-python-plugin
+"""
+
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+from hub_pip.BlackDuckConfig import BlackDuckConfig
+from hub_pip.BlackDuckCore import BlackDuckCore
+
+from . import __version__ as VERSION
+
+
+def cli():
+    """Main CLI entrypoint."""
+    options = docopt(__doc__, version=VERSION)
+    main(options)
+
+
+def main(options):
+    """Build config file from options"""
+    config_str = "[Black Duck Config]\n"
+
+    for key, value in options.items():
+        if "--" in key and value is not None and value is not "--Config":
+            field = key.replace("--", "")
+            config_option = field + " = " + str(value) + "\n"
+            config_str += config_option
+
+    config = None
+    if options["-c"] or options["--Config"]:
+        config_file_path = options["<hub_config.ini>"]
+        config = BlackDuckConfig.from_file(config_file_path)
+
+    config = BlackDuckConfig.from_string(config_str, black_duck_config=config)
+
+    core = BlackDuckCore(config)
+    core.run()
---
+++ packages/hub-pip/hub-pip-0.0.2.tar.gz/hub-pip-0.0.2/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It"s nice, because now 1) we have a top level
+# README file and 2) it"s easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+package_name = "hub-pip"
+package_version = "0.0.2"
+
+setup(
+    name=package_name,
+    version=package_version,
+    author="Black Duck Software",
+    author_email="bdsoss@blackducksoftware.com",
+    description=(
+        "Generates and deploys Black Duck I/O files for use with the Black Duck Hub"),
+    license="Apache 2.0",
+    keywords="hub-pip blackduck",
+    url="https://github.com/blackducksoftware/hub-pip",
+    install_requires=["configparser", "requests", "six", "docopt"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    long_description=read("README.md"),
+    classifiers=[],
+    packages=["hub_pip"],
+    include_package_data=True,
+    package_data={
+        "": ["*.md", "*.rst"],
+    },
+    entry_points={
+        "console_scripts": [
+            "hub_pip=hub_pip.Main:cli",
+        ],
+        "distutils.commands": [
+            "hub_pip=hub_pip.BlackDuckPlugin:BlackDuckCommand",
+        ]
+    },
+)
