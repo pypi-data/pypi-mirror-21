06a10d3608dd7c50bb3aa7e10a5307cbe8a6fb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package idempotent-sequence/1.0.2/idempotent-sequence-1.0.2.tar.gz

---
+++ packages/idempotent-sequence/idempotent-sequence-1.0.2.tar.gz/idempotent-sequence-1.0.2/idemseq/command.py
@@ -0,0 +1,69 @@
+import logging
+
+from funcsigs import signature
+
+from idemseq.base import Options
+
+log = logging.getLogger(__name__)
+
+
+class CommandOptions(Options):
+    _valid_options = {
+        'name': None,
+        'order': -1,
+        'run_always': None,
+        'run_until_finished': None,
+    }
+
+
+class Command(object):
+    """
+    Represents a function and its meta information (run_options) to be used
+    when the function is run as part of a sequence.
+    """
+
+    def __init__(self, func=None, **options):
+        self._func = func
+
+        self._options = CommandOptions(options)
+
+        self._signature = None
+        if self._func:
+            self._signature = signature(self._func)
+
+    def __str__(self):
+        return '{}(name={})'.format(self.__class__.__name__, self.name)
+
+    def __repr__(self):
+        return '<{}>'.format(self)
+
+    @property
+    def name(self):
+        return self.options.name or self._func.__name__
+
+    @property
+    def parameters(self):
+        """
+        Returns a view of parameters from underlying function's signature.
+        """
+        return self._signature.parameters.values()
+
+    @property
+    def options(self):
+        """
+        Options passed to the Command decorator/constructor.
+        """
+        return self._options
+
+    def __call__(self, *args, **kwargs):
+        log.debug('Command "{}" starting'.format(self.name))
+        try:
+            result = self._func(*args, **kwargs)
+            log.debug('Command "{}" finished'.format(self.name))
+            return result
+        except Exception:
+            log.error('Command "{}" failed with an exception'.format(self.name))
+            raise
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self._func == other._func and self._options == other._options
---
+++ packages/idempotent-sequence/idempotent-sequence-1.0.2.tar.gz/idempotent-sequence-1.0.2/idemseq/examples/example01.py
@@ -0,0 +1,76 @@
+import argparse
+import logging
+
+from idemseq.log import configure_logging
+from idemseq.sequence import SequenceBase
+
+
+example = SequenceBase()
+
+
+@example.command(run_always=True)
+def greeting(name):
+    print('Hello {}'.format(name))
+
+
+@example.command
+def step_one():
+    pass
+
+
+@example.command
+def step_two():
+    pass
+
+
+@example.command
+def step_three():
+    pass
+
+
+@example.command
+def step_four():
+    pass
+
+
+def main():
+    configure_logging(log_level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--run-id', help='Path to SQLite database to use, defaults to /tmp/example-01.db', default='/tmp/example-01.db')
+    parser.add_argument('--name', help='A dummy argument to pass to the command sequence')
+
+    parser.add_argument('--reset', action='store_true', help='Forget previous progress and exit')
+    parser.add_argument('--list', action='store_true', help='List all commands and exit')
+    parser.add_argument('--command', help='Attempt to run a single command, will fail if previous commands have not been completed')
+
+    parser.add_argument('--dry-run', action='store_true', help='Just simulate a run')
+    parser.add_argument('--start-at', help='Specify command to start with irrespective of completion of previous commands')
+    parser.add_argument('--stop-before', help='Specify command before which to stop sequence execution')
+
+    args = parser.parse_args()
+
+    context = dict(name=args.name)
+    sequence = example(args.run_id, dry_run=args.dry_run, start_at=args.start_at, stop_before=args.stop_before)
+
+    if args.reset:
+        sequence.reset()
+        return
+
+    if args.list:
+        for command in sequence:
+            print(' * {} - {}'.format(command.name, 'completed' if command.is_finished else 'todo'))
+        return
+
+    with sequence.env(context=context):
+
+        if args.command:
+            sequence[args.command].run()
+            return
+        else:
+            sequence.run()
+
+
+if __name__ == '__main__':
+    main()
