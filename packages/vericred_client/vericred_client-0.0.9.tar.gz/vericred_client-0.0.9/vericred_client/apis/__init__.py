9686ff14d6c269d7f0533c6d9ad45a5129038f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package vericred_client/0.0.9/vericred_client-0.0.9.tar.gz

---
+++ packages/vericred_client/vericred_client-0.0.9.tar.gz/vericred_client-0.0.9/vericred_client/apis/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .drug_coverages_api import DrugCoveragesApi
+from .drug_packages_api import DrugPackagesApi
+from .drugs_api import DrugsApi
+from .network_sizes_api import NetworkSizesApi
+from .networks_api import NetworksApi
+from .plans_api import PlansApi
+from .providers_api import ProvidersApi
+from .zip_counties_api import ZipCountiesApi
