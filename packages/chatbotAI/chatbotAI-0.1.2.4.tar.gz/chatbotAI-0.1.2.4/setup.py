10d1e42eb79cc5aedd1928c7cd7ffb721f4fc3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:03 +0000

    Add package chatbotAI/0.1.2.4/chatbotAI-0.1.2.4.tar.gz

---
+++ packages/chatbotAI/chatbotAI-0.1.2.4.tar.gz/chatbotAI-0.1.2.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'chatbotAI',
+    version='0.1.2.4',
+    author = "Ahmad Faizal B H",
+    author_email = "ahmadfaizalbh726@gmail.com",
+    url="https://github.com/ahmadfaizalbh/Chatbot",
+    description="A chatbot AI engine is a chatbot builder platform that provids both bot intelligence and chat handler with minimal codding",
+    long_description=open("README.rst").read(),
+    packages=['chatbot'],
+    license='MIT',
+    keywords='chatbot ai engine and chat builder platform',
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    package_dir={ 'chatbot': 'chatbot'},
+    include_package_data = True,
+    package_data   = { "chatbot":  ["default.template"]},
+    install_requires=[
+          'requests',
+      ]
+)
