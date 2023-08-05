d4a84561c9dee0724aa8b923c4a5e56c3e7253c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:03 +0000

    Add package imaplibext/0.1.0/imaplibext-0.1.0.tar.gz

---
+++ packages/imaplibext/imaplibext-0.1.0.tar.gz/imaplibext-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='imaplibext',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'typing',
+    ],
+    author='Thomas Ward',
+    author_email='teward@dark-net.io',
+    description="An imaplib extension module, that provides versions of IMAP4 and IMAP4_SSL which use functions (such "
+                "as 'search', 'fetch', 'store') which return or use UID-based message-set identifiers to better handle "
+                "messages uniquely.",
+    license='GNU AGPL 3.0+',
+    url='https://github.com/teward/imaplibext',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Other Environment', 
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='imaplib imap UID email'
+)
