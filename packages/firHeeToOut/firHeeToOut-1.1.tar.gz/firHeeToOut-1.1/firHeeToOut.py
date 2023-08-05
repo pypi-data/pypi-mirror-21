02496398401a5a6101b8834b679c3469187c4344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package firHeeToOut/1.1/firHeeToOut-1.1.tar.gz

---
+++ packages/firHeeToOut/firHeeToOut-1.1.tar.gz/firHeeToOut-1.1/firHeeToOut.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+
+def printHallowed():
+    """这是一个学习例子"""
+    print("hello world!111")
+    print("中国人", "1", "2", "aa")
+
+
+printHallowed()
---
+++ packages/firHeeToOut/firHeeToOut-1.1.tar.gz/firHeeToOut-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='firHeeToOut',
+    version='1.1',
+    packages=[''],
+    url='http://www.lenovo.com',
+    license='',
+    author='yaoq',
+    author_email='yaoq@lenovo.com',
+    description='print the good news'
+)
