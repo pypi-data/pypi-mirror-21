1f9b4c24b9b56582aab92d02e2d472cccbedd86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package django-fileprovider/0.1.2/django-fileprovider-0.1.2.tar.gz

---
+++ packages/django-fileprovider/django-fileprovider-0.1.2.tar.gz/django-fileprovider-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+__version__  = '0.1.2'
+
+setup(
+    name='django-fileprovider',
+    version=__version__,
+    description='django middleware for serving media files ( or protect them )',
+    author='Renjith Thankachan',
+    author_email='mail3renjith@gmail.com',
+    url='https://github.com/instapk/django-fileprovider.git',
+    download_url='https://github.com/instapk/django-fileprovider/archive/0.1.2.tar.gz',
+    long_description=open('README.md', 'r').read(),
+    packages=[
+        'fileprovider',
+    ],
+    zip_safe=False,
+    requires=[
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Utilities'
+    ],
+)
