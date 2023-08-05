41e8367398d07c748597c69888a3706cccc48a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package CloudshellController/0.8/CloudshellController-0.8-py2.py3-none-any.whl

---
+++ packages/CloudshellController/CloudshellController-0.8-py2.py3-none-any.whl/CloudshellController/__init__.py
@@ -0,0 +1,52 @@
+import cloudshell.api.cloudshell_api as cs_api
+import cloudshell.helpers.scripts.cloudshell_dev_helpers
+from cloudshell.api.common_cloudshell_api import CloudShellAPIError
+
+import json
+
+
+class CloudshellController:
+
+    def __init__(self, configs):
+        
+        self.configs = configs
+        self.session = ''
+
+        self.session = cs_api.CloudShellAPISession(host=self.configs["quali_server_hostname"],
+                                              username=self.configs["quali_admin_username"],
+                                              password=self.configs["quali_admin_password"],
+                                              domain=self.configs["quali_domain"])
+        
+    def set_resource_attribute(self, resource_full_path, attribute_name, attribute_value):
+        try:
+            self.session.SetAttributeValue(resourceFullPath=resource_full_path, attributeName=attribute_name, attributeValue=attribute_value)
+
+        except CloudShellAPIError as error:
+            print 'CLOUDSHELL API ERROR!!! ' + error.message
+
+    def set_resource_attributes(self, attribute_values):
+        self.session.SetAttributesValues(resourcesAttributesUpdateRequests=attribute_values)
+
+    def get_attribute_object(self, attribute_name, attribute_value):
+        return cs_api.AttributeNameValue(attribute_name, attribute_value)
+
+    def get_resource_by_attribute(self, attribute_values):
+        return self.session.FindResources(attributeValues=attribute_values)
+
+    def get_attribute_value(self, resource_full_path, attribute_name):
+        val='-1'
+        try:
+            att_list = self.session.GetResourceDetails(resourceFullPath=resource_full_path).ResourceAttributes
+            if self.item_in_list(attribute_name, att_list):
+                val = self.session.GetAttributeValue(resource_full_path=resource_full_path, attributeName=attribute_name).Value
+        except CloudShellAPIError as e:
+            print 'CLOUDSHELL API ERROR!!! ' + e.message
+        return val
+
+    def item_in_list(self, item, list):
+        try:
+            index = list.index(item)
+            return True
+        except:
+            return False
+
