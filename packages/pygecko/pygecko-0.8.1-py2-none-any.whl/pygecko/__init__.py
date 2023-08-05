fe2ab0ab4b1156978ea2602f9beaac01fe207ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package pygecko/0.8.1/pygecko-0.8.1-py2-none-any.whl

---
+++ packages/pygecko/pygecko-0.8.1-py2-none-any.whl/pygecko/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+__version__ = '0.8.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2010 Kevin Walchko'
+__author__ = 'Kevin J. Walchko'
+__doc__ = """
+pyGecko
+=========
+
+A simple robot library modelled after ROS (www.ros.org), but written in
+python.
+
+"""
+
+from Bag import Bag, Record, Play
+from ZmqClass import Sub, Pub, ServiceProvider, ServiceClient
+# import Microphone  # travis fails of this, no pyaudio
+from Sound import Audio
+from pyWit import pyWit
+from TTS import TTS
+from Transforms import from_euler
+from FileStorage import FileStorage
+from Topic import TopicPub, TopicSub
