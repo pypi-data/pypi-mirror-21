d284e789e94bba614adb79b6fa3802f5a6807370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package revit/0.1/revit-0.1.tar.gz

---
+++ packages/revit/revit-0.1.tar.gz/revit-0.1/setup.py
@@ -0,0 +1,18 @@
+"""
+revit-py setup
+"""
+
+from distutils.core import setup
+
+setup(
+      name='revit',
+      packages=['revit'],
+      version='0.1',
+      description='Cli interface for rev, online code review tool',
+      author='brwnrclse (Barry Harris)',
+      author_email='dev@vaemoi.co',
+      url='https://rev.vaemoi.co',
+      download_url='https://bitbucket.com/vaemoi/revit/',
+      keywords=['code review', 'rev', 'oss'],
+      classifiers=[],
+)
