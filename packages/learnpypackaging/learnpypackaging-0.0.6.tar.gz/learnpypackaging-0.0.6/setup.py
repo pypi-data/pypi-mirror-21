4c7e59f5f5e32c9a28e9763ae1cda6d63308a6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:06 +0000

    Add package learnpypackaging/0.0.6/learnpypackaging-0.0.6.tar.gz

---
+++ packages/learnpypackaging/learnpypackaging-0.0.6.tar.gz/learnpypackaging-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+__VERSION__ = '0.0.6'
+
+
+setup(
+    name="learnpypackaging",
+    version=__VERSION__,
+    package_data={
+        'learnpypackaging': ['data/*.html'],
+    },
+)
