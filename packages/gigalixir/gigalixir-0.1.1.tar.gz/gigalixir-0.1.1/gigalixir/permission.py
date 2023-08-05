de32505bf47f3cf748c9fc28d5c2a9550d990d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package gigalixir/0.1.1/gigalixir-0.1.1.tar.gz

---
+++ packages/gigalixir/gigalixir-0.1.1.tar.gz/gigalixir-0.1.1/gigalixir/permission.py
@@ -0,0 +1,32 @@
+import requests
+import urllib
+import json
+import click
+
+def get(host, app_name):
+    r = requests.get('%s/api/apps/%s/permissions' % (host, urllib.quote(app_name.encode('utf-8'))), headers = {
+        'Content-Type': 'application/json',
+    })
+    if r.status_code != 200:
+        raise Exception(r.text)
+    else:
+        data = json.loads(r.text)["data"]
+        click.echo(json.dumps(data, indent=2, sort_keys=True))
+
+def create(host, app_name, email):
+    r = requests.post('%s/api/apps/%s/permissions' % (host, urllib.quote(app_name.encode('utf-8'))), headers = {
+        'Content-Type': 'application/json',
+    }, json = {
+        "email": email,
+    })
+    if r.status_code != 201:
+        raise Exception(r.text)
+
+def delete(host, app_name, email):
+    r = requests.delete('%s/api/apps/%s/permissions' % (host, urllib.quote(app_name.encode('utf-8'))), headers = {
+        'Content-Type': 'application/json',
+    }, json = {
+        "email": email,
+    })
+    if r.status_code != 200:
+        raise Exception(r.text)
---
+++ packages/gigalixir/gigalixir-0.1.1.tar.gz/gigalixir-0.1.1/gigalixir/ssh_key.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import click
+
+def get(host):
+    r = requests.get('%s/api/ssh_keys' % host, headers = {
+        'Content-Type': 'application/json',
+    })
+    if r.status_code != 200:
+        raise Exception(r.text)
+    else:
+        data = json.loads(r.text)["data"]
+        click.echo(json.dumps(data, indent=2, sort_keys=True))
+
+def create(host, key):
+    r = requests.post('%s/api/ssh_keys' % host, headers = {
+        'Content-Type': 'application/json',
+    }, json = {
+        "ssh_key": key,
+    })
+    if r.status_code != 201:
+        raise Exception(r.text)
+
+def delete(host, key_id):
+    r = requests.delete('%s/api/ssh_keys/%s' % (host, key_id), headers = {
+        'Content-Type': 'application/json',
+    })
+    if r.status_code != 200:
+        raise Exception(r.text)
