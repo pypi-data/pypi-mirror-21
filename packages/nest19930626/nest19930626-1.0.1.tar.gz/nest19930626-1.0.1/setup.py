1874481a604375f5cac125616f730b7e47ec7548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package nest19930626/1.0.1/nest19930626-1.0.1.tar.gz

---
+++ packages/nest19930626/nest19930626-1.0.1.tar.gz/nest19930626-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nest19930626',
+		version = '1.0.1',
+		py_modules = ['nest19930626'],
+		author = 'ccl1050',
+		author_email = 'chenchengle1050@gmail.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nest'
+)
