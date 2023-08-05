fc4ddf747ac927f7039112c53f13fa2163a987fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package basepage/1.1/basepage-1.1.tar.gz

---
+++ packages/basepage/basepage-1.1.tar.gz/basepage-1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='basepage',
+    packages=['basepage'],
+    version='1.1',
+    description='A wrapper for WebDriver that provides some extended and additional functionality',
+    author='Jim Brannlund',
+    author_email='jbrannlund@planview.com',
+    url='https://github.com/Projectplace/basepage',
+    download_url='https://github.com/Projectplace/basepage/tarball/1.1',
+    keywords=['testing', 'webdriver', 'selenium'],
+    install_requires=['selenium'],
+    classifiers=[],
+)
