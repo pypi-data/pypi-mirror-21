4c46d0747bde27f20f6c9fe5f4bd2bb413bdd7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package django-perimeter/0.8.1/django-perimeter-0.8.1.tar.gz

---
+++ packages/django-perimeter/django-perimeter-0.8.1.tar.gz/django-perimeter-0.8.1/setup.py
@@ -0,0 +1,35 @@
+from os import path, pardir, chdir
+from setuptools import setup, find_packages
+
+README = open(path.join(path.dirname(__file__), 'README.rst')).read()
+# requirements.txt must be included in MANIFEST.in and include_package_data must be True
+# in order for this to work; ensures that tox can use the setup to enforce requirements
+REQUIREMENTS = '\n'.join(open(path.join(path.dirname(__file__), 'requirements.txt')).readlines())
+
+# allow setup.py to be run from any path
+chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
+
+setup(
+    name='django-perimeter',
+    version='0.8.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Site-wide perimeter access control for Django projects.',
+    long_description=README,
+    url='https://github.com/yunojuno/django-perimeter',
+    author='YunoJuno',
+    author_email='code@yunojuno.com',
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
