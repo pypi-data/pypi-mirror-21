9bbdd9a5272374a15165292ea8903169f3af0556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package nestertesterkj2/1.0.0/nestertesterkj2-1.0.0.zip

---
+++ packages/nestertesterkj2/nestertesterkj2-1.0.0.zip/nestertesterkj2-1.0.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+        name            = 'nestertesterkj2',
+        version         = '1.0.0',
+        py_modules      = ['nestertesterkj2'],
+        author          = 'kevin_jarrett',
+        author_email    = 'kevin_jarrett@yahoo.com',
+        url             = 'http://about.me/kjarrett',
+        description     = 'A simple printer of nested lists'
+)
