6d7393bbaecfc49cb2b3999847d27ed79ccbf922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package amaasutils/0.1.1/amaasutils-0.1.1.tar.gz

---
+++ packages/amaasutils/amaasutils-0.1.1.tar.gz/amaasutils-0.1.1/amaasutils/random_utils.py
@@ -0,0 +1,28 @@
+""" Helper functions for generating random data """
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from datetime import date
+from decimal import Decimal
+import random
+import string
+
+
+def random_string(length):
+    """
+    Generates a random string of length equal to the length parameter
+    """
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+
+def random_decimal():
+    """
+    Generates a random "sensible" decimal for things like prices
+    """
+    return Decimal(random.random() * 100).quantize(Decimal('0.01'))
+
+
+def random_date(start_year=2000, end_year=2020):
+    """
+    Generates a random "sensible" date for use in things like issue dates and maturities
+    """
+    return date(random.randint(start_year, end_year), random.randint(1, 12), random.randint(1, 28))
