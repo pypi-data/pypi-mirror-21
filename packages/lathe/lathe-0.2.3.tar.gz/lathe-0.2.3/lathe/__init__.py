780f12bb367ad938b903e0cf9a1c73caa6647b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package lathe/0.2.3/lathe-0.2.3.tar.gz

---
+++ packages/lathe/lathe-0.2.3.tar.gz/lathe-0.2.3/lathe/__init__.py
@@ -0,0 +1,9 @@
+from lathe.visual import bar, plot
+from lathe.cli import parse_args
+from lathe.data import load, shuffle, split, k_fold
+from lathe.metrics import measure_error, measure_accuracy, evaluate
+
+__all__ = [
+    'bar', 'plot', 'parse_args', 'load', 'shuffle', 'split', 'k_fold'
+    'measure_error', 'measure_accuracy', 'evaluate'
+]
