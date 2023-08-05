70d3d065349787c2e732c56ab25097b92c1ebb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package listreader/1.3.0/listreader-1.3.0.tar.gz

---
+++ packages/listreader/listreader-1.3.0.tar.gz/listreader-1.3.0/ListReader.py
@@ -0,0 +1,15 @@
+'''This is a 'nester.py' module which has a function 'print_lol'
+that is used to print the message'''
+
+def read(theList, indent= False, level= 0):
+        '''This function takes in a list and prints the items in the list.
+A second optional argument called "level" is used to insert a tab stop when a nested list
+is encountered.'''
+        for listItem in theList:
+                if(isinstance(listItem, list)):
+                        read(listItem, indent, level+1)
+                else:
+                        if(indent):
+                                for tabStop in range(level):
+                                        print('\t', end='')
+                        print(listItem)
