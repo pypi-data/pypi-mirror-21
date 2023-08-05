77cc03d858b0272d8bc39891c5007dcf6f4b6bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:36 +0000

    Add package zhangbinnester/1.1.0/zhangbinnester-1.1.0.tar.gz

---
+++ packages/zhangbinnester/zhangbinnester-1.1.0.tar.gz/zhangbinnester-1.1.0/zhangbinnester.py
@@ -0,0 +1,35 @@
+#encoding:utf-8
+
+def prin_lol(thr_times):
+    for each_times in thr_times:
+        if isinstance(each_times,list):
+            prin_lol(each_times)
+        else:
+            print each_times
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
