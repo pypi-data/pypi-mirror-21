fc5fdd6ae113a3b19b94458f1270a8bd8fd30e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package guillotina/1.0.0a5/guillotina-1.0.0a5-cp35-cp35m-macosx_10_12_x86_64.whl

---
+++ packages/guillotina/guillotina-1.0.0a5-cp35-cp35m-macosx_10_12_x86_64.whl/guillotina/transactions.py
@@ -0,0 +1,45 @@
+import logging
+
+
+logger = logging.getLogger('guillotina')
+
+
+async def commit(request, warn=True):
+    try:
+        await get_tm(request).commit()
+    except AttributeError as e:
+        if warn:
+            logger.warn('Could not locate transaction manager to commit', exc_info=True)
+
+
+async def abort(request):
+    try:
+        await request._tm.abort()
+    except AttributeError:
+        # not part of transaction, ignore
+        pass
+        # logger.warn('Could not locate transaction manager to abort', exc_info=True)
+
+
+def get_tm(request):
+    """Return shared transaction manager (from request)
+
+    This is used together with "with" syntax for wrapping mutating
+    code into a request owned transaction.
+
+    :param request: request owning the transaction
+
+    Example::
+
+        with get_tm(request) as txn:  # begin transaction txn
+
+            # do something
+
+        # transaction txn commits or raises ConflictError
+
+    """
+    return request._tm
+
+
+def get_transaction(request):
+    return request._tm.get()
