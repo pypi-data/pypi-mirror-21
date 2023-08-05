281a7473d684ceaa3e30b4f7dff44521473ac5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package shouty/0.1.dev2/shouty-0.1.dev2.tar.gz

---
+++ packages/shouty/shouty-0.1.dev2.tar.gz/shouty-0.1.dev2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='shouty',
+    version='0.1.dev2',
+
+    description='Minimal and pythonic wrapper to libshout2',
+    url='https://github.com/edne/shouty',
+
+    author='Edoardo Negri',
+    author_email='edne@gmx.com',
+
+    packages=['shouty'],
+    license='MIT',
+)
