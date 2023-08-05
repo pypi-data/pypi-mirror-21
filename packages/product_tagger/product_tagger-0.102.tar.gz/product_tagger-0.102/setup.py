3103e683bf2c472b64f82d3a6e7b1174abdc9257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:21 +0000

    Add package product_tagger/0.102/product_tagger-0.102.tar.gz

---
+++ packages/product_tagger/product_tagger-0.102.tar.gz/product_tagger-0.102/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'product_tagger',
+	version = '0.102',
+	packages = ['product_tagger',],
+	license = 'MIT licenced',
+	long_description = open('README.txt').read()
+	)
+
