29b11c4e99adc8ac8fef3ae2e7224ca77cbd5493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package cupy/1.0.0b1/cupy-1.0.0b1.tar.gz

---
+++ packages/cupy/cupy-1.0.0b1.tar.gz/cupy-1.0.0b1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import cupy_setup_build
+
+
+setup_requires = []
+install_requires = [
+    'filelock',
+    'nose',
+    'numpy>=1.9.0',
+    'six>=1.9.0',
+]
+
+ext_modules = cupy_setup_build.get_ext_modules()
+
+setup(
+    name='cupy',
+    version='1.0.0b1',
+    description='CuPy: NumPy-like API accelerated with CUDA',
+    author='Seiya Tokui',
+    author_email='tokui@preferred.jp',
+    url='http://docs.cupy.chainer.org/',
+    license='MIT License',
+    packages=['cupy',
+              'cupy.binary',
+              'cupy.core',
+              'cupy.creation',
+              'cupy.cuda',
+              'cupy.ext',
+              'cupy.indexing',
+              'cupy.io',
+              'cupy.linalg',
+              'cupy.logic',
+              'cupy.manipulation',
+              'cupy.math',
+              'cupy.padding',
+              'cupy.prof',
+              'cupy.random',
+              'cupy.sorting',
+              'cupy.statistics',
+              'cupy.testing'],
+    package_data={
+        'cupy': ['core/carray.cuh'],
+    },
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=['mock',
+                   'nose'],
+    ext_modules=ext_modules,
+)
