44758b9f2ac8a081b1ebf5468c699bfd0184d8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:00 +0000

    Add package Appium-UIAutomation/0.0.4/Appium-UIAutomation-0.0.4.tar.gz

---
+++ packages/Appium-UIAutomation/Appium-UIAutomation-0.0.4.tar.gz/Appium-UIAutomation-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='Appium-UIAutomation',
+    version='0.0.4',
+    packages=['automation', 'automation.mobile'],
+    package_data={'': ['*.txt']},
+    url='https://github.com/ayshrimali/Appium-UIAutomation',
+    license='Apache 2.0',
+    author='Anjum Shrimali',
+    author_email='ayshrimali@gmail.com',
+    description='A handy tool based on Appium-Python to write common UIAutomation test cases (for both iOS and Android) easily.',
+    keywords=[
+        'appium',
+        'appium 1.6',
+        'selenium',
+        'selenium 3',
+        'python client',
+        'mobile automation'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
+    ],
+    install_requires=['setuptools', 'selenium>=2.47.0', 'Appium-Python-Client=="0.24"']
+
+)
