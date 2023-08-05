861563d8f08ccedf08c1be1d9d03de37d7ef1aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package jupyter-hdfscontents/0.2/jupyter-hdfscontents-0.2.tar.gz

---
+++ packages/jupyter-hdfscontents/jupyter-hdfscontents-0.2.tar.gz/jupyter-hdfscontents-0.2/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='jupyter-hdfscontents',
+    version='0.2',
+    author='Ahmad Al-Shishtawy',
+    author_email='alshishtawy@gmail.com',
+    description='Jupyter content manager that uses the HDFS filesystem',
+    download_url = 'https://github.com/alshishtawy/hdfscontents/archive/0.2.tar.gz',
+    license='Apache License 2.0',
+    keywords='Jupyter, HDFS',
+    url='https://github.com/alshishtawy/hdfscontents',
+    packages=['hdfscontents', 'tests'],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: IPython',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['traitlets', 'notebook', 'hdfs3', 'tornado', 'nbformat', 'ipython_genutils'],
+)
