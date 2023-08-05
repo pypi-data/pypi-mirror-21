5d4b9082e9b0c8a6e3648977de2547a458f791e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package helloworld-package/0.1/helloworld-package-0.1.tar.gz

---
+++ packages/helloworld-package/helloworld-package-0.1.tar.gz/helloworld-package-0.1/helloworld-package/helloworld.py
@@ -0,0 +1,3 @@
+def hello_world():
+    for _ in range(1, 6, 1):
+        print ("Hello World!")
---
+++ packages/helloworld-package/helloworld-package-0.1.tar.gz/helloworld-package-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'helloworld-package',
+  packages = ['helloworld-package'],
+  version = '0.1',
+  description = 'Hello world!',
+  author = 'Anton Russkikh',
+  author_email = 'russkikh.a@gmail.com',
+  url = 'https://github.com/andrnd/helloworld-package',
+  download_url = 'https://github.com/andrnd/helloworld-package/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
