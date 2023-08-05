490e20d6d337ee62d534825ae63a29868fb2d81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:48 +0000

    Add package ApiRepl/2/ApiRepl-2.tar.gz

---
+++ packages/ApiRepl/ApiRepl-2.tar.gz/ApiRepl-2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='ApiRepl',
+      version='2',
+      description='Elastic API to database worker framework.',
+      author='Ryan Birmingham',
+      author_email='birm@rbirm.us',
+      url='http://rbirm.us',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 3.3',
+                   'Topic :: Database',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: SQL',
+                   'Programming Language :: Python :: 2.7'],
+      long_description=open('README.md', 'r').read(),
+      packages=['ApiRepl'],
+      )
