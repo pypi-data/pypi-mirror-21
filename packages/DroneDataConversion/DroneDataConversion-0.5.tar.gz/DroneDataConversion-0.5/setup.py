e105e17ee661f6c6dce8badbc4247c6e9c1a0ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:29 +0000

    Add package DroneDataConversion/0.5/DroneDataConversion-0.5.tar.gz

---
+++ packages/DroneDataConversion/DroneDataConversion-0.5.tar.gz/DroneDataConversion-0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+ 
+setup(
+	name='DroneDataConversion',
+	packages = ['DroneDataConversion'],
+	version='0.5',
+	description = 'Parses Bebop Drones flight logs for further processing and analysis',
+	author = 'Johannes Kinzig',
+	author_email = 'johannes_kinzig@icloud.com',
+	url = 'https://johanneskinzig.de/software-development/python-parser-bebop-logs.html',
+	include_package_data=True,
+	license='Apache License, Version 2.0',
+	keywords='extract parrot bebop convert gpx pud json',
+	install_requires=[
+        "geopy",
+        "gpxpy",
+    ]
+	)
