4cdc9628cdf4cae927b5f7fc24b00bf39ffd14d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package deep_list/1.3.0/deep_list-1.3.0.zip

---
+++ packages/deep_list/deep_list-1.3.0.zip/deep_list-1.3.0/deep_list.py
@@ -0,0 +1,14 @@
+''' his is a sample module 'deep_list.py' that provide a function called print_dem() which print lists that may or may not include nested lists. '''
+def print_dem(n, indent=False, level=0):
+    ''' This function takes one positional argument called 'n', which is any python list (possibly nested). Each data item in the list provied is (recursively) displayed on the screen on its own line. A second argument called "level" is used to insert tab-stops when a nested list is encounted.'''
+    for i in n:
+        if isinstance(i, list):
+            print_dem(i, indent, level + 1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(i)
+
+
+
