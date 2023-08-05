b846d10d44294be8e35641cdf9e38fd12f946446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:00 +0000

    Add package cfblog/0.2.12/cfblog-0.2.12.tar.bz2

---
+++ packages/cfblog/cfblog-0.2.12.tar.bz2/cfblog-0.2.12/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except Exception:
+    long_description = ""
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="cfblog",
+    description="Awesome CMS for Django",
+    long_description=long_description,
+
+    version="0.2.12",
+    author='Vinay Karanam, Ashish Nayan',
+    author_email="vinayinvicible@gmail.com, nayanashish@gmail.com",
+
+    url='https://github.com/Coverfox/cfblog/',
+    license='BSD',
+
+    install_requires=requirements,
+    packages=find_packages(),
+    include_package_data=True,
+
+    zip_safe=True,
+)
