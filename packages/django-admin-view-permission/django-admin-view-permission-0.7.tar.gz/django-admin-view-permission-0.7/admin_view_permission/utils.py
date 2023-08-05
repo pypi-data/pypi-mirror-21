e4909f777698a3ec6dc241324e002751d4a4aa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:59 +0000

    Add package django-admin-view-permission/0.7/django-admin-view-permission-0.7.tar.gz

---
+++ packages/django-admin-view-permission/django-admin-view-permission-0.7.tar.gz/django-admin-view-permission-0.7/admin_view_permission/utils.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+import django
+
+from .enums import DjangoVersion
+
+
+def django_version():
+    if django.get_version().startswith('1.9'):
+        return DjangoVersion.DJANGO_19
+    elif django.get_version().startswith('1.8'):
+        return DjangoVersion.DJANGO_18
+    elif django.get_version().startswith('1.10'):
+        return DjangoVersion.DJANGO_110
+    elif django.get_version().startswith('1.11'):
+        return DjangoVersion.DJANGO_111
