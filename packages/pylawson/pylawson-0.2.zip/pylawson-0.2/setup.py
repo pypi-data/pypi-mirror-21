fb88f68a1934fb910971b1d19af29da8bd3993cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:34 +0000

    Add package pylawson/0.2/pylawson-0.2.zip

---
+++ packages/pylawson/pylawson-0.2.zip/pylawson-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pylawson',
+    version='0.2',
+    description='Infor Lawson IOS API Wrapper',
+    long_description=open('README.rst').read(),
+    keywords='infor lawson erp',
+    url='https://github.com/indepndnt/pylawson',
+    author='Joe Carey',
+    author_email='joe@accountingdatasolutions.com',
+    license='Apache 2.0',
+    packages=['pylawson', 'pylawson.client'],
+    install_requires=['beautifulsoup4', 'requests'],
+    extras_require={
+        'sec_api': ['clr']
+    },
+    zip_safe=False
+)
