152c271cefbb5d54a108700e07569d78ef5dcb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package collective.instancebehavior/0.4/collective.instancebehavior-0.4.tar.gz

---
+++ packages/collective.instancebehavior/collective.instancebehavior-0.4.tar.gz/collective.instancebehavior-0.4/setup.py
@@ -0,0 +1,59 @@
+import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+version = '0.4'
+shortdesc = "Enable behaviors per content type instance."
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+
+
+setup(name='collective.instancebehavior',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+          'Environment :: Web Environment',
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ],
+      author='JC Brand, Johannes Raggam, Robert Niederreiter',
+      author_email='',
+      keywords='Python Plone Dexterity Behavior Development',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      url='https://pypi.python.org/pypi/collective.instancebehavior',
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.behavior',
+          'plone.dexterity',
+          'zope.annotation',
+          'zope.component',
+          'zope.interface',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.event',
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+
---
+++ packages/collective.instancebehavior/collective.instancebehavior-0.4.tar.gz/collective.instancebehavior-0.4/src/collective/instancebehavior/tests/test_instancebehavior.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import  unittest
+from collective.instancebehavior.tests import \
+    InstanceBehavior_INTEGRATION_TESTING
+
+
+class TestInstanceBehavior(unittest.TestCase):
+    layer = InstanceBehavior_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def test_foo(self):
+        self.assertEquals(1, 1)
