4ed42c40472565174ab2af363cf56814459291c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package shortrate/0.1/shortrate-0.1-py2-none-any.whl

---
+++ packages/shortrate/shortrate-0.1-py2-none-any.whl/shortrate/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#  shortrate
+#  -----------
+#  risk factor model library python style.
+#
+#  Author:  pbrisk <pbrisk@icloud.com>
+#  Website: https://github.com/pbrisk/shortrate
+#  License: MIT (see LICENSE file)
+
+
+from risk_factor_model import *
+from market_risk_factor import *
+from hullwhite_model import *
+from hullwhite_multicurrency_model import *
