74fd588ca8e46c503b5466ea2f60ecac3568fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:24 +0000

    Add package ufo2ft/0.4.2/ufo2ft-0.4.2.zip

---
+++ packages/ufo2ft/ufo2ft-0.4.2.zip/ufo2ft-0.4.2/Lib/ufo2ft/__init__.py
@@ -0,0 +1,68 @@
+from __future__ import print_function, division, absolute_import
+
+from ufo2ft.featureCompiler import FeatureCompiler
+from ufo2ft.kernFeatureWriter import KernFeatureWriter
+from ufo2ft.markFeatureWriter import MarkFeatureWriter
+from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
+from ufo2ft.postProcessor import PostProcessor
+
+
+__version__ = "0.4.2"
+
+
+def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
+               featureCompilerClass=FeatureCompiler, mtiFeaFiles=None,
+               kernWriterClass=KernFeatureWriter, markWriterClass=MarkFeatureWriter,
+               glyphOrder=None, useProductionNames=True, optimizeCFF=True,
+               roundTolerance=None):
+    """Create FontTools CFF font from a UFO.
+
+    *optimizeCFF* sets whether the CFF table should be subroutinized.
+
+    *roundTolerance* (float) controls the rounding of point coordinates.
+      It is defined as the maximum absolute difference between the original
+      float and the rounded integer value.
+      By default, all floats are rounded to integer (tolerance 0.5); a value
+      of 0 completely disables rounding; values in between only round floats
+      which are close to their integral part within the tolerated range.
+    """
+
+    outlineCompiler = outlineCompilerClass(
+        ufo, glyphOrder, roundTolerance=roundTolerance)
+    otf = outlineCompiler.compile()
+
+    featureCompiler = featureCompilerClass(
+        ufo, otf, kernWriterClass=kernWriterClass, markWriterClass=markWriterClass,
+        mtiFeaFiles=mtiFeaFiles)
+    featureCompiler.compile()
+
+    postProcessor = PostProcessor(otf, ufo)
+    otf = postProcessor.process(useProductionNames, optimizeCFF)
+
+    return otf
+
+
+def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler,
+               featureCompilerClass=FeatureCompiler, mtiFeaFiles=None,
+               kernWriterClass=KernFeatureWriter, markWriterClass=MarkFeatureWriter,
+               glyphOrder=None, useProductionNames=True,
+               convertCubics=True, cubicConversionError=2):
+    """Create FontTools TrueType font from a UFO.
+
+    *convertCubics* and *cubicConversionError* specify how the conversion from cubic
+    to quadratic curves should be handled.
+    """
+
+    outlineCompiler = outlineCompilerClass(
+        ufo, glyphOrder, convertCubics, cubicConversionError)
+    otf = outlineCompiler.compile()
+
+    featureCompiler = featureCompilerClass(
+        ufo, otf, kernWriterClass=kernWriterClass, markWriterClass=markWriterClass,
+        mtiFeaFiles=mtiFeaFiles)
+    featureCompiler.compile()
+
+    postProcessor = PostProcessor(otf, ufo)
+    otf = postProcessor.process(useProductionNames)
+
+    return otf
---
+++ packages/ufo2ft/ufo2ft-0.4.2.zip/ufo2ft-0.4.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division, absolute_import
+import sys
+from setuptools import setup, find_packages
+
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="ufo2ft",
+    version="0.4.2",
+    author="Tal Leming, James Godfrey-Kittle",
+    author_email="tal@typesupply.com",
+    maintainer="James Godfrey-Kittle",
+    maintainer_email="jamesgk@google.com",
+    description="A bridge between UFOs and FontTools.",
+    long_description=long_description,
+    url="https://github.com/googlei18n/ufo2ft",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
+    license="MIT",
+    setup_requires=pytest_runner + wheel,
+    tests_require=[
+        'pytest>=2.8',
+    ],
+    install_requires=[
+        "fonttools>=3.9",
+        "ufoLib>=2.0.0",
+        "defcon>=0.2.4",
+        "cu2qu>=1.1.1",
+        "compreffor>=0.4.4",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
