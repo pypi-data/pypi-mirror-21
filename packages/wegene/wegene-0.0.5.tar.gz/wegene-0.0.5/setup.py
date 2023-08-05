bda72ebb75e528e16409e624d72c8c837df11f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package wegene/0.0.5/wegene-0.0.5.tar.gz

---
+++ packages/wegene/wegene-0.0.5.tar.gz/wegene-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst', format='md')
+except IOError:
+    long_description = open("README.md").read()
+
+setup(
+    name="wegene",
+    version="0.0.5",
+    url="https://github.com/xraywu/wegene-python-sdk",
+    description="WeGene SDK for Python",
+    license="MIT",
+    author="Eddie Wu",
+    author_email="xrayxiaoli@gmail.com",
+    package_dir={ 'wegene': 'wegene' },
+    packages=find_packages(),
+    install_requires=[
+        "Unirest==1.1.6",
+        "jsonpickle==0.7.1",
+        "poster==0.8.1",
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
