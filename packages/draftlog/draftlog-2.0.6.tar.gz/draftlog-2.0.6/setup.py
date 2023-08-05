a55d1f75035122b67e80795f83bbacea1147a1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package draftlog/2.0.6/draftlog-2.0.6.tar.gz

---
+++ packages/draftlog/draftlog-2.0.6.tar.gz/draftlog-2.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name =         'draftlog',
+    version =      '2.0.6',
+    description =  'Create updatable log lines into the terminal.',
+    url =          'https://github.com/kepoorhampond/python-draftlog',
+    author =       'Kepoor Hampond',
+    author_email = 'kepoorh@gmail.com',
+    license =      'MIT',
+    packages =     ['draftlog'],
+    install_requires= [
+        # Notice that there is nothing here. :D-|-<
+    ],
+)
