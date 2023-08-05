8bf636259d901da7a7471272b3cc619eba7fa88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:00 +0000

    Add package ezrpy/0.1.01/ezrpy-0.1.01.tar.gz

---
+++ packages/ezrpy/ezrpy-0.1.01.tar.gz/ezrpy-0.1.01/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ezrpy',
+      version='0.1.01',
+      description='Automatic REST API for Prototypes',
+      author='Marcelo Jara',
+      author_email='marjara35@gmail.com',
+      keywords=['rest', 'prototype', 'api'],
+      url='https://github.com/mijara/ezrpy',
+      packages=['ezrpy'],
+      )
