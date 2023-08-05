5d4bd63291b74f8d8cebcfe976d0c65d793fb0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package extargsparse/0.9.6/extargsparse-0.9.6.zip

---
+++ packages/extargsparse/extargsparse-0.9.6.zip/extargsparse-0.9.6/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from setuptools import setup
+import os
+
+setup(name='extargsparse',
+	version='0.9.6',
+	description='extension class for argparse with json configuration',
+	url='http://github.com/jeppeter/extargsparse',
+	author='jeppeter Wang',
+	author_email='jeppeter@gmail.com',
+	license='MIT',
+	packages=['extargsparse'],
+	keyword = ['commandline','argparse'],
+	zip_safe=False)
