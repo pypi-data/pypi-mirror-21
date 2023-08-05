0c1d75b43045dd76c6df97c9939873375e90cc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package youtube2zim/1.1.1.1/youtube2zim-1.1.1.1.tar.gz

---
+++ packages/youtube2zim/youtube2zim-1.1.1.1.tar.gz/youtube2zim-1.1.1.1/youtube/build/Popular-Videos-Club-Mate/JS/data.js
@@ -0,0 +1,23 @@
+var json_All = [
+    {
+        "thumbnail": "Club-Mate-Papertown-Thomas-Patrick-Maguire-cover/thumbnail.jpg",
+        "description": "From CLUB MATE PLAYS THE SONGS OF THOMAS PATRICK MAGUIRE.\n\nhttp://thomaspatrickmaguire.bandcamp.com/album/club-mate-plays-the-songs-of-thomas-patrick-maguire",
+        "speaker": "gramparsnips",
+        "id": "Club-Mate-Papertown-Thomas-Patrick-Maguire-cover",
+        "title": "Club Mate - Papertown (Thomas Patrick Maguire cover)"
+    },
+    {
+        "thumbnail": "Club-Mate-Unemployment-Dreams-Official-Video-Thomas-Patrick-Maguire-cover/thumbnail.jpg",
+        "description": "http://thomaspatrickmaguire.bandcamp.com/album/club-mate-plays-the-songs-of-thomas-patrick-maguire\n\nVideo by Justin Remer, made by manipulating the original Thomas Patrick Maguire video.\n\nView the original here: http://www.youtube.com/watch?v=N2VbQNZKtLg",
+        "speaker": "Justin Remer",
+        "id": "Club-Mate-Unemployment-Dreams-Official-Video-Thomas-Patrick-Maguire-cover",
+        "title": "Club Mate - Unemployment Dreams (Official Video) (Thomas Patrick Maguire cover)"
+    },
+    {
+        "thumbnail": "Club-Mate-Evening-News-Official-Video-Thomas-Patrick-Maguire-cover/thumbnail.jpg",
+        "description": "http://thomaspatrickmaguire.bandcamp.com/album/club-mate-plays-the-songs-of-thomas-patrick-maguire\n\nVideo by Justin Remer, made by manipulating the original Thomas Patrick Maguire video.\n\nView the original here: http://www.youtube.com/watch?v=iIxeT0y0Xuw",
+        "speaker": "Justin Remer",
+        "id": "Club-Mate-Evening-News-Official-Video-Thomas-Patrick-Maguire-cover",
+        "title": "Club Mate - Evening News (Official Video) (Thomas Patrick Maguire cover)"
+    }
+]; 
