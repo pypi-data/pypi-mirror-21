602268243c90bcfcbd968bb9eb160cc9442f16dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:59 +0000

    Add package ADLES/0.7.17/ADLES-0.7.17.tar.gz

---
+++ packages/ADLES/ADLES-0.7.17.tar.gz/ADLES-0.7.17/adles/scripts/cleanup_vms.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cleanup and Destroy VMs and VM Folders in a vSphere environment.
+
+Usage:
+    cleanup_vms.py [options]
+    cleanup_vms.py --version
+    cleanup_vms.py (-h | --help)
+
+Options:
+    -h, --help          Prints this page
+    --version           Prints current version
+    --no-color          Do not color terminal output
+    -v, --verbose       Emit debugging logs to terminal
+    -f, --file FILE     Name of JSON file with server connection information
+
+"""
+
+import logging
+
+from docopt import docopt
+
+from adles.utils import prompt_y_n_question, default_prompt, script_setup, resolve_path
+import adles.vsphere.vm_utils as vm_utils
+from adles.vsphere.folder_utils import enumerate_folder, \
+    format_structure, cleanup, retrieve_items
+
+__version__ = "0.5.5"
+
+
+def main():
+    args = docopt(__doc__, version=__version__, help=True)
+    server = script_setup('cleanup_vms.log', args, (__file__, __version__))
+
+    if prompt_y_n_question("Multiple VMs? ", default="yes"):
+        folder, folder_name = resolve_path(server, "folder",
+                                           "that has the VMs/folders you want to destroy")
+
+        # Display folder structure
+        if prompt_y_n_question("Display the folder structure? "):
+            logging.info("Folder structure: \n%s", format_structure(
+                enumerate_folder(folder, recursive=True, power_status=True)))
+
+        # Prompt user to configure destruction options
+        vm_prefix = default_prompt("Prefix of VMs you wish to destroy"
+                                   " (CASE SENSITIVE!)", default='')
+        recursive = prompt_y_n_question("Recursively descend into folders? ")
+        destroy_folders = prompt_y_n_question("Destroy folders in addition to VMs? ")
+        if destroy_folders:
+            folder_prefix = default_prompt("Prefix of folders you wish to destroy"
+                                           " (CASE SENSITIVE!)", default='')
+            destroy_self = prompt_y_n_question("Destroy the folder itself? ")
+        else:
+            folder_prefix = ''
+            destroy_self = False
+
+        # Show user what options they selected (TODO: do these options as cmdline arg flags?)
+        logging.info("Options selected\nVM Prefix: %s\nFolder Prefix: %s\nRecursive: %s\n"
+                     "Folder-destruction: %s\nSelf-destruction: %s", str(vm_prefix),
+                     str(folder_prefix), recursive, destroy_folders, destroy_self)
+
+        # Show how many items matched the options
+        v, f = retrieve_items(folder, vm_prefix, folder_prefix, recursive=True)
+        num_vms = len(v)
+        if destroy_folders:
+            num_folders = len(f)
+            if destroy_self:
+                num_folders += 1
+        else:
+            num_folders = 0
+        logging.info("%d VMs and %d folders match the options", num_vms, num_folders)
+
+        # Confirm and destroy
+        if prompt_y_n_question("Continue with destruction? "):
+            logging.info("Destroying folder '%s'...", folder_name)
+            cleanup(folder, vm_prefix=vm_prefix, folder_prefix=folder_prefix, recursive=recursive,
+                    destroy_folders=destroy_folders, destroy_self=destroy_self)
+        else:
+            logging.info("Destruction cancelled")
+    else:
+        vm, vm_name = resolve_path(server, "vm", "to destroy")
+
+        if prompt_y_n_question("Display VM info? "):
+            logging.info(vm_utils.get_vm_info(vm, detailed=True, uuids=True,
+                                              snapshot=True, vnics=True))
+
+        if vm.config.template:  # Warn if template
+            if not prompt_y_n_question("VM '%s' is a Template. Continue? " % vm_name):
+                exit(0)
+
+        if prompt_y_n_question("Continue with destruction? "):
+            logging.info("Destroying VM '%s'", vm_name)
+            vm_utils.destroy_vm(vm)
+        else:
+            logging.info("Destruction cancelled")
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ADLES/ADLES-0.7.17.tar.gz/ADLES-0.7.17/adles/scripts/clone_vms.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Clone multiple Virtual Machines in vSphere.
+
+Usage:
+    clone_vms.py [options]
+    clone_vms.py --version
+    clone_vms.py (-h | --help)
+
+Options:
+    -h, --help          Prints this page
+    --version           Prints current version
+    --no-color          Do not color terminal output
+    -v, --verbose       Emit debugging logs to terminal
+    -f, --file FILE     Name of JSON file with server connection information
+
+"""
+
+import logging
+
+from docopt import docopt
+
+from adles.utils import prompt_y_n_question, pad, default_prompt, script_setup, resolve_path
+from adles.vsphere.vm_utils import clone_vm
+from adles.vsphere.folder_utils import format_structure, retrieve_items
+
+__version__ = "0.5.2"
+
+
+def main():
+    args = docopt(__doc__, version=__version__, help=True)
+    server = script_setup('clone_vms.log', args, (__file__, __version__))
+
+    vms = []
+    vm_names = []
+
+    # Single-vm source
+    if prompt_y_n_question("Do you want to clone from a single VM?"):
+        v, v_name = resolve_path(server, "VM", "or template you wish to clone")
+        vms.append(v)
+        vm_names.append(str(input("Base name for instances to be created: ")))
+    # Multi-VM source
+    else:
+        folder_from, from_name = resolve_path(server, "folder", "you want to clone all VMs in")
+        v, _ = retrieve_items(folder_from)  # Get VMs in the folder, ignore any folders
+        vms.extend(v)
+        logging.info("%d VMs found in source folder %s\n%s",
+                     len(v), from_name, format_structure(v))
+        if not prompt_y_n_question("Keep the same names? "):
+            names = []
+            for i in range(len(v)):
+                names.append(str(input("Enter base name for VM %d: " % i)))
+        else:
+            names = list(map(lambda x: x.name, v))  # Same names as sources
+        vm_names.extend(names)
+
+    create_in, create_in_name = resolve_path(server, "folder", "in which to create VMs")
+    instance_folder_base = None
+    if prompt_y_n_question("Do you want to create a folder for each instance? "):
+        instance_folder_base = str(input("Enter instance folder base name: "))
+
+    num_instances = int(input("Number of instances to be created: "))
+
+    pool = server.get_pool().name
+    pool = default_prompt(prompt="Resource pool to assign VMs to", default=pool)
+
+    logging.info("Creating %d instances under folder %s", num_instances, create_in_name)
+    for instance in range(num_instances):
+        for vm, name in zip(vms, vm_names):
+            spec = server.gen_clone_spec(pool_name=pool)  # Generate clone specification
+            if instance_folder_base:  # Create instance folders for a nested clone
+                f = server.create_folder(instance_folder_base + pad(instance), create_in=create_in)
+                vm_name = name
+                clone_vm(vm=vm, folder=f, name=vm_name, clone_spec=spec)
+            else:
+                vm_name = name + pad(instance)    # Append instance number
+                clone_vm(vm=vm, folder=create_in, name=vm_name, clone_spec=spec)
+
+
+if __name__ == '__main__':
+    main()
