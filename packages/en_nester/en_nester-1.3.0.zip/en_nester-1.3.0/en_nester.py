9653c0e3bd6709a9e9ea6f520fa5dedc783dad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package en_nester/1.3.0/en_nester-1.3.0.zip

---
+++ packages/en_nester/en_nester-1.3.0.zip/en_nester-1.3.0/en_nester.py
@@ -0,0 +1,15 @@
+def print_lol(the_list,indent=False,level=0):
+    """这个函数取一位参数，名为‘the_list’，这个可以是任何python列表。所指定的列表中的每个数据项会输出到屏幕上"""
+    
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+                
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end="")      
+            print(each_item)
+            
+                    
+                    
