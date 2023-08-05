7e97041dda9f62ae3f8cc04690a3059433f113c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package quicknet/3.0/quicknet-3.0.tar.gz

---
+++ packages/quicknet/quicknet-3.0.tar.gz/quicknet-3.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = "quicknet",
+      author = "Stratis Markou",
+      author_email = "stratismar@gmail.com",
+      description = "Collection of methods for a quick start with neural networks.",
+      version = "3.0",
+      scripts=['quicknet.py'])
