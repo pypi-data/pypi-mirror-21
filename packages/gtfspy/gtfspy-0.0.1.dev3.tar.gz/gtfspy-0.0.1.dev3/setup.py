3b64a8f1ab491bfd99950f76b2aa2a5cfef33263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package gtfspy/0.0.1.dev3/gtfspy-0.0.1.dev3.tar.gz

---
+++ packages/gtfspy/gtfspy-0.0.1.dev3.tar.gz/gtfspy-0.0.1.dev3/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, Extension, find_packages
+
+version="0.0.1.dev3"
+
+setup(
+    name="gtfspy",
+    version=version,
+    description="Python package for analyzing public transport timetables",
+    url="https://github.com/CxAalto/gtfspy",
+    packages=find_packages(exclude=["java_routing", "examples"]),
+    author="Rainer Kujala",
+    author_email="Rainer.Kujala@gmail.com",
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ], 
+    install_requires = [
+        "setuptools>=18.0",
+        "pandas",
+        "networkx",
+        "pyshp",
+        "smopy",
+        "nose",
+        "Cython",
+        "six",
+        "geoindex",
+        "osmread==0.1dev-r0"
+    ],
+    dependency_links = [
+        'git+https://github.com/rmkujala/osmread.git#egg=osmread-0.1dev-r0@ee5929b001b30fe07ffa6656de1ab72276caee6e'
+    ],
+    ext_modules=[
+        Extension(
+            'gtfspy.routing.label',
+            sources=["gtfspy/routing/label.pyx"],
+        ),
+    ],
+    keywords = ['transit', 'routing' 'gtfs', 'public transport', 'analysis', 'visualization'], # arbitrary keywords
+)
