5b7f68383ace63b510141b6807c17247b8a6c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package hfbpnester/1.4.0/hfbpnester-1.4.0.zip

---
+++ packages/hfbpnester/hfbpnester-1.4.0.zip/hfbpnester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'hfbpnester',
+        version     = '1.4.0',
+        py_modules  = ['hfbpnester'],
+        author      = 'bigpython',
+        author_email= 'hfpython@headfirstlabs.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+     )
