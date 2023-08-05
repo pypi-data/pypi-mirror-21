86f1565fddc3abde026bff9e48d23c93ca7f5b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package trx/0.2.0/trx-0.2.0.tar.gz

---
+++ packages/trx/trx-0.2.0.tar.gz/trx-0.2.0/trx/__init__.py
@@ -0,0 +1,12 @@
+from . import azav
+from . import utils
+from . import mask
+from . import cell
+from . import filters
+from . import id9
+from . import dataReduction
+from datastorage import DataStorage, read, save
+try:
+  from . import peaks
+except ImportError as err:
+  print("Can't import submodule peaks, reason was:",e)
