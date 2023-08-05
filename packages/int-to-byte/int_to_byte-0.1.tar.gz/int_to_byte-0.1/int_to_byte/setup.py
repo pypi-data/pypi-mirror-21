105fc57e128c015e53e9e1435dce3dccbcfc4611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package int-to-byte/0.1/int_to_byte-0.1.tar.gz

---
+++ packages/int-to-byte/int_to_byte-0.1.tar.gz/int_to_byte-0.1/int_to_byte/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='int_to_byte',
+      version='0.1',
+      description='converter numbers from int to byte',
+      url='https://github.com/AlexOdlin/lab8',
+      author='Alexandr Sokolov IU8-27',
+      license='BMSTU',
+      packages=['int_to_byte'],
+      zip_safe=False)
