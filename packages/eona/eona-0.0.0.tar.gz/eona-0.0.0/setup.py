88695645911b4aedb0dc50d6c629067cdb99c648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package eona/0.0.0/eona-0.0.0.tar.gz

---
+++ packages/eona/eona-0.0.0.tar.gz/eona-0.0.0/setup.py
@@ -0,0 +1,27 @@
+'''
+@author: jimfan
+'''
+import os
+from setuptools import setup
+
+NAME = 'eona'
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(name=NAME,
+      version='0.0.0',
+      author='J.F.',
+      long_description=read('README.rst'),
+      license='GPLv3',
+      packages=[NAME],
+      classifiers=[
+          "Development Status :: 1 - Planning",
+          "Programming Language :: Python :: 3"
+      ],
+      # install_requires=read('requirements.txt').strip().splitlines(),
+      # include_package_data=True,
+      # zip_safe=False
+)
