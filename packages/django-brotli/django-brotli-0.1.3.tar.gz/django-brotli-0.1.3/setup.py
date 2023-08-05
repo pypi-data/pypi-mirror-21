52688eafd550ed99e6f6767840a50a673d8900e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:24 +0000

    Add package django-brotli/0.1.3/django-brotli-0.1.3.tar.gz

---
+++ packages/django-brotli/django-brotli-0.1.3.tar.gz/django-brotli-0.1.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+
+from setuptools import setup
+
+setup(
+    name='django-brotli',
+    version='0.1.3',
+    description="""Middleware that compresses response using brotli algorithm.""",
+    long_description=io.open("README.rst", 'r', encoding="utf-8").read(),
+    url='https://github.com/illagrenan/django-brotli',
+    license='MIT',
+    author='Vasek Dohnal',
+    author_email='vaclav.dohnal@gmail.com',
+    packages=['django_brotli'],
+    install_requires=['django', 'brotlipy'],
+    extras_require={
+        ":python_version<'3.5'": ['typing'],
+    },
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
