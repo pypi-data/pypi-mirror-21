6200375d78e99e2439c0dd1fce6ba42107a1aa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package placer/0.0.3/placer-0.0.3-py2.py3-none-any.whl

---
+++ packages/placer/placer-0.0.3-py2.py3-none-any.whl/placer/draw_python.py
@@ -0,0 +1,56 @@
+WHITE = "0"
+BLACK = "3"
+GREY = "1"
+YELLOW = "8"
+# 9
+# A
+# B
+# C
+BLUE = "D"
+
+translate = {color: num for num, color in enumerate("0123456789ABCDEF")}
+
+
+python = [
+    GREY * 26,
+    GREY * 9 + BLUE * 8 + GREY * 9,
+    GREY * 8 + BLUE * 2 + WHITE + BLUE * 7 + GREY * 8,
+    GREY * 8 + BLUE * 10 + GREY * 8,
+    GREY * 8 + BLUE * 10 + GREY * 8,
+    GREY * 14 + BLUE * 4 + YELLOW * 4 + GREY * 4,
+    GREY * 6 + BLUE * 12 + YELLOW * 5 + GREY * 3,
+    GREY * 5 + BLUE * 13 + YELLOW * 5 + GREY * 3,
+    GREY * 5 + BLUE * 13 + YELLOW * 5 + GREY * 3,
+    GREY * 5 + BLUE * 13 + YELLOW * 5 + GREY * 3,
+    GREY * 5 + BLUE * 5 + YELLOW * 13 + GREY * 3,
+    GREY * 5 + BLUE * 5 + YELLOW * 13 + GREY * 3,
+    GREY * 5 + BLUE * 5 + YELLOW * 13 + GREY * 3,
+    GREY * 5 + BLUE * 5 + YELLOW * 12 + GREY * 4,
+    GREY * 6 + BLUE * 4 + YELLOW * 4 + GREY * 12,
+    GREY * 10 + YELLOW * 10 + GREY * 6,
+    GREY * 10 + YELLOW * 10 + GREY * 6,
+    GREY * 10 + YELLOW * 7 + WHITE + YELLOW * 2 + GREY * 6,
+    GREY * 11 + YELLOW * 8 + GREY * 7,
+    GREY * 26,
+    GREY * 26,
+    #
+    GREY * 1 + BLACK * 3 + GREY + BLACK + GREY + BLACK + GREY + BLACK * 3 + GREY +
+    BLACK + GREY + BLACK + GREY + BLACK * 3 + GREY + BLACK + GREY * 2 + BLACK + GREY,
+    #
+    GREY * 1 + BLACK + GREY + BLACK + GREY + BLACK + GREY + BLACK + GREY * 2 + BLACK + GREY * 2 +
+    BLACK + GREY + BLACK + GREY + BLACK + GREY + BLACK + GREY + BLACK * 2 + GREY + BLACK + GREY,
+    #
+    GREY * 1 + BLACK * 3 + GREY * 2 + BLACK + GREY * 3 + BLACK + GREY * 2 + BLACK *
+    3 + GREY + BLACK + GREY + BLACK + GREY + BLACK + GREY + BLACK * 2 + GREY,
+    #
+    GREY * 1 + BLACK + GREY * 4 + BLACK + GREY * 3 + BLACK + GREY * 2 + BLACK +
+    GREY + BLACK + GREY + BLACK * 3 + GREY + BLACK + GREY * 2 + BLACK + GREY,
+    #
+    GREY * 26
+]
+
+python_logo = []
+for x in range(26):
+    for y in range(26):
+        color = translate[python[y][x]]
+        python_logo.append({"x": x, "y": y, "color": color})
