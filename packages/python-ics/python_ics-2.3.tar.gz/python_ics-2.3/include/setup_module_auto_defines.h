6e332c8b92e40fa9c6043407484f48a9b207c3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package python-ics/2.3/python_ics-2.3.tar.gz

---
+++ packages/python-ics/python_ics-2.3.tar.gz/python_ics-2.3/include/setup_module_auto_defines.h
@@ -0,0 +1,12 @@
+#ifndef __SETUP_MODULE_AUTO_DEFINES_H__
+#define __SETUP_MODULE_AUTO_DEFINES_H__
+
+#ifndef _USE_INTERNAL_HEADER_
+#define _USE_INTERNAL_HEADER_
+#endif
+
+#include <Python.h>
+
+int setup_module_auto_defines(PyObject * module);
+
+#endif // __SETUP_MODULE_AUTO_DEFINES_H__
