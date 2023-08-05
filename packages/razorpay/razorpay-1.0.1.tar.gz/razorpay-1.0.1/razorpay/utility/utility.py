7bf40d0bbb87b39d16944938a6a7816588f6133a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package razorpay/1.0.1/razorpay-1.0.1.tar.gz

---
+++ packages/razorpay/razorpay-1.0.1.tar.gz/razorpay-1.0.1/razorpay/utility/utility.py
@@ -0,0 +1,39 @@
+import hmac
+import hashlib
+import sys
+
+
+from ..errors import SignatureVerificationError
+
+
+class Utility(object):
+    def __init__(self, client=None):
+        self.client = client
+
+    def verify_payment_signature(self, parameters):
+        order_id = str(parameters['razorpay_order_id'])
+        payment_id = str(parameters['razorpay_payment_id'])
+        razorpay_signature = str(parameters['razorpay_signature'])
+
+        msg = "{}|{}".format(order_id, payment_id)
+
+        self.verify_signature(razorpay_signature, msg)
+
+    def verify_webhook_signature(self, signature, body):
+        self.verify_signature(signature, body)
+
+    def verify_signature(self, signature, body):
+        key = str(self.client.auth[1])
+
+        if sys.version_info[0] == 3:  # pragma: no cover
+            key = bytes(key, 'utf-8')
+            body = bytes(body, 'utf-8')
+
+        dig = hmac.new(key=key,
+                       msg=body,
+                       digestmod=hashlib.sha256)
+
+        generated_signature = dig.hexdigest()
+
+        if not hmac.compare_digest(generated_signature, signature):
+            raise SignatureVerificationError('Payment Signature Verification Failed')
