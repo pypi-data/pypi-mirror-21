ed100f754f266e5090894d4ecc4d807ddef511ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package riseml/0.3.6/riseml-0.3.6.tar.gz

---
+++ packages/riseml/riseml-0.3.6.tar.gz/riseml-0.3.6/setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "riseml"
+VERSION = "0.3.6"
+
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+REQUIRES += ['pyyaml', 'requests', 'flask', 'flask-cors', 'jsonschema']
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="RiseML API client",
+    author_email="contact@riseml.com",
+    url="https://riseml.com",
+    keywords=["RiseML"],
+    entry_points={
+        'console_scripts': [
+            'riseml = riseml.__main__:main'
+        ]
+    },
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    
+    """
+)
+
