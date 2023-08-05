97813dc90c168232f7bf33f8918d43ee70efcaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package wrc/1.1.7/wrc-1.1.7.tar.gz

---
+++ packages/wrc/wrc-1.1.7.tar.gz/wrc-1.1.7/wrc/codegen/cgjson.py
@@ -0,0 +1,38 @@
+''' Backend for JSON.  '''
+import json
+from wrc.sema.ast import WCADocument, Guideline
+from wrc.codegen.cg import CGDocument
+from wrc.codegen.cghtml import simple_md2html
+
+REGULATIONS_ROOT = "https://www.worldcubeassociation.org/regulations/"
+
+
+class WCADocumentJSON(CGDocument):
+    ''' Implement a simple JSON generator from Regulations and Guidelines ASTs. '''
+
+    name = "JSON"
+    def __init__(self, versionhash, language, pdf):
+        # We don't need them
+        del versionhash, language
+        super(WCADocumentJSON, self).__init__(list)
+        self.urls = {'regulations': REGULATIONS_ROOT,
+                     'guidelines': REGULATIONS_ROOT + "guidelines.html",
+                     'pdf': pdf}
+
+    def emit(self, regulations, guidelines):
+        reg_list, guide_list = super(WCADocumentJSON, self).emit(regulations, guidelines)
+        reg_list.extend(guide_list)
+        return json.dumps(reg_list), ""
+
+
+    def visitRule(self, reg):
+        url = "/regulations/"
+        if isinstance(reg, Guideline):
+            url += "guidelines.html"
+        url += "#" + reg.number
+        self.codegen.append({'class': 'regulation', 'id': reg.number,
+                             'content_html': simple_md2html(reg.text, self.urls),
+                             'url': url})
+        retval = super(WCADocumentJSON, self).visitRule(reg)
+        return retval
+
