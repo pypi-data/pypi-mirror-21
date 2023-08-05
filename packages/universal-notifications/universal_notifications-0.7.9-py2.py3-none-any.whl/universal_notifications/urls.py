0b41a597a08e6b77399d0dbfc820bd6c007da362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package universal-notifications/0.7.9/universal_notifications-0.7.9-py2.py3-none-any.whl

---
+++ packages/universal-notifications/universal_notifications-0.7.9-py2.py3-none-any.whl/universal_notifications/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+from universal_notifications.docs import (UniversalNotificationsApiView, UniversalNotificationsResourcesView,
+                                          UniversalNotificationsUIView)
+
+urlpatterns = [
+    url(r'^$', UniversalNotificationsUIView.as_view(), name="django.swagger.base.view"),
+    url(r'^api-docs/$', UniversalNotificationsResourcesView.as_view(), name="django.swagger.resources.view"),
+    url(r'^api-docs/(?P<path>.*)/?$', UniversalNotificationsApiView.as_view(), name='django.swagger.api.view'),
+
+    url(r'api/', include('universal_notifications.api_urls')),
+]
+
+router = DefaultRouter()
+urlpatterns += router.urls
