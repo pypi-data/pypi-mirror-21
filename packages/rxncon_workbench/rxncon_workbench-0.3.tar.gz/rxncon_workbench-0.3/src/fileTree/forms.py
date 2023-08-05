db462c8c6ce961d021a9f6569bbae506eda9c0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package rxncon_workbench/0.3/rxncon_workbench-0.3.tar.gz

---
+++ packages/rxncon_workbench/rxncon_workbench-0.3.tar.gz/rxncon_workbench-0.3/src/fileTree/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import File
+
+class FileForm(forms.ModelForm):
+    class Meta:
+        model = File
+        fields =[
+            "project_name",
+            "file",
+            "comment",
+        ]
+
+class DeleteFileForm(forms.ModelForm):
+    class Meta:
+        model = File
+        fields = []
