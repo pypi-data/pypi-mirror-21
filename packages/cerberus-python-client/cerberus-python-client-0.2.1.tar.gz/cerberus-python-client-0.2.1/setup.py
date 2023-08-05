a2cf7d92991234ee94d118550f719f972006eec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package cerberus-python-client/0.2.1/cerberus-python-client-0.2.1.tar.gz

---
+++ packages/cerberus-python-client/cerberus-python-client-0.2.1.tar.gz/cerberus-python-client-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='cerberus-python-client',
+    version='0.2.1',
+    install_requires=requirements,
+    author='Ann Wallace',
+    author_email='ann.wallace@nike.com',
+    description="A python client for interacting with Cerberus and Vault",
+    url='https://github.com/Nike-Inc/cerberus-python-client',
+    license='Apache License, v2.0',
+    keywords='cerberus nike',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite="tests",
+  )
