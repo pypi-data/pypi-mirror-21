692294fc7511a865c5ecccdc443647e2c1f9ed69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package botify/0.1.3/botify-0.1.3.zip

---
+++ packages/botify/botify-0.1.3.zip/botify-0.1.3/botify/utils.py
@@ -0,0 +1,20 @@
+import inspect
+
+def _get_args_count_helper(function, argspec_func):
+    args = argspec_func(function).args
+    try:
+        if args[0] in ('self', 'cls'):
+            return len(args) - 1
+    except IndexError:
+        pass
+    return len(args)
+
+def get_args_count(function):
+    try:
+        return len(inspect.signature(function).parameters)
+    except AttributeError:
+        try:
+            return _get_args_count_helper(function, inspect.getfullargspec)
+        except AttributeError:
+            return _get_args_count_helper(function, inspect.getargspec)
+        
