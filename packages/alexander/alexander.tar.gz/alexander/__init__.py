05491f2fd2511ad1bf19061195c661b701cf3f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package alexander/0.1/alexander.tar.gz

---
+++ packages/alexander/alexander.tar.gz/alexander/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return (pow(3, 4))
---
+++ packages/alexander/alexander.tar.gz/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='alexander',
+      version='0.1',
+      description='Test',
+      url='https://github.com/alexandermlabs',
+      author='alexander',
+      author_email='alexandermlabs@yandex.ru',
+      license='GPL',
+      packages=['alexander'],
+      zip_safe=False)
