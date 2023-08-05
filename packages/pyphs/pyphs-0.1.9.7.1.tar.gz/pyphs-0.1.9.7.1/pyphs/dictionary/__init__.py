87bb06d370e35481eaa527a25872c71645f845c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package pyphs/0.1.9.7.1/pyphs-0.1.9.7.1.tar.gz

---
+++ packages/pyphs/pyphs-0.1.9.7.1.tar.gz/pyphs-0.1.9.7.1/pyphs/dictionary/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 13:07:33 2016
+
+@author: Falaize
+"""
+
+from __future__ import absolute_import, division, print_function
+
+#from . import edges
+from . import electronics
+from . import mechanics
+from . import connectors
+from . import mechanics_dual
+from . import magnetics
+
+__all__ = ['electronics', 'mechanics', 'magnetics', 'connectors',
+           'mechanics_dual']
---
+++ packages/pyphs/pyphs-0.1.9.7.1.tar.gz/pyphs-0.1.9.7.1/pyphs/dictionary/rhodes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 18:45:38 2017
+
+@author: Falaize
+"""
+
