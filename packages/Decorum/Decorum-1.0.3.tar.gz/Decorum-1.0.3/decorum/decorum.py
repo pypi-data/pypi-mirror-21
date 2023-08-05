a117af2dcb96c4b7808da8bd861d04603fba612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package Decorum/1.0.3/Decorum-1.0.3.tar.gz

---
+++ packages/Decorum/Decorum-1.0.3.tar.gz/Decorum-1.0.3/decorum/decorum.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import functools
+
+
+class Decorum(object):
+    """Decorator class that simplifies writing and testing decorators."""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Unifies decorator interface, so that it can be used
+        both with and without arguments the same way.
+
+        >>> decor = Decorum()
+        >>> decor.assigned == functools.WRAPPER_ASSIGNMENTS
+        True
+        >>> decor = Decorum(assigned=None)
+        >>> bool(decor.assigned)
+        False
+
+        """
+        #: Wrapped function.
+        self._wrapped = None
+
+        #: Function name. Can be overriden with decorated function name,
+        #: depending on values of :py:attr:`assigned`.
+        self.__name__ = self.__class__.__name__
+
+        if args and callable(args[0]):
+            # used as decorator without being called
+            self.init()
+            self.wraps(args[0])
+        else:
+            # used as decorator and called
+            self.init(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        """Actually run the decorated function.
+
+        Uses :meth:`wrap` to handle decoration. Restores :attr:`__doc__` and
+        :attr:`__name__`.
+
+        """
+        if self._wrapped:
+            return self.call(*args, **kwargs)
+        else:
+            return self.wraps(args[0])
+
+    def __get__(self, instance, owner):
+        """Allows a decorator to decorate an instance method."""
+        return functools.partial(self.call, instance)
+
+    def wraps(self, f):
+        """Wraps the function and returns it"""
+        self._wrapped = f
+        functools.update_wrapper(self, f, self.assigned or (), ())
+        return self
+
+    def init(self, assigned=functools.WRAPPER_ASSIGNMENTS):
+        """Passed any possible arguments to decorator"""
+        #: Specify which attributes of the original function are assigned
+        #: directly to the matching attributes on the decorator.
+        self.assigned = assigned
+        return self
+
+    def call(self, *args, **kwargs):
+        return self._wrapped(*args, **kwargs)
+
+
+def decorator(cls):
+    class decorated(cls, Decorum):
+        def __init__(self, *args, **kwargs):
+            Decorum.__init__(self, *args, **kwargs)
+            for attribute in functools.WRAPPER_ASSIGNMENTS:
+                if not self.assigned or attribute not in self.assigned:
+                    try:
+                        setattr(self, attribute, getattr(cls, attribute))
+                    except AttributeError:
+                        pass
+    return decorated
---
+++ packages/Decorum/Decorum-1.0.3.tar.gz/Decorum-1.0.3/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+import os
+import sys
+
+from setuptools import setup
+
+
+#: Absolute path to directory containing setup.py file.
+here = os.path.abspath(os.path.dirname(__file__))
+#: Boolean, ``True`` if environment is running Python version 2.
+IS_PYTHON2 = sys.version_info[0] == 2
+
+
+# Data for use in setup.
+NAME = 'Decorum'
+DESCRIPTION = 'Tool for writing simple decorators.'
+README = open(os.path.join(here, 'README.rst')).read()
+AUTHOR = u'Zach Kelling'
+EMAIL = 'zk@monoid.io'
+LICENSE = 'MIT'
+URL = 'https://pypi.python.org/pypi/{name}'.format(name=NAME)
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+]
+KEYWORDS = [
+    'decorator',
+    'decorators',
+]
+PACKAGES = ['decorum']
+REQUIREMENTS = []
+SETUP_REQUIREMENTS = [
+    'setuptools',
+]
+ENTRY_POINTS = {}
+
+
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(
+        author=AUTHOR,
+        author_email=EMAIL,
+        classifiers=CLASSIFIERS,
+        description=DESCRIPTION,
+        entry_points=ENTRY_POINTS,
+        include_package_data=True,
+        install_requires=REQUIREMENTS,
+        keywords=' '.join(KEYWORDS),
+        license=LICENSE,
+        long_description=README,
+        name=NAME,
+        packages=PACKAGES,
+        setup_requires=SETUP_REQUIREMENTS,
+        url=URL,
+        version='1.0.3',
+        zip_safe=False,
+    )
