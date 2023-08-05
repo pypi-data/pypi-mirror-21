756ad702ea374760958412efa215fe0eb8bd3a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package swipt/0.1.0/swipt-0.1.0.tar.gz

---
+++ packages/swipt/swipt-0.1.0.tar.gz/swipt-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='swipt',
+    packages=find_packages(),
+    version='0.1.0',
+    description='StoneWork Is Pedro Ten',
+    author='StoneWork Solutions',
+    author_email='dev@stoneworksolutions.net',
+    url='https://github.com/stoneworksolutions/swipt',
+    download_url='https://github.com/stoneworksolutions/swipt/tarball/0.1.0',
+    keywords=['swipt', 'pedroten', 'stonework'],
+    classifiers=[],
+    install_requires=[],
+)
