49b1b0e65a6f55aeb30236ea5bc14d2f8aee3f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:00 +0000

    Add package setmeup/0.1/setmeup-0.1.tar.gz

---
+++ packages/setmeup/setmeup-0.1.tar.gz/setmeup-0.1/setmeup.py
@@ -0,0 +1,15 @@
+import os
+
+
+def from_env(cls):
+    """A class decorator to populate fields from os env"""
+    for k, default in cls.__dict__.items():
+        if not k.startswith("_"):
+            vtype = type(default)
+            val = os.environ.get(k.upper(), default)
+
+            if vtype == bool and val != default:
+                val = False if val == "0" else True
+
+            setattr(cls, k, vtype(val))
+    return cls
---
+++ packages/setmeup/setmeup-0.1.tar.gz/setmeup-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='setmeup',
+    version='0.1',
+    description='Runtime settings for minimalists',
+    url='https://github.com/szastupov/setmeup',
+    license='MIT',
+    author='Stepan Zastupov',
+    author_email='stepan.zastupov@gmail.com',
+
+    py_modules=['setmeup'],
+
+    keywords='environ settings env config',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
