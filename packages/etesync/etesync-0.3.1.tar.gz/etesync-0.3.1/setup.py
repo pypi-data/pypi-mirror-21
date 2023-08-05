c9707625be87ebdcfe6aa7ee566c3b0fc5381588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:46 +0000

    Add package etesync/0.3.1/etesync-0.3.1.tar.gz

---
+++ packages/etesync/etesync-0.3.1.tar.gz/etesync-0.3.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='etesync',
+    version='0.3.1',
+    author='EteSync',
+    author_email='development@etesync.com',
+    url='https://github.com/etesync/pyetesync',
+    description='Python client library for EteSync',
+    keywords=['etesync', 'encryption', 'sync', 'pim'],
+    license='LGPL',
+    long_description=open('DESCRIPTION.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+       'appdirs>=1.4',
+       'furl>=0.5',
+       'orderedmultidict>=0.7',
+       'packaging>=16.8',
+       'peewee>=2.9',
+       'pyaes>=1.6',
+       'pyparsing>=2.2',
+       'pyscrypt>=1.6',
+       'python-dateutil>=2.6',
+       'six>=1.10',
+       'vobject>=0.9',
+    ]
+)
