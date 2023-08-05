b194d2ebcabee8ad19389f3bbfe24a5e95e8fb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:54 +0000

    Add package acpibacklight/0.1.3/acpibacklight-0.1.3.tar.gz

---
+++ packages/acpibacklight/acpibacklight-0.1.3.tar.gz/acpibacklight-0.1.3/acpibacklight/__init__.py
@@ -0,0 +1,1 @@
+from .acpibacklight import AcpiBacklightControl
---
+++ packages/acpibacklight/acpibacklight-0.1.3.tar.gz/acpibacklight-0.1.3/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name='acpibacklight',
+    version='0.1.3',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=['PyTweening==1.0.3'],
+
+    entry_points={
+        'console_scripts': [
+            'acpi-ease-backlight = acpibacklight.cli:backlight_cli'
+        ],
+    },
+
+    author='Aaron Abbott',
+    author_email='aabmass@gmail.com',
+    description='Library and script for changing brightness on Linux via acpi. '
+                'Allows for easing animations too!',
+
+    keywords='acpi brightness backlight easing animation',
+    long_description=read('README.rst'),
+)
