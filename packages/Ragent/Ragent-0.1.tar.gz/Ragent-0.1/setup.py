80865054593f9924a4cdd8400010665972274e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:55 +0000

    Add package Ragent/0.1/Ragent-0.1.tar.gz

---
+++ packages/Ragent/Ragent-0.1.tar.gz/Ragent-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Ragent',
+    version='0.1',
+    packages=[''],
+    url='http://www.github.com/ExitStars',
+    license='GPL',
+    author='ExitStars',
+    author_email='exitstars@protonmail.com',
+    description='User-Agent and Proxy'
+)
