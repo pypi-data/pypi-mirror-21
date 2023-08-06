cbaa169850883d6d0f5752429648d850c7077644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:08 +0000

    Add package xphyle/3.0.0/xphyle-3.0.0.tar.gz

---
+++ packages/xphyle/xphyle-3.0.0.tar.gz/xphyle-3.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import sys
+
+requirements = []
+
+if sys.version_info < (3, 3):
+    sys.stdout.write("At least Python 3.3 is required.\n")
+    sys.exit(1)
+elif sys.version_info < (3, 5):
+    # typing was added in 3.5, but a backport is avaialble for 3.3+
+    requirements.append('typing')
+
+import versioneer
+
+setup(
+    name='xphyle',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Utilities for working with files.',
+    url='https://github.com/jdidion/xphyle',
+    author='John Didion',
+    author_email='john.didion@nih.gov',
+    license='Public Domain',
+    packages = ['xphyle'],
+    install_requires = requirements,
+    tests_require = ['pytest', 'pytest-cov'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
