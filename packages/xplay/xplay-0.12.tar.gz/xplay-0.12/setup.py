c98e30817fcd916354fb1dc4451c9446c5d52311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:33 +0000

    Add package xplay/0.12/xplay-0.12.tar.gz

---
+++ packages/xplay/xplay-0.12.tar.gz/xplay-0.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name="xplay",
+      version="0.12",
+      description="play any song from terminal",
+      url="http://github.com/zuck007/xplay",
+      author="Pradeep Khileri",
+      author_email="pradeepchoudhary009@gmail.com",
+      license="MIT",
+      scripts=["./bin/xplay"],
+      keywords='music song-tags metadata unix linux script cli-tool',
+      install_requires=[
+          "musicquery",
+          "colorama",
+      ],
+      zip_safe=False)
+
