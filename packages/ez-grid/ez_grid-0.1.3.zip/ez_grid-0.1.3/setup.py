bf5bf436c3e41008f6ca0a57559878c70a994bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:37 +0000

    Add package ez-grid/0.1.3/ez_grid-0.1.3.zip

---
+++ packages/ez-grid/ez_grid-0.1.3.zip/ez_grid-0.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ez_grid',
+      packages=['ez_grid'],
+      version='0.1.3',
+      description="""A lightweight convenient grid class
+
+ez_grid provides the class Grid that allows for the construction and manipulation of 2D gridded structures.
+
+
+No need to install the whole of Pandas for this simple task!""",
+      author='0Hughman',
+      author_email="rammers2@hotmail.co.uk",
+      license="MIT",
+      url='https://github.com/0Hughman0/ez_grid',
+      download_url='https://github.com/0Hughman0/ez_grid/tarball/0.1.3',
+      keywords=["grid", "config", "lightweight", "no dependencies", "configuration", "csv", "config"],
+      classifiers=[]
+     )
