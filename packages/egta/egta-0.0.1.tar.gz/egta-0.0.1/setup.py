5069ed6c237e5eef66f494ce3db74671b4b340fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:01 +0000

    Add package egta/0.0.1/egta-0.0.1.tar.gz

---
+++ packages/egta/egta-0.0.1.tar.gz/egta-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name='egta',
+    version='0.0.1',
+    description='Scripts to perform EGTA',
+    url='https://github.com/egtaonline/quiesce.git',
+    author='Strategic Reasoning Group',
+    author_email='strategic.reasoning.group@umich.edu',
+    license='Apache 2.0',
+    entry_points=dict(console_scripts=['egta=egta.egta:main']),
+    install_requires=[
+        'gameanalysis~=3.1',
+        'egtaonlineapi~=0.1',
+        'numpy~=1.12',
+        'tabulate~=0.7',
+    ],
+    packages=['egta', 'egta.script'],
+)
