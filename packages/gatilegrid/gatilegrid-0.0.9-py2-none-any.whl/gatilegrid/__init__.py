0cbb7925395f64395b7686d59bd22d5f1df9952f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:16 +0000

    Add package gatilegrid/0.0.9/gatilegrid-0.0.9-py2-none-any.whl

---
+++ packages/gatilegrid/gatilegrid-0.0.9-py2-none-any.whl/gatilegrid/__init__.py
@@ -0,0 +1,13 @@
+from .tilegrids import GeoadminTileGridLV03, GeoadminTileGridLV95
+from .tilegrids import GlobalMercatorTileGrid
+from .grid import Grid
+
+
+def getTileGrid(srs):
+    assert srs in (21781, 2056, 3857), 'Unsupported tile grid'
+    if srs == 21781:
+        return GeoadminTileGridLV03
+    elif srs == 2056:
+        return GeoadminTileGridLV95
+    elif srs == 3857:
+        return GlobalMercatorTileGrid
