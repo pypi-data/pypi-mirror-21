cd42794bb6b617e0209e3390a557fa602d038955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:15 +0000

    Add package gossip/2.2.0/gossip-2.2.0.tar.gz

---
+++ packages/gossip/gossip-2.2.0.tar.gz/gossip-2.2.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "gossip", "__version__.py")) as version_file:
+    exec(version_file.read())  # pylint: disable=W0122
+
+_INSTALL_REQUIERS = [
+    'logbook>=0.12.0',
+    'vintage',
+    'sentinels',
+]
+
+setup(name="gossip",
+      classifiers=[
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+      ],
+      description="Signaling and hooking library",
+      license="BSD3",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      version=__version__,  # pylint: disable=E0602
+      packages=find_packages(exclude=["tests"]),
+
+      url="https://github.com/vmalloc/gossip",
+
+      install_requires=_INSTALL_REQUIERS,
+      scripts=[],
+      namespace_packages=[]
+     )
