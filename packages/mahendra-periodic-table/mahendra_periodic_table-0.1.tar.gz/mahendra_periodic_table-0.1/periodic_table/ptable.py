cc52c2ad7cea0bad99c3565090e8a3dd8583c572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package mahendra-periodic-table/0.1/mahendra_periodic_table-0.1.tar.gz

---
+++ packages/mahendra-periodic-table/mahendra_periodic_table-0.1.tar.gz/mahendra_periodic_table-0.1/periodic_table/ptable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+ptable_data = {
+    "H": "Hydrogen",
+    "O": "Oxygen",
+    "Zn": "Zink"
+}
+
+
+def get_name(symb):
+    return ptable_data.get(symb, None)
+
+
+def get_symb(name):
+    for item in ptable_data:
+        if ptable_data[item] == name:
+            return item
+    return None
---
+++ packages/mahendra-periodic-table/mahendra_periodic_table-0.1.tar.gz/mahendra_periodic_table-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='mahendra_periodic_table',
+      version='0.1',
+      description='chemistry symbols and name',
+      url='',
+      author='mahendra',
+      author_email='mahendra.k12@gmail.com',
+      license='MIT',
+      packages=['periodic_table'],
+      zip_safe=False)
