9fd7e817cec4b12d777d5740c8cb149636dbfdba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:46 +0000

    Add package bitfusion/0.0.12/bitfusion-0.0.12.tar.gz

---
+++ packages/bitfusion/bitfusion-0.0.12.tar.gz/bitfusion-0.0.12/bitfusion/api/__init__.py
@@ -0,0 +1,5 @@
+from bitfusion.api.gpu import GpuFactory
+from bitfusion.api.node import NodeFactory
+from bitfusion.api.volume import VolumeFactory
+from bitfusion.api.workspace import WorkspaceFactory
+from bitfusion.api.user import UserFactory
---
+++ packages/bitfusion/bitfusion-0.0.12.tar.gz/bitfusion-0.0.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+INSTALL_DEPS = [
+  'python-dateutil >= 2.6.0',
+  'pytz >= 2016.10',
+  'requests >= 2.13.0',
+]
+
+TEST_DEPS = [
+  'mock',
+  'pytest',
+]
+
+setup(
+  name='bitfusion',
+  version='0.0.12',
+  description='Python SDK for developing against the Bitfusion Platform',
+  packages=find_packages(),
+  author='Brian Schultz',
+  author_email='brian@bitfusion.io',
+  url='http://www.bitfusion.io',
+  install_requires=INSTALL_DEPS,
+  tests_require=TEST_DEPS,
+  extras_require={'test': TEST_DEPS},
+  setup_requires=['pytest-runner'],
+)
