f879e070b17ef99cb95b8509600ee3a82cab360b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package pyoptflow/1.0.0/pyoptflow-1.0.0.tar.gz

---
+++ packages/pyoptflow/pyoptflow-1.0.0.tar.gz/pyoptflow-1.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+req = ['nose','pillow','scipy','numpy','matplotlib','pathlib2']
+# %%
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:
+    import pip
+    pip.main(['install'] + req)
+# %%
+from setuptools import setup
+
+setup(name='pyoptflow',
+      packages=['pyoptflow'],
+      author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scivision/pyoptflow',
+      version='1.0.0',
+      classifiers=[
+      'Topic :: Scientific/Engineering',
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python :: 3',
+      ],
+      description='Pure Python optical flow: Horn-Schunck, Lucas-Kanade',
+	  )
