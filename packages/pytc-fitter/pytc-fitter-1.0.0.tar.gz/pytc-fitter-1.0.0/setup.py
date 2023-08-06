20e097fa08a894fa78b19a4ea384e054815f0d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:45 +0000

    Add package pytc-fitter/1.0.0/pytc-fitter-1.0.0.tar.gz

---
+++ packages/pytc-fitter/pytc-fitter-1.0.0.tar.gz/pytc-fitter-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import sys
+
+if sys.version_info[0] < 3:
+    sys.exit('Sorry, Python < 3.x is not supported')
+
+# Try using setuptools first, if it's installed
+from setuptools import setup, find_packages
+
+# Need to add all dependencies to setup as we go!
+setup(name='pytc-fitter',
+      packages=find_packages(),
+      version='1.0.0',
+      description="Python software package for analyzing Isothermal Titration Calorimetry data",
+      long_description=open("README.rst").read(),
+      author='Michael J. Harms',
+      author_email='harmsm@gmail.com',
+      url='https://github.com/harmslab/pytc',
+      download_url='https://github.com/harmslab/pytc/tarball/0.1.3',
+      zip_safe=False,
+      install_requires=["matplotlib","scipy","numpy","pandas"],
+      classifiers=['Programming Language :: Python'])
+
