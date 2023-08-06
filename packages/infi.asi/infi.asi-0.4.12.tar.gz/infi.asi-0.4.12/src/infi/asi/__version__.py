e446edf35b47d0fe473d9b4d6c593bcf5bdb6147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:27 +0000

    Add package infi.asi/0.4.12/infi.asi-0.4.12.tar.gz

---
+++ packages/infi.asi/infi.asi-0.4.12.tar.gz/infi.asi-0.4.12/src/infi/asi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.12"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.4.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2d3d6c9c001e379f011a9854f11492294d6cd102'
+__git_head_subject__ = u'Finished Release v0.4.12'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-04-24 11:29:01'
