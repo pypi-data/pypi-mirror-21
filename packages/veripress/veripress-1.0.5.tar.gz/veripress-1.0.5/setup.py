2edfa2d046a7359ac8275714690d9c8036661a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:44 +0000

    Add package veripress/1.0.5/veripress-1.0.5.tar.gz

---
+++ packages/veripress/veripress-1.0.5.tar.gz/veripress-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='veripress',
+    version='1.0.5',
+    packages=find_packages(),
+    url='https://github.com/veripress/veripress',
+    license='MIT License',
+    author='Richard Chien',
+    author_email='richardchienthebest@gmail.com',
+    description='A blog engine for hackers.',
+    install_requires=[
+        'Flask', 'Flask-Caching', 'PyYAML', 'mistune', 'Pygments'
+    ],
+    include_package_data=True,
+    platforms='any',
+    entry_points=dict(
+        console_scripts=['veripress=veripress_cli:main']
+    )
+)
---
+++ packages/veripress/veripress-1.0.5.tar.gz/veripress-1.0.5/veripress_cli/__init__.py
@@ -0,0 +1,15 @@
+import click
+
+cli = click.Group(help='This is a blog engine for hackers. '
+                       'You can use this to serve a blog, a wiki or anything else you like.')
+
+
+def main():
+    cli.main()
+
+
+import veripress_cli.init
+import veripress_cli.serve
+import veripress_cli.theme
+import veripress_cli.generate
+import veripress_cli.deploy
