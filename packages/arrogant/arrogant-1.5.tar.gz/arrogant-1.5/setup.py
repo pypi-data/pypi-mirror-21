385c87eaac115833426276bb7fd9b7e86eb3290d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:54 +0000

    Add package arrogant/1.5/arrogant-1.5.tar.gz

---
+++ packages/arrogant/arrogant-1.5.tar.gz/arrogant-1.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'arrogant',
+    packages = ['arrogant'],
+    version = '1.5',
+    description = 'An API for 企業徵才.',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/Stufinite/arrogant',
+    download_url = 'https://github.com/Stufinite/arrogant/archive/v1.5.tar.gz',
+    keywords = ['arrogant', 'campass'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+        'djangoApiDec',
+    ],
+    zip_safe=True,
+)
