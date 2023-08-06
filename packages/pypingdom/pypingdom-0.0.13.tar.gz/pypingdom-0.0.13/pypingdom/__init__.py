39ed42d6868caffd8d91f7e18d704a36060b696b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:22 +0000

    Add package pypingdom/0.0.13/pypingdom-0.0.13.tar.gz

---
+++ packages/pypingdom/pypingdom-0.0.13.tar.gz/pypingdom-0.0.13/pypingdom/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'Paolo Sechi <sekipaolo@gmail.com>'
+__license__ = "Apache v2.0"
+
+from pypingdom.client import Client
+from pypingdom.check import Check
+from pypingdom.maintenance import Maintenance
