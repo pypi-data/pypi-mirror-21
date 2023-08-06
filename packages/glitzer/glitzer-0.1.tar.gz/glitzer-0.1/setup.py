8598bc2b5f63db1313d90df99b1ae8606e369917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:56 +0000

    Add package glitzer/0.1/glitzer-0.1.tar.gz

---
+++ packages/glitzer/glitzer-0.1.tar.gz/glitzer-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'glitzer',
+  packages = ['glitzer'],
+  version = '0.1',
+  description = 'Lay emphasis on desired string by adding pretty and customizable borders',
+  author = 'Mayowa Falade',
+  author_email = 'falademayowa240@gmail.com',
+  url = 'https://github.com/mfalade/glitzer',
+  download_url = 'https://github.com/mfalade/glitzer/archive/0.1.tar.gz',
+  keywords = ['border', 'print', 'printing', 'log', 'pretty'],
+  classifiers = [],
+)
