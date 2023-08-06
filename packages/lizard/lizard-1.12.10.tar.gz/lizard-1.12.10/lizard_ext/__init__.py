5e288f4aef5f8efd35e47b5206132fbeef3d3511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:21 +0000

    Add package lizard/1.12.10/lizard-1.12.10.tar.gz

---
+++ packages/lizard/lizard-1.12.10.tar.gz/lizard-1.12.10/lizard_ext/__init__.py
@@ -0,0 +1,11 @@
+''' extensions of lizard '''
+
+from __future__ import print_function
+from .htmloutput import html_output
+from .xmloutput import xml_output
+from .auto_open import auto_open, auto_read
+
+
+def print_xml(results, options, _):
+    print(xml_output(list(results), options.verbose))
+    return 0
