8f44840faa5fb0573e060dee829d6acb63a51d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:54 +0000

    Add package funnykulka/0.2/funnykulka-0.2.tar.gz

---
+++ packages/funnykulka/funnykulka-0.2.tar.gz/funnykulka-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup 
+
+setup(name='funnykulka',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/GanadiniAkshay',
+      author='Akshay Kulkarni',
+      author_email='akshaykulkarni.2104@gmail.com',
+      license='MIT',
+      packages=['funnykulka'],
+      install_requires=[
+        'markdown'
+      ],
+      zip_safe=False
+)
