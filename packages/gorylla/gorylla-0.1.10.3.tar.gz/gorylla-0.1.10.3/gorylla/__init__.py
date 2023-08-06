fda805d5b64b71b582611b332a0f5c7a155c8720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package gorylla/0.1.10.3/gorylla-0.1.10.3.tar.gz

---
+++ packages/gorylla/gorylla-0.1.10.3.tar.gz/gorylla-0.1.10.3/gorylla/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1.1'
+__author__ = 'Boyang'
+"""
+for get tick data
+"""
+from . import mis
+from . import tick
+from . import future
+from . import ex
+from .mis import *
+from .tick import *
+from .future import *
+from .ex import *
+from gorylla import (mis,tick,future,ex)
+
