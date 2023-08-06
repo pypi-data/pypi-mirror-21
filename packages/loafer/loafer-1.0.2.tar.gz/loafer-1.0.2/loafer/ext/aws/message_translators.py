e044fa299bead363dd87b8c40f168ea05cec5c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package loafer/1.0.2/loafer-1.0.2.tar.gz

---
+++ packages/loafer/loafer-1.0.2.tar.gz/loafer-1.0.2/loafer/ext/aws/message_translators.py
@@ -0,0 +1,52 @@
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SQSMessageTranslator:
+
+    def translate(self, message):
+        translated = {'content': None, 'metadata': {}}
+        try:
+            body = message['Body']
+        except (KeyError, TypeError):
+            logger.error('missing Body key in SQS message. It really came from SQS ?'
+                         '\nmessage={!r}'.format(message))
+            return translated
+
+        try:
+            translated['content'] = json.loads(body)
+        except json.decoder.JSONDecodeError as exc:
+            logger.error('error={!r}, message={!r}'.format(exc, message))
+            return translated
+
+        message.pop('Body')
+        translated['metadata'].update(message)
+        return translated
+
+
+class SNSMessageTranslator:
+
+    def translate(self, message):
+        translated = {'content': None, 'metadata': {}}
+        try:
+            body = json.loads(message['Body'])
+            message_body = body.pop('Message')
+        except (KeyError, TypeError):
+            logger.error(
+                'Missing Body or Message key in SQS message. It really came from SNS ?'
+                '\nmessage={!r}'.format(message))
+            return translated
+
+        translated['metadata'].update(message)
+        translated['metadata'].pop('Body')
+
+        try:
+            translated['content'] = json.loads(message_body)
+        except (json.decoder.JSONDecodeError, TypeError) as exc:
+            logger.error('error={!r}, message={!r}'.format(exc, message))
+            return translated
+
+        translated['metadata'].update(body)
+        return translated
