9969a8a5d308901d50aec7b78542cc8270f80aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:09 +0000

    Add package django-instant/0.3.4/django-instant-0.3.4.tar.gz

---
+++ packages/django-instant/django-instant-0.3.4.tar.gz/django-instant-0.3.4/instant/templates/instant/js/handlers.js
@@ -0,0 +1,8 @@
+{% load instant_tags %}
+
+function handlers_for_event(event_class, channel, message, data, site, uid) {
+	/* ex: if ( channel == "{% get_public_channel %}" ) {
+		console.log(message);
+	}*/
+	{% include "instant/extra_handlers.js" %}
+}
