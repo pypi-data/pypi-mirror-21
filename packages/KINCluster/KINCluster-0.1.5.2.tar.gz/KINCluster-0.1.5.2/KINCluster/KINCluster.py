7b7f878d607ae0d706f2114f9895adccffe9d735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:36 +0000

    Add package KINCluster/0.1.5.2/KINCluster-0.1.5.2.tar.gz

---
+++ packages/KINCluster/KINCluster-0.1.5.2.tar.gz/KINCluster-0.1.5.2/KINCluster/KINCluster.py
@@ -0,0 +1,21 @@
+from KINCluster.core.cluster import Cluster 
+from KINCluster.core.pipeline import Pipeline 
+from KINCluster.core.extractor import Extractor 
+from KINCluster.lib.tokenizer import tokenize
+from KINCluster import settings
+
+class KINCluster:
+    def __init__(self, pipeline, cluster=Cluster, Extractor=Extractor):
+        self.pipeline = pipeline
+        self.cluster = Cluster()
+        self.Extractor = Extractor
+
+    def run(self):
+        for item in self.pipeline.capture_item():
+            self.cluster.put_item(item)
+        self.cluster.cluster()
+
+        extractor = self.Extractor(self.cluster)
+        for idx, dump in enumerate(self.cluster.dumps):
+            ext = extractor.dump(idx)
+            self.pipeline.dress_item(ext)
