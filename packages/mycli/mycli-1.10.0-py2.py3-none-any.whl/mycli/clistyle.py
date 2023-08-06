31fcec4f41efa499309ca96be90130233c02cd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:20 +0000

    Add package mycli/1.10.0/mycli-1.10.0-py2.py3-none-any.whl

---
+++ packages/mycli/mycli-1.10.0-py2.py3-none-any.whl/mycli/clistyle.py
@@ -0,0 +1,19 @@
+from pygments.token import string_to_tokentype
+from pygments.util import ClassNotFound
+from prompt_toolkit.styles import default_style_extensions, style_from_dict
+import pygments.styles
+
+
+def style_factory(name, cli_style):
+    try:
+        style = pygments.styles.get_style_by_name(name)
+    except ClassNotFound:
+        style = pygments.styles.get_style_by_name('native')
+
+    styles = {}
+    styles.update(style.styles)
+    styles.update(default_style_extensions)
+    custom_styles = {string_to_tokentype(x): y for x, y in cli_style.items()}
+    styles.update(custom_styles)
+
+    return style_from_dict(styles)
