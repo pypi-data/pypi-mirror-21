eb2bbf29f4b42e46f826bec75bc9a2df4d9fcb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package otree-redwood/0.1.2/otree-redwood-0.1.2.tar.gz

---
+++ packages/otree-redwood/otree-redwood-0.1.2.tar.gz/otree-redwood-0.1.2/otree_redwood/otree_extensions/admin.py
@@ -0,0 +1,7 @@
+from otree_redwood import views
+
+# NOTE: otree_extensions is part of
+# otree-core's private API, which may change at any time.
+data_export_views = [
+	views.ExportEvents,
+]
---
+++ packages/otree-redwood/otree-redwood-0.1.2.tar.gz/otree-redwood-0.1.2/otree_redwood/views.py
@@ -0,0 +1,43 @@
+import csv
+import datetime
+import functools
+import operator
+import vanilla
+
+from django.http import HttpResponse
+from django.core import serializers
+
+from otree_redwood.models import Event
+
+
+class ExportEvents(vanilla.View):
+
+    url_name = 'otree_redwood_export_events'
+    url_pattern = '^{}/$'.format(url_name)
+    display_name = 'oTree-Redwood extension raw events file.'
+
+    def get(request, *args, **kwargs):
+
+        response = HttpResponse(content_type='application/json')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+            'Events (accessed {}).json'.format(
+                datetime.date.today().isoformat()
+            )
+        )
+
+        events = Event.objects.all()
+
+        response.write(serializers.serialize('json', events).encode('utf-8'))
+
+        return response
+
+
+class DebugView(vanilla.TemplateView):
+
+    url_name = 'otree_redwood_export_events'
+    url_pattern = r"^redwood-debug/$"
+    template_name = 'otree_redwood/Debug.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
