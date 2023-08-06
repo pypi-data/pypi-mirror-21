f54348cf56d2bcda70c857e692679bc763025615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:31 +0000

    Add package pyborgeous/0.5.0/pyborgeous-0.5.0.tar.gz

---
+++ packages/pyborgeous/pyborgeous-0.5.0.tar.gz/pyborgeous-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyborgeous',
+    version='0.5.0',
+    description='Pyborgeous is an implementation of Jorge Luis Borges\' Library of Babel',
+    long_description=long_description,
+    url='https://github.com/Spacehug/pyborgeous',
+    author='Spacehug',
+    author_email='spacehug.o0@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6.1',
+    ],
+    keywords='Gorge Luis Borges Library of Babel unicode',
+    packages=find_packages(),
+    install_requires=['markovify'],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={'console_scripts': ['pyborgeous=pyborgeous:main']},
+)
