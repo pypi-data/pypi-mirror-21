bcd28ec81abb08ff4f3c7bda641b9b7856e3adcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:11 +0000

    Add package pyminc/0.51/pyminc-0.51.tar.gz

---
+++ packages/pyminc/pyminc-0.51.tar.gz/pyminc-0.51/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pyminc',
+      version = '0.51',
+      description = "Python interface to libminc",
+      url = "https://github.com/Mouse-Imaging-Centre/pyminc",
+      author = "Jason Lerch",
+      author_email = "jason@mouseimaging.ca",
+      license = "BSD",
+      packages = find_packages(),
+      install_requires = ["numpy"],
+      scripts = ["scripts/sva2mnc.py", "scripts/pyminc_test2.py"],
+      test_suite="test",
+      )
