e0a4a0b0f0f88fa112933840657ffdfd4019b5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:29 +0000

    Add package pvtk/0.0.0/pvtk-0.0.0.tar.gz

---
+++ packages/pvtk/pvtk-0.0.0.tar.gz/pvtk-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'pvtk',
+      description = 'precipitation verification toolkit',
+      url = 'https://pypi.python.org/pypi',
+      author = 'Fan Han',
+      author_email = 'hanfan5598@gmail.com',
+      packages = ['pvtk'],
+      )
+
