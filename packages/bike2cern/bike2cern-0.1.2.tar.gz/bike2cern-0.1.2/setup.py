eee39ae089e40ef99f5dec822d3372cb30f19681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:58 +0000

    Add package bike2cern/0.1.2/bike2cern-0.1.2.tar.gz

---
+++ packages/bike2cern/bike2cern-0.1.2.tar.gz/bike2cern-0.1.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='bike2cern',
+    version='0.1.2',
+    description='Sync rides from Strava to CERN Sharepoint',
+    author='Matthias Wolf',
+    author_email='m@sushinara.net',
+    url='https://github.com/matz-e/bike2cern',
+    download_url='https://github.com/matz-e/bike2cern/archive/0.1.2.tar.gz',
+    keywords=['strava'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['bike2cern'],
+    install_requires=[
+        'keyring',
+        'pyxdg',
+        'sharepoint',
+        'stravalib'
+    ],
+    entry_points={
+        'console_scripts': ['bike2cern = bike2cern.sync:run']
+    }
+)
