bad165f46ff2e6cfb94f1d0b1c716dcbddae1822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:27 +0000

    Add package chapter7/1.0.0/chapter7-1.0.0.tar.gz

---
+++ packages/chapter7/chapter7-1.0.0.tar.gz/chapter7-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='chapter7',
+        version='1.0.0',
+        py_modules=['web20170415'],
+        author='Jasper',
+        author_email='jasper.wj@foxmail.com',
+        url='https://github.com/Jasperzhuangxin/jaspergit',
+        description='create a web app',
+	)
