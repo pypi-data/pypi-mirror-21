bd65b273d968473ad31d3d59729d0f19e8e5723a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:26 +0000

    Add package AISTLAB_nitrotyper/0.6.10/AISTLAB_nitrotyper-0.6.10-py2.py3-none-any.whl

---
+++ packages/AISTLAB_nitrotyper/AISTLAB_nitrotyper-0.6.10-py2.py3-none-any.whl/nitrotyper/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# https://www.nitrotype.com/race auto typer
+# python 3.5 + opencv 3.2 + pyautogui
+# winxos
+# since:2017-03-17
+
+__version__ = "0.6.10"
+__title__ = "AISTLAB_nitrotyper"
+__description__ = "https://www.nitrotype.com auto typer"
+__long_description__ = open('README.rst').read()
+__url__ = "https://github.com/winxos/nitrotyper"
+__docsurl__ = ""
+__author__ = "winxos"
+__email__ = "winxos@hotmail.com"
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) AISTLAB"
