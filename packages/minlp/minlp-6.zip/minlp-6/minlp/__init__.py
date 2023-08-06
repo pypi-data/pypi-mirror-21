ac2561d217b2fc4cec61c7ffe546ce53a188649e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:36 +0000

    Add package minlp/6/minlp-6.zip

---
+++ packages/minlp/minlp-6.zip/minlp-6/minlp/__init__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    from minlp.infile_one import myfunc
+
+    myfunc()
+    print('after my func!!')
---
+++ packages/minlp/minlp-6.zip/minlp-6/minlp/infile_one.py
@@ -0,0 +1,8 @@
+HELLO_WORLD_MESSAGE = 'Hello world! PyOhio Demo - 3! CLEpy'
+def get_message():
+    return HELLO_WORLD_MESSAGE
+
+def myfunc():
+	print("hello world2")
+	print("this is my func")
+	print(get_message())
