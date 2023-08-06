819af89d20f7d8af351d1bf060e753e2c845c4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:49 +0000

    Add package homeapi/0.1.0/homeapi-0.1.0.tar.gz

---
+++ packages/homeapi/homeapi-0.1.0.tar.gz/homeapi-0.1.0/README.txt
@@ -0,0 +1,1 @@
+HomeAPI - An API, for your home.
---
+++ packages/homeapi/homeapi-0.1.0.tar.gz/homeapi-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='homeapi',
+    version='0.1.0',
+    author='Aaron Coffey',
+    author_email='aaron+pypi@coffey.works',
+    url='https://aaron.coffey.works/src',
+    packages=['homeapi'],
+    license='LICENSE.txt',
+    description='Just another home-automation API.',
+    long_description=open('README.txt').read(),
+)
