0850df5f562fe355184f728986c73d86ff05b743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:56 +0000

    Add package nodeconductor-openstack/0.23.0/nodeconductor-openstack-0.23.0.tar.gz

---
+++ packages/nodeconductor-openstack/nodeconductor-openstack-0.23.0.tar.gz/nodeconductor-openstack-0.23.0/src/nodeconductor_openstack/openstack_tenant/extension.py
@@ -0,0 +1,53 @@
+from nodeconductor.core import NodeConductorExtension
+
+
+class OpenStackTenantExtension(NodeConductorExtension):
+
+    class Settings:
+        NODECONDUCTOR_OPENSTACK_TENANT = {
+            'MAX_CONCURRENT_PROVISION': {
+                'OpenStack.Instance': 4,
+                'OpenStack.Volume': 4,
+                'OpenStack.Snapshot': 4,
+            },
+        }
+
+    @staticmethod
+    def django_app():
+        return 'nodeconductor_openstack.openstack_tenant'
+
+    @staticmethod
+    def rest_urls():
+        from .urls import register_in
+        return register_in
+
+    @staticmethod
+    def celery_tasks():
+        from datetime import timedelta
+        return {
+            'openstacktenant-schedule-backups': {
+                'task': 'openstack_tenant.ScheduleBackups',
+                'schedule': timedelta(minutes=10),
+                'args': (),
+            },
+            'openstacktenant-delete-expired-backups': {
+                'task': 'openstack_tenant.DeleteExpiredBackups',
+                'schedule': timedelta(minutes=10),
+                'args': (),
+            },
+            'openstacktenant-schedule-snapshots': {
+                'task': 'openstack_tenant.ScheduleSnapshots',
+                'schedule': timedelta(minutes=10),
+                'args': (),
+            },
+            'openstacktenant-delete-expired-snapshots': {
+                'task': 'openstack_tenant.DeleteExpiredSnapshots',
+                'schedule': timedelta(minutes=10),
+                'args': (),
+            },
+            'openstacktenant-set-erred-stuck-resources': {
+                'task': 'openstack_tenant.SetErredStuckResources',
+                'schedule': timedelta(minutes=10),
+                'args': (),
+            },
+        }
