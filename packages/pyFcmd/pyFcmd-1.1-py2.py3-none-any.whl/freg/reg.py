5cb7c26a653e333b1498a1be4a347f043c5fb9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:48 +0000

    Add package pyFcmd/1.1/pyFcmd-1.1-py2.py3-none-any.whl

---
+++ packages/pyFcmd/pyFcmd-1.1-py2.py3-none-any.whl/freg/reg.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import os,sys
+from neko import color_str, ProcBar, Ssh
+
+class RegUser:
+
+	def __init__(self, debug=False):
+		self.__ssh = None
+		self.__debug = debug
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_value, exc_tb):
+		if exc_tb:
+			return False
+		else:
+			self.__del__()
+
+	def __del__(self):
+		self.__ssh = None
+
+	def login(self, host='', user='root', password=''):
+		self.__host = host
+		self.__user = user
+		self.__password = password
+
+		try:
+			if self.__debug:
+				p = ProcBar().start("logining server %s@%s ..." % (user, host))
+			self.__ssh = Ssh(host, username=user, password=password)
+			if self.__debug:
+				p.stop(color_str("OK", "green"))
+		except Exception as err:
+			if 'p' in locals().keys():
+				p and p.stop(color_str(str(err), "red"))
+			raise Exception(str(err))
+			return False
+
+		return True
+
+	def run(self, profile='internal', number=''):
+		if self.__debug:
+			p = ProcBar().start("searching number %s on profile %s ..." % (color_str(number or "[all]" , "sky_blue"), color_str(profile, "yellow")))
+		try:
+			stdin, self.__out, stderr = self.__ssh.exec_command('/usr/local/freeswitch/bin/fs_cli -x "sofia status profile %s reg %s"' % (profile, number))
+		except Exception as err:
+			if 'p' in locals().keys():
+				p and p.stop(color_str(str(err), "red"))
+			return False
+		if self.__debug:
+			p.stop(color_str("OK", "green"))
+
+	def show(self):
+		for l in self.__out.readlines():
+			sys.stdout.write(l)
