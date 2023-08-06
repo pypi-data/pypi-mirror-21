4b7b7a8fbc42685fc3f1665e2f4fc9c112a404ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:33 +0000

    Add package pymultinest/2.2/pymultinest-2.2.tar.gz

---
+++ packages/pymultinest/pymultinest-2.2.tar.gz/pymultinest-2.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+long_description = None
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name = "pymultinest",
+    version = "2.2",
+    description = "Access modules for the MultiNest, APEMoST, Cuba and PolyChord integration libraries",
+    author = "Johannes Buchner",
+    author_email = "johannes.buchner.acad@gmx.com",
+    maintainer = "Johannes Buchner",
+    maintainer_email = "johannes.buchner.acad@gmx.com",
+    url = "http://johannesbuchner.github.com/PyMultiNest/",
+    license = "GPLv3",
+    packages = ["pymultinest", "pyapemost", "pycuba", "pypolychord"],
+    provides = ["pymultinest", "pyapemost", "pycuba", "pypolychord"],
+    requires = ["ctypesGsl", "numpy (>=1.5)", "matplotlib", "scipy"],
+    scripts=['multinest_marginals.py'],
+    long_description=long_description,
+)
+
