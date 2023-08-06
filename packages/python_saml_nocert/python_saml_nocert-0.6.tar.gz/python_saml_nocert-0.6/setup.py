0a69cbcc64e6b3f58b041b119e87ada80e2f34a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:43 +0000

    Add package python_saml_nocert/0.6/python_saml_nocert-0.6.tar.gz

---
+++ packages/python_saml_nocert/python_saml_nocert-0.6.tar.gz/python_saml_nocert-0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+      name = 'python_saml_nocert',
+      packages = ['onelogin', 'onelogin/saml2', 'onelogin/saml2/schemas'], # this must be the same as the name above
+      version = '0.6',
+      description = 'Same as Python Saml removing Certitifcatie Validation',
+      author = 'Abhishek K',
+      author_email = 'abhishek.k6006@gmail.com',
+      url = 'https://github.com/abhishek246/python-saml.git', # use the URL to the github repo
+      download_url = 'https://github.com/abhishek246/python-saml.git/archive/0.6.tar.gz', # I'll explain this in a second
+      keywords = ['SSO', 'logging', 'example'], # arbitrary keywords
+      classifiers = [],
+
+)
