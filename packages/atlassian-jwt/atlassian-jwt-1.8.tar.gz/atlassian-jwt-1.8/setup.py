3faec47b80e8bab4edf17d86255451bc2cf25fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:41 +0000

    Add package atlassian-jwt/1.8/atlassian-jwt-1.8.tar.gz

---
+++ packages/atlassian-jwt/atlassian-jwt-1.8.tar.gz/atlassian-jwt-1.8/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = ''
+
+setup(
+    name = 'atlassian-jwt',
+    packages = ['atlassian_jwt'],
+    version = '1.8',
+    description = 'JSON web token: pyjwt plus Atlassian query-string-hash claim',
+    long_description=long_description,
+    author = 'Atlassian',
+    author_email = 'bedwards@atlassian.com',
+    url = 'https://bitbucket.org/atlassian/atlassian-jwt-py',
+    keywords = [
+        'jwt',
+        'json',
+        'web',
+        'token',
+        'pyjwt',
+        'atlassian',
+        'connect',
+        'addon',
+        'query',
+        'string',
+        'hash',        
+        'qsh',
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
