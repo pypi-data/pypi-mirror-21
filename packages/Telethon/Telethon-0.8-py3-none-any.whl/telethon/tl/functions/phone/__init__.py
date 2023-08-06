206afa2c7f106c1770746ab0768ab89252a098e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:16 +0000

    Add package Telethon/0.8/Telethon-0.8-py3-none-any.whl

---
+++ packages/Telethon/Telethon-0.8-py3-none-any.whl/telethon/tl/functions/phone/__init__.py
@@ -0,0 +1,4 @@
+from telethon.tl.functions.phone.request_call import RequestCallRequest
+from telethon.tl.functions.phone.accept_call import AcceptCallRequest
+from telethon.tl.functions.phone.discard_call import DiscardCallRequest
+from telethon.tl.functions.phone.received_call import ReceivedCallRequest
---
+++ packages/Telethon/Telethon-0.8-py3-none-any.whl/telethon/tl/types/help/__init__.py
@@ -0,0 +1,7 @@
+from telethon.tl.types.help.app_update import AppUpdate
+from telethon.tl.types.help.no_app_update import NoAppUpdate
+from telethon.tl.types.help.invite_text import InviteText
+from telethon.tl.types.help.support import Support
+from telethon.tl.types.help.app_changelog_empty import AppChangelogEmpty
+from telethon.tl.types.help.app_changelog import AppChangelog
+from telethon.tl.types.help.terms_of_service import TermsOfService
