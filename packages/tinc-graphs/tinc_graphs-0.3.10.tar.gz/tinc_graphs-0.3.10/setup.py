720b89ac5e4d2599453c8829074a0273973ab0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:50 +0000

    Add package tinc-graphs/0.3.10/tinc_graphs-0.3.10.tar.gz

---
+++ packages/tinc-graphs/tinc_graphs-0.3.10.tar.gz/tinc_graphs-0.3.10/setup.py
@@ -0,0 +1,43 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='tinc_graphs',
+    version='0.3.10',
+
+    description='Create Graphs from tinc Stats',
+    long_description=open("README.md").read(),
+    license='WTFPL',
+    url='http://krebsco.de/',
+    download_url='https://pypi.python.org/pypi/tinc_graphs/',
+    author='krebs',
+    author_email='spam@krebsco.de',
+    # you will also need graphviz and imagemagick
+    install_requires = [ 'pygeoip' ],
+    scripts = ['scripts/all-the-graphs', 'scripts/build-graphs'],
+    packages=['tinc_graphs'],
+    package_data = {'tinc_graphs': ['static/*/*.html',
+                                    'static/*/css/*.css' ]},
+    entry_points={
+        'console_scripts' : [
+            'tinc-stats2json = tinc_graphs.Log2JSON:main',
+            'tinc-build-graph = tinc_graphs.Graph:main',
+            'tincstats-static-dir = tinc_graphs.Geo:get_static_dir',
+            'add-geodata = tinc_graphs.Geo:main',
+            'tinc-availability-stats = tinc_graphs.Availability:generate_stats',
+            ]
+        },
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
+
