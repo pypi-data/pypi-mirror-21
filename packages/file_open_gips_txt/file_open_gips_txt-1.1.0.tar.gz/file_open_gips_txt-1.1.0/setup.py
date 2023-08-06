200a0de4e1aa00ea948b244f59a5e3e88bbd2d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:31 +0000

    Add package file_open_gips_txt/1.1.0/file_open_gips_txt-1.1.0.tar.gz

---
+++ packages/file_open_gips_txt/file_open_gips_txt-1.1.0.tar.gz/file_open_gips_txt-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'file_open_gips_txt',
+        version      = '1.1.0',
+        py_modules   = ['file_open'],
+        author       = 'gipson',
+        author_email = 'gipsonpulla@live.com',
+        url          = 'http://www.gipsonpulla.com',
+        description  = 'A simple excetion handling for file handling'
+)
