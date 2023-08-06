c7f03a78db810043239042ec35928b19f79bfa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:23 +0000

    Add package geo_code/0.0.5/geo_code-0.0.5.tar.gz

---
+++ packages/geo_code/geo_code-0.0.5.tar.gz/geo_code-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='geo_code',
+      version='0.0.5',
+      description='Lightweight Python module for geocoding addresses via the Google Geocoding API',
+      url='https://github.com/jwilber/geo_code',
+      author='Jared Wilber',
+      author_email='jdwlbr@gmail.com',
+      license='GNU3',
+      packages=['geo_code'],
+      install_requires=[
+            'pandas',
+            'requests',
+      ],
+      zip_safe=False)
