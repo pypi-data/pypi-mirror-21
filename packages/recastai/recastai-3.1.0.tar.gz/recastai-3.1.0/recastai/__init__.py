aa8407a60578239f7db9417bf5b50119e6f4c0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:27 +0000

    Add package recastai/3.1.0/recastai-3.1.0.tar.gz

---
+++ packages/recastai/recastai-3.1.0.tar.gz/recastai-3.1.0/recastai/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from .client import Client
+from .apis import Connect, Request
+
+__all__ = ['Client', 'Connect', 'Request']
---
+++ packages/recastai/recastai-3.1.0.tar.gz/recastai-3.1.0/recastai/apis/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from .connect import Connect
+from .request import Request
+
+__all__ = ['Connect', 'Request']
