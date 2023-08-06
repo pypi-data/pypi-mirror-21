c7465545b2af2f70ee7692a88d21fc637f1d3bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:57 +0000

    Add package apistar/0.1.8/apistar-0.1.8.tar.gz

---
+++ packages/apistar/apistar-0.1.8.tar.gz/apistar-0.1.8/apistar/exceptions.py
@@ -0,0 +1,14 @@
+class SchemaError(Exception):
+    def __init__(self, schema, code):
+        self.schema = schema
+        self.code = code
+        msg = schema.errors[code].format(**schema.__dict__)
+        super().__init__(msg)
+
+
+class NoCurrentApp(Exception):
+    pass
+
+
+class ConfigurationError(Exception):
+    pass
---
+++ packages/apistar/apistar-0.1.8.tar.gz/apistar-0.1.8/apistar/main.py
@@ -0,0 +1,51 @@
+"""
+The `apistar` command line client.
+"""
+import importlib.util
+import os
+import sys
+
+import click
+
+from apistar import App
+from apistar.exceptions import ConfigurationError, NoCurrentApp
+
+sys.dont_write_bytecode = True
+
+
+def get_current_app():
+    app_path = os.path.join(os.getcwd(), 'app.py')
+    if not os.path.exists(app_path):
+        raise NoCurrentApp("No app.py module exists.")
+
+    spec = importlib.util.spec_from_file_location("app", app_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    if not hasattr(module, 'app'):
+        raise ConfigurationError("The app.py module did not contain an 'app' variable.")
+
+    app = module.app
+    return app
+
+
+def setup_pythonpath():
+    cwd = os.getcwd()
+    sys.path.insert(0, cwd)
+
+
+def main():  # pragma: no cover
+    setup_pythonpath()
+    try:
+        app = get_current_app()
+    except NoCurrentApp:
+        app = App()
+
+    try:
+        app.click()
+    except (NoCurrentApp, ConfigurationError) as exc:
+        click.echo(str(exc))
+        sys.exit(1)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
