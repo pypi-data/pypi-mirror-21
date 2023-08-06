69615ae3daca046e27da89eb20fa78e17a6f7ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:12 +0000

    Add package pyspin/1.1.1/pyspin-1.1.1.tar.gz

---
+++ packages/pyspin/pyspin-1.1.1.tar.gz/pyspin-1.1.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+import sys
+
+from setuptools import setup
+
+import pyspin
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pyspin',
+    version=pyspin.__version__,
+    description='Little terminal spinner lib.',
+    long_description=long_description,
+    url='http://github.com/lord63/py-spin',
+    author='lord63',
+    author_email='lord63.j@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='terminal spin spinner',
+    packages=['pyspin'],
+    include_package_data=True,
+    setup_requires=[
+        'setuptools >= 20.8.1',
+    ],
+    install_requires=[
+        'futures; python_version < "3.2"',
+    ],
+)
