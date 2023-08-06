088fc54762a2f2db0aa61805bb91f7a055c41628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package areaofthings/1.0.0/areaofthings-1.0.0.tar.gz

---
+++ packages/areaofthings/areaofthings-1.0.0.tar.gz/areaofthings-1.0.0/areaofthings.py
@@ -0,0 +1,27 @@
+""" This module lets you calculate area of circle,rectangle and square..more to come later !!"""
+
+def areaofcircle(x):
+	a = 3.14 * x * x
+	print("Area of circle is :",a)
+
+
+
+
+
+def areaofrectangle(l,b):
+	c = l * b
+	print("Area of rectangle is :",c)
+
+
+
+
+
+
+def areaofsquare(s):
+	area = s*s
+	print("Area of square is :",area)
+
+
+
+
+
---
+++ packages/areaofthings/areaofthings-1.0.0.tar.gz/areaofthings-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name = 'areaofthings',
+	
+	version = '1.0.0',
+	
+	py_modules = ['areaofthings'],
+
+	author = 'suryapoojarypy',
+	
+	author_email = 'littlefinger.py@gmail.com',
+	
+	description = 'A simple area finder for squares,rectangles and circles..eg. to find the area of a circle invoke : areaofcircle(value)'
+)
