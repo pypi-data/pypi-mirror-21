bac809505e33f7b13c2f57b04a7764a2a2bfaf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:59 +0000

    Add package nativeconfig/3.3.1/nativeconfig-3.3.1.tar.gz

---
+++ packages/nativeconfig/nativeconfig-3.3.1.tar.gz/nativeconfig-3.3.1/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+from sys import platform
+
+
+with open(os.path.join(os.path.dirname(__file__), 'nativeconfig', 'version.py')) as f:
+    VERSION = None
+    code = compile(f.read(), 'version.py', 'exec')
+    exec(code)
+    assert VERSION
+
+
+setup(
+    version=VERSION,
+    packages=['nativeconfig', 'nativeconfig.options', 'nativeconfig.configs'],
+    test_suite='test',
+)
