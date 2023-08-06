aecf50acf46992a232bb81061a520eba7c7f9221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:02 +0000

    Add package gerobust/0.3.0/gerobust-0.3.0.tar.gz

---
+++ packages/gerobust/gerobust-0.3.0.tar.gz/gerobust-0.3.0/gerobust/build_geolib.py
@@ -0,0 +1,17 @@
+"""Build of python module wrapping geolib.c, as defined by CFFI lib.
+
+"""
+
+import os
+from cffi import FFI
+
+ffi = FFI()
+SOURCE = os.path.join(os.path.split(__file__)[0], 'geolib.c')
+
+with open(SOURCE) as sfd:
+    ffi.set_source("gerobust._wrapped_geolib.py", sfd.read(),
+                   extra_compile_args=['-O3', '-frounding-math', '-fsignaling-nans'],
+                   libraries=['c'])
+
+if __name__ == "__main__":
+    ffi.compile()
