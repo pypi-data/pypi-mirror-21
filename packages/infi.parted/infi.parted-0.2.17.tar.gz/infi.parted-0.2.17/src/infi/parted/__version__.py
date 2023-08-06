a52bbff7fdead3cdebe9f886084c00860e027b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:25 +0000

    Add package infi.parted/0.2.17/infi.parted-0.2.17.tar.gz

---
+++ packages/infi.parted/infi.parted-0.2.17.tar.gz/infi.parted-0.2.17/src/infi/parted/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.17"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.17)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '00aefb16660250de2f1093c7f7814816c4ea1388'
+__git_head_subject__ = u'Finished Release v0.2.17'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-04-24 11:09:47'
