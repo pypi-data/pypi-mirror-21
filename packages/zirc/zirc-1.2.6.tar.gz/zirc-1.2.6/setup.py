0f9f3823b4813eb26502f05e038a484f2d573309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:54 +0000

    Add package zirc/1.2.6/zirc-1.2.6.tar.gz

---
+++ packages/zirc/zirc-1.2.6.tar.gz/zirc-1.2.6/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from sys import version_info, exit, stderr
+
+if version_info[0] == 2 or version_info >= (3, 2, 0) and version_info < (3, 3, 0):
+    FileNotFoundError = IOError
+try:
+    with open("README.rst") as f:
+        long_description = f.read()
+except FileNotFoundError:
+     long_description = "This library implements the IRC protocol, it's an event-driven IRC Protocol framework."
+
+if version_info < (2, 7, 0) or (version_info[0] == 3 and version_info < (3, 2, 0)):
+    stderr.write('zIRC requires Python 2.7 or 3.2 and higher')
+    exit(-1)
+
+setup(name='zirc',
+      version='1.2.6',
+      description='Python IRCP Library',
+      long_description=long_description,
+      url='https://github.com/itslukej/zirc',
+      author='Luke J.',
+      author_email='me+zirc@lukej.me',
+      license='GNU',
+      packages=find_packages(),
+      install_requires=['six', 'pysocks'],
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Communications :: Chat :: Internet Relay Chat',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
---
+++ packages/zirc/zirc-1.2.6.tar.gz/zirc-1.2.6/zirc/ext/sasl.py
@@ -0,0 +1,58 @@
+# SASL authentication for zirc
+
+import base64
+from ..errors import SASLError
+
+class Sasl(object):
+    name = "sasl"
+
+    def __init__(self, username, password, method="plain"):
+        self.username = username
+        self.password = password
+        self.method = method
+        self.retries = 0
+
+    def run(self, bot, args=None):
+        if args is None:
+            mechanisms = ["external", "plain"]
+        else:
+            mechanisms = args
+        self.bot = bot
+        bot.listen(self.on_authenticate, "authenticate")
+        bot.listen(self.on_saslfailed, "saslfailed")
+        bot.listen(self.on_saslsuccess, "saslsuccess")
+        if self.method in mechanisms:
+            if self.method == "plain":
+                bot.send("AUTHENTICATE PLAIN")
+            elif self.method == "external":
+                bot.send("AUTHENTICATE EXTERNAL")
+            else:
+                raise SASLError("Not implemented yet")
+        else:
+            raise SASLError("Not supported by server")
+
+    def on_authenticate(self, event):
+        if event.arguments[0] == "+":
+            if self.method == 'plain':
+                password = base64.b64encode("{0}\x00{0}\x00{1}".format(self.username, self.password).encode("UTF-8")).decode("UTF-8")
+            elif self.method == 'external':
+                password = base64.b64encode(self.username.encode("UTF-8")).decode("UTF-8")
+            self.bot.send("AUTHENTICATE {0}".format(password))
+
+    def on_saslfailed(self, event):
+        self.retries += 1
+        if self.method == 'external':
+            if self.retries == 2:
+                self.retries = 1
+                self.method = 'plain'
+                self.bot.send("AUTHENTICATE PLAIN")
+            else:
+                self.bot.send("AUTHENTICATE EXTERNAL")
+        elif self.method == 'plain':
+            if not self.retries == 2:
+                self.bot.send("AUTHENTICATE PLAIN")
+            else:
+                raise SASLError("SASL authentication failed!")
+
+    def on_saslsuccess(self, event):
+        self.bot.send("CAP END")
