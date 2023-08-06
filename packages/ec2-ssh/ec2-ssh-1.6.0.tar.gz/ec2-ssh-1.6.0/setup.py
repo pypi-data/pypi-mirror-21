6f9e4c7a0abdb8206815bc33c77f6ec39498c1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:07 +0000

    Add package ec2-ssh/1.6.0/ec2-ssh-1.6.0.tar.gz

---
+++ packages/ec2-ssh/ec2-ssh-1.6.0.tar.gz/ec2-ssh-1.6.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from setuptools import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+setup(
+    name="ec2-ssh",
+    version="1.6.0",
+    author="Adam Johnson",
+    author_email="me@adamj.eu",
+    description="SSH into EC2 instances via tag name",
+    long_description=readme + '\n\n' + history,
+    license="MIT",
+    url="https://github.com/YPlan/ec2-ssh",
+    keywords=["amazon", "aws", "ec2", "ami", "ssh", "cloud", "boto"],
+    install_requires=['boto3>=1.1.0'],
+    scripts=[
+        "bin/ec2-host",
+        "bin/ec2-ssh",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        "Topic :: Utilities"
+    ],
+)
