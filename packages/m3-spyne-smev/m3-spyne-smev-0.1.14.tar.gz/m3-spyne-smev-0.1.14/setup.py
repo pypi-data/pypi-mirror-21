d27c95cfb53c4cef0f10079a300356c7c549ce2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:14 +0000

    Add package m3-spyne-smev/0.1.14/m3-spyne-smev-0.1.14.tar.gz

---
+++ packages/m3-spyne-smev/m3-spyne-smev-0.1.14.tar.gz/m3-spyne-smev-0.1.14/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+requires = []
+with open('REQUIREMENTS', 'r') as f:
+    requires.extend(f.readlines())
+
+setup(
+    name="m3-spyne-smev",
+    version="0.1.14",
+    url="http://bitbucket.org/bars-group/spyne-smev",
+    license='MIT',
+    author='BARS Group',
+    description=u'Набор протоколов фреймворка spyne для работы со СМЭВ',
+    author_email='bars@bars-open.ru',
+    package_dir={"": "src"},
+    packages=[
+        "spyne_smev", "spyne_smev.smev256", "spyne_smev.smev255",
+        "spyne_smev.server", "spyne_smev.wsse"],
+    package_data={"": ["xsd/*"]},
+    install_requires=requires,
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
