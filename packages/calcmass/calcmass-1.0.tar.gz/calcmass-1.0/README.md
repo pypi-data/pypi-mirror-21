0f2f423542b3369060520cb517b6ca86dee1bbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:53 +0000

    Add package calcmass/1.0/calcmass-1.0.tar.gz

---
+++ packages/calcmass/calcmass-1.0.tar.gz/calcmass-1.0/README.md
@@ -0,0 +1,19 @@
+# Mass
+A python script to find the molecular mass in g/mol of a chemical equation
+## Example
+```
+$ molmass C6H12O6
+180.15588
+$ molmass Cr2O7
+215.98800
+$ molmass C11H22
+154.29238
+```
+## Setup
+You can download using PyPi
+Using pip:
+```
+$ pip install molmass
+```
+## License
+Mass is licensed under the MIT License - see the LICENSE file for details.
---
+++ packages/calcmass/calcmass-1.0.tar.gz/calcmass-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+  name = 'calcmass',
+  packages = ['calcmass'],
+    version = '1.0',
+  description = 'A script to calculate the molecular mass of a chemical compound in g/mol with its chemical formula',
+  author = 'Manu Kondapaneni',
+  author_email = 'manukonda11@gmail.com',
+  url = 'https://github.com/konman2/Mass',
+  download_url = 'https://github.com/konman2/Mass/archive/1.0.tar.gz',
+  keywords = ['mass', 'chemical', 'compound','chemistry','element','calculator'],
+  include_package_data = True,
+  package_data = {'calcmass' : ['data/*.csv']},
+  entry_points={
+    'console_scripts': [
+            'calcmass = calcmass.__main__:main'
+    ],
+  }
+)
