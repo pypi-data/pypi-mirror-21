fa88957ee6a57e2335058a4df31c06a8d01060c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:55 +0000

    Add package pyxsim/1.2.6/pyxsim-1.2.6-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl

---
+++ packages/pyxsim/pyxsim-1.2.6-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl/pyxsim/__init__.py
@@ -0,0 +1,34 @@
+__version__ = "1.2.6"
+
+from pyxsim.source_models import \
+   SourceModel, \
+   ThermalSourceModel, \
+   LineSourceModel, \
+   PowerLawSourceModel
+
+from pyxsim.photon_list import \
+    PhotonList
+
+from pyxsim.utils import \
+    merge_files
+
+from pyxsim.event_list import \
+    EventList
+
+from pyxsim.spectral_models import \
+    XSpecThermalModel, \
+    XSpecAbsorbModel, \
+    TableApecModel, \
+    TableAbsorbModel, \
+    TBabsModel, WabsModel
+
+from pyxsim.responses import \
+    AuxiliaryResponseFile, \
+    RedistributionMatrixFile
+
+from pyxsim.instruments import \
+    InstrumentSimulator, \
+    ACIS_I, ACIS_S, \
+    XRS_Imager, XRS_Calorimeter, \
+    Hitomi_SXS, Athena_WFI, \
+    Athena_XIFU
