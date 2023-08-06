51c82854f193cada8fa635e8e054c0a404784658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:16 +0000

    Add package storj/0.2.0/storj-0.2.0.tar.gz

---
+++ packages/storj/storj-0.2.0.tar.gz/storj-0.2.0/storj/metadata.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+"""Storj metadata module."""
+
+__author__ = (
+    'Daniel Hawkins',
+    'Fabian Barkhau',
+    'James Prestwich',
+    'Marc Schubert',
+    'Marco Rosa',
+    'Mike Bailey',
+    'Pedro Salgado',
+    'Shawn Wilkinson',
+    'Wiktor Jezioro'
+)
+__author_email__ = ';'.join([
+    'hwkns@alum.mit.edu',
+    'f483@storj.io',
+    'james@storj.io',
+    'marc.schubert@gmail.com',
+    '',
+    'mibgranny@aol.com',
+    'steenzout@ymail.com',
+    '',
+    'lakewik@lakewik.pl'
+])
+__version__ = '0.2.0'
