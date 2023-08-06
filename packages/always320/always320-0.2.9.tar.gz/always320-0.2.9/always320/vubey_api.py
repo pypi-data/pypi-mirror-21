c2092b8c631831303d6d498de7252a1bc26ed919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:27 +0000

    Add package always320/0.2.9/always320-0.2.9.tar.gz

---
+++ packages/always320/always320-0.2.9.tar.gz/always320-0.2.9/always320/vubey_api.py
@@ -0,0 +1,26 @@
+try:
+    import BeautifulSoup
+except Exception as e:
+    from bs4 import BeautifulSoup
+
+
+
+
+def get_mp3_url(yt_url):
+    try:
+        import mechanize
+        b = mechanize.Browser()
+        b.open("https://vubey.yt")
+        b.select_form('wf-form-signup-form')
+        b.form['videoURL'] = yt_url
+        fs = b.submit()
+        d = fs.get_data()
+
+        bs = BeautifulSoup(d)
+        links = bs.findAll("a")
+
+        link = [l for l in links if "here" in l]
+        url = link[0].get("href")
+        return (url,url.split('/')[-1])
+    except Exception as e:
+        raise e
