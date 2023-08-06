4d2e25f32667a2e31b3af9a22cbe96de3ef302d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:41 +0000

    Add package zope.app.schema/3.6.0/zope.app.schema-3.6.0.tar.gz

---
+++ packages/zope.app.schema/zope.app.schema-3.6.0.tar.gz/zope.app.schema-3.6.0/src/zope/app/schema/tests.py
@@ -0,0 +1,20 @@
+import doctest
+import unittest
+
+from zope.app.testing import setup
+from zope.app.schema.vocabulary import _clear
+
+def setUp(test):
+    setup.placefulSetUp()
+    _clear()
+
+def tearDown(test):
+    setup.placefulTearDown()
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+                     setUp=setUp, tearDown=tearDown,
+                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     ),
+        ))
