441863f15f9a66f18a070c4e4f2bae2382252356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:54 +0000

    Add package siringa/0.1.2/siringa-0.1.2.tar.gz

---
+++ packages/siringa/siringa-0.1.2.tar.gz/siringa-0.1.2/History.rst
@@ -0,0 +1,26 @@
+History
+=======
+
+v0.1.2 / 2017-04-25
+-------------------
+
+  * feat(examples): add inject flag example
+  * fix(injector): process inject flag "!" based annotations accordingly
+  * fix(register): return decorated injectable callable object, if needed
+  * fix(register): return decorated injectable callable object, if needed
+  * refactor(analyzer): use method access notation
+  * refactor(docs): typo in design philosophy section
+  * fix(docs): typo in rst syntax
+  * fix(examples): type on mocking example comment
+
+v0.1.1 / 2017-04-24
+-------------------
+
+  * fix(core): handle not present __init__ member in classes. feat(examples): add mocking example
+  * fix(docs): type in about section
+  * fix(setup.py): package description
+
+v0.1.0 (2017-04-23)
+-------------------
+
+* First version (beta).
---
+++ packages/siringa/siringa-0.1.2.tar.gz/siringa-0.1.2/siringa/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*
+"""
+`siringa` is a minimalist dependency injection library.
+
+Usage example::
+
+    import siringa
+
+    @siringa.register
+    class MyUtilityClass(object):
+        pass
+
+    @siringa.inject
+    def fn(foo, MyUtilityClass: '!'):
+        return MyUtilityClass(foo)
+
+"""
+
+# Required import to load the Hy <> Python AST transformer
+import hy  # noqa
+
+from .api import *  # noqa
+from .api import __all__ as api_symbols
+from .layer import Layer                           # noqa
+from .analyzer import analyze                      # noqa
+from .container import Container                   # noqa
+from .dependency import Dependency                 # noqa
+from .errors import (SiringaError,                 # noqa
+                     MissingDependencyError,       # noqa
+                     DuplicatedDependencyError,    # noqa
+                     InvalidDependencyTypeError)   # noqa
+
+# Export public package API symbols
+__all__ = (
+    'Layer', 'analyze', 'Container',
+    'SiringaError', 'Dependency',
+    'MissingDependencyError',
+    'DuplicatedDependencyError',
+    'InvalidDependencyTypeError'
+) + api_symbols
+
+# Package metadata
+__author__ = 'Tomas Aparicio'
+__license__ = 'MIT'
+
+# Current package version
+__version__ = '0.1.2'
