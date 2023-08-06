b36e0c3d0c08ced92514d7835c0540d7e0a9cf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:47 +0000

    Add package condor_csv/0.1.1/condor_csv-0.1.1.tar.gz

---
+++ packages/condor_csv/condor_csv-0.1.1.tar.gz/condor_csv-0.1.1/condor_csv/_metadata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+__version_tuple__ = ('0', '1', '1')
+__version__ = '.'.join(__version_tuple__)
+__author__ = 'Nate Vack'
+__author_email__ = 'njvack@wisc.edu'
+__copyright__ = '2017 Board of Regents of the University of Wisconsin System'
+__license__ = 'MIT'
