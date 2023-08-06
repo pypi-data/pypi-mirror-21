6b7bf57be109ddb29877914e6e539f1e124f0768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:57 +0000

    Add package gblinks/0.2.4/gblinks-0.2.4.tar.gz

---
+++ packages/gblinks/gblinks-0.2.4.tar.gz/gblinks-0.2.4/gblinks/cli.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import click
+import json
+import sys
+
+from gblinks import Gblinks
+
+def check_broken_links(gblinks, verbose):
+    broken_links = gblinks.check_broken_links()
+
+    if broken_links:
+        if verbose:
+            print_links(broken_links)
+
+        click.echo(
+            click.style(
+                '%d broken links found in the given path' % len(broken_links)
+                , fg='red'
+            )
+        )
+
+        sys.exit(-2)
+    else:
+        click.echo('No broken links found in the given path')
+
+def list_links(gblinks):
+    links = gblinks.get_links()
+    print_links(links)
+    click.echo('%d links found in the given path' % len(links))
+
+def print_links(links):
+    for link in links:
+        click.echo(json.dumps(link, sort_keys=True, indent=4))
+
+@click.command()
+@click.argument('path')
+@click.option('--check/--no-check', default=False)
+@click.option('--list/--no-list', default=False)
+@click.option('--verbose/--no-verbose', default=True)
+def main(path, check, list, verbose):
+    try:
+        gblinks = Gblinks(path)
+
+        if check:
+            check_broken_links(gblinks, verbose)
+
+        if list:
+            list_links(gblinks)
+    except ValueError, e:
+        click.echo(click.style(str(e), fg='red'))
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
