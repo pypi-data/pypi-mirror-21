debc078617d67b9ac8870655b1041f364ad716cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:40 +0000

    Add package django-hijack/2.1.4/django-hijack-2.1.4.tar.gz

---
+++ packages/django-hijack/django-hijack-2.1.4.tar.gz/django-hijack-2.1.4/hijack/tests/urls.py
@@ -0,0 +1,10 @@
+"""URLs to run the tests."""
+from compat import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hijack/', include('hijack.urls')),
+    url(r'^hello/', include('hijack.tests.test_app.urls')),
+]
+
