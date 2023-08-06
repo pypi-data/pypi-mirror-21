e44f032afee5738069cf15e5a73226b299522bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package fjq_nester/1.0.0/fjq_nester-1.0.0.zip

---
+++ packages/fjq_nester/fjq_nester-1.0.0.zip/fjq_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         =   'fjq_nester',
+    version      =   '1.0.0',
+    py_modules   =   ['fjq_nester'],
+    author       =   'fanjq7465',
+    author_email =   'fanjq7465@163.com',
+    url          =   'http://www.headfirstlabs.com',
+    description  =   'A simple printer of nester list',
+    )
