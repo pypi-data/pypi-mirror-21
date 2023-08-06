ab2c3963eb71f3b8536abb6cd4d74c6ae4212f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:40 +0000

    Add package kubetop/17.4.17.1/kubetop-17.4.17.1.tar.gz

---
+++ packages/kubetop/kubetop-17.4.17.1.tar.gz/kubetop-17.4.17.1/README.rst
@@ -0,0 +1,81 @@
+kubetop
+=======
+
+.. image:: http://img.shields.io/pypi/v/kubetop.svg
+   :target: https://pypi.python.org/pypi/kubetop
+   :alt: PyPI Package
+
+.. image:: https://travis-ci.org/LeastAuthority/kubetop.svg
+   :target: https://travis-ci.org/LeastAuthority/kubetop
+   :alt: CI status
+
+.. image:: https://codecov.io/github/LeastAuthority/kubetop/coverage.svg
+   :target: https://codecov.io/github/LeastAuthority/kubetop
+   :alt: Coverage
+
+What is this?
+-------------
+
+kubetop is a top(1)-like tool for `Kubernetes`_.
+
+Usage Sample
+------------
+
+.. code-block:: sh
+
+   $ kubetop
+
+Output Sample
+-------------
+
+.. code-block::
+
+   kubetop - 13:02:57
+   Node 0 CPU%   9.80 MEM% 57.97 (   2 GiB/   4 GiB)  POD%  7.27 (  8/110) Ready
+   Node 1 CPU%  21.20 MEM% 59.36 (   2 GiB/   4 GiB)  POD%  3.64 (  4/110) Ready
+   Node 2 CPU%  99.90 MEM% 58.11 (   2 GiB/   4 GiB)  POD%  7.27 (  8/110) Ready
+   Pods:       20 total        0 running        0 terminating        0 pending
+                    POD               (CONTAINER)        %CPU         MEM   %MEM
+   s4-infrastructure-3073578190-2k2vw                    75.5  782.05 MiB  20.76
+                         (subscription-converger)        72.7  459.11 MiB
+                                    (grid-router)         2.7   98.07 MiB
+                                            (web)         0.1   67.61 MiB
+                           (subscription-manager)         0.0   91.62 MiB
+                          (foolscap-log-gatherer)         0.0   21.98 MiB
+                                          (flapp)         0.0   21.46 MiB
+                                 (wormhole-relay)         0.0   22.19 MiB
+
+Installing
+----------
+
+To install the latest version of kubetop using pip::
+
+  $ pip install kubetop
+
+Testing
+-------
+
+kubetop uses pyunit-style tests.
+After installing the development dependencies, you can run the test suite with trial::
+
+  $ pip install kubetop[dev]
+  $ trial kubetop
+
+Version
+-------
+
+kubetop uses the `CalVer`_ versioning convention.
+The first three segments of a kubetop version tell you the year (two digit), month, and day that version was released.
+The fourth segment of a kubetop version is a bugfix release counter.
+It is present if a new release is made that diffs from a previous release only by including one or more bug fixes.
+For each bug fix release, the fourth segment is incremented.
+
+License
+-------
+
+txkube is open source software released under the MIT License.
+See the LICENSE file for more details.
+
+
+.. _Kubernetes: https://kubernetes.io/
+.. _CalVer: http://calver.org/
---
+++ packages/kubetop/kubetop-17.4.17.1.tar.gz/kubetop-17.4.17.1/src/kubetop/_topdata.py
@@ -0,0 +1,135 @@
+# Copyright Least Authority Enterprises.
+# See LICENSE for details.
+
+"""
+Retrieve resource usage data by Kubernetes Objects.
+
+Theory of Operation
+===================
+
+#. Combine Kubernetes API server location with a resource collection object.
+#. Collect resource usage information via the Heapster service on the
+   Kubernetes API server.
+"""
+
+from __future__ import unicode_literals
+
+from twisted.internet.defer import gatherResults
+
+import attr
+import attr.validators
+
+from treq import json_content
+from treq.client import HTTPClient
+
+from txkube import IKubernetes, v1, network_kubernetes_from_context
+
+
+def make_source(reactor, config_path, context_name):
+    """
+    Get a source of Kubernetes resource usage data.
+    """
+    kubernetes = network_kubernetes_from_context(reactor, context_name, config_path)
+    return _Source(kubernetes=kubernetes)
+
+
+@attr.s(frozen=True)
+class _Source(object):
+    kubernetes = attr.ib(validator=attr.validators.provides(IKubernetes))
+
+
+    def pods(self):
+        client = self._client()
+        base_url = self.kubernetes.base_url
+        return gatherResults([
+            self._pod_usage_from_client(client, base_url),
+            self._pod_info(self.kubernetes.client()),
+        ]).addCallback(
+            lambda (usage, info): {
+                "usage": usage,
+                "info": info,
+            },
+        )
+
+
+    def nodes(self):
+        client = self._client()
+        base_url = self.kubernetes.base_url
+        return gatherResults([
+            self._node_usage_from_client(client, base_url),
+            self._node_info_from_client(client, base_url),
+        ]).addCallback(
+            lambda (usage, info): {
+                "usage": usage,
+                "info": info,
+            },
+        )
+
+
+    def _client(self):
+        k8s_client = self.kubernetes.client()
+        agent = k8s_client.agent
+        return HTTPClient(agent=agent)
+
+
+    def _pod_usage_from_client(self, client, base_url):
+        d = self.kubernetes.client().list(v1.Namespace)
+        def got_namespaces(namespaces):
+            d = gatherResults(
+                client.get(
+                    base_url.asText() + self.pod_location(ns.metadata.name)
+                ).addCallback(
+                    json_content
+                )
+                for ns
+                in namespaces.items
+            )
+            def combine(pod_usages):
+                result = []
+                for usage in pod_usages:
+                    if usage["items"] is None:
+                        continue
+                    for item in usage["items"]:
+                        result.append(item)
+                return {"items": result}
+            d.addCallback(combine)
+            return d
+        d.addCallback(got_namespaces)
+        return d
+
+
+    def _pod_info(self, client):
+        return client.list(v1.Pod)
+
+
+    def pod_location(self, namespace):
+        # kubectl --v=11 top pods
+        return (
+            "/api/v1/namespaces/kube-system/services/http:heapster:"
+            "/proxy/apis/metrics/v1alpha1/namespaces/{namespace}/pods?"
+            "labelSelector="
+        ).format(namespace=namespace)
+
+
+    def _node_usage_from_client(self, client, base_url):
+        d = client.get(base_url.asText() + self.node_location())
+        d.addCallback(json_content)
+        return d
+
+
+    def node_location(self):
+        # url-hacked from pod_location... I found no docs that clearly explain
+        # what is going on here.
+        # https://github.com/kubernetes/heapster/blob/master/docs/model.md
+        # provides some hints but it's documenting the actual Heapster API
+        # which requires port-forwarding into the Heapster pod to access.
+        return (
+            "/api/v1/namespaces/kube-system/services/http:heapster:"
+            "/proxy/apis/metrics/v1alpha1/nodes"
+        )
+
+
+    def _node_info_from_client(self, client, base_url):
+        d = client.get(base_url.asText() + "/api/v1/nodes")
+        d.addCallback(json_content)
+        return d
