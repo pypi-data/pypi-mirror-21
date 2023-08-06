b707dc32761234cacc55602bfa42442e72885424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:10 +0000

    Add package casadi/3.1.1/casadi-3.1.1.post3-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/casadi/casadi-3.1.1.post3-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/casadi/include/casadi/solvers/importer/casadi_importer_shell_export.h
@@ -0,0 +1,42 @@
+
+#ifndef CASADI_IMPORTER_SHELL_EXPORT_H
+#define CASADI_IMPORTER_SHELL_EXPORT_H
+
+#ifdef CASADI_IMPORTER_SHELL_STATIC_DEFINE
+#  define CASADI_IMPORTER_SHELL_EXPORT
+#  define CASADI_IMPORTER_SHELL_NO_EXPORT
+#else
+#  ifndef CASADI_IMPORTER_SHELL_EXPORT
+#    ifdef casadi_importer_shell_EXPORTS
+        /* We are building this library */
+#      define CASADI_IMPORTER_SHELL_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define CASADI_IMPORTER_SHELL_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef CASADI_IMPORTER_SHELL_NO_EXPORT
+#    define CASADI_IMPORTER_SHELL_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef CASADI_IMPORTER_SHELL_DEPRECATED
+#  define CASADI_IMPORTER_SHELL_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef CASADI_IMPORTER_SHELL_DEPRECATED_EXPORT
+#  define CASADI_IMPORTER_SHELL_DEPRECATED_EXPORT CASADI_IMPORTER_SHELL_EXPORT CASADI_IMPORTER_SHELL_DEPRECATED
+#endif
+
+#ifndef CASADI_IMPORTER_SHELL_DEPRECATED_NO_EXPORT
+#  define CASADI_IMPORTER_SHELL_DEPRECATED_NO_EXPORT CASADI_IMPORTER_SHELL_NO_EXPORT CASADI_IMPORTER_SHELL_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef CASADI_IMPORTER_SHELL_NO_DEPRECATED
+#    define CASADI_IMPORTER_SHELL_NO_DEPRECATED
+#  endif
+#endif
+
+#endif
