d3df3d761043528c0e287950ae208bd3d1e48f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package RUP/0.1.4/RUP-0.1.4-py2-none-any.whl

---
+++ packages/RUP/RUP-0.1.4-py2-none-any.whl/rup/__main__.py
@@ -0,0 +1,88 @@
+import logging
+import signal
+import time
+import sys
+import yaml
+
+from collector import Collector
+from processor import Processor
+from rules import parse_rules_from_file
+from watcher import Watcher
+from rundb.client import RunDbc
+
+
+def parse_config(path):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rules', help='Path to rules file', required=True)
+    parser.add_argument('--config', help='Path to rules file', required=True)
+    parser.add_argument('--log', help='Path to log file')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    if args.log:
+        file_log = logging.FileHandler(args.log)
+        file_log.setLevel(logging.INFO)
+        file_log.setFormatter(formatter)
+        root.addHandler(file_log)
+
+    root.info("Started RUP")
+
+    settings_file = args.config
+    rules_file = args.rules
+
+    settings = parse_config(settings_file)
+
+    rules = parse_rules_from_file(rules_file)
+
+    search = RunDbc(settings['runDB']['API_key'], settings['runDB']['host'])
+
+    col = Collector()
+
+    watchers = []
+    timeout = int(settings['watch']['timeout'])
+    for d in settings['watch']['directories']:
+        w = Watcher(d, timeout)
+        col.attach(w)
+        w.run()
+        watchers.append(w)
+
+    processor = Processor(search, rules, settings['hostname'])
+    processor.attach_collector(col)
+
+    def sighup_handler(_, __):
+        log = logging.getLogger("SIGHUP")
+        log.info("Received SIGHUP. Reloading {} and {}".format(rules_file, settings_file))
+        try:
+            processor.set_rules(parse_rules_from_file(rules_file))
+        except Exception as e:
+            log.error("Failed reloading rules due to: {}".format(e.message))
+
+    signal.signal(signal.SIGHUP, sighup_handler)
+
+    while True:
+        time.sleep(1000)
+
+
+if __name__ == '__main__':
+
+    main()
+
+
