2b7f0c75ec01d5e350e642699b1ba58ad68c71bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:33 +0000

    Add package zope.app.principalannotation/4.0.0/zope.app.principalannotation-4.0.0-py2.py3-none-any.whl

---
+++ packages/zope.app.principalannotation/zope.app.principalannotation-4.0.0-py2.py3-none-any.whl/zope/app/principalannotation/bootstrap.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Bootstrap code for principal annotation utility.
+"""
+
+import transaction
+
+from zope.app.appsetup.bootstrap import ensureUtility, getInformationFromEvent
+
+from zope.principalannotation.utility import PrincipalAnnotationUtility
+from zope.principalannotation.interfaces import IPrincipalAnnotationUtility
+
+def bootStrapSubscriber(event):
+    """Subscriber to the IDatabaseOpenedWithRootEvent
+
+    Create utility at that time if not yet present
+    """
+
+    _db, connection, _root, root_folder = getInformationFromEvent(event)
+
+    ensureUtility(root_folder, IPrincipalAnnotationUtility,
+                  'PrincipalAnnotation', PrincipalAnnotationUtility)
+
+    transaction.commit()
+    connection.close()
