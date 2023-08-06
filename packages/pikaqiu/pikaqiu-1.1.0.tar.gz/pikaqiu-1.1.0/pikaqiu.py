b8736d6b1344f1184a720cb7d1c98b1195ef88a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package pikaqiu/1.1.0/pikaqiu-1.1.0.tar.gz

---
+++ packages/pikaqiu/pikaqiu-1.1.0.tar.gz/pikaqiu-1.1.0/pikaqiu.py
@@ -0,0 +1,10 @@
+﻿'''这是"nester.py"模块，提供了一个名为islist（）的函数，这个函数的作用是打印列表，列表中有可能包含嵌套列表。'''
+def islist(thelist,level):
+#这个函数取一个列表名为a作为参数，所指定的列表中的每一项会（递归的）输出到屏幕上。每个数据用逗号隔开。
+   for each in thelist:
+       if isinstance(each,list):
+           islist(each,level+1)
+       else:
+           for eeach in range(level):
+               print('\t',end='')
+           print(each)
