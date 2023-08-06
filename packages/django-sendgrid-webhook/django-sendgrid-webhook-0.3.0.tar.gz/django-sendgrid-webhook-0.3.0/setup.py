2752c22cf414c23da6decc557638240f522571eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:33 +0000

    Add package django-sendgrid-webhook/0.3.0/django-sendgrid-webhook-0.3.0.tar.gz

---
+++ packages/django-sendgrid-webhook/django-sendgrid-webhook-0.3.0.tar.gz/django-sendgrid-webhook-0.3.0/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='django-sendgrid-webhook',
+    version=__import__('sendgrid').__version__,
+    author='Jann Kleen',
+    author_email='jann.kleen@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/resmio/django-sendgrid',
+    license='BSD 2-Clause',
+    description=' '.join(__import__('sendgrid').__doc__.splitlines()).strip(),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.4',
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.md'),
+    test_suite='runtests.run_tests',
+    zip_safe=False,
+)
