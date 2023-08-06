ddceb30ab166e012fbf8bcc1fb183d6052f63191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:14 +0000

    Add package cadnano/2.5.1.10/cadnano-2.5.1.10-cp35-none-macosx_10_10_x86_64.whl

---
+++ packages/cadnano/cadnano-2.5.1.10-cp35-none-macosx_10_10_x86_64.whl/cadnano/__init__.py
@@ -0,0 +1,50 @@
+import sys
+from .cnproxy import tapp
+
+__version__ = '2.5.1.10'
+
+global shared_app
+shared_app = tapp
+
+global batch
+batch = False
+global reopen
+reopen = False
+
+
+def getBatch():
+    global batch
+    return batch
+
+
+def setBatch(is_batch):
+    global batch
+    batch = is_batch
+
+
+def getReopen():
+    global reopen
+    return reopen
+
+
+def setReopen(is_reopen):
+    global reopen
+    reopen = is_reopen
+
+
+def app():
+    global shared_app
+    return shared_app
+
+
+def initAppWithGui(app_args=None, do_exec=True):
+    global shared_app
+    from cadnano.cadnanoqt import CadnanoQt
+    # 1. Create the application object
+    shared_app = CadnanoQt(app_args)
+    # 2. Use the object to finish importing and creating
+    # application wide objects
+    shared_app.finishInit()
+    if do_exec:
+        sys.exit(shared_app.exec_())
+    return shared_app
---
+++ packages/cadnano/cadnano-2.5.1.10-cp35-none-macosx_10_10_x86_64.whl/cadnano/fileio/nnoencode.py
@@ -0,0 +1,39 @@
+import io
+import json
+import numpy as np
+import cadnano.fileio.v3encode as v3encode
+
+
+def encodeToFile(filename, document):
+    json_string = encode(document)
+    with io.open(filename, 'w', encoding='utf-8') as fd:
+        fd.write(json_string)
+# end def
+
+
+def encode(document):
+    obj = v3encode.encodeDocument(document)
+    json_string = json.dumps(obj, separators=(',', ':'), cls=EncoderforPandas)  # compact encoding
+    return json_string
+
+
+class EncoderforPandas(json.JSONEncoder):
+    """ Special encoder to coerce numpy number types
+    python types
+    """
+    def default(self, obj):
+        if isinstance(obj, (np.integer, np.floating, np.bool_)):
+            return obj.item()
+        # if isinstance(obj, (np.integer):
+        #     return int(obj)
+        # elif isinstance(obj, np.floating):
+        #     return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            try:
+                return super(EncoderforPandas, self).default(obj)
+            except:
+                print(type(obj))
+                raise
+# end class
