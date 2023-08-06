4c89bd5da0775ff7656fc8bd84d90170c73aa226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:52 +0000

    Add package ankita/3.1/ankita-3.1.tar.gz

---
+++ packages/ankita/ankita-3.1.tar.gz/ankita-3.1/ankita/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Name = Ankita
+version = 3.1
+Executable Command = ankita
+Package Name = ankita
+Python Module Name = ankita
+Debian Dependency = python-qt4, python-pil
+Description = A well designed PyQt4 Paint program
+Changes :
+        Default resolution is 800x600 in new canvas dialog.
+  v3.0  Transformation tools (resize, rotate, flip)
+        Added brush texture
+        Remembers last canvas size
+        Added Rounded rect, Spray, circle
+        Added Spline
+        Added Eraser
+        Flood fill supports opacity replacement
+        Canvas background changed
+"""
+# TODO:
+#       Snap to grid
+#       Add Image
+#       Show resolution
