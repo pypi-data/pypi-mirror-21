b9ea3c08da82681693054221e3d6ada7f6680dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:41 +0000

    Add package fhzhengNester/1.2.0/fhzhengNester-1.2.0.zip

---
+++ packages/fhzhengNester/fhzhengNester-1.2.0.zip/fhzhengNester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+                name               = 'fhzhengNester',
+                version             = '1.2.0',
+                py_modules      = ['fhzhnegNester'],
+                author              = 'fhzheng',
+                author_email     = 'fhzheng@cuit.edu.cn',
+                url                     = 'http://fhzheng.cuit.edu.cn',
+                description        = 'A simple printer of nested lists',
+        )
