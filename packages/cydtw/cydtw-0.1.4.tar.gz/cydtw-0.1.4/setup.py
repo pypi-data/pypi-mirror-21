186f5590ce8076f4135eeb26a8646d13f00e45c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:54 +0000

    Add package cydtw/0.1.4/cydtw-0.1.4.tar.gz

---
+++ packages/cydtw/cydtw-0.1.4.tar.gz/cydtw-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name="cydtw",
+    description = "DTW computation made efficient using Cython",
+    ext_modules=cythonize("cydtw.pyx"),
+    include_dirs=[numpy.get_include()],
+    install_requires=['Cython', 'numpy', 'scipy'],
+    version="0.1.4",
+    url="https://github.com/rtavenar/cydtw",
+    author="Romain Tavenard",
+    author_email="romain.tavenard@univ-rennes2.fr"
+)
