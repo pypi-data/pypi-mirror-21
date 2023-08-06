e2a44dd156c666e13b06ea5cb66b66295710902d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:22 +0000

    Add package kaeru_parser/0.0.2/kaeru_parser-0.0.2.tar.gz

---
+++ packages/kaeru_parser/kaeru_parser-0.0.2.tar.gz/kaeru_parser-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="kaeru_parser",
+    packages=["kaeru_parser"],
+    version="0.0.2",
+    url="https://github.com/harukaeru/KaeruParser",
+    description="parser of kaeru",
+    author="harukaeru",
+    author_email="nekoneko.myaomyao@gmail.com",
+    license="MIT",
+)
