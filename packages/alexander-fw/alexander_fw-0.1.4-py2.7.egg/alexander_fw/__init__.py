2976cd39272a556dbb67d704652045ff5a93470b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:35 +0000

    Add package alexander-fw/0.1.4/alexander_fw-0.1.4-py2.7.egg

---
+++ packages/alexander-fw/alexander_fw-0.1.4-py2.7.egg/alexander_fw/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# from __future__ import unicode_literals
+
+__author__ = "d01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2017, Florian JUNG"
+__license__ = "MIT"
+__version__ = "0.1.4"
+__date__ = "2017-04-23"
+# Created: 2017-04-17 20:26
+
+from . import dto
+from . import reactor
+from . import manager
+from .reactor import ReactorModule
+from .reactor.events import setup_kombu
+from .manager import IntentManager, ActorManager
+
+__all_ = ["dto", "reactor", "IntentManager", "ActorManager", "setup_kombu"]
