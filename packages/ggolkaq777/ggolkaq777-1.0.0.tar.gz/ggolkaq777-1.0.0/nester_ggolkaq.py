b8056c07df8d0587e505c7ec53eb11bdeb8da84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:36 +0000

    Add package ggolkaq777/1.0.0/ggolkaq777-1.0.0.tar.gz

---
+++ packages/ggolkaq777/ggolkaq777-1.0.0.tar.gz/ggolkaq777-1.0.0/nester_ggolkaq.py
@@ -0,0 +1,13 @@
+# 모듈 만들기
+
+def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			print(each_item)
+
+test = [ 'a', 'b', 'c', ['aa','bb','cc'], 'd' ]
+
+print_lol(test)
+
---
+++ packages/ggolkaq777/ggolkaq777-1.0.0.tar.gz/ggolkaq777-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'ggolkaq777',
+	version = '1.0.0',
+	py_modules = ['nester_ggolkaq'],
+	author = 'yoo',
+	author_email = 'ggolkaq@naver.com',
+	description = 'Python Test Code'
+	)
