1155ea391f6bc87cd9add805285cc83cf6e8d8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:46 +0000

    Add package kolla/4.0.1/kolla-4.0.1.tar.gz

---
+++ packages/kolla/kolla-4.0.1.tar.gz/kolla-4.0.1/tools/deploy_aio.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+export KOLLA_BASE=$1
+export KOLLA_TYPE=$2
+export KOLLA_ANSIBLE_DIR=$(mktemp -d)
+
+cat > /tmp/clonemap <<EOF
+clonemap:
+ - name: openstack/kolla-ansible
+   dest: ${KOLLA_ANSIBLE_DIR}
+EOF
+
+/usr/zuul-env/bin/zuul-cloner -m /tmp/clonemap --workspace "$(pwd)" \
+    --cache-dir /opt/git git://git.openstack.org \
+    openstack/kolla-ansible
+
+pushd "${KOLLA_ANSIBLE_DIR}"
+# Copy configs
+sudo cp -a etc/kolla /etc/
+# Generate passwords
+sudo tools/generate_passwords.py
+sudo ./tools/deploy_aio.sh "$KOLLA_BASE" "$KOLLA_TYPE"
+popd
