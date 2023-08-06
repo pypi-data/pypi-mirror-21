b7030e702dc44c21429a6063bf767334a8dba4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:41 +0000

    Add package aurorapy/0.1.0/aurorapy-0.1.0.tar.gz

---
+++ packages/aurorapy/aurorapy-0.1.0.tar.gz/aurorapy-0.1.0/setup.py
@@ -0,0 +1,41 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name="aurorapy",
+    version="0.1.0",
+    description='Python implementation of Aurora Protocol',
+    long_description=readme,
+    author="E.Va Energie Valsabbia",
+    author_email="it@energievalsabbia.it",
+    maintainer='Claudio Catterina',
+    maintainer_email='ccatterina@energievalsabbia.it',
+    license='MIT',
+    packages=['aurorapy'],
+    url='https://code.energievalsabbia.it/ccatterina/aurorapy',
+    install_requires=[
+        'pyserial>=3.2.1',
+        'PyCRC>=1.21',
+    ],
+    classifiers=(
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    )
+)
