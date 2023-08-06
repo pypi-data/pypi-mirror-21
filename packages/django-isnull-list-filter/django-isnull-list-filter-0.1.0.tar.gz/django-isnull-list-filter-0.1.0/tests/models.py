1a2d28914556d4050963cb0427f60bf1dbb6a062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:09 +0000

    Add package django-isnull-list-filter/0.1.0/django-isnull-list-filter-0.1.0.tar.gz

---
+++ packages/django-isnull-list-filter/django-isnull-list-filter-0.1.0.tar.gz/django-isnull-list-filter-0.1.0/tests/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Author(models.Model):
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+    name = models.TextField()
+
+
+class Album(models.Model):
+    author = models.ForeignKey(
+        Author,
+        null=True,
+        blank=True,
+    )
+    coauthors = models.ManyToManyField(
+        Author,
+        related_name="coauthored_albums",
+    )
+    released = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+
+class Song(models.Model):
+    album = models.ForeignKey(
+        Album,
+        related_name="song_set",
+    )
