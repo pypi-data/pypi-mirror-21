19ad3affe3e1f06a83149e1e2fd36fe2c5aab0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:37 +0000

    Add package zenmai/0.2.1/zenmai-0.2.1-py2.py3-none-any.whl

---
+++ packages/zenmai/zenmai-0.2.1-py2.py3-none-any.whl/zenmai/driver.py
@@ -0,0 +1,33 @@
+from dictknife import loading
+from zenmai.actions import concat
+from zenmai.compile import compile
+from zenmai.core.context import Context
+
+
+class Driver:
+    def __init__(self, module, srcfile, format=None, data=None):
+        self.module = module
+        self.srcfile = srcfile
+        self.format = format
+        self.data = data or []
+
+    def transform(self, d, wrap=None):
+        def context_factory(*args, **kwargs):
+            context = Context(*args, **kwargs)
+            context.assign("data", concat(self.data))  # xxx
+            return context
+        r = compile(d, self.module, filename=self.srcfile, context_factory=context_factory)
+        if wrap is not None:
+            r = wrap(r)
+        return r
+
+    def load(self, fp):
+        return loading.load(fp)
+
+    def dump(self, d, fp):
+        return loading.dump(d, fp, format=self.format)
+
+    def run(self, inp, outp, wrap=None):
+        data = self.load(inp)
+        result = self.transform(data, wrap=wrap)
+        self.dump(result, outp)
