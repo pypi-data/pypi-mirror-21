4f551e2d345d9a926c16a4daacaa6ecb59f343fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:58 +0000

    Add package stepist/0.0.2/stepist-0.0.2.tar.gz

---
+++ packages/stepist/stepist-0.0.2.tar.gz/stepist-0.0.2/stepist/tests/reducer/test_reducer_workers.py
@@ -0,0 +1,33 @@
+from stepist.flow import step, run, setup, just_do_it, reducer_step
+
+
+@reducer_step()
+def r_step(iter):
+    print("ASKDJKASD")
+    for i in iter:
+        print(i)
+
+
+@step(r_step, as_worker=True, wait_result=True)
+def step3(a):
+    return dict(c='d',
+                a=a)
+
+
+@step(step3, as_worker=True, wait_result=True)
+def step2():
+    return {'a': 'b'}
+
+
+@step(step2)
+def step1():
+    print("ASD")
+    return {}
+
+
+setup()
+just_do_it(5, step2, step3, _warning=False)
+
+
+print(step1.config(reducer_step=r_step).execute())
+
