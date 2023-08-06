3e15656fdb9aa499dd48f2b7744e1e7a1af51036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:07 +0000

    Add package hedgehog-station-controller/2.4.1/hedgehog-station-controller-2.4.1.tar.gz

---
+++ packages/hedgehog-station-controller/hedgehog-station-controller-2.4.1.tar.gz/hedgehog-station-controller-2.4.1/visaweb/urls.py
@@ -0,0 +1,53 @@
+"""visaweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'assets', views.AssetViewSet)
+
+
+
+urlpatterns = [
+    # url(r'^$', views.health, name='health'),
+
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url(r'^devicemanager/attach$', views.devicemanager_attach, name='devicemanager_attach'),
+    url(r'^devicemanager/active', views.devicemanager_active, name='devicemanager_active'),
+    url(r'^devicemanager/inactive$', views.devicemanager_inactive, name='devicemanager_inactive'),
+    url(r'^devicemanager/unknown$', views.devicemanager_unknown, name='devicemanager_unknown'),
+    url(r'^visa/list$', views.visa_list, name='visa_list'),
+    url(r'^visa/alias', views.visa_alias, name='visa_alias'),
+    url(r'^visa/read$', views.visa_read, name='visa_read'),
+    url(r'^visa/write', views.visa_write, name='visa_write'),
+    url(r'^visa/query', views.visa_query, name='visa_query'),
+
+    # ex: /polls/5/
+    url(r'^(?P<asset_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<asset_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
---
+++ packages/hedgehog-station-controller/hedgehog-station-controller-2.4.1.tar.gz/hedgehog-station-controller-2.4.1/visaweb/views.py
@@ -0,0 +1,214 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from rest_framework import viewsets, views, serializers, response, status, decorators
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+
+from django.contrib.auth.models import User, Group
+from .models import Asset
+
+import logging
+import visaweb
+
+from .serializers import UserSerializer, GroupSerializer, AssetSerializer,AssetListSerializer
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+# Create your views here.
+def health(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def index(request):
+    asset_list = Asset.objects.all()
+    context = {
+        'shell': 'visaweb/src/hedgehog-app.html',
+        'service_worker': 'visaweb/service-worker.js',
+        'shell_element': 'hedgehog-app',
+        'asset_list': asset_list
+    }
+    return render(request, 'visaweb/index.html', context)
+
+def detail(request, asset_id):
+    return HttpResponse("You're looking at question %s." % asset_id)
+
+def results(request, asset_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % asset_id)
+
+def vote(request, asset_id):
+    return HttpResponse("You're voting on question %s." % asset_id)
+
+@permission_classes((permissions.AllowAny,))
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+@permission_classes((permissions.AllowAny,))
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+@permission_classes((permissions.AllowAny,))
+class AssetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+@decorators.api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def visa_list(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        try:
+            data = visaweb.client.list()
+            return response.Response(data, status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+@decorators.api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def devicemanager_active(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    logging.getLogger().info("KAKAKAPRONO")
+    if request.method == 'GET':
+        try:
+            resources = visaweb.client.list()
+            connected_assets = Asset.objects.filter(address__in=resources)
+            return response.Response(AssetSerializer(connected_assets, many=True).data, status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+@decorators.api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def devicemanager_inactive(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        try:
+            resources = visaweb.client.list()
+            connected_assets = Asset.objects.exclude(address__in=resources).all()
+            logging.info('-------------- Inactive assets:', connected_assets)
+            return response.Response(AssetSerializer(connected_assets, many=True).data, status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+@decorators.api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def devicemanager_unknown(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        try:
+            resources = visaweb.client.list()
+            asset_addresses = Asset.objects.all().values_list('address')
+
+            result = [item for item in resources if item not in asset_addresses]
+
+            return response.Response(result, status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+@decorators.api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def visa_alias(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        try:
+            connected = visaweb.client.alias(request.data)
+            return response.Response(status=status.HTTP_200_OK)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+@decorators.api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def devicemanager_attach(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        try:
+            mapping = {}
+
+            alias = request.data['key']
+            if (alias == None):
+
+                asset_count = Asset.objects.count()
+                alias = 'asset' + str(asset_count)
+
+            address = request.data['value']
+
+            mapping[alias] = address
+
+            connected = visaweb.client.alias(mapping)['connected']
+
+            if (connected > 0):
+                a = Asset(alias=alias, address=address)
+                a.save()
+                return response.Response({ u'id': a.id }, status=status.HTTP_200_OK)
+            else:
+                return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@decorators.api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def visa_read(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        try:
+            logger.info('Execute VISA command: read')
+            data = visaweb.client.read(request.data['alias'], request.data['command'])
+            return response.Response(data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@decorators.api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def visa_write(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        try:
+            logger.info('Execute VISA command: write')
+            data = visaweb.client.write(request.data['alias'], request.data['command'])
+            return response.Response(data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return response.Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@decorators.api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def visa_query(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        try:
+            logger.info('Execute VISA command: query')
+            data = visaweb.client.query(request.data['alias'], request.data['command'])
+            return response.Response(data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return response.Response({ 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
