f19535a20284f97b4f99d63038295b6035558e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:33 +0000

    Add package graf-py/0.1.0/graf-py-0.1.0.tar.gz

---
+++ packages/graf-py/graf-py-0.1.0.tar.gz/graf-py-0.1.0/graf/__init__.py
@@ -0,0 +1,3 @@
+from .client import Client
+instance = Client.instance()
+__all__ = ['Client', 'instance']
---
+++ packages/graf-py/graf-py-0.1.0.tar.gz/graf-py-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='graf-py',
+    version='0.1.0',
+    description='Graf Python Client',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: System :: Logging',
+        'Topic :: System :: Monitoring',
+        'Topic :: Software Development :: Quality Assurance'
+    ],
+    keywords='graf iograf metrics events',
+    url='http://github.com/iograf/graf-py',
+    author='Victor Gama',
+    author_email='hey@vito.io',
+    license='MIT',
+    packages=['graf'],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose', 'mock']
+)
