f6487392ffe80a9442ec8f4577dd06a855360314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:49 +0000

    Add package ravi-periodic-table/0.1/ravi_periodic_table-0.1.tar.gz

---
+++ packages/ravi-periodic-table/ravi_periodic_table-0.1.tar.gz/ravi_periodic_table-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ravi_periodic_table',
+      version='0.1',
+      description='chemistry symbols and name',
+      url='http://github.com/ravigadila/ravi_periodic_table',
+      author='ravigadila',
+      author_email='ravi@micropyramid.com',
+      license='MIT',
+      packages=['periodic_table'],
+      zip_safe=False)
