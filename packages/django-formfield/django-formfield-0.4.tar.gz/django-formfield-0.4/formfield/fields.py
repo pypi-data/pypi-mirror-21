1a21824bd13345c7a56dda0c1e7a4de8a861063a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:09 +0000

    Add package django-formfield/0.4/django-formfield-0.4.tar.gz

---
+++ packages/django-formfield/django-formfield-0.4.tar.gz/django-formfield-0.4/formfield/fields.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import json
+import six
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+from django.utils.safestring import mark_safe
+
+from .widgets import FormFieldWidget
+
+
+class JSONField(models.TextField):
+    """
+    JSONField is a generic textfield that serializes/unserializes
+    the data from our form fields
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.dump_kwargs = kwargs.pop('dump_kwargs',
+                                      {'cls': DjangoJSONEncoder})
+        self.load_kwargs = kwargs.pop('load_kwargs', {})
+
+        super(JSONField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+
+        if isinstance(value, six.string_types):
+            try:
+                return json.loads(value, **self.load_kwargs)
+            except ValueError:
+                pass
+
+        return value
+
+    def get_db_prep_value(self, value, *args, **kwargs):
+
+        if isinstance(value, six.string_types):
+            return value
+
+        return json.dumps(value, **self.dump_kwargs)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+
+class FormField(forms.MultiValueField):
+    """The form field we can use in forms"""
+
+    def __init__(self, form, **kwargs):
+        import inspect
+        if inspect.isclass(form) and issubclass(form, forms.Form):
+            form_class = form
+        elif callable(form):
+            form_class = form()
+            self.form = form_class()
+        elif isinstance(form, six.string_types):
+            from django.utils import module_loading
+            if hasattr(module_loading, 'import_by_path'):
+                form_class = module_loading.import_by_path(form)
+            else:
+                form_class = module_loading.import_string(form)
+        self.form = form_class()
+
+        # Set the widget and initial data
+        kwargs['widget'] = FormFieldWidget([f for f in self.form])
+        kwargs['initial'] = [f.field.initial for f in self.form]
+        # The field it self should not be required, this allows us to
+        # have optional fields in a sub form
+        kwargs['required'] = False
+
+        self.max_length = kwargs.pop('max_length', None)
+
+        super(FormField, self).__init__(**kwargs)
+
+        self.fields = [f.field for f in self.form]
+
+    def compress(self, data_list):
+        """
+        Return the cleaned_data of the form, everything should already be valid
+        """
+        data = {}
+        if data_list:
+            return dict(
+                (f.name, data_list[i]) for i, f in enumerate(self.form))
+        return data
+
+    def clean(self, value):
+        """
+        Call the form is_valid to ensure every value supplied is valid
+        """
+        if not value:
+            raise ValidationError(
+                'Error found in Form Field: Nothing to validate')
+
+        data = dict((bf.name, value[i]) for i, bf in enumerate(self.form))
+        self.form = form = self.form.__class__(data)
+        if not form.is_valid():
+            error_dict = list(form.errors.items())
+            raise ValidationError([
+                ValidationError(mark_safe('{} {}'.format(
+                    k.title(), v)), code=k) for k, v in error_dict])
+
+        # This call will ensure compress is called as expected.
+        return super(FormField, self).clean(value)
+
+
+class ModelFormField(JSONField):
+    """The json backed field we can use in our models"""
+
+    def __init__(self, *args, **kwargs):
+        """
+        This field needs to be nullable and blankable. The supplied form
+        will provide the validation.
+        """
+        self.form = kwargs.pop('form', None)
+
+        kwargs['null'] = True
+        kwargs['blank'] = True
+
+        super(ModelFormField, self).__init__(*args, **kwargs)
+
+    def formfield(self, form_class=FormField, **kwargs):
+        # Need to supply form to FormField
+        return super(ModelFormField, self).formfield(form_class=form_class,
+            form=self.form, **kwargs)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^formfield\.fields\.JSONField"])
+    add_introspection_rules([], ["^formfield\.fields\.ModelFormField"])
+except ImportError:
+    pass
---
+++ packages/django-formfield/django-formfield-0.4.tar.gz/django-formfield-0.4/formfield/tests.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.db import models
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .fields import ModelFormField, FormField
+
+
+class MetaForm(forms.Form):
+    age = forms.IntegerField()
+    sex = forms.ChoiceField(
+        required=False, choices=((1, 'Make'), (2, 'Female')))
+
+
+class Coordinates(forms.Form):
+    lng = forms.CharField(required=False)
+    lat = forms.CharField(required=False)
+
+
+class Address(forms.Form):
+    street = forms.CharField()
+    country = forms.CharField(required=False)
+    zipcode = forms.IntegerField(required=False)
+
+    coords = FormField(Coordinates)
+
+
+class PersonForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    suffix = forms.CharField(required=False)
+    age = forms.IntegerField(required=False)
+
+    address = FormField(Address)
+
+
+class TestModel(models.Model):
+    name = models.CharField(max_length=255)
+    meta_data = ModelFormField(MetaForm)
+
+    personal = ModelFormField(PersonForm)
+
+
+class FormFieldTests(TestCase):
+    """
+    Tests for django-formfield
+    """
+    maxDiff = None
+
+    def setUp(self):
+        self.model = TestModel.objects.create(
+            name="John",
+            meta_data={'age': 32, 'sex': 1})
+
+    def test_01_get_field(self):
+        """Ensure the data is a proper dictionary"""
+        self.assertEqual(self.model.meta_data, {'age': 32, 'sex': 1})
+
+    def test_02_modify_field(self):
+        """After a change is made, ensure the data is still a dictionary"""
+        self.model.meta_data = {'age': 18, 'sex': 2}
+        self.model.save()
+        self.assertEqual(self.model.meta_data, {'age': 18, 'sex': 2})
+
+        # Ensure we can also pass set a string representation of the json
+        self.model.meta_data = {"age": 15, "sex": 1}
+        self.model.save()
+        self.assertEqual(self.model.meta_data, {'age': 15, 'sex': 1})
+
+    def test_03_formfield(self):
+        """Ensure the field cleans properly"""
+        initial_data = {'age': 23, 'sex': 1}
+        field = FormField(MetaForm, initial=initial_data)
+
+        # A multi-value field will receive the values as a list
+        # in the correct order of the form
+        self.assertEqual(field.clean([10, 1]), {'age': 10, 'sex': '1'})
+
+        self.assertRaises(ValidationError, field.clean, [])
+
+    def test_04_form(self):
+        """
+        Ensure a django form with the form field is valid if supplied with the
+        correct data
+        """
+        class MyForm(forms.Form):
+            name = forms.CharField()
+            meta_data = FormField(MetaForm)
+
+        form = MyForm()
+        self.assertFalse(form.is_valid())
+
+        # We need to supply the django form with the proper multi value
+        # data i.e. <field>_0 <field>_1 that represent each (inner) form field
+        form = MyForm({'name': 'john', 'meta_data_0': 12, 'meta_data_1': 1})
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+
+        # Ensure the cleaned data is what we expect
+        self.assertEqual(form.cleaned_data,
+            {'name': 'john', 'meta_data': {'age': 12, 'sex': '1'}})
+
+        class MyFormToo(forms.Form):
+            name = forms.CharField()
+            meta_data = FormField('formfield.tests.MetaForm')
+
+        form = MyFormToo()
+        self.assertFalse(form.is_valid())
+
+        form = MyFormToo({'name': 'john', 'meta_data_0': 12, 'meta_data_1': 1})
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+
+        # Ensure the cleaned data is what we expect
+        self.assertEqual(form.cleaned_data,
+            {'name': 'john', 'meta_data': {'age': 12, 'sex': '1'}})
+
+        class MyFormThree(forms.Form):
+            name = forms.CharField()
+            meta_data = FormField(lambda: MetaForm)
+
+        form = MyFormThree()
+        self.assertFalse(form.is_valid())
+
+        form = MyFormThree({'name': 'john', 'meta_data_0': 12, 'meta_data_1': 1})
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+
+        # Ensure the cleaned data is what we expect
+        self.assertEqual(form.cleaned_data,
+            {'name': 'john', 'meta_data': {'age': 12, 'sex': '1'}})
+
+    def test_05_sub_formfield(self):
+        """Ensure sub formfields validate"""
+        payload = {
+            'first_name': '1',
+            'last_name': '2',
+            'suffix': '3',
+            'age': 4,
+            'address': {
+                'street': '5',
+                'country': '6',
+                'zipcode': 7,
+                'coords': {
+                    'lng': '8',
+                    'lat': '9'
+                    }
+                }
+            }
+        field = FormField(PersonForm)
+        data_to_clean = ['1', '2', '3', '4', ['5', '6', '7', ['8', '9']]]
+        self.assertEqual(field.clean(data_to_clean), payload)
+
+    def test_06_invalidate_required_subform_field(self):
+        """Ensure a proper validation error is raised with sub formfield is
+        invalid (required field)"""
+
+        field = FormField(PersonForm)
+        # Invalidate a required field (street)
+        data_to_clean = ['1', '2', '3', '4', [None, '6', '7', ['8', '9']]]
+        self.assertRaises(ValidationError, field.clean, data_to_clean)
+
+    def test_07_invalidate_int_subform_field(self):
+        """Ensure a proper validation error is raised with sub formfield is
+        invalid (integer only)"""
+        field = FormField(PersonForm)
+        # Invalidate a integer field (zipcode)
+        data_to_clean = ['1', '2', '3', '4', ['5', '6', 'aaa', ['8', '9']]]
+        self.assertRaises(ValidationError, field.clean, data_to_clean)
