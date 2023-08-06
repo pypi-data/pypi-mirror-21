25b3e4de6360353d47a315cc51182a8a33f07c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:50 +0000

    Add package vy/2.2.0/vy-2.2.0.tar.gz

---
+++ packages/vy/vy-2.2.0.tar.gz/vy-2.2.0/vyapp/plugins/javascript_completion.py
@@ -0,0 +1,109 @@
+"""
+Overview
+========
+
+This module implements javascript autocompletion using the tern javascript library.
+
+
+Key-Commands
+============
+
+Namespace: javascript-completion
+
+Mode: INSERT
+Event: <Control-Key-period>
+Description: Open the completion window with possible python words for
+completion.
+"""
+
+from vyapp.completion import CompletionWindow, Option
+from subprocess import Popen, PIPE
+import json
+import requests
+import sys
+import atexit
+from os.path import expanduser, join, exists, dirname
+from os import getcwd
+from shutil import copyfile
+from vyapp.plugins import ENV
+from vyapp.areavi import AreaVi
+
+filename = join(expanduser('~'), '.tern-config')
+if not exists(filename): copyfile(join(dirname(__file__), 'tern-config'), filename)
+
+class JavascriptCompletionWindow(CompletionWindow):
+    """
+    """
+
+    def __init__(self, path, port, area, *args, **kwargs):
+        # If ~/.tern-port doesn't exist then run the server.
+        self.path   = path
+        self.port   = port
+        source      = area.get('1.0', 'end')
+        line, col   = area.indcur()
+
+        if not exists(join(expanduser('~'), '.tern-port')): 
+            self.run_server()
+
+        completions = self.completions(source, line - 1, col, area.filename)
+
+        CompletionWindow.__init__(self, area, completions, *args, **kwargs)
+        self.bind('<F1>', lambda event: 
+        sys.stdout.write('/*%s*/\n%s\n' % ('*' * 80, 
+        self.box.selection_docs())))
+
+    def run_server(self):
+        self.child = Popen([self.path, '--port', str(self.port), 
+        '--persistent'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        self.child.stdout.readline()
+        atexit.register(self.child.terminate)
+
+    def completions(self, data, line, col, filename):
+        """
+        """
+
+        payload = {
+                    'query': { 
+                                'type': 'completions', 
+                                'file':filename,
+                                'docs': 'true',
+                                'types': 'true', 
+                                'end': {'line': line, 'ch':col},
+                             },
+
+                    'files': [{"type": "full",
+                               "name": filename,
+                               "text": data}] 
+                  }
+        
+        addr = 'http://localhost:%s' % self.port
+        req  = requests.post(addr, data=json.dumps(payload))
+        return self.build(req.text)
+
+    def build(self, data):
+        data = json.loads(data)
+        return map(lambda ind: Option(**ind), data['completions'])
+
+class JavascriptCompletion(object):
+    def __init__(self, area, path='tern', port=1234):
+        self.path = path
+        self.port = port
+        trigger = lambda event: area.hook('javascript-completion', 'INSERT', '<Control-Key-period>', 
+                  lambda event: JavascriptCompletionWindow(self.path, self.port, event.widget), add=False)
+        remove_trigger = lambda event: area.unhook('INSERT', '<Control-Key-period>')
+        area.install('javascript-completion', (-1, '<<Load-application/x-javascript>>', trigger),
+                     (-1, '<<Load-text/html>>', trigger), 
+                     (-1, '<<Save-application/x-javascript>>', trigger), 
+                     (-1, '<<Save-text/html>>', trigger), 
+                     (-1, '<<LoadData>>', remove_trigger), (-1, '<<SaveData>>', remove_trigger))
+
+def javascript_tools(path='tern', port=1234):
+    active_completion = lambda :AreaVi.ACTIVE.hook('javascript-completion', 'INSERT', '<Control-Key-period>', 
+                  lambda event: JavascriptCompletionWindow(path, port, event.widget), add=False)
+    ENV['active_javascript_completion'] = active_completion
+
+install = JavascriptCompletion
+
+
+
+
---
+++ packages/vy/vy-2.2.0.tar.gz/vy-2.2.0/vyapp/plugins/mc.py
@@ -0,0 +1,114 @@
+from subprocess import check_output, call, Popen
+from os.path import expanduser, dirname, join
+from vyapp.app import root
+
+class Mc(object):
+    COLOR_SCHEME = {'(MC-DIRECTORY)': {'foreground': 'red'},
+    '(MC-FILE)': {'foreground': 'blue'}}
+
+    clipboard = []
+
+    def __init__(self, area):
+        self.area = area
+        self.ph   = expanduser('~')
+
+        area.install('mc', ('NORMAL', '<Key-H>', lambda e: self.up()),
+        ('NORMAL', '<Key-L>', lambda e: self.down()),
+        ('NORMAL', '<Key-Y>', lambda e: self.cp()),
+        ('NORMAL', '<Key-T>', lambda e: self.mv()),
+        ('NORMAL', '<Key-R>', lambda e: self.rm()),
+        ('NORMAL', '<Key-K>', lambda e: self.select()),
+        ('NORMAL', '<Control-g>', lambda e: self.clear_clipboard()),
+        ('NORMAL', '<Key-G>', lambda e: self.list_clipboard()),
+        ('NORMAL', '<Key-I>', lambda e: self.load()),
+        ('NORMAL', '<Key-U>', lambda e: self.open()),
+        ('NORMAL', '<Key-F>', lambda e: self.info()),
+
+        ('NORMAL', '<Key-J>', lambda e:self.ls()))
+
+        for indi, indj in self.COLOR_SCHEME.iteritems():
+            area.tag_config(indi, **indj)
+
+    def list_clipboard(self):
+        self.area.delete('1.0', 'end')
+        self.area.insert('1.0', 'Files in the clipboard!\n\n')
+        self.area.insert('end', '\n'.join(Mc.clipboard))
+
+    def clear_clipboard(self):
+        del Mc.clipboard[:]
+        root.status.set_msg('Cleared mc clipboard!')
+
+    def select(self):
+        filename = self.area.get_line()
+        Mc.clipboard.append('"%s"' % filename)
+        root.status.set_msg('Appended %s!' % filename)
+
+    def down(self):
+        self.ph = self.area.get_line()
+        self.ls()
+
+    def up(self):
+        self.ph = dirname(self.ph)
+        self.ls()
+
+    def info(self):
+        filename = self.area.get_line()
+
+        data = check_output('stat "%s"' % filename, shell=1)
+        self.area.delete('1.0', 'end')
+        self.area.append(data, '(MC-FILE)')
+
+    def ls(self):
+        self.area.delete('1.0', 'end')
+
+        data = check_output('find "%s" -maxdepth 1 -type d' % 
+        self.ph, shell=1)
+        self.area.append(data, '(MC-DIRECTORY)')
+
+        data = check_output('find "%s" -maxdepth 1 -type f' % 
+        self.ph, shell=1)
+        self.area.append(data, '(MC-FILE)')
+
+    def cp(self):
+        destin = self.area.get_line()
+        code = call('cp -R %s "%s"' % (' '.join(Mc.clipboard), 
+        destin), shell=1)
+
+        root.status.set_msg('Files copied!')
+        del Mc.clipboard[:]
+        self.ls()
+
+    def mv(self):
+        destin = self.area.get_line()
+        code = call('mv %s "%s"' % (' '.join(Mc.clipboard), 
+        destin), shell=1)
+
+        root.status.set_msg('Files moved!')
+        del Mc.clipboard[:]
+        self.ls()
+
+    def rm(self):
+        code = call('rm -fr %s' % ' '.join(Mc.clipboard), shell=1)
+        del Mc.clipboard[:]
+        root.status.set_msg('Deleted files!')
+        self.ls()
+
+    def load(self):
+        filename = self.area.get_line()
+        root.note.load([[filename]])
+
+    def open(self):
+        filename = self.area.get_line()
+        # No need for "" because it is passing the entire filename
+        # as parameter.
+        Popen(['xdg-open', '%s'  % filename])
+
+install = Mc
+
+
+
+
+
+
+
+
