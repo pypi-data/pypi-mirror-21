682cc39995dddee4cf3254b0d97c94e4d3b91f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:22 +0000

    Add package pyardu/1/pyardu-1.tar.gz

---
+++ packages/pyardu/pyardu-1.tar.gz/pyardu-1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+name='pyardu',
+version='1',
+description='Example package for pyardu',
+url='https://github.com/RashadGarayev/pyardu',
+author='Rashad Garayev',
+author_email='pythonaz@yahoo.com',
+license='MIT'
+)
