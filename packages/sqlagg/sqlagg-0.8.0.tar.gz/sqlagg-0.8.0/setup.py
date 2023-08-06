b2b8593a31870552dcec50a0c83e1079a916f8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:38 +0000

    Add package sqlagg/0.8.0/sqlagg-0.8.0.tar.gz

---
+++ packages/sqlagg/sqlagg-0.8.0.tar.gz/sqlagg-0.8.0/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='sqlagg',
+    version='0.8.0',
+    description='SQL aggregation tool',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://github.com/dimagi/sql-agg',
+    packages=['sqlagg', 'sqlagg.queries'],
+    license='MIT',
+    install_requires=[
+        'SQLAlchemy>=1.0.9',
+    ],
+    tests_require=[
+        'unittest2==0.5.1',
+        'nose',
+        'SQLAlchemy-Fixtures>=0.1.5',
+        'fixture>=1.4',
+        'psycopg2'
+    ],
+    setup_requires=['nose']
+)
