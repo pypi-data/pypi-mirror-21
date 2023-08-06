642d71f2f5317e153e3c43cd82027ce27a0ea8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package vernam/2.4.0/vernam-2.4.0.tar.gz

---
+++ packages/vernam/vernam-2.4.0.tar.gz/vernam-2.4.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open( path.join(path.abspath(path.dirname(__file__)) , 'README.md' )) as f:
+    long_description = f.read()
+
+setup(
+    name = 'vernam',
+    version = '2.4.0',
+    scripts=['bin/vernam'],
+    entry_points = {
+        'console_scripts': ['vernam=vernam.command_line:main']
+    },
+
+    description = 'Vernam cipher',
+    long_description = long_description,
+
+    url = 'https://github.com/roysoup/Vernam',
+
+    author = 'Roy Siu',
+    author_email = '',
+
+    license = 'MIT Licence',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable'
+    ],
+    
+    install_requires = [''],
+)
