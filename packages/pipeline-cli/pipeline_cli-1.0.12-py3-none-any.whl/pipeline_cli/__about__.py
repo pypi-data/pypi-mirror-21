eb0dd499211ba05003aa1a486976be95ffdc77ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:34 +0000

    Add package pipeline-cli/1.0.12/pipeline_cli-1.0.12-py3-none-any.whl

---
+++ packages/pipeline-cli/pipeline_cli-1.0.12-py3-none-any.whl/pipeline_cli/__about__.py
@@ -0,0 +1,17 @@
+# Copyright 2016 (c) Sepior Aps, all rights reserved.
+
+'''
+   Information about the package.
+'''
+
+__title__ = 'pipeline_cli'
+__description__ = 'Command-line interface to Sepior pipeline server.'
+__url__ = 'https://github.com/Sepior/pipeline-cli'
+__version_info__ = ('1', '0', '6')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Sepior ApS'
+__author_email__ = 'jfn@sepior.com'
+__maintainer__ = 'Janus Friis Nielsen'
+__maintainer_email__ = 'jfn@sepior.com'
+__license__ = 'Proprietary'
+__copyright__ = 'Copyright 2016 Sepior ApS'
