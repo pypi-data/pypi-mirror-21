4d866a07270b86223636e9787fd7350392e94602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:50 +0000

    Add package ziggeo/1.19/ziggeo-1.19.tar.gz

---
+++ packages/ziggeo/ziggeo-1.19.tar.gz/ziggeo-1.19/ZiggeoVideos.py
@@ -0,0 +1,32 @@
+class ZiggeoVideos:
+
+    def __init__(self, application):
+        self.__application = application
+
+    def index(self, data = None):
+        return self.__application.connect.getJSON('/videos/', data)
+
+    def get(self, token_or_key):
+        return self.__application.connect.getJSON('/videos/' + token_or_key + '')
+
+    def download_video(self, token_or_key):
+        return self.__application.connect.get('/videos/' + token_or_key + '/video')
+
+    def download_image(self, token_or_key):
+        return self.__application.connect.get('/videos/' + token_or_key + '/image')
+
+    def push_to_service(self, token_or_key, data = None):
+        return self.__application.connect.postJSON('/videos/' + token_or_key + '/push', data)
+
+    def apply_effect(self, token_or_key, data = None):
+        return self.__application.connect.postJSON('/videos/' + token_or_key + '/effect', data)
+
+    def update(self, token_or_key, data = None):
+        return self.__application.connect.postJSON('/videos/' + token_or_key + '', data)
+
+    def delete(self, token_or_key):
+        return self.__application.connect.delete('/videos/' + token_or_key + '')
+
+    def create(self, data = None, file = None):
+        return self.__application.connect.postJSON('/videos/', data, file)
+
