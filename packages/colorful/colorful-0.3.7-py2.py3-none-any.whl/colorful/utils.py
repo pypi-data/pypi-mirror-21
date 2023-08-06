10c1627a126a0805bf174e78fb8ecb43e1d62644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:12 +0000

    Add package colorful/0.3.7/colorful-0.3.7-py2.py3-none-any.whl

---
+++ packages/colorful/colorful-0.3.7-py2.py3-none-any.whl/colorful/utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+    colorful
+    ~~~~~~~~
+
+    Terminal string styling done right, in Python.
+
+    :copyright: (c) 2017 by Timo Furrer <tuxtimo@gmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+import re
+
+
+def hex_to_rgb(value):
+    """
+    Convert the given hex string to a
+    valid RGB channel triplet.
+    """
+    value = value.lstrip('#')
+    length = len(value)
+
+    check_hex(value, length)
+
+    step = int(length / 3)
+    return tuple(int(value[i:i+step], 16) for i in range(0, length, step))
+
+
+def check_hex(value, length):
+    if length not in (3, 6):
+        raise ValueError('Hex string #{} is too long'.format(value))
+
+    regex = r'[0-9a-f]{{{length}}}'.format(length=length)
+    if not re.search(regex, value, re.I):
+        raise ValueError('Invalid Hex String: #{}'.format(value))
