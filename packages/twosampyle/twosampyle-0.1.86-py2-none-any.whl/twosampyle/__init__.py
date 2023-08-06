6036b2154a475fbd6f61f499f8867946e2f4fe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:59 +0000

    Add package twosampyle/0.1.86/twosampyle-0.1.86-py2-none-any.whl

---
+++ packages/twosampyle/twosampyle-0.1.86-py2-none-any.whl/twosampyle/__init__.py
@@ -0,0 +1,9 @@
+from .permtest import PermTest
+from .permchisquare import ChiSquaredTest
+from .ttest import TTest
+import pandas as pd
+
+
+__all__ = ['PermTest',
+			'TTest'
+            'ChiSquaredTest']
