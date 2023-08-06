5c273e294855c506a0ae6900e5e2349d9a73dab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:10 +0000

    Add package ovation/1.15.0/ovation-1.15.0.tar.gz

---
+++ packages/ovation/ovation-1.15.0.tar.gz/ovation-1.15.0/ovation/util.py
@@ -0,0 +1,20 @@
+import warnings
+import functools
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning) #turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2,
+                      filename=func.func_code.co_filename,
+                      lineno=func.func_code.co_firstlineno + 1)
+        warnings.simplefilter('default', DeprecationWarning) #reset filter
+        return func(*args, **kwargs)
+
+    return new_func
