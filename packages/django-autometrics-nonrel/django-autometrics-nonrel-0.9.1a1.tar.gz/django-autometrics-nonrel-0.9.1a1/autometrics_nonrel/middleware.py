cbfb3aec1297527c8bcea79cad788bdfb5fc2d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:04 +0000

    Add package django-autometrics-nonrel/0.9.1a1/django-autometrics-nonrel-0.9.1a1.tar.gz

---
+++ packages/django-autometrics-nonrel/django-autometrics-nonrel-0.9.1a1.tar.gz/django-autometrics-nonrel-0.9.1a1/autometrics_nonrel/middleware.py
@@ -0,0 +1,47 @@
+import logging
+
+from .models import UserSession
+
+
+log = logging.getLogger(__name__)
+
+
+class UserSessionTrackingMiddleware(object):
+
+    def process_request(self, request):
+        request.autometrics_key = (
+            request.session.session_key
+            if hasattr(request, 'session')
+            else None
+        )
+
+    def process_response(self, request, response):
+        if hasattr(request, 'user'):
+            request_key = request.autometrics_key
+            if request.session.session_key is None:
+                request.session.cycle_key()
+            response_key = request.session.session_key
+            if request_key != response_key:
+                log.debug(
+                    'user {0} session key changed from {1}... to {2}'.format(
+                        request.user,
+                        request_key[:5] if request_key else '<none>',
+                        response_key[:5] if response_key else '<none>',
+                    )
+                )
+                if UserSession.objects.filter(
+                        session=request_key
+                        ).count() == 0:
+                    if UserSession.objects.filter(
+                            session=response_key
+                            ).count() == 0:
+                        response_key = None
+                    request_user = (
+                        None if request.user.is_anonymous() else request.user
+                    )
+                    UserSession.objects.create(
+                        session=request_key,
+                        user=request_user,
+                        previous_id=response_key,
+                    )
+        return response
