a76f92ae471b101bdfeac78b0b70b412d6709d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:50 +0000

    Add package requests_handler/0.0.6/requests_handler-0.0.6-py2.py3-none-any.whl

---
+++ packages/requests_handler/requests_handler-0.0.6-py2.py3-none-any.whl/requests_handler/test_handlers.py
@@ -0,0 +1,39 @@
+import logbook
+from iologger import iologger
+
+from requests_handler import RequestsHandler
+
+
+def test_handler():
+    logger = logbook.Logger("TestLogger")
+    requests_handler = RequestsHandler(
+        "https://autosweet-data.com/api/v1/logs",
+        auth=('admin', 'aut0sw33t'))
+
+    requests_handler.push_application()
+    logger.debug("A debug message")
+    logger.critical("A critical message!")
+    requests_handler.pop_application()
+
+
+def test_iologger():
+    @iologger()
+    def my_func(string: str, integer: int) -> str:
+        return f"my string was {string} and my integer was {integer}."
+
+    requests_handler = RequestsHandler(
+        "https://autosweet-data.com/api/v1/logs",
+        auth=('admin', 'aut0sw33t'))
+
+    requests_handler.push_application()
+    my_func("Abraham", integer=15)
+    my_func("Isaac", 0)
+    requests_handler.pop_application()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    test_iologger()
