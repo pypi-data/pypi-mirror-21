3d1e4609b14f034e7c0d0828a98217ca7d6aa3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:19 +0000

    Add package odoo9-addon-medical-prescription/9.0.1.0.0.99.dev4/odoo9_addon_medical_prescription-9.0.1.0.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-prescription/odoo9_addon_medical_prescription-9.0.1.0.0.99.dev4-py2-none-any.whl/odoo_addons/medical_prescription/models/medical_prescription_order_line.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2004 Tech-Receptives
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class MedicalPrescriptionOrderLine(models.Model):
+    _name = 'medical.prescription.order.line'
+    _inherit = ['abstract.medical.medication']
+    _inherits = {'medical.patient.medication': 'medical_medication_id'}
+    _rec_name = 'medical_medication_id'
+
+    prescription_order_id = fields.Many2one(
+        comodel_name='medical.prescription.order',
+        string='Prescription Order',
+        required=True,
+    )
+    medical_medication_id = fields.Many2one(
+        comodel_name='medical.patient.medication',
+        string='Medication',
+        required=True,
+        ondelete='cascade',
+    )
+    is_substitutable = fields.Boolean()
+    qty = fields.Float(
+        string='Quantity',
+    )
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        args = args or []
+        domain = [
+            '|', '|', '|', '|',
+            ('medicament_id.product_id.name', operator, name),
+            ('medicament_id.strength', operator, name),
+            ('medicament_id.strength_uom_id.name', operator, name),
+            ('medicament_id.drug_form_id.code', operator, name),
+            ('patient_id.name', operator, name),
+        ]
+        recs = self.search(domain + args, limit=limit)
+        return recs.name_get()
