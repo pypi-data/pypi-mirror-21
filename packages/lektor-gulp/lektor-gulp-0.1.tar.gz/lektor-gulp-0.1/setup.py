17fdcac907b0cf38df65fb42136727a366b7bbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:17 +0000

    Add package lektor-gulp/0.1/lektor-gulp-0.1.tar.gz

---
+++ packages/lektor-gulp/lektor-gulp-0.1.tar.gz/lektor-gulp-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lektor-gulp',
+    version='0.1',
+    author=u'Maurizio Turatti',
+    author_email='maurizio@softinstigate.com',
+    url = "https://github.com/SoftInstigate/lektor-gulp",
+    license='BSD',
+    py_modules=['lektor_gulp'],
+    entry_points={
+        'lektor.plugins': [
+            'gulp = lektor_gulp:GulpPlugin',
+        ]
+    }
+)
