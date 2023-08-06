e180b7f4ae090bfa0390d30fcf1aff9b115ce38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:32 +0000

    Add package reusables/0.9.0/reusables-0.9.0.tar.gz

---
+++ packages/reusables/reusables-0.9.0.tar.gz/reusables-0.9.0/reusables/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Part of the Reusables package.
+#
+# Copyright (c) 2014-2017 - Chris Griffith - MIT License
+from __future__ import absolute_import
+from .namespace import *
+from .dt import *
+from .log import *
+from .wrappers import *
+from .web import *
+from .shared_variables import *
+from .cli import *
+from .file_operations import *
+from .processes import *
+from .string_manipulation import *
+
+__author__ = "Chris Griffith"
+__version__ = "0.9.0"
