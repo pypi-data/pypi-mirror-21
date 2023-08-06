eeb869cb0b134daa46c44f376fa27dfafdd5a5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:05 +0000

    Add package kgit/1.0.3/kgit-1.0.3.tar.gz

---
+++ packages/kgit/kgit-1.0.3.tar.gz/kgit-1.0.3/kgit/enforce.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+########################################################################################################################
+#
+# MIT License
+#
+# Copyright (c) [2016] [Kris Nova] [kris@nivenly.com]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+########################################################################################################################
+
+import kgit, os, time, subprocess
+
+my_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+#
+# enforce_p
+#
+# This will actually enforce the git hooks for our profiles
+# Note: enforce requires a workspace directory
+# TODO This should have tab hinting and support ~ expansion
+def run():
+    kgit.out("Enforcing git hooks across workspaces")
+    workspaces = kgit.get_file("workspaces")
+    if workspaces == "":
+        kgit.out("No workspaces are configured for kgit")
+        w = raw_input("Add workspace (Enter no value to move on): ")
+        workspacesToWrite = w + "\n"
+        while w != "":
+            w = raw_input("Add workspace (Enter no value to move on): ")
+            workspacesToWrite += w + "\n"
+        kgit.write_data(workspacesToWrite, "workspaces")
+        kgit.out("Writing workspaces")
+    build_hook()
+    workspace_init()
+
+
+#
+# workspace_init
+#
+# Will init all valid git repositories in each of the configured workspaces
+def workspace_init():
+    workspaces = kgit.get_file("workspaces").split("\n")
+    for workspace in workspaces:
+        if workspace == "":
+            continue
+        for dir in os.listdir(workspace):
+            if is_git_repo(workspace + "/" + dir):
+                # Idempotent enforcemenet
+                pre_commit = workspace + "/" + dir + "/.git/hooks/pre-commit"
+                try:
+                    os.unlink(pre_commit)
+                except:
+                    pass
+                p = subprocess.Popen(["git", "init"], cwd=workspace + "/" + dir, stdout=subprocess.PIPE)
+                out, err = p.communicate()
+                errcode = p.returncode
+                kgit.out(out)
+                # +x for good measure
+                st = os.stat(pre_commit)
+                os.chmod(pre_commit, st.st_mode | 0o111)
+
+
+#
+# is_git_repo
+#
+# Well validate a dir is a valid git repo
+def is_git_repo(dir):
+    if os.path.exists(dir + "/.git"):
+        return True
+    return False
+
+
+#
+# build_hook
+#
+# Will build the pre-commit git template
+def build_hook():
+    tag = "\n[init]\n"
+    tag += "        templatedir = ~/.kgit\n"
+
+    # Write the tag if nothing exists
+    if not os.path.exists(os.path.expanduser('~') + "/.gitconfig"):
+        f = open(os.path.expanduser('~') + "/.gitconfig", 'w')
+        f.write(tag)
+        f.close()
+
+    # Get the config
+    with open(os.path.expanduser('~') + "/.gitconfig") as f:
+        config = f.read()
+
+    # Check if [init] is already in there
+    if "templatedir = ~/.kgit" not in config:
+        newConfig = config + tag
+        f = open(os.path.expanduser('~') + "/.gitconfig", 'w')
+        f.write(newConfig)
+        f.close()
+
+    var_replace = "KGIT_MANAGED_REPOS_DICT = {"
+    profiles = kgit.get_file("profiles").split("\n")
+    i = 0
+    plen = len(profiles)
+    for line in profiles:
+        if line == "":
+            continue
+        vals = line.split("|")  # 0-repo 1-email 2-name
+        str = '"' + vals[0] + '": ["' + vals[1] + '", "' + vals[2] + '"]'
+        if i != plen - 2:
+            str += ","
+        i += 1
+        var_replace += str
+    var_replace += "}"
+
+    # Write the template
+    with open(my_dir + "/pre-commit.py") as f:
+        template = f.read()
+
+    # Replace config
+    newTemplate = template.replace('KGIT_MANAGED_REPOS_DICT = ""', var_replace)
+    newTemplate = newTemplate.replace('KGIT_MANAGED_VERSIONS = ""', 'KGIT_MANAGED_VERSIONS = "' + kgit.version + '"')
+    newTemplate = newTemplate.replace("<<t>>", time.strftime("%a, %d %b %Y %H:%M:%S"))
+
+    # Idempotent Write
+    try:
+        os.unlink(os.path.expanduser('~') + "/.kgit/hooks/pre-commit")
+    except:
+        pass
+    f = open(os.path.expanduser('~') + "/.kgit/hooks/pre-commit", 'w')
+    f.write(newTemplate)
+    f.close()
---
+++ packages/kgit/kgit-1.0.3.tar.gz/kgit-1.0.3/kgit/pre-commit.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+########################################################################################################################
+# MIT License
+#
+# Copyright (c) [2016] [Kris Nova] [kris@nivenly.com]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+########################################################################################################################
+#
+# [pre-commit]
+#
+# This is an AUTOMATICALLY GENERATED pre-commit executable that was created by kgit. This script will attempt to
+# enforce kgit profile configuration for every commit.
+#
+# creation time : <<t>>
+#
+# It is recommended to let kgit manage this file.
+#
+########################################################################################################################
+
+import os, sys, subprocess
+
+########################################################################################################################
+# Automatically generated variables by kgit
+# DO NOT CHANGE
+#
+#
+KGIT_MANAGED_REPOS_DICT = ""
+KGIT_MANAGED_VERSIONS = ""
+#
+#
+# DO NOT CHANGE
+# Automatically generated variables by kgit
+########################################################################################################################
+
+# Friendly names
+r = KGIT_MANAGED_REPOS_DICT
+version = KGIT_MANAGED_VERSIONS
+
+# The directory of this script
+my_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Repository configuration at time of commit
+vars = {}
+
+
+def main():
+    global vars
+    out("===========================================================================================")
+    out("[kgit] v" + version)
+    out("This pre-commit hook is located in " + my_dir)
+    vars = get_git_config_vars()
+    s = validate()
+    if s:
+        # Okay!
+        out("Valid commit..")
+        exit_code = 0
+    else:
+        # Fail!
+        exit_code = 1
+
+    out("===========================================================================================")
+    sys.exit(exit_code)
+
+
+def validate():
+    this_remote = vars["remote.origin.url"]
+    this_user = vars["user.name"]
+    this_email = vars["user.email"]
+
+    for repo in r:
+        email = r[repo][0]
+        user = r[repo][1]
+
+        # We have a kgit managed profile
+        if repo in this_remote:
+            out("Running in kgit managed repository")
+            if this_user == user and this_email == email:
+                return True
+            else:
+                out("Invalid Username/Email configuration!")
+                out("----------------------------------------------------")
+                out("| Current   : " + this_user + " " + this_email)
+                out("| Expecting : " + user + " " + email)
+                out("----------------------------------------------------")
+                try_to_recover(this_remote)
+                return False
+
+    out("This repository is not managed by kgit")
+    return True
+
+
+def try_to_recover(remote):
+    with open(os.path.expanduser('~') + "/.kgit/profiles") as f:
+        profiles = f.read()
+
+    for line in profiles.split("\n"):
+        if line == "":
+            continue
+        vals = line.split("|")  # 0-repo 1-email 2-name
+        if vals[0] in remote:
+            out("Found matching profile: " + vals[0])
+            yn = raw_input("Switch profiles? [y/n] : ")
+            if yn != "y":
+                return
+            subprocess.Popen(["git", "config", "--global", "user.name", vals[2]])
+            subprocess.Popen(["git", "config", "--global", "user.email", vals[1]])
+            out("Profile updated. You can now try to commit again.")
+
+
+def get_git_config_vars():
+    vars = {}
+    raw = sh("git config -l")
+    for line in raw.split("\n"):
+        if line == "":
+            continue
+        kv = line.split("=")
+        vars[kv[0]] = kv[1]
+    return vars
+
+
+def sh(cmd):
+    p = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
+
+########################################################################################################################
+#
+# OUTPUT
+#
+def out(message):
+    print "\033[1mkgit : \033[0m \033[95m%s\033[0m" % (message)
+
+
+if __name__ == "__main__":
+    main()
