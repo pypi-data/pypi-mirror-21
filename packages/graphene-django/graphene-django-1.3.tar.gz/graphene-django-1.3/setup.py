c4baf54991cad27cfb98408fd72072806145a9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:31 +0000

    Add package graphene-django/1.3/graphene-django-1.3.tar.gz

---
+++ packages/graphene-django/graphene-django-1.3.tar.gz/graphene-django-1.3/setup.py
@@ -0,0 +1,61 @@
+from setuptools import find_packages, setup
+
+tests_require = [
+    'pytest>=2.7.2',
+    'pytest-cov',
+    'coveralls',
+    'mock',
+    'pytz',
+    'django-filter',
+    'pytest-django==2.9.1',
+]
+
+setup(
+    name='graphene-django',
+    version='1.3',
+
+    description='Graphene Django integration',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/graphql-python/graphene-django',
+
+    author='Syrus Akbary',
+    author_email='me@syrusakbary.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='api graphql protocol rest relay graphene',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[
+        'six>=1.10.0',
+        'graphene>=1.4',
+        'Django>=1.6.0',
+        'iso8601',
+        'singledispatch>=3.4.0.3',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+)
