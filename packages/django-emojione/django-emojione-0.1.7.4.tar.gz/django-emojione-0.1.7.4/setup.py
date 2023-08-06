53ef9a6c3a5a407d9b5ab25fa97d264b292c9abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:02 +0000

    Add package django-emojione/0.1.7.4/django-emojione-0.1.7.4.tar.gz

---
+++ packages/django-emojione/django-emojione-0.1.7.4.tar.gz/django-emojione-0.1.7.4/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-emojione',
+    version='0.1.7.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Wrapper for emojione to use in django.',
+    url='https://github.com/phuong/django-emojione',
+    download_url='https://github.com/phuong/django-emojione/archive/master.zip',
+    author='Phuong',
+    author_email='boingonline@gmail.com',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        "emojipy>=0.1.3",
+    ],
+)
