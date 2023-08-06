7bf8c2a6195a04971f2fd113736e3b2da14d3709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:14 +0000

    Add package asphalt-serialization/4.0.0/asphalt-serialization-4.0.0.tar.gz

---
+++ packages/asphalt-serialization/asphalt-serialization-4.0.0.tar.gz/asphalt-serialization-4.0.0/tests/test_component.py
@@ -0,0 +1,34 @@
+from asphalt.core.context import Context
+import pytest
+
+from asphalt.serialization.api import Serializer
+from asphalt.serialization.component import SerializationComponent
+from asphalt.serialization.serializers.json import JSONSerializer
+
+
+@pytest.mark.asyncio
+async def test_component_start():
+    component = SerializationComponent(serializers={
+        'json': {'encoder_options': {'allow_nan': False}},
+        'msgpack': {'unpacker_options': {'encoding': 'iso-8859-1'}},
+        'pickle': {'protocol': 3},
+        'yaml': {'safe': False}
+    })
+    async with Context() as ctx:
+        await component.start(ctx)
+
+        assert ctx.json
+        assert ctx.msgpack
+        assert ctx.pickle
+        assert ctx.yaml
+
+
+@pytest.mark.asyncio
+async def test_default_config():
+    component = SerializationComponent(backend='json')
+    async with Context() as ctx:
+        await component.start(ctx)
+
+        resource = ctx.require_resource(Serializer)
+        assert isinstance(resource, JSONSerializer)
+        assert ctx.json is resource
