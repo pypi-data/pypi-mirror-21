846c7f7cab1d2fbad4c52d54b85eae617c6e22d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package nutsml/1.0.7/nutsml-1.0.7-py2-none-any.whl

---
+++ packages/nutsml/nutsml-1.0.7-py2-none-any.whl/nutsml/examples/mnist/view_train_images.py
@@ -0,0 +1,6 @@
+from mlp_train import load_samples
+from nutsflow import Take, Consume
+from nutsml import ViewImageAnnotation
+
+train_samples, val_samples = load_samples()
+(train_samples >> Take(10) >> ViewImageAnnotation(0, 1, pause=1) >> Consume())
