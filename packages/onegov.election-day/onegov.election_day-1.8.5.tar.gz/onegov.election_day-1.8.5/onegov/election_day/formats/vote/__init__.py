7f5e3bf2c4a3940afcc66092d39983019f34e8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package onegov.election-day/1.8.5/onegov.election_day-1.8.5.tar.gz

---
+++ packages/onegov.election-day/onegov.election_day-1.8.5.tar.gz/onegov.election_day-1.8.5/onegov/election_day/formats/vote/__init__.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import object_session
+
+
+BALLOT_TYPES = {'proposal', 'counter-proposal', 'tie-breaker'}
+
+HEADERS = [
+    'Bezirk',
+    'ID',
+    'Name',
+    'Ja Stimmen',
+    'Nein Stimmen',
+    'Stimmberechtigte',
+    'Leere Stimmzettel',
+    'Ungültige Stimmzettel'
+]
+
+
+def clear_ballot(ballot):
+    session = object_session(ballot)
+    for result in ballot.results:
+        session.delete(result)
+
+
+def guessed_group(entity, other):
+    result = entity['name']
+
+    if other:
+        if '/' in other[0].group:
+            result = '/'.join(
+                p for p in (
+                    entity.get('district'),
+                    entity.get('name')
+                ) if p is not None
+            )
+
+    return result
