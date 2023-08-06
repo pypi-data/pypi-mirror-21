2a0d01749cd978fe6488b010e6cce58e12c367d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package seq2vec/0.3.0/seq2vec-0.3.0.tar.gz

---
+++ packages/seq2vec/seq2vec-0.3.0.tar.gz/seq2vec-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='seq2vec',
+    version='0.3.0',
+    description='A pip package',
+    license='GNU 3.0',
+    author='cph',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'keras',
+        'yoctol_utils',
+        'gensim',
+        'h5py'
+    ],
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
