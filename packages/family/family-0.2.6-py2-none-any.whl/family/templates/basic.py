9bc767b0480f27274daaa244725dffc72ff7ada1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:28 +0000

    Add package family/0.2.6/family-0.2.6-py2-none-any.whl

---
+++ packages/family/family-0.2.6-py2-none-any.whl/family/templates/basic.py
@@ -0,0 +1,46 @@
+
+
+
+import os
+from paste.script.templates import Template, var
+from paste.util.template import paste_script_template_renderer
+
+
+class FalconTemplate(Template):
+
+    _template_dir = 'falcon'
+    summary = "A basic daixm microserivce based on falcon"
+    vars = [
+        var('version', 'Version (like 0.1)'),
+        var('description', 'One-line description of the package'),
+        var('long_description', 'Multi-line description (in reST)'),
+        var('keywords', 'Space-separated keywords/tags'),
+        var('author', 'Author name'),
+        var('author_email', 'Author email'),
+        var('url', 'URL of homepage'),
+        ]
+
+    template_renderer = staticmethod(paste_script_template_renderer)
+
+    def post(self, command, output_dir, vars):
+        os.rename('%s/gitignore' % output_dir, '%s/.gitignore' % output_dir)
+
+
+class FlaskTemplate(Template):
+
+    _template_dir = 'flask'
+    summary = "A basic daixm microserivce based on flask"
+    vars = [
+        var('version', 'Version (like 0.1)'),
+        var('description', 'One-line description of the package'),
+        var('long_description', 'Multi-line description (in reST)'),
+        var('keywords', 'Space-separated keywords/tags'),
+        var('author', 'Author name'),
+        var('author_email', 'Author email'),
+        var('url', 'URL of homepage'),
+        ]
+
+    template_renderer = staticmethod(paste_script_template_renderer)
+
+    def post(self, command, output_dir, vars):
+        os.rename('%s/gitignore' % output_dir, '%s/.gitignore' % output_dir)
