87b886e89cfc46fcd114c790be46b4d756bf7daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:59 +0000

    Add package serial_mock/0.1/serial_mock-0.1.zip

---
+++ packages/serial_mock/serial_mock-0.1.zip/serial_mock-0.1/serial_mock/__init__.py
@@ -0,0 +1,6 @@
+from decorators import serial_query
+from mock import MockSerial, EmittingSerial, DummySerial
+
+
+
+__VERSION__ = 0.1
---
+++ packages/serial_mock/serial_mock-0.1.zip/serial_mock-0.1/setup.py
@@ -0,0 +1,17 @@
+import os
+from distutils.core import setup
+
+import sys
+
+setup(
+    name='serial_mock',
+    version='0.1',
+    packages=['serial_mock'],
+    url='http://serialmock.readthedocs.io/en/latest/',
+    download_url="https://github.com/joranbeasley/SerialMock",
+    license='MIT',
+    author='Joran Beasley',
+    author_email='joranbeasley',
+    description='mocks serial ports for use with com0com(windows) or socat(*nix)'
+)
+
