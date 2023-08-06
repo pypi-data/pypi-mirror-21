4952e50fbf8592ec4def666f9428e1dcfd4d1c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:06 +0000

    Add package tag/0.3.2/tag-0.3.2.tar.gz

---
+++ packages/tag/tag-0.3.2.tar.gz/tag-0.3.2/tag/cli/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (C) 2016 Daniel Standage <daniel.standage@gmail.com>
+#
+# This file is part of tag (http://github.com/standage/tag) and is licensed
+# under the BSD 3-clause license: see LICENSE.
+# -----------------------------------------------------------------------------
+
+import argparse
+import sys
+import tag
+from . import gff3
+from . import occ
+from . import pmrna
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', action='version',
+                        version='tag v{}'.format(tag.__version__))
+    parser.add_argument('-l', '--logfile', metavar='FILE', default=sys.stderr,
+                        type=argparse.FileType('w'))
+    subparsers = parser.add_subparsers(dest='cmd', metavar='cmd',
+                                       help='gff3 | occ | pmrna')
+    tag.cli.gff3.subparser(subparsers)
+    tag.cli.occ.subparser(subparsers)
+    tag.cli.pmrna.subparser(subparsers)
+
+    return parser
