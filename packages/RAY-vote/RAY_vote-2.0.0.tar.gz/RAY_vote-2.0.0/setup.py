ead6e39c26ba197c70b888742827cb3085ec041b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:19 +0000

    Add package RAY-vote/2.0.0/RAY_vote-2.0.0.tar.gz

---
+++ packages/RAY-vote/RAY_vote-2.0.0.tar.gz/RAY_vote-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(name='RAY_vote',
+      version='2.0.0',
+      description='A voting program',
+      py_modoules=['RAY_vote'],
+      author='ray',
+      author_email='leiou@sina.com',
+      url='http://user.qzone.qq.com/187357880',
+      keywords='sample voting Python')
+
