94ca29c733857571079348995e8042014259467c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:15 +0000

    Add package quickbase2/0.1/quickbase2-0.1.tar.gz

---
+++ packages/quickbase2/quickbase2-0.1.tar.gz/quickbase2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quickbase2',
+      version='0.1',
+      description='quickbase sdk python package',
+      url='http://github.com/Evoluto/quickbas-sdk-python',
+      author='unknown',
+      author_email='support@quickbase.com',
+      license='MIT',
+      packages=['quickbase2'],
+      zip_safe=False)
