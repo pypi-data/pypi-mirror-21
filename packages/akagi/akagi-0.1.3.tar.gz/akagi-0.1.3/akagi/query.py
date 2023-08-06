4814743877952b80dc787c02618a2a49b0f4c7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:54 +0000

    Add package akagi/0.1.3/akagi-0.1.3.tar.gz

---
+++ packages/akagi/akagi-0.1.3.tar.gz/akagi-0.1.3/akagi/query.py
@@ -0,0 +1,40 @@
+import re
+
+
+class Query(object):
+    def __init__(self, body):
+        self.body = body
+
+    def wrap(self, query):
+        raise NotImplementedError
+
+    def __str__(self):
+        return self.body
+
+class UnloadQuery(Query):
+    def __init__(self, body, bundle, sort=False):
+        super(UnloadQuery, self).__init__(body)
+
+        self.bundle = bundle
+        self.sort = sort
+
+    @classmethod
+    def wrap(cls, query_str, bundle, sort=False):
+        return UnloadQuery(query_str, bundle, sort)
+
+    @property
+    def sql(self):
+        return """
+unload ('%(query)s')
+to '%(bundle_url)s'
+credentials '%(credential_string)s'
+gzip
+allowoverwrite
+parallel %(enable_sort)s
+delimiter ',' escape addquotes
+            """ % ({
+                'query': re.sub(r"'", "\\\\'", self.body),
+                'bundle_url': self.bundle.url,
+                'credential_string': self.bundle.credential_string,
+                'enable_sort': "on" if self.sort else "off"
+                })
