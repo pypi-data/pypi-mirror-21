f0de5dc4e0cf96f112e97683c39f483c8a842738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:25 +0000

    Add package ftport/0.0.2/ftport-0.0.2.tar.gz

---
+++ packages/ftport/ftport-0.0.2.tar.gz/ftport-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ftport',
+    version='0.0.2',
+    description='Python ftplib extension for registering an alternative IP for'
+                ' the PORT command',
+    url='https://github.com/jgomo3/ftport',
+    author='Jesús Gómez',
+    author_email='jgomo3@gmail.com',
+    license='MIT',
+    packages=['ftport'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',  
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Internet :: File Transfer Protocol (FTP)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='ftp-client ftp',
+)
