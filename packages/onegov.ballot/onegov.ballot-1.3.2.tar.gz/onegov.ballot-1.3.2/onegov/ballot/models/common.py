2195b95368d6775cbb7811287cd536c0bd284394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:28 +0000

    Add package onegov.ballot/1.3.2/onegov.ballot-1.3.2.tar.gz

---
+++ packages/onegov.ballot/onegov.ballot-1.3.2.tar.gz/onegov.ballot-1.3.2/onegov/ballot/models/common.py
@@ -0,0 +1,34 @@
+from onegov.core.orm.types import JSON
+from sqlalchemy import Column, Enum
+from sqlalchemy.ext.declarative import declared_attr
+
+
+class DomainOfInfluenceMixin(object):
+
+    #: defines the scope of the vote - eCH-0155 calls this the domain of
+    #: influence. Unlike eCH-0155 we refrain from putting this in a separate
+    #: model. We also only include domains we currently support.
+    @declared_attr
+    def domain(cls):
+        return Column(
+            Enum(
+                'federation',
+                'canton',
+                'municipality',
+                name='domain_of_influence'
+            ),
+            nullable=False
+        )
+
+
+class MetaMixin(object):
+    """ Mixin providing a meta/content JSON pair. Meta is a JSON column loaded
+    with each request, content is a JSON column loaded deferred (to be shown
+    only in the detail view).
+
+    """
+
+    #: metadata associated with the form, for storing small amounts of data
+    @declared_attr
+    def meta(cls):
+        return Column(JSON, nullable=False, default=dict)
