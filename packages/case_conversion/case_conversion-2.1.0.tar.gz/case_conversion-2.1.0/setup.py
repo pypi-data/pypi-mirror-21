d4b367b4cf53d65fab8a2955462cf9c3b3115c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:50 +0000

    Add package case_conversion/2.1.0/case_conversion-2.1.0.tar.gz

---
+++ packages/case_conversion/case_conversion-2.1.0.tar.gz/case_conversion-2.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+  name='case_conversion',
+  version='2.1.0',
+  packages=['case_conversion'],
+  install_requires=['regex>=2016.2.25'],
+  description='Convert between different types of cases (unicode supported)',
+  license='MIT',
+  author='Alejandro Frias',
+  author_email='joker454@gmail.com',
+  url='https://github.com/AlejandroFrias/case-conversion',
+  download_url='https://github.com/AlejandroFrias/case-conversion/tarball/2.0.0',
+  keywords='case convert conversion camel pascal snake kebab spinal unicode',
+  classifiers=[],
+)
