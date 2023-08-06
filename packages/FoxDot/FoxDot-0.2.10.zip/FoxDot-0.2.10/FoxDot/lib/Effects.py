8c27dbb218a41d2a28a3140ddee42894028c1551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:21 +0000

    Add package FoxDot/0.2.10/FoxDot-0.2.10.zip

---
+++ packages/FoxDot/FoxDot-0.2.10.zip/FoxDot-0.2.10/FoxDot/lib/Effects.py
@@ -0,0 +1,135 @@
+from Settings import EFFECTS_DIR, SC3_PLUGINS
+from ServerManager import Server
+
+class Effect:
+    server=Server
+    def __init__(self, foxdot_name, synthdef, args=[]):
+
+        self.name      = foxdot_name
+        self.synthdef  = synthdef
+        self.filename  = EFFECTS_DIR + "/{}.scd".format(self.synthdef)
+        self.args      = args
+        self.effects   = []
+        
+    def __repr__(self):
+        return "<Fx '{}' -- args: {}>".format(self.synthdef, ",".join(self.args))
+
+    def add(self, string):
+        self.effects.append(string)
+        return
+
+    def list_effects(self):
+        s = ""
+        for p in self.effects:
+            s += p + ";\n"
+        return s
+        
+    def __str__(self):
+        s  = "SynthDef.new(\{},\n".format(self.synthdef)
+	s += "{" + "|bus, {}|\n".format(", ".join(self.args))
+        s += "var osc;\n"
+        s += "osc = In.ar(bus, 2);\n"
+        s += self.list_effects()
+        s += "ReplaceOut.ar(bus, osc)}).add;"
+        return s
+
+    def save(self):
+        ''' writes to file and sends to server '''
+        with open(self.filename, 'w') as f:
+            f.write(self.__str__())
+        if self.server is not None:
+            self.server.loadSynthDef(self.filename)
+        return
+
+class PreEffect(Effect):
+    """ SynthDef that modulates argumentes such as frequency
+        *before* being used in a UGen. """
+    def __init__(self, *args, **kwargs):
+        Effect.__init__(self, *args, **kwargs)
+
+class Out(Effect):
+    def __init__(self):
+        Effect.__init__(self, 'makeSound', 'makeSound')
+        self.save()
+    def __str__(self):
+        s  = "SynthDef.new(\makeSound,\n"
+	s += "{ arg bus, sus; var osc;\n"
+	s += "	osc = In.ar(bus, 2);\n"
+	s += "	Line.ar(dur: sus, doneAction: 14);\n"
+	s += "	DetectSilence.ar(osc, amp:0.0001, time: 0.1, doneAction: 14);\n"
+	s += "	Out.ar(0, osc)}).add;\n"
+	return s
+
+class EffectManager(dict):
+    def __init__(self):
+
+        dict.__init__(self)
+        self.pre_kw=[]
+        self.kw=[]
+
+    def new(self, foxdot_arg_name, synthdef, args):
+        self[foxdot_arg_name] = Effect(foxdot_arg_name, synthdef, args)
+        self.kw.append(foxdot_arg_name)
+        return self[foxdot_arg_name]
+    
+    def new_pre_effect(self, foxdot_arg_name, synthdef, args):
+        self[foxdot_arg_name] = PreEffect(foxdot_arg_name, synthdef, args)
+        self.pre_kw.append(foxdot_arg_name)
+        return self[foxdot_arg_name]
+
+    def kwargs(self):
+        return tuple(self.pre_kw) + tuple(self.kw)
+
+    def __iter__(self):
+        for key in self.pre_kw + self.kw:
+            yield key, self[key]
+    
+
+FxList = EffectManager()
+
+# Frequency Effects
+
+##fx = FxList.new_pre_effect("vibrato", "vibrato", ["vibrato", "freq"])
+##fx.add("osc = Vibrato.ar()")
+##fx.save()
+
+# Sound effects
+
+fx = FxList.new('hpf','highPassFilter',['hpf'])
+fx.add('osc = HPF.ar(osc, hpf)')
+fx.save()
+
+fx = FxList.new('lpf','lowPassFilter',['lpf'])
+fx.add('osc = LPF.ar(osc, lpf)')
+fx.save()
+
+if SC3_PLUGINS:
+
+    fx = FxList.new('bits', 'bitcrush', ['bits', 'sus', 'amp'])
+    fx.add("osc = Decimator.ar(osc, rate: 44100, bits: bits)")
+    fx.add("osc = osc * Line.ar(amp * 0.85, 0.0001, sus * 2)") 
+    fx.save()
+
+fx = FxList.new('chop', 'chop', ['chop', 'sus'])
+fx.add("osc = osc * LFPulse.ar(chop / sus, add: 0.1)")
+fx.save()
+
+fx = FxList.new('echo', 'combDelay', ['echo'])
+fx.add('osc = osc + CombL.ar(osc, delaytime: echo, maxdelaytime: 2)')
+fx.save()
+
+fx = FxList.new('spin', 'spinPan', ['spin','sus'])
+fx.add('osc = osc * [FSinOsc.ar(spin / 2, iphase: 1, mul: 0.5, add: 0.5), FSinOsc.ar(spin / 2, iphase: 3, mul: 0.5, add: 0.5)]')
+fx.save()
+
+fx = FxList.new("cut", "trimLength", ["cut", "sus"])
+fx.add("osc = osc * EnvGen.ar(Env(levels: [1,1,0.01], curve: 'step', times: [sus * cut, 0.01]))")
+fx.save()
+
+fx = FxList.new('verb', 'reverb', ['verb', 'room'])
+fx.add("osc = FreeVerb.ar(osc, verb, room)")
+fx.save()
+
+Out()
+
+    
---
+++ packages/FoxDot/FoxDot-0.2.10.zip/FoxDot-0.2.10/FoxDot/lib/Workspace/MenuBar.py
@@ -0,0 +1,76 @@
+from Tkinter import Menu
+
+class MenuBar(Menu):
+    def __init__(self, master, visible=True):
+
+        self.root = master
+
+        Menu.__init__(self, master.root)
+
+        # Set font
+        
+        self.config(font="CodeFont")
+
+        # File menu
+
+        filemenu = Menu(self, tearoff=0)
+        filemenu.add_command(label="New Document",  command=self.root.newfile,  accelerator="Ctrl+N")
+        filemenu.add_command(label="Open",          command=self.root.openfile, accelerator="Ctrl+O")
+        filemenu.add_command(label="Save",          command=self.root.save,     accelerator="Ctrl+S")
+        filemenu.add_command(label="Save As...",    command=self.root.saveAs )
+        self.add_cascade(label="File", menu=filemenu)
+
+        # Edit menu
+
+        editmenu = Menu(self, tearoff=0)
+        editmenu.add_command(label="Undo",        command=self.root.undo, accelerator="Ctrl+Z")
+        editmenu.add_command(label="Redo",        command=self.root.redo, accelerator="Ctrl+Y")
+        editmenu.add_separator()
+        editmenu.add_command(label="Cut",        command=self.root.edit_cut,   accelerator="Ctrl+X")
+        editmenu.add_command(label="Copy",       command=self.root.edit_copy,  accelerator="Ctrl+C")
+        editmenu.add_command(label="Paste",      command=self.root.edit_paste, accelerator="Ctrl+V")
+        editmenu.add_command(label="Select All", command=self.root.selectall,  accelerator="Ctrl+A")
+        editmenu.add_separator()
+        editmenu.add_command(label="Increase Font Size",      command=self.root.zoom_in, accelerator="Ctrl+=")
+        editmenu.add_command(label="Decrease Font Size",      command=self.root.zoom_out, accelerator="Ctrl+-")
+        editmenu.add_separator()
+        editmenu.add_command(label="Toggle Menu", command=self.root.toggle_menu, accelerator="Ctrl+M")
+        self.add_cascade(label="Edit", menu=editmenu)
+
+        # Code menu
+
+        codemenu = Menu(self, tearoff=0)
+        codemenu.add_command(label="Evaluate Block",         command=self.root.exec_block,  accelerator="Ctrl+Return")
+        codemenu.add_command(label="Evaluate Line",          command=self.root.exec_line,   accelerator="Alt+Return")
+        codemenu.add_command(label="Clear Scheduling Clock", command=self.root.killall,     accelerator="Ctrl+.")
+        codemenu.add_command(label="Toggle Console",         command=self.root.toggle_console)
+        codemenu.add_command(label="Export Console Log",     command=self.root.export_console)
+        self.add_cascade(label="Code", menu=codemenu)
+
+        # Settings
+
+##        settingsmenu = Menu(self, tearoff=0)
+##        settingsmenu.add_command(label="Preferences...",      command=self.root.toggleMenu)
+##        settingsmenu.add_command(label="Change Colours...",   command=self.root.toggleMenu)
+##        settingsmenu.add_command(label="Open Samples Folder", command=self.root.toggleMenu)
+##        self.add_cascade(label="Settings", menu=settingsmenu)
+
+        # Help
+
+        helpmenu = Menu(self, tearoff=0)
+        helpmenu.add_command(label="Visit FoxDot Homepage", command=self.root.openhomepage)
+        helpmenu.add_command(label="Documentation",   command=self.root.opendocumentation)
+        self.add_cascade(label="Help", menu=helpmenu)
+
+        # Add to root
+
+        self.visible = visible
+        
+        if self.visible:
+            
+            master.root.config(menu=self)
+
+    def toggle(self):
+        self.root.root.config(menu=self if not self.visible else 0)
+        self.visible = not self.visible
+        return
