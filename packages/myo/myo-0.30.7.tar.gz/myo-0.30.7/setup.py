0ae27622265fbb53713a3e8700b54ded62532bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package myo/0.30.7/myo-0.30.7.tar.gz

---
+++ packages/myo/myo-0.30.7.tar.gz/myo-0.30.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version_parts = (0, 30, 7)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='myo',
+    description='command and layout management for neovim',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/myo',
+    packages=find_packages(exclude=['unit', 'unit.*', 'integration',
+                                    'integration.*']),
+    install_requires=[
+        'ribosome>=10.1.2',
+        'amino>=9.5.7',
+        'libtmux',
+        'psutil',
+        'networkx',
+    ]
+)
