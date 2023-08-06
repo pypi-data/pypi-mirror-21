fe5c0f86677ec5b06f71774f921e349f20ba0bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:30 +0000

    Add package japaneselogger/0.1.1/japaneselogger-0.1.1.tar.gz

---
+++ packages/japaneselogger/japaneselogger-0.1.1.tar.gz/japaneselogger-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+# from codecs import open
+# import markdown
+# md = markdown.Markdown()
+# with open('README.md', 'r', 'utf-8') as f:
+#     readme = f.read()
+# readme_html = md.convert(readme)
+
+setup(
+    name = "japaneselogger",
+    version = "0.1.1",
+    packages=['japaneselogger'],
+    description = '日本人向けのログ作成ライブラリ',
+    long_description="How to use https://github.com/tzatter/japaneselogger",
+)
