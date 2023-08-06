7098c644144c0090613c0df54a33851b62d82591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:45 +0000

    Add package django-model-revision/0.0.3/django-model-revision-0.0.3.tar.gz

---
+++ packages/django-model-revision/django-model-revision-0.0.3.tar.gz/django-model-revision-0.0.3/model_revision/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django import get_version
+
+default_app_config = 'model_revision.apps.ModelRevisionConfig'
+
+__all__ = [
+
+]
+
+VERSION = (0, 0, 3, 'alpha', 0)
+
+__version__ = get_version(VERSION)
