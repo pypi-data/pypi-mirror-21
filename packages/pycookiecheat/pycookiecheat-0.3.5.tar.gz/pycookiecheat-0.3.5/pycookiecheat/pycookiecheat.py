a725884c27482d533a8bdd4a9eb60dad052cd2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:36 +0000

    Add package pycookiecheat/0.3.5/pycookiecheat-0.3.5.tar.gz

---
+++ packages/pycookiecheat/pycookiecheat-0.3.5.tar.gz/pycookiecheat-0.3.5/pycookiecheat/pycookiecheat.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""pyCookieCheat.py
+See relevant post at http://n8h.me/HufI1w
+
+Use your browser's cookies to make grabbing data from login-protected sites
+easier. Intended for use with Python Requests http://python-requests.org
+
+Accepts a URL from which it tries to extract a domain. If you want to force the
+domain, just send it the domain you'd like to use instead.
+
+Adapted from my code at http://n8h.me/HufI1w
+
+"""
+
+import os.path
+import sqlite3
+import sys
+
+try:
+    from urllib.error import URLError
+    from urllib.parse import urlparse
+except ImportError:
+    from urllib2 import URLError
+    from urlparse import urlparse
+
+import keyring
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from cryptography.hazmat.primitives.ciphers.modes import CBC
+from cryptography.hazmat.primitives.hashes import SHA1
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def chrome_cookies(url, cookie_file=None):
+
+    salt = b'saltysalt'
+    iv = b' ' * 16
+    length = 16
+
+    def chrome_decrypt(encrypted_value, key=None):
+
+        # Encrypted cookies should be prefixed with 'v10' or 'v11' according to
+        # the Chromium code. Strip it off.
+        encrypted_value = encrypted_value[3:]
+
+        # Strip padding by taking off number indicated by padding
+        # eg if last is '\x0e' then ord('\x0e') == 14, so take off 14.
+        def clean(x):
+            last = x[-1]
+            if isinstance(last, int):
+                return x[:-last].decode('utf8')
+            else:
+                return x[:-ord(last)].decode('utf8')
+
+        cipher = Cipher(
+            algorithm=AES(key),
+            mode=CBC(iv),
+            backend=default_backend(),
+        )
+        decryptor = cipher.decryptor()
+        decrypted = decryptor.update(encrypted_value) + decryptor.finalize()
+
+        return clean(decrypted)
+
+    # If running Chrome on OSX
+    if sys.platform == 'darwin':
+        my_pass = keyring.get_password('Chrome Safe Storage', 'Chrome')
+        iterations = 1003
+        cookie_file = cookie_file or os.path.expanduser(
+            '~/Library/Application Support/Google/Chrome/Default/Cookies'
+        )
+
+    elif sys.platform.startswith('linux'):
+
+        # Set the default linux password
+        my_pass = 'peanuts'
+
+        # Try to get from Gnome / libsecret if it seems available
+        # https://github.com/n8henrie/pycookiecheat/issues/12
+        try:
+            import gi
+            gi.require_version('Secret', '1')
+            from gi.repository import Secret
+        except ImportError:
+            my_pass = 'peanuts'
+        else:
+            flags = Secret.ServiceFlags.LOAD_COLLECTIONS
+            service = Secret.Service.get_sync(flags)
+
+            gnome_keyring = service.get_collections()
+            unlocked_keyrings = service.unlock_sync(gnome_keyring).unlocked
+
+            for unlocked_keyring in unlocked_keyrings:
+                for item in unlocked_keyring.get_items():
+                    if item.get_label() == "Chrome Safe Storage":
+                        item.load_secret_sync()
+                        my_pass = item.get_secret().get_text()
+                        break
+                else:
+                    # Inner loop didn't `break`, keep looking
+                    continue
+
+                # Inner loop did `break`, so `break` outer loop
+                break
+
+        iterations = 1
+        cookie_file = cookie_file or os.path.expanduser(
+            '~/.config/chromium/Default/Cookies'
+        )
+    else:
+        raise OSError("This script only works on OSX or Linux.")
+
+    # Generate key from values above
+    kdf = PBKDF2HMAC(
+        algorithm=SHA1(),
+        length=length,
+        salt=salt,
+        iterations=iterations,
+        backend=default_backend(),
+    )
+    key = kdf.derive(my_pass.encode('utf8'))
+
+    parsed_url = urlparse(url)
+
+    if not parsed_url.scheme:
+        raise URLError("You must include a scheme with your URL")
+    domain = urlparse(url).netloc
+
+    conn = sqlite3.connect(cookie_file)
+
+    sql = ('select name, value, encrypted_value from cookies where host_key '
+           'like ?')
+
+    cookies = {}
+
+    for host_key in generate_host_keys(domain):
+        cookies_list = []
+        for k, v, ev in conn.execute(sql, (host_key,)):
+            # if there is a not encrypted value or if the encrypted value
+            # doesn't start with the 'v1[01]' prefix, return v
+            if v or (ev[:3] not in (b'v10', b'v11')):
+                cookies_list.append((k, v))
+            else:
+                decrypted_tuple = (k, chrome_decrypt(ev, key=key))
+                cookies_list.append(decrypted_tuple)
+        cookies.update(cookies_list)
+
+    conn.rollback()
+    return cookies
+
+
+def generate_host_keys(hostname):
+    """Yield Chrome keys for `hostname`, from least to most specific.
+
+    Given a hostname like foo.example.com, this yields the key sequence:
+
+    example.com
+    .example.com
+    foo.example.com
+    .foo.example.com
+
+    """
+    labels = hostname.split('.')
+    for i in range(2, len(labels) + 1):
+        domain = '.'.join(labels[-i:])
+        yield domain
+        yield '.' + domain
---
+++ packages/pycookiecheat/pycookiecheat-0.3.5.tar.gz/pycookiecheat-0.3.5/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import re
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rst')
+    history = pypandoc.convert('CHANGELOG.md', 'rst')
+except (ImportError, OSError):
+    with open('README.md') as readme_file, \
+            open('CHANGELOG.md') as history_file:
+        readme = readme_file.read()
+        history = history_file.read()
+
+version_regex = re.compile(r'__version__ = [\'\"]((\d+\.?)+)[\'\"]')
+with open('pycookiecheat/__init__.py') as f:
+    vlines = f.readlines()
+__version__ = next(re.match(version_regex, line).group(1) for line in vlines
+                   if re.match(version_regex, line))
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open('requirements-dev.txt') as dev_requirements_file:
+    dev_requirements = dev_requirements_file.read().splitlines()
+
+setup(
+    name='pycookiecheat',
+    version=__version__,
+    description="Borrow cookies from your browser's authenticated session for"
+                "use in Python scripts.",
+    long_description=readme + '\n\n' + history,
+    author='Nathan Henrie',
+    author_email='nate@n8henrie.com',
+    url='https://github.com/n8henrie/pycookiecheat',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='pycookiecheat chrome cookies',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    extras_require={
+        "dev": dev_requirements
+        },
+    test_suite='tests',
+    tests_require=['pytest==2.9.2']
+)
