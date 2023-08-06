9d4ce42d7d39b734eb66ecc603c567a0cdc9935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:53 +0000

    Add package pinax-invitations/5.0.0/pinax_invitations-5.0.0-py2.py3-none-any.whl

---
+++ packages/pinax-invitations/pinax_invitations-5.0.0-py2.py3-none-any.whl/pinax/invitations/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+
+from .views import (
+    AddToAllView,
+    AddToUserView,
+    InviteView,
+    TopOffAllView,
+    InviteStatView,
+    TopOffUserView
+)
+
+
+urlpatterns = [
+    url(r"^invite/$", InviteView.as_view(), name="invite"),
+    url(r"^invite-stat/(?P<pk>\d+)/$", InviteStatView.as_view(), name="invite_stat"),
+    url(r"^topoff/$", TopOffAllView.as_view(), name="topoff_all"),
+    url(r"^topoff/(?P<pk>\d+)/$", TopOffUserView.as_view(), name="topoff_user"),
+    url(r"^addto/$", AddToAllView.as_view(), name="addto_all"),
+    url(r"^addto/(?P<pk>\d+)/$", AddToUserView.as_view(), name="addto_user"),
+]
