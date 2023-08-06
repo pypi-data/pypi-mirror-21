6841b09389917b39cb217b81aeebe8138b102c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:23 +0000

    Add package target-stitch/0.7.7/target-stitch-0.7.7.tar.gz

---
+++ packages/target-stitch/target-stitch-0.7.7.tar.gz/target-stitch-0.7.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='target-stitch',
+      version='0.7.7',
+      description='Singer.io target for the Stitch API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['target_stitch'],
+      install_requires=[
+          'jsonschema',
+          'mock==2.0.0',
+          'singer-python>=0.2.1',
+          'stitchclient>=0.4.6',
+          'strict-rfc3339',
+      ],
+      entry_points='''
+          [console_scripts]
+          target-stitch=target_stitch:main
+      ''',
+      packages=['target_stitch'],
+)
