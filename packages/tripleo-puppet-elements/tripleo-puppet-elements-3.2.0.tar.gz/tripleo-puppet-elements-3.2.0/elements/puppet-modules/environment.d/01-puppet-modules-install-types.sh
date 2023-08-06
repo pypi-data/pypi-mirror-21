ba296bef91bb35d03a7dc90218b01dd2657e3867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:56 +0000

    Add package tripleo-puppet-elements/3.2.0/tripleo-puppet-elements-3.2.0.tar.gz

---
+++ packages/tripleo-puppet-elements/tripleo-puppet-elements-3.2.0.tar.gz/tripleo-puppet-elements-3.2.0/elements/puppet-modules/environment.d/01-puppet-modules-install-types.sh
@@ -0,0 +1,76 @@
+DIB_INSTALLTYPE_puppet_modules=${DIB_INSTALLTYPE_puppet_modules:-"source"}
+
+if [ "$DIB_INSTALLTYPE_puppet_modules" = "source" ]; then
+  for module_name in \
+    puppetlabs-apache \
+    puppet-aodh \
+    puppet-cassandra \
+    puppet-ceph \
+    puppet-ceilometer \
+    puppet-gnocchi \
+    puppet-certmonger \
+    puppet-cinder \
+    puppet-common \
+    puppet-contrail \
+    puppetlabs-concat \
+    puppet-datacat \
+    puppet-elasticsearch \
+    puppetlabs-firewall \
+    konstantin-fluentd \
+    puppet-galera \
+    puppetlabs-git \
+    puppet-glance \
+    puppet-gluster \
+    puppetlabs-haproxy \
+    puppet-heat \
+    puppet-healthcheck \
+    puppet-horizon \
+    puppetlabs-inifile \
+    puppet-kafka \
+    puppet-keystone \
+    puppet-kibana3 \
+    puppet-memcached \
+    puppet-mistral \
+    puppetlabs-mongodb \
+    puppetlabs-mysql \
+    puppet-neutron \
+    puppet-nova \
+    puppet-nssdb \
+    puppet-puppet \
+    puppet-qpid \
+    puppetlabs-rabbitmq \
+    puppet-redis \
+    puppetlabs-rsync \
+    puppet-sahara \
+    puppet-trove \
+    puppet-ssh \
+    puppet-staging \
+    puppetlabs-stdlib \
+    puppet-swift \
+    puppetlabs-sysctl \
+    puppet-tempest \
+    puppet-timezone \
+    puppetlabs-vcsrepo \
+    puppet-vlan \
+    puppet-vswitch \
+    puppetlabs-xinetd \
+    puppet-zookeeper \
+    puppet-openstacklib \
+    puppet-module-keepalived \
+    puppetlabs-ntp \
+    puppet-snmp \
+    puppet-tripleo \
+    puppet-ironic \
+    puppet-openstack_extras \
+    puppet_aviator \
+    puppetlabs-corosync \
+    puppet-pacemaker \
+    puppet-manila \
+    puppet-tuskar \
+    puppet-uchiwa \
+    sensu-puppet \
+    puppet-kmod \
+    puppet-zaqar; do
+    export DIB_INSTALLTYPE_${module_name//\-/_}='source'
+  done
+fi
