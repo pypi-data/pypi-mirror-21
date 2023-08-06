41be34ee3917b99db34cf9d0ccb19ec5cd363cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:54 +0000

    Add package python-amazon-mws-tools/0.0.0/python-amazon-mws-tools-0.0.0.tar.gz

---
+++ packages/python-amazon-mws-tools/python-amazon-mws-tools-0.0.0.tar.gz/python-amazon-mws-tools-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.0.3'
+
+REQUIREMENTS = [
+    'lxml',
+    'python-amazon-mws',
+    'python-dateutil',
+    'requests'
+]
+
+PACKAGES = find_packages()
+
+setup(
+    name='python-amazon-mws-tools',
+    packages=PACKAGES,
+    url='https://github.com/ziplokk1/python-amazon-mws-tools',
+    license='LICENSE.txt',
+    author='Mark Sanders',
+    author_email='sdscdeveloper@gmail.com',
+    install_requires=REQUIREMENTS,
+    description='Wrapper modules to request and parse responses for python-amazon-mws.',
+    include_package_data=True
+)
