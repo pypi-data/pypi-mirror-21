254c64b42c4a78695cc093e48be0288bcb839b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:40 +0000

    Add package almy/0.0.0/almy-0.0.0.tar.gz

---
+++ packages/almy/almy-0.0.0.tar.gz/almy-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='almy',
+      version='0.0.0',
+      description='YAML-files parsing tool.',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url='https://github.com/lycantropos/yaml',
+      download_url='https://github.com/lycantropos/yaml/archive/master.tar.gz',
+      keywords=['yaml'])
