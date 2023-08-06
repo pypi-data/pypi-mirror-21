ae8aed7eb6d1bdb771cc56f2db5bb0ec2cc1f2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:25 +0000

    Add package clodius/0.5.0/clodius-0.5.0.tar.gz

---
+++ packages/clodius/clodius-0.5.0.tar.gz/clodius-0.5.0/clodius/fast.pyx
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+cimport numpy as np
+
+def aggregate(np.ndarray[np.float32_t,ndim=1] in_array, int num_to_agg):
+    '''
+    Calculate a new array which contains the sums of every
+    num_to_agg elements of the original array.
+
+    Example:
+
+    aggregate([1,2,3,4], 2) #=> [3,7]
+
+    :param in_array: A numpy array
+    :param num_to_agg: The number of elements to aggregate into one
+    :return: A numpy array
+    '''
+    cdef int length = len(in_array)
+    cdef np.ndarray[np.float32_t,ndim=1] out_array = np.zeros(int(math.ceil(length / float(num_to_agg))), 
+                                                              dtype=np.float32)
+    cdef int i = 0
+    cdef int j = 0
+
+    while i < length:
+        out_array[i / num_to_agg] = 0;
+        j = 0
+        while j < num_to_agg and (i+j) < length:
+            out_array[i / num_to_agg] += in_array[i+j]
+            j += 1
+        i += num_to_agg
+
+    return out_array
