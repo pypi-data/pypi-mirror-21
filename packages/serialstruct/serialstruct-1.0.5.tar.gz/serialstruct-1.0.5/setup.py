2475ff8af73f29b3704bf4b4cc66bb43442cb6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package serialstruct/1.0.5/serialstruct-1.0.5.tar.gz

---
+++ packages/serialstruct/serialstruct-1.0.5.tar.gz/serialstruct-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='serialstruct',
+        version='1.0.5',
+        description=('Implements a StructuredPacket for pySerial\'s '
+            'serial.threaded module'),
+        author='Eric Oswald',
+        author_email='eoswald39@gmail.com',
+        url='https://github.com/eoswald/serialstruct',
+        license='MIT',
+        packages=find_packages(exclude=['test']),
+        install_requires=['pyserial']
+)
