e103101390a7fa7c4c7e28383e6c5407b62230d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:43 +0000

    Add package pyGlacierVault/0.1rc0/pyGlacierVault-0.1rc0.tar.gz

---
+++ packages/pyGlacierVault/pyGlacierVault-0.1rc0.tar.gz/pyGlacierVault-0.1rc0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup, find_packages
+import json
+
+setup(
+    name = "pyGlacierVault",
+    version = "0.1c",
+    packages = find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['boto3>=1.4.4'],
+
+    # metadata for upload to PyPI
+    author = "Leeroy Brun - Matthieu Berthomé fork",
+    author_email = "rienafairefr@gmail.com",
+    description = "Tool used to remove all archives stored inside an Amazon Glacier vault.",
+    license = "MIT",
+    keywords = "aws amazon glacier boto archives vaults",
+    url = "https://github.com/rienafairefr/glacier-vault-remove",
+	entry_points={
+          'console_scripts': [
+              'glacier-vault = glaciervault:main'
+          ]
+      }
+)
+
+with open("credentials.json", "w") as outfile:
+    json.dump({'AWSAccessKeyId': '<key>', 'AWSSecretKey': '<secretkey>'}, outfile, indent=4)
