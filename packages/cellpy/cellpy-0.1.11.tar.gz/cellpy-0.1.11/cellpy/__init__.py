00b91edf1f2da8186e6e2885dc76170ea89d6ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:35 +0000

    Add package cellpy/0.1.11/cellpy-0.1.11.tar.gz

---
+++ packages/cellpy/cellpy-0.1.11.tar.gz/cellpy-0.1.11/cellpy/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+__author__ = 'Tor Kristian Vara', 'Jan Petter Maehlen'
+__email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
+
+import logging
+import warnings
+from cellpy.readers import cellreader
+from cellpy.readers import dbreader
+from cellpy.readers import filefinder
+from cellpy.parameters import prmreader
+
+
+
+__all__ = ["cellreader", "dbreader", "prmreader", "prms", "filefinder"]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+try:
+    prmreader._read_prm_file(prmreader._get_prm_file())
+except:
+    warnings.warn("obs! could not load the config-file")
+
+
