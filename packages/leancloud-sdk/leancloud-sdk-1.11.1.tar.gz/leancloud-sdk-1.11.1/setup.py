a60b97dedb562f1ae8cfe9f1e33193ea044b84e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:51 +0000

    Add package leancloud-sdk/1.11.1/leancloud-sdk-1.11.1.tar.gz

---
+++ packages/leancloud-sdk/leancloud-sdk-1.11.1.tar.gz/leancloud-sdk-1.11.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+
+here = path.abspath(path.dirname(__file__))
+
+install_requires = [
+    'arrow',
+    'iso8601',
+    'qiniu',
+    'requests',
+    'werkzeug',
+]
+
+if sys.version_info < (3, 5, 0):
+    install_requires.append('typing')
+
+setup(
+    name='leancloud-sdk',
+    version='1.11.1',
+    description='LeanCloud Python SDK',
+    url='https://leancloud.cn/',
+    author='asaka',
+    author_email='lan@leancloud.rocks',
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='Leancloud SDK',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    test_suite='nose.collector',
+    install_requires=install_requires,
+    extras_require={
+        'dev': ['sphinx', 'sphinx_rtd_theme'],
+        'test': ['nose', 'wsgi_intercept', 'flask'],
+    }
+)
