f6ed458bb71f1f55ed309664e28b4f59ba750aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:08 +0000

    Add package sparkhpc/0.2.post1/sparkhpc-0.2.post1.tar.gz

---
+++ packages/sparkhpc/sparkhpc-0.2.post1.tar.gz/sparkhpc-0.2.post1/sparkhpc/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import os, sys
+import logging
+import sparkjob
+import lsfsparkjob
+import slurmsparkjob
+from lsfsparkjob import LSFSparkJob
+from slurmsparkjob import SLURMSparkJob
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def show_clusters():
+    sparkjob.sparkjob().show_clusters()
