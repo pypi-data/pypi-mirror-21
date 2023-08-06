8e1f0ad3c7a9ae42f072fac8dc622244b664c69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:04 +0000

    Add package chatterbot-voice/0.1.3/chatterbot_voice-0.1.3-py3-none-any.whl

---
+++ packages/chatterbot-voice/chatterbot_voice-0.1.3-py3-none-any.whl/chatterbot_voice/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+if 'install' not in sys.argv and 'egg_info' not in sys.argv:
+    from .voice import VoiceInput
+    from .voice import VoiceOutput
+
+__version__ = '0.1.3'
+__author__ = 'Gunther Cox'
+__email__ = 'gunthercx@gmail.com'
