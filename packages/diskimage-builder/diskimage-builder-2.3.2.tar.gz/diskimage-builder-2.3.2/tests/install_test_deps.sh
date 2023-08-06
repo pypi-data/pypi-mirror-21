10c30043a45ef98f224edbc16799f150a51b2bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:15 +0000

    Add package diskimage-builder/2.3.2/diskimage-builder-2.3.2.tar.gz

---
+++ packages/diskimage-builder/diskimage-builder-2.3.2.tar.gz/diskimage-builder-2.3.2/tests/install_test_deps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+sudo apt-get update || true
+sudo apt-get install -y --force-yes \
+        apt-transport-https \
+        debootstrap \
+        docker.io \
+        inetutils-ping \
+        lsb-release \
+        kpartx \
+        python-lzma \
+        qemu-utils \
+        rpm \
+        uuid-runtime \
+        yum-utils || \
+    sudo yum -y install \
+        debootstrap \
+        docker \
+        kpartx \
+        util-linux \
+        qemu-img || \
+    sudo emerge \
+        app-emulation/qemu \
+        dev-python/pyyaml \
+        sys-block/parted \
+        sys-fs/multipath-tools \
+        qemu-img \
+        yum-utils
