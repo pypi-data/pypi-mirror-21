d5b02b340091ccd1e9a25adf1fde8b38fdeb1ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:19 +0000

    Add package head_firstpy/1.0.0/head_firstpy-1.0.0.tar.gz

---
+++ packages/head_firstpy/head_firstpy-1.0.0.tar.gz/head_firstpy-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name        = 'head_firstpy',
+        version     = '1.0.0',
+        py_modules  = ['nester'],
+        author      = 'DonnieBwin',
+        author_email= '18767090346@163.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+
+    )
