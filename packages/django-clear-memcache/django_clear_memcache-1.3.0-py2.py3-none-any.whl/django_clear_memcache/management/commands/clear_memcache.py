fc12bc53e3ae3c46fa080ce40254b9e73da4b953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:33 +0000

    Add package django-clear-memcache/1.3.0/django_clear_memcache-1.3.0-py2.py3-none-any.whl

---
+++ packages/django-clear-memcache/django_clear_memcache-1.3.0-py2.py3-none-any.whl/django_clear_memcache/management/commands/clear_memcache.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django_clear_memcache.clear import ClearMemcacheController
+from django.core.management.base import BaseCommand
+
+
+########################################################################
+class Command(BaseCommand):
+
+    help = ("Clear all keys from the configured default memcache server using the configured prefix")
+    can_import_settings = True
+
+    # ----------------------------------------------------------------------
+    def handle(self, **options):
+        verbosity = int(options.get("verbosity", 0))
+
+        controller = ClearMemcacheController()
+        result = controller.clear_cache()
+
+        if verbosity >= 1:
+            return result
