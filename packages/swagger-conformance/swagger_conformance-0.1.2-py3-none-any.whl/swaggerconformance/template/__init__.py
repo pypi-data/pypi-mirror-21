6e0b39f129b5fb2889ee617c8f0575632563ee4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:12 +0000

    Add package swagger-conformance/0.1.2/swagger_conformance-0.1.2-py3-none-any.whl

---
+++ packages/swagger-conformance/swagger_conformance-0.1.2-py3-none-any.whl/swaggerconformance/template/__init__.py
@@ -0,0 +1,5 @@
+from .apitemplate import APITemplate
+from .valuetemplatefactory import ValueFactory
+from .swaggerparameter import SwaggerParameter
+
+__all__ = ["APITemplate", "ValueFactory", "SwaggerParameter"]
