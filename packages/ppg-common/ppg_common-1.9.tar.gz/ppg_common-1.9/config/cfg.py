a53fe1a532d6bb54eb8ca73cc42360f1e97255ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:50 +0000

    Add package ppg-common/1.9/ppg_common-1.9.tar.gz

---
+++ packages/ppg-common/ppg_common-1.9.tar.gz/ppg_common-1.9/config/cfg.py
@@ -0,0 +1,34 @@
+import io
+import yaml
+from sqlalchemy import create_engine
+
+
+class Config(object):
+    def __init__(self, config_path):
+        self.config_path = config_path
+        with io.open(config_path, 'r') as cfg:
+            self.data = yaml.safe_load(cfg)
+        application = self.data.get('application')
+        database = self.data.get('db')
+        self.BASE_URL = 'http://{host}{port}/v0'.format(**application)
+        if database:
+            self.DATABASE = 'postgresql://{user}:{pass}@{host}:{port}/{name}'.\
+                format(**database)
+        self.APP_PORT = application.get('port')
+        self.engine = create_engine(self.DATABASE, echo=True)
+
+    @property
+    def application(self):
+        return self.data.get('application')
+
+    @property
+    def db(self):
+        return self.data.get('db')
+
+    @property
+    def redis_params(self):
+        return self.data.get('redis_params')
+
+    @property
+    def session_params(self):
+        return self.data.get('session_params')
