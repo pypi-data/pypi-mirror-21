4b3afa0a4869f937c149f6ddfbbab6721cfa3973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:04 +0000

    Add package bw2analyzer/0.9.2/bw2analyzer-0.9.2.tar.gz

---
+++ packages/bw2analyzer/bw2analyzer-0.9.2.tar.gz/bw2analyzer-0.9.2/bw2analyzer/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*
+__all__ = [
+    'ContributionAnalysis',
+    'DatabaseExplorer',
+    'DatabaseHealthCheck',
+    'GTManipulator',
+    'PageRank',
+    'SerializedLCAReport',
+    'traverse_tagged_databases',
+]
+
+__version__ = (0, 9, 2)
+
+from .contribution import ContributionAnalysis
+from .explorer import DatabaseExplorer
+from .health_check import DatabaseHealthCheck
+from .page_rank import PageRank
+from .report import SerializedLCAReport
+from .sc_graph import GTManipulator
+from .tagged import traverse_tagged_databases
---
+++ packages/bw2analyzer/bw2analyzer-0.9.2.tar.gz/bw2analyzer-0.9.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='bw2analyzer',
+    version="0.9.2",
+    packages=["bw2analyzer", "bw2analyzer.tests"],
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE.txt').read(),
+    install_requires=[
+        "brightway2>=2.0",
+        "bw2data>=2.3",
+        "numpy",
+        "requests",
+        "scipy",
+    ],
+    long_description=open('README.rst').read(),
+    url="https://bitbucket.org/cmutel/brightway2-analyzer",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
