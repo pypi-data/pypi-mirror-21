59b7f8490e1233e053a3a8b22294d8c9f28c461a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:17 +0000

    Add package odoo9-addon-medical-base-us/9.0.1.0.0.99.dev5/odoo9_addon_medical_base_us-9.0.1.0.0.99.dev5-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-base-us/odoo9_addon_medical_base_us-9.0.1.0.0.99.dev5-py2-none-any.whl/odoo_addons/medical_base_us/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_abstract_luhn
+from . import medical_abstract_dea
+from . import medical_abstract_npi
