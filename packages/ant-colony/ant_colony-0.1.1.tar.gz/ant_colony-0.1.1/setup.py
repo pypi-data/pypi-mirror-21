0b9329dae57dcd6d012f38187a406243d5f52410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:09 +0000

    Add package ant-colony/0.1.1/ant_colony-0.1.1.tar.gz

---
+++ packages/ant-colony/ant_colony-0.1.1.tar.gz/ant_colony-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="ant_colony",
+    author="Jerzy Pawlikowski",
+    author_email="me@jerzypawlikowski.pl",
+    version="0.1.1",
+    url="https://github.com/jurekpawlikowski/ant-colony",
+    package_dir={"ant_colony": "ant_colony"},
+    packages=["ant_colony"],
+    license=open("LICENSE").read(),
+    description="Implementation of the Ant Colony system",
+    long_description=open("README.rst").read()
+)
