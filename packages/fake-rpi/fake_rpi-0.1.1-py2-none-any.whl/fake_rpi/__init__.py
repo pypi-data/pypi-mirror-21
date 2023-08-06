06fd86be1935a8381eeee98ebd0b2794b64dd7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package fake-rpi/0.1.1/fake_rpi-0.1.1-py2-none-any.whl

---
+++ packages/fake-rpi/fake_rpi-0.1.1-py2-none-any.whl/fake_rpi/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+from . import serial
+from . import smbus
+from . import RPi
+from .Adafruit import LSM303
+from .wrappers import printf
+from .wrappers import toggle_print
+from . import picamera
+
+__version__ = '0.1.1'
+__author__ = 'Kevin Walchko'
+__license__ = 'MIT'
+
+
+print('<<< WARNING: using fake raspberry pi interfaces >>>')
