359ca85247e6228dfad6c6618d6ce9ecb80d70ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:01 +0000

    Add package predictiveanalytics/0.0.3/predictiveanalytics-0.0.3.tar.gz

---
+++ packages/predictiveanalytics/predictiveanalytics-0.0.3.tar.gz/predictiveanalytics-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='predictiveanalytics',
+      version='0.0.3',
+      description='Tools and Algorithms used for Predictive Analytics and Machine Learning',
+      url='http://github.com/bawdiest/predictiveanalytics',
+      author='bawdiest',
+      author_email='com@mikmak.cc',
+      license='MIT',
+      packages=['predictiveanalytics'],
+      zip_safe=False)
