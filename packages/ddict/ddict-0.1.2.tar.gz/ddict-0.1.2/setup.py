4f6885d0e1a0ca94ca319c1b87f2ceff93be26bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:00 +0000

    Add package ddict/0.1.2/ddict-0.1.2.tar.gz

---
+++ packages/ddict/ddict-0.1.2.tar.gz/ddict-0.1.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='ddict',
+    version='0.1.2',
+    description='Dict class with dot notation (like attributes) for accessing nested values',
+    long_description=long_description,
+    url='https://github.com/rbehzadan/ddict',
+    author='Reza Behzadan',
+    author_email='rbehzadan@gmail.com',
+    license='MIT',
+    classifiers=[
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        # 'Topic :: Software Development :: Build Tools',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='dict dot notation access',
+
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    py_modules=["ddict"],
+
+    install_requires=[],
+
+    # $ pip install -e .[dev]
+    extras_require={
+        'dev': ['pytest-cov'],
+    },
+    setup_requires=[
+        'pytest-runner',
+        'pytest-cov'
+    ],
+)
+
