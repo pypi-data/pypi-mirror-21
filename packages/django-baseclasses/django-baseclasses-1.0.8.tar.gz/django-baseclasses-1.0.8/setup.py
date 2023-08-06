c5743aae2e1748dc9fb142752a73722b0043b5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:18 +0000

    Add package django-baseclasses/1.0.8/django-baseclasses-1.0.8.tar.gz

---
+++ packages/django-baseclasses/django-baseclasses-1.0.8.tar.gz/django-baseclasses-1.0.8/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import os
+from setuptools import setup, find_packages
+
+
+# if there's a converted readme, use it, otherwise fall back to markdown
+if os.path.exists('README.rst'):
+    readme_path = 'README.rst'
+else:
+    readme_path = 'README.md'
+
+# avoid importing the module
+exec(open('baseclasses/_version.py').read())
+
+setup(
+    name='django-baseclasses',
+    version=__version__,
+    description='A set of reusable base classes and helpers for django',
+    long_description=open(readme_path).read(),
+    author='Greg Brown',
+    author_email='greg@gregbrown.co.nz',
+    url='https://github.com/gregplaysguitar/django-baseclasses',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Django>=1.8', 'django-next-prev>=1.0'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Framework :: Django',
+    ],
+)
