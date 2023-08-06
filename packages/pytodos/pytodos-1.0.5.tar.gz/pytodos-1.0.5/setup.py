da0795bc75ec2dcc6a277193757550200bfdf299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:00 +0000

    Add package pytodos/1.0.5/pytodos-1.0.5.tar.gz

---
+++ packages/pytodos/pytodos-1.0.5.tar.gz/pytodos-1.0.5/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+entry_points = [
+    "add=PyTodos.todo:add",
+    "list=PyTodos.todo:list",
+    "got=PyTodos.todo:kill"
+]
+
+setup(
+    name="pytodos",
+    version='1.0.5',
+    description="",
+    long_description="",
+    author="chuanwu",
+    author_email="chuanwusun@gmail.com",
+    packages=['pytodos'],
+    url="https://github.com/chuanwu/PyToDos.py",
+    entry_points={"console_scripts": entry_points},
+    install_requires=[],
+)
