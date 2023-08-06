ef28cef0de4b60f525010695cbfe8c82ed936394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:25 +0000

    Add package filters/1.1.7/filters-1.1.7.tar.gz

---
+++ packages/filters/filters-1.1.7.tar.gz/filters-1.1.7/setup.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+# :bc: Not importing unicode_literals because in Python 2 distutils,
+# some values are expected to be byte strings.
+from __future__ import absolute_import, division, print_function
+
+from codecs import StreamReader, open
+from sys import version_info
+
+from setuptools import setup
+
+
+##
+# Load long description for PyPi.
+with open('README.rst', 'r', 'utf-8') as f: # type: StreamReader
+    long_description = f.read()
+
+
+##
+# Certain dependencies are optional depending on Python version.
+dependencies = [
+    'iso3166',
+    'language_tags',
+    'py-moneyed',
+    'python-dateutil',
+    'pytz',
+    'regex',
+    'six',
+]
+
+if version_info[0] < 3:
+    # noinspection SpellCheckingInspection
+    dependencies.extend([
+        'py2casefold',
+        'typing',
+    ])
+
+
+##
+# Off we go!
+setup(
+    name        = 'filters',
+    description = 'Validation and data pipelines made easy!',
+    url         = 'https://filters.readthedocs.io/',
+
+    version = '1.1.7',
+
+    packages = ['filters'],
+
+    long_description = long_description,
+
+    install_requires = dependencies,
+
+    test_suite    = 'test',
+    test_loader   = 'nose.loader:TestLoader',
+    tests_require = [
+        'nose',
+    ],
+
+    data_files = [
+        ('', ['LICENSE']),
+    ],
+
+    license = 'MIT',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+    ],
+
+    keywords = 'data validation',
+
+    author          = 'Phoenix Zerin',
+    author_email    = 'phoenix.zerin@eflglobal.com',
+)
