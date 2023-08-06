7f7235e92319b5ecf8d84bdf7b97dd00dc647990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package flask_simple_sitemap/0.0.3/flask_simple_sitemap-0.0.3.tar.gz

---
+++ packages/flask_simple_sitemap/flask_simple_sitemap-0.0.3.tar.gz/flask_simple_sitemap-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_simple_sitemap',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
