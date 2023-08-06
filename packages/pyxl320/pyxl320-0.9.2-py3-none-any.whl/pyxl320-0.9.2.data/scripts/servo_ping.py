d934cc5a8fb5ec3d65a479f278bf3da944d40b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:49 +0000

    Add package pyxl320/0.9.2/pyxl320-0.9.2-py3-none-any.whl

---
+++ packages/pyxl320/pyxl320-0.9.2-py3-none-any.whl/pyxl320-0.9.2.data/scripts/servo_ping.py
@@ -0,0 +1,71 @@
+#!python
+
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2016 Kevin Walchko
+# see LICENSE for full details
+##############################################
+# Send ping commands to all servos
+
+from __future__ import print_function
+from pyxl320.Packet import makePingPacket, packetToDict
+from pyxl320 import ServoSerial
+# from pyxl320 import DummySerial
+from pyxl320 import utils
+import argparse
+import time
+from pyxl320 import xl320
+
+
+def sweep(port, rate, ID, retry=3):
+	"""
+	Sends a ping packet to ID's from 0 to maximum and prints out any returned
+	messages.
+
+	Actually send a broadcast and will retry (resend) the ping 3 times ...
+	"""
+	if port == 'dummy':
+		s = ServoSerial(port, rate, fake=True)
+	else:
+		s = ServoSerial(port, rate)
+
+	if ID < 0:
+		ID = xl320.XL320_BROADCAST_ADDR
+
+	s.open()
+	pkt = makePingPacket(ID)
+	print('ping', pkt)
+	s.write(pkt)
+
+	# as more servos add up, I might need to increase the cnt number???
+	for cnt in range(retry):
+		ans = s.read()
+
+		if ans:
+			for pkt in ans:
+				servo = packetToDict(pkt)
+				utils.prettyPrintPacket(servo)
+				print('raw pkt: {}'.format(pkt))
+		else:
+			print('Try {}: no servos found'.format(cnt))
+
+		time.sleep(0.1)
+
+	s.close()
+
+
+def handleArgs():
+	parser = argparse.ArgumentParser(description='ping servos')
+	# parser.add_argument('-m', '--max', help='max id', type=int, default=253)
+	parser.add_argument('-r', '--rate', help='servo baud rate', type=int, default=1000000)
+	parser.add_argument('-i', '--id', help='ping servo ID', type=int, default=-1)
+	parser.add_argument('-p', '--port', help='serial port name, set to "dummy" for testing', type=str, default='/dev/serial0')
+
+	args = vars(parser.parse_args())
+	return args
+
+
+if __name__ == '__main__':
+	args = handleArgs()
+	print('Finding all servos:')
+	sweep(port=args['port'], rate=args['rate'], ID=args['id'])
