5ea1ab87772c3eb674fe535480318f58dd39f7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:20 +0000

    Add package flask_doc/0.1.3/flask_doc-0.1.3.tar.gz

---
+++ packages/flask_doc/flask_doc-0.1.3.tar.gz/flask_doc-0.1.3/flask_doc/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf8
+
+import generator
+import describer
+
+__all__ = ['Generator', 'forms', 'args', 'gathering_form', 'gathering_args']
+
+Generator = generator.Generator
+
+forms = describer.forms
+
+args = describer.args
+
+gathering_form = describer.gathering_form
+
+gathering_args = describer.gathering_args
