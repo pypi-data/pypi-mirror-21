89e3baab1050d40bce35429a4371eccf57762be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:48 +0000

    Add package vania/0.2.0/vania-0.2.0.tar.gz

---
+++ packages/vania/vania-0.2.0.tar.gz/vania-0.2.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import setuptools.command.build_py
+import subprocess
+
+
+class GenDocsCommand(setuptools.command.build_py.build_py):
+
+    """Command to generate docs."""
+
+    def run(self):
+        subprocess.Popen(
+            ['pdoc', '--html', 'vania/', '--html-dir=docs', '--overwrite'])
+        setuptools.command.build_py.build_py.run(self)
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="vania",
+    version="0.2.0",
+    description="A module to fairly distribute objects among targets considering weights.",
+    license="MIT",
+    author="Hackathonners",
+    author_email="contact@hackathonners.org",
+    packages=find_packages(),
+    install_requires=[
+        'pulp',
+        'pdoc'
+    ],
+    package_dir={'': '.'},
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    cmdclass={
+        'gendocs': GenDocsCommand
+    },
+)
