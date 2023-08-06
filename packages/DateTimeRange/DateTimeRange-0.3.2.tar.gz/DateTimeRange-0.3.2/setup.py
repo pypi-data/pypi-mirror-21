3f3df70c591303b1dfcd40c595879c63138c1544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:24 +0000

    Add package DateTimeRange/0.3.2/DateTimeRange-0.3.2.tar.gz

---
+++ packages/DateTimeRange/DateTimeRange-0.3.2.tar.gz/DateTimeRange-0.3.2/setup.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import os.path
+import sys
+
+import setuptools
+
+
+MISC_DIR = "misc"
+REQUIREMENT_DIR = "requirements"
+
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+with open(os.path.join(MISC_DIR, "summary.txt")) as f:
+    summary = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_require = [line.strip() for line in f if line.strip()]
+
+setuptools.setup(
+    name="DateTimeRange",
+    version="0.3.2",
+    author="Tsuyoshi Hombashi",
+    author_email="gogogo.vm@gmail.com",
+    url="https://github.com/thombashi/DateTimeRange",
+    keywords=["datetimerange", "date", "time", "range"],
+    license="MIT License",
+    description=summary,
+    long_description=long_description,
+    include_package_data=True,
+    install_requires=install_requires,
+    packages=setuptools.find_packages(exclude=["test*"]),
+    setup_requires=pytest_runner,
+    tests_require=tests_require,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
