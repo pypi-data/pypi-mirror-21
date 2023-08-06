248827e32a81a811d74a5591c3edf1fe928c740a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package kcommonmodule/1.5.0/kcommonmodule-1.5.0.tar.gz

---
+++ packages/kcommonmodule/kcommonmodule-1.5.0.tar.gz/kcommonmodule-1.5.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='kcommonmodule',
+    version='1.5.0',
+    description='Simple python logging',
+    author='zhaodianle',
+    author_email='zhaodl@knownsec.com',
+    packages=find_packages()
+)
