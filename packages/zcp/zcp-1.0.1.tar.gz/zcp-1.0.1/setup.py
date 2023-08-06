c363bdfccf13697bbca9d11b6f28f5e839dc57ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:26 +0000

    Add package zcp/1.0.1/zcp-1.0.1.tar.gz

---
+++ packages/zcp/zcp-1.0.1.tar.gz/zcp-1.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    version="1.0.1",
+    author="hanxi.liu",
+    author_email="apolloliuhx@gmail.com",
+    packages=find_packages(),
+    scripts=['bin/eszcp-polling'],
+    description="A Timer task for polling ceilometer metrics into zabbix"
+)
