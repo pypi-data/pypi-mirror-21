c8dbfd0a27e58341e22aebfcc7199a81ed8c2020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:22 +0000

    Add package twitter_data_equipment/1.4.4/twitter_data_equipment-1.4.4.tar.gz

---
+++ packages/twitter_data_equipment/twitter_data_equipment-1.4.4.tar.gz/twitter_data_equipment-1.4.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='twitter_data_equipment',
+      version='1.4.4',
+      description='A package to cluster tools for harvesting, hydrating, cleaning and visualize twitter data',
+      url='https://github.com/HolzmanoLagrene/FacharbeitLenzBaumannHS16FS17',
+      author='Lenz Baumann',
+      author_email='lnzbmnn@gmail.com',
+      license='MIT',
+      packages=['twitter_data_equipment'],
+      include_package_data=True,
+      zip_safe=False)
+
---
+++ packages/twitter_data_equipment/twitter_data_equipment-1.4.4.tar.gz/twitter_data_equipment-1.4.4/twitter_data_equipment/__init__.py
@@ -0,0 +1,8 @@
+from . import Slicing
+from . import Information
+from . import Demographics
+from . import Unshortening
+from . import Processing
+from . import GraphFactory
+from . import Hydration
+
