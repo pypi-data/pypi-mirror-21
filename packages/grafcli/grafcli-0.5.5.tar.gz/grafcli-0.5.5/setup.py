8ea1a2bc6a454194af4eedb280f2a8bcb2644ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:43 +0000

    Add package grafcli/0.5.5/grafcli-0.5.5.tar.gz

---
+++ packages/grafcli/grafcli-0.5.5.tar.gz/grafcli-0.5.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from subprocess import check_output
+from setuptools import setup, find_packages
+
+git_version = check_output(["git", "describe", "--tags"]).strip().decode()
+
+setup(name='grafcli',
+      version=git_version,
+      description='Grafana CLI management tool',
+      author='Milosz Smolka',
+      author_email='m110@m110.pl',
+      url='https://github.com/m110/grafcli',
+      packages=find_packages(exclude=['tests']),
+      scripts=['scripts/grafcli'],
+      data_files=[('/etc/grafcli', ['grafcli.conf.example'])],
+      install_requires=['climb>=0.3.2'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: System :: Systems Administration',
+      ])
