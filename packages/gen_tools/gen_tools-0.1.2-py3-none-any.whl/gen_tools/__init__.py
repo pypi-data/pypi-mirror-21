455daa68ca21a2ed9e7ac0eb1bf884118ef28842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:47 +0000

    Add package gen_tools/0.1.2/gen_tools-0.1.2-py3-none-any.whl

---
+++ packages/gen_tools/gen_tools-0.1.2-py3-none-any.whl/gen_tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+Functions and accompanying CLI Tools to generate pseudo-random passwords and
+UUIDs (GUIDs)
+"""
+
+from .gen_pass_cli import gen_pass
+from .gen_uuid_cli import gen_uuid
+
+__version__ = "0.1.2"
---
+++ packages/gen_tools/gen_tools-0.1.2-py3-none-any.whl/gen_tools/gen_uuid_cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+
+import sys
+
+from gen_tools.tools import gen_uuid
+
+
+def main(use_caps: bool = False):
+    sys.stdout.write(gen_uuid(capitalize=use_caps))
+
+if __name__ == '__main__':
+    try:
+        main(bool(sys.argv[1]))
+    except IndexError as e:
+        main()
