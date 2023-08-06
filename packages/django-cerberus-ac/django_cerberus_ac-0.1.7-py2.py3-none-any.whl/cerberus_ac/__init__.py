edcfd0a5db1da0e5293b68150964131ff8fe875f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:49 +0000

    Add package django-cerberus-ac/0.1.7/django_cerberus_ac-0.1.7-py2.py3-none-any.whl

---
+++ packages/django-cerberus-ac/django_cerberus_ac-0.1.7-py2.py3-none-any.whl/cerberus_ac/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+u"""Cerberus Access Control package."""
+
+__version__ = '0.1.7'
+
+default_app_config = 'cerberus_ac.apps.CerberusACConfig'
+
+# TODO: make use of the namespace setting everywhere it is needed
