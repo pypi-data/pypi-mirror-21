3e6118d5382aa9aa30dffa7110631d4929b8d9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:26 +0000

    Add package hellotest989/1.1.2/hellotest989-1.1.2.tar.gz

---
+++ packages/hellotest989/hellotest989-1.1.2.tar.gz/hellotest989-1.1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='hellotest989',
+    version='1.1.2',
+    description='Sample package for Gemfury',
+    author='Michael Rykov',
+    author_email='michael@gemfury.com',
+    packages=['py_sample'],
+    include_package_data=True,
+    url='http://www.gemfury.com')
