3bdeb5bdf0a09877cad4068960aaad148184f541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:31 +0000

    Add package car_package/0.1/car_package-0.1.tar.gz

---
+++ packages/car_package/car_package-0.1.tar.gz/car_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='car_package',
+      version='0.1',
+      description='Start your car and drive off in it',
+      url='',
+      author='Peter Begle',
+      author_email='p_begle@u.pacific.edu',
+      license='MIT',
+      packages=['car_package'],
+      zip_safe=False)
