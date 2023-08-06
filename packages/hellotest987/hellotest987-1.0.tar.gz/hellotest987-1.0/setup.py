25bc827aa0d88e91f8621a42876703522c0868de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:14 +0000

    Add package hellotest987/1.0/hellotest987-1.0.tar.gz

---
+++ packages/hellotest987/hellotest987-1.0.tar.gz/hellotest987-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='hellotest987',
+    version='1.0',
+    description='Sample package for Gemfury',
+    author='Michael Rykov',
+    author_email='michael@gemfury.com',
+    url='http://www.gemfury.com')
