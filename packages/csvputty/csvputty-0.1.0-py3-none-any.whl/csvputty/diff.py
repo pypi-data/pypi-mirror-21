49ea0339afe17d6cd3e6d7686dc280633964977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:57 +0000

    Add package csvputty/0.1.0/csvputty-0.1.0-py3-none-any.whl

---
+++ packages/csvputty/csvputty-0.1.0-py3-none-any.whl/csvputty/diff.py
@@ -0,0 +1,67 @@
+import csv as csv
+
+
+"""csvdiff: Module for diffing CSV files."""
+
+
+def _build_diff_values():
+    global DIF_COL_NAME
+    for idx, row in enumerate(DIF_CSV):
+        if idx > 0:
+            DIF_VALUES.append(row[DIF_COL].strip().lower())
+        else:
+            DIF_COL_NAME = row[DIF_COL]
+
+
+def _setup_env(**options):
+    global SRC_CSV, DIF_CSV, OUT_CSV, SRC_COL, DIF_COL, DIF_VALUES, DIF_COL_NAME, DIF_TYPE
+
+    SRC_CSV = csv.reader(options['input_csv'])
+    DIF_CSV = csv.reader(options['diff_file'])
+    OUT_CSV = csv.writer(options['out_file'])
+    SRC_COL = options['source_col']
+    DIF_COL = options['diff_col']
+    DIF_TYPE = options['diff_type']
+
+    DIF_VALUES = []
+    DIF_COL_NAME = ''
+
+
+def _validate_parameters(input_csv=None, out_file=None, diff_file=None, diff_type=None,
+                         source_col=None, diff_col=None):
+    if input_csv is None:
+        return False
+    if out_file is None:
+        return False
+    if diff_file is None:
+        return False
+    if diff_type is None:
+        return False
+    if source_col is None:
+        return False
+    if diff_col is None:
+        return False
+    return True
+
+
+def run(**options):
+    if _validate_parameters(**options) is False:
+        return
+    _setup_env(**options)
+    _build_diff_values()
+    for idx, row in enumerate(SRC_CSV):
+        if idx == 0:
+            print('Comparing columns: {} {} {}'.format(
+                row[SRC_COL], DIF_TYPE, DIF_COL_NAME
+            ))
+            OUT_CSV.writerow(row)
+            continue
+        if check_row(row):
+            OUT_CSV.writerow(row)
+
+
+def check_row(row):
+    if DIF_TYPE == 'subtract':
+        if row[SRC_COL].strip().lower() in DIF_VALUES:
+            return False
+        return True
---
+++ packages/csvputty/csvputty-0.1.0-py3-none-any.whl/csvputty/markup.py
@@ -0,0 +1,49 @@
+import csv
+import click
+from string import Formatter
+
+"""markup: Module for generating markup from csv files."""
+
+
+def _prep_template_str(template):
+    try:
+        template_str = template.read()
+        if len(template_str) == 0:
+            raise
+    except:
+        template_str = ('{},' * len(cols)).strip(',') + '\n'
+    finally:
+        return template_str
+
+
+def _parse_row(cols, row, func=None):
+    if func is not None:
+        return func(row)
+    if isinstance(row, list):
+        return [row[idx] for idx in cols]
+    if isinstance(row, dict):
+        return {key: row[key] for idx, key in enumerate(row.keys()) if key in cols}
+
+
+def generate(custom_row_parser=None, cols=None, csv_file=None, template_file=None,
+             output_file=None, use_dict_reader=False):
+    template_str = _prep_template_str(template_file)
+    html = ''
+    try:
+        if use_dict_reader:
+            reader = csv.DictReader(csv_file)
+            cols = [i[1] for i in Formatter().parse(template_str)]
+        else:
+            reader = csv.reader(csv_file)
+        for idx, row in enumerate(reader):
+            parsed = _parse_row(cols, row, custom_row_parser)
+            try:
+                html += template_str.format(*parsed)
+            except KeyError:
+                html += template_str.format(**parsed)
+        output_file.write(html)
+    except FileNotFoundError:
+        click.echo('FileNotFoundError: File not found.')
+        return False
+    except KeyError as err:
+        click.echo('KeyError: Template key %s not found in %s' % (err, csv_file.name))
