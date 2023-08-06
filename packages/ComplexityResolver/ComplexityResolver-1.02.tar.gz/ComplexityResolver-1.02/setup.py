6d806de4f9f6a8b49c79819e82a28780d48a62b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:56 +0000

    Add package ComplexityResolver/1.02/ComplexityResolver-1.02.tar.gz

---
+++ packages/ComplexityResolver/ComplexityResolver-1.02.tar.gz/ComplexityResolver-1.02/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='ComplexityResolver',
+    packages=['ComplexityResolver'],
+    version='1.02',
+    description='Complexity resolver',
+    author='F Szesto',
+    author_email='filipszestowicki@gmail.com',
+    url='https://github.com/AGHPythonCourse2017/zad2-Oposow',
+    keywords=['PythonCourse', 'complexity'],
+    classifiers=[],
+)
