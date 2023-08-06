a33f725975db80a7319358c394cbcba5578423a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:53 +0000

    Add package rmotr-b12-cpyp-g7-jobs-detector/0.0.1/rmotr-b12-cpyp-g7-jobs-detector-17.1.0.dev0.tar.gz

---
+++ packages/rmotr-b12-cpyp-g7-jobs-detector/rmotr-b12-cpyp-g7-jobs-detector-17.1.0.dev0.tar.gz/rmotr-b12-cpyp-g7-jobs-detector-17.1.0.dev0/setup.py
@@ -0,0 +1,118 @@
+import codecs
+import os
+import re
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+NAME = 'rmotr-b12-cpyp-g7-jobs-detector'
+PACKAGES = find_packages(where="jobs_detector")
+META_PATH = os.path.join("jobs_detector", "__init__.py")
+KEYWORDS = ["class", "attribute", "boilerplate"]
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+INSTALL_REQUIRES = []
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=find_meta("uri"),
+        version=find_meta("version"),
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        keywords=KEYWORDS,
+        long_description=read("README.rst"),
+        packages=PACKAGES,
+        package_dir={"": "jobs_detector"},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
+
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ["tests/"]
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import sys, pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='rmotr.com | Jobs Detector',
+    version='0.0.1',
+    description="rmotr.com Group Project | Jobs Detector",
+    author='rmotr.com',
+    author_email='questions@rmotr.com',
+    license='CC BY-SA 4.0 License',
+    packages=['jobs_detector'],
+    maintainer='rmotr.com',
+    tests_require=[
+        'pytest==2.9.1'
+    ],
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+)
+
+
+
+
