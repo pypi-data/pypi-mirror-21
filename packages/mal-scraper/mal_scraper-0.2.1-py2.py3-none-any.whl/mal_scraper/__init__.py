667e3b7cad4987b04e3ba5af7c259d6f42beaa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package mal-scraper/0.2.1/mal_scraper-0.2.1-py2.py3-none-any.whl

---
+++ packages/mal-scraper/mal_scraper-0.2.1-py2.py3-none-any.whl/mal_scraper/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.1"
+
+# Import Public API
+from .anime import retrieve_anime  # noqa
+from .consts import AgeRating, AiringStatus, ConsumptionStatus, Format, Season  # noqa
+from .exceptions import ParseError  # noqa
+from .users import discover_users, get_user_anime_list, get_user_stats  # noqa
