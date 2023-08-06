79e2d03531a7fe729c50393b0163aa8738f0064a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:26 +0000

    Add package urlman/1.1.0/urlman-1.1.0.tar.gz

---
+++ packages/urlman/urlman-1.1.0.tar.gz/urlman-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='urlman',
+    version='1.1.0',
+    description='Django URL pattern helpers',
+    url='https://github.com/andrewgodwin/urlman',
+    py_modules=['urlman'],
+    author='Andrew Godwin',
+    author_email='andrew@aeracode.org',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+    ],
+)
