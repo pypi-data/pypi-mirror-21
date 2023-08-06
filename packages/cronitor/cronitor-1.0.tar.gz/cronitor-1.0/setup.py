8d8e0e310874367fc5e4b61ff4666b6194dba75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:33 +0000

    Add package cronitor/1.0/cronitor-1.0.tar.gz

---
+++ packages/cronitor/cronitor-1.0.tar.gz/cronitor-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cronitor',
+    version='1.0',
+    packages=[],
+    url='https://github.com/vylabs/cronitor',
+    license='MIT License',
+    author='xrage',
+    author_email='',
+    description='Python wrapper for cronitor monitors',
+    install_requires=[
+        'requests',
+    ],
+)
