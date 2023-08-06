0e82d2592ff27947837b19df2cf2dad283b10a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:21 +0000

    Add package vumi-msisdn-normalize-middleware/0.1.3/vumi_msisdn_normalize_middleware-0.1.3.tar.gz

---
+++ packages/vumi-msisdn-normalize-middleware/vumi_msisdn_normalize_middleware-0.1.3.tar.gz/vumi_msisdn_normalize_middleware-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+
+version = '0.1.3'
+
+install_requires = [
+    'vumi',
+]
+
+
+setup(
+    name='vumi_msisdn_normalize_middleware',
+    version=version,
+    description="Middleware to normalize msisdns",
+    long_description=README,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='',
+    author='Praekelt.org',
+    author_email='',
+    url='',
+    license='',
+    packages=[
+        'vumi_msisdn_normalize_middleware',
+    ],
+    package_dir={'vumi_msisdn_normalize_middleware':
+                 'vumi_msisdn_normalize_middleware'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['vumi_msisdn_normalize_middleware=vumi_msisdn_normalize_middleware:main']
+    }
+)
