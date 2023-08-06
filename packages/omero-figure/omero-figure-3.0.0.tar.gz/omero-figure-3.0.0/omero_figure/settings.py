38ea1550ffc4400a6e27db3294ce433a916387e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:54 +0000

    Add package omero-figure/3.0.0/omero-figure-3.0.0.tar.gz

---
+++ packages/omero-figure/omero-figure-3.0.0.tar.gz/omero-figure-3.0.0/omero_figure/settings.py
@@ -0,0 +1,8 @@
+
+# This settings.py file will be imported by omero.settings file AFTER it has
+# initialised custom settings.
+# See "App Settings" on
+# https://www.openmicroscopy.org/
+#   site/support/omero5.3/developers/Web/CreateApp.html
+
+OMERO_FIGURE_VERSION = "3.0.0"
