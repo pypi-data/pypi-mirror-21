ce65cb1e7fd8a56f2aff7b466e9cbd011bddc460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:22 +0000

    Add package ct/0.9.7.7/ct-0.9.7.7-py2.7.egg

---
+++ packages/ct/ct-0.9.7.7-py2.7.egg/cantools/web/dez_server/mail.py
@@ -0,0 +1,17 @@
+from ..util import config, log
+import yagmail
+yag = None
+if config.mailer:
+	mailer = config.mailer
+	if config.mailername:
+		mailer = {}
+		mailer[config.mailer] = config.mailername
+	yag = yagmail.SMTP(mailer, config.cache("email password? "))
+
+def send_mail(to=None, sender=None, subject=None, body=None, html=None):
+	if not yag:
+		return log("failed to send email -- no MAILER specified in ct.cfg!")
+	if type(to) == unicode:
+		to = str(to)
+	log('emailing "%s" to %s'%(subject, to))
+	yag.send(to, subject, html or body) # ignore sender -- same every time
