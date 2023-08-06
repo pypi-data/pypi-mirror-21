a274ce5523a2b92c483bb2d5834f11743035c4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:44 +0000

    Add package ppg-common/1.14/ppg_common-1.14.tar.gz

---
+++ packages/ppg-common/ppg_common-1.14.tar.gz/ppg_common-1.14/ppg_common/errors/__init__.py
@@ -0,0 +1,3 @@
+from .bubbles import BubblesHttpError, BubblesHttpErrors
+from .blossom import BlossomHTTPError, BlossomHTTPErrors
+from .buttercup import ButterCupHTTPException, ButterCupHTTPErrors
