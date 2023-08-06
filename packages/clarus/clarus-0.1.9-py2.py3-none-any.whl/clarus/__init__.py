630490a65c956b43d5d646c162a9201d66a468f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:28 +0000

    Add package clarus/0.1.9/clarus-0.1.9-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.9-py2.py3-none-any.whl/clarus/__init__.py
@@ -0,0 +1,18 @@
+#Name: clarus.py
+#Runtimes: Python,Python_Lambda
+#Leave blank line below to mark end of header
+
+from __future__ import print_function
+from six.moves import urllib
+import requests
+import json
+import os
+import sys
+import logging
+
+from clarus.services import compliance, frtb, hedge, margin, market, portfolio, profitloss, risk, sdr, simm, trade, upload, util, xva
+from clarus.api_config import ApiConfig
+from clarus.models import ApiResponse
+from clarus.output_types import *
+from clarus.api import request
+from clarus.resource_util import read
