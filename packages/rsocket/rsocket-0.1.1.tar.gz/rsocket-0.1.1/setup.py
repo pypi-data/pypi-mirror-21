6f68b9b906f3ae9779b6fa34272c845f629d95ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:52 +0000

    Add package rsocket/0.1.1/rsocket-0.1.1.tar.gz

---
+++ packages/rsocket/rsocket-0.1.1.tar.gz/rsocket-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='rsocket',
+    version='0.1.1',
+    description='Python RSocket library',
+    url='https://github.com/RSocket/rsocket-py',
+    author='Vijayan Rajan',
+    author_email='',
+    license='MIT',
+    packages=[
+        'rsocket',
+        'reactivestreams'
+    ],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
