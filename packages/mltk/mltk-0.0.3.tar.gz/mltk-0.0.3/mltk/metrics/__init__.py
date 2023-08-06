d9e8c5dc64dde1743f79cd2f36227f78ef9b89bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:45 +0000

    Add package mltk/0.0.3/mltk-0.0.3.tar.gz

---
+++ packages/mltk/mltk-0.0.3.tar.gz/mltk-0.0.3/mltk/metrics/__init__.py
@@ -0,0 +1,3 @@
+from auc import auc
+from edit_distance import *
+from confusion_matrix import ConfusionMatrix
---
+++ packages/mltk/mltk-0.0.3.tar.gz/mltk-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+requirements = ["numpy", "pandas"]
+
+setup(
+  name = 'mltk',
+  packages = ['mltk', 'mltk.metrics'], # this must be the same as the name above
+  version = '0.0.3',
+  description = 'Machine learning toolkit for python',
+  author = 'Manan Shah',
+  author_email = 'manan.shah.777@gmail.com',
+  url = 'https://github.com/mananshah99/mltk', # use the URL to the github repo
+  keywords = ['ml', 'metrics', 'toolkit'], # arbitrary keywords
+  install_requires = requirements,
+  classifiers = [],
+)
