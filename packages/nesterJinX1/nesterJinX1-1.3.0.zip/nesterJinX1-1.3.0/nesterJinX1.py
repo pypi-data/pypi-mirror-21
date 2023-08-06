3664a68234454c8d032cfb926cf41281172f4185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:35 +0000

    Add package nesterJinX1/1.3.0/nesterJinX1-1.3.0.zip

---
+++ packages/nesterJinX1/nesterJinX1-1.3.0.zip/nesterJinX1-1.3.0/nesterJinX1.py
@@ -0,0 +1,16 @@
+"""This is the "nester.py" module and it provides on function called print_lol()
+   which prints lists that may or may not include nested lists."""
+def print_lol(the_list, indent=False, level=0):
+    """This function takes one positional argument called "the list", which
+       is any Python list (of - possibly - nested lists). Each data item in the
+       provided list is (recursively) printed to the screen on it's own line."""
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                print("\t" *level, end='')
+                for tap_stop in level:
+                    print("\t", end='')
+            print(each_item)
