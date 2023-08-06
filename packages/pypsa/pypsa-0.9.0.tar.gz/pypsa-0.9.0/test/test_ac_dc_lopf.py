aec95aa4cdcf94788267459f8342d46f84ee5f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:39 +0000

    Add package pypsa/0.9.0/pypsa-0.9.0.tar.gz

---
+++ packages/pypsa/pypsa-0.9.0.tar.gz/pypsa-0.9.0/test/test_ac_dc_lopf.py
@@ -0,0 +1,55 @@
+from __future__ import print_function, division
+from __future__ import absolute_import
+
+import pypsa
+
+import datetime
+import pandas as pd
+
+import networkx as nx
+
+import numpy as np
+
+from itertools import chain, product
+
+import os
+
+
+
+from distutils.spawn import find_executable
+
+
+
+def test_lopf():
+
+
+    csv_folder_name = "../examples/ac-dc-meshed/ac-dc-data"
+
+    network = pypsa.Network(csv_folder_name=csv_folder_name)
+
+    results_folder_name = os.path.join(csv_folder_name,"results-lopf")
+
+    network_r = pypsa.Network(csv_folder_name=results_folder_name)
+
+
+    #test results were generated with GLPK and other solvers may differ
+    solver_name = "glpk"
+
+    snapshots = network.snapshots
+
+    for formulation, free_memory in product(["angles", "cycles", "kirchhoff", "ptdf"],
+                                            [{}, {"pypsa"}, {"pypsa", "pyomo-hack"}]):
+        network.lopf(snapshots=snapshots,solver_name=solver_name,formulation=formulation, free_memory=free_memory)
+        print(network.generators_t.p.loc[:,network.generators.index])
+        print(network_r.generators_t.p.loc[:,network.generators.index])
+
+        np.testing.assert_array_almost_equal(network.generators_t.p.loc[:,network.generators.index],network_r.generators_t.p.loc[:,network.generators.index])
+
+        np.testing.assert_array_almost_equal(network.lines_t.p0.loc[:,network.lines.index],network_r.lines_t.p0.loc[:,network.lines.index])
+
+        np.testing.assert_array_almost_equal(network.links_t.p0.loc[:,network.links.index],network_r.links_t.p0.loc[:,network.links.index])
+
+
+
+if __name__ == "__main__":
+    test_lopf()
