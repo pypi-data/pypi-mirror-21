6a784adee5f7578d7a3ca1d37cf3968620b44256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:54 +0000

    Add package nester10086/1.1.0/nester10086-1.1.0.tar.gz

---
+++ packages/nester10086/nester10086-1.1.0.tar.gz/nester10086-1.1.0/nester.py
@@ -0,0 +1,11 @@
+''' this is a zhushi......'''
+def print_lol(the_list, tab):
+	''' again a zhushi'''
+	for item in the_list:
+		if(isinstance(item,list)):
+			print_lol(item, tab+1)
+		else:
+                        for t in range(tab):
+                                print("\t", end='')
+                        print(item)
+
