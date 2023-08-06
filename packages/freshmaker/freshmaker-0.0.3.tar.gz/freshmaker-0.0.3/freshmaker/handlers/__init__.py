891a95526d1e205e6a992e75fa7a22840d26b81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:21 +0000

    Add package freshmaker/0.0.3/freshmaker-0.0.3.tar.gz

---
+++ packages/freshmaker/freshmaker-0.0.3.tar.gz/freshmaker-0.0.3/freshmaker/handlers/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016  Red Hat, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Written by Jan Kaluza <jkaluza@redhat.com>
+
+import abc
+import fedmsg.utils
+
+from freshmaker import conf
+
+
+def load_handlers():
+    """ Import and instantiate all handlers listed in the given config. """
+    for import_path in conf.handlers:
+        cls = fedmsg.utils.load_class(import_path)
+        handler = cls()
+        yield handler
+
+
+class BaseHandler(object):
+    """
+    Abstract base class for event handlers.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def can_handle(self, event):
+        """
+        Returns true if this class can handle this type of event.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def handle(self, event):
+        """
+        Handles the event. Can return another BaseEvent instances to
+        generate another events to be used by other local handlers.
+
+        :return: List of BaseEvent objects which will be handled by other
+        handlers after this handler handles the event. This can be used to
+        generate internal events for other handlers in Freshmaker.
+        """
+        raise NotImplementedError()
