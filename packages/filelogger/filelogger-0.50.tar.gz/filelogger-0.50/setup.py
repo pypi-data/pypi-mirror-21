19873284fbec233ed5d0399bc14e90806ac62d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:29 +0000

    Add package filelogger/0.50/filelogger-0.50.tar.gz

---
+++ packages/filelogger/filelogger-0.50.tar.gz/filelogger-0.50/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    url='https://github.com/nickmetal/FileLogger',
+    description='Wrapper class for RotatingFileHandler from logging module',
+    name='filelogger',
+    version='0.50',
+    packages=['filelogger'],
+    author='Me',
+    author_email='nickmetal92@gmail.com',
+    download_url='https://github.com/nickmetal/filelogger/archive/0.50.tar.gz',
+)
