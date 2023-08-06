02575e1b617c1979361a5b8e147d1363f87e0eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package leakPasswd/1.0/leakPasswd-1.0.tar.gz

---
+++ packages/leakPasswd/leakPasswd-1.0.tar.gz/leakPasswd-1.0/setup.py
@@ -0,0 +1,12 @@
+import leakPasswd
+
+from distutils.core import setup
+
+setup(
+    name="leakPasswd",
+    version=leakPasswd.__version__,
+    description="This is the password leak query module",
+    author="lauix",
+    author_email="lauixData@gmail.com",
+    url="https://github.com/lauixData/leakPasswd",
+)
