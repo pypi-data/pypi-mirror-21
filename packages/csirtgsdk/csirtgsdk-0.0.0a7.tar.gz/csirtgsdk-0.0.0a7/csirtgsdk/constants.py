1eca67a7a49021c2a92d1b8a460148d00b4711c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:57 +0000

    Add package csirtgsdk/0.0.0a7/csirtgsdk-0.0.0a7.tar.gz

---
+++ packages/csirtgsdk/csirtgsdk-0.0.0a7.tar.gz/csirtgsdk-0.0.0a7/csirtgsdk/constants.py
@@ -0,0 +1,23 @@
+import os.path
+
+API_VERSION = 0
+
+REMOTE = 'https://csirtg.io/api'
+REMOTE = os.environ.get('CSIRTG_REMOTE', REMOTE)
+
+TOKEN = os.environ.get('CSIRTG_TOKEN', None)
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'
+
+LIMIT = 500
+LIMIT = os.environ.get('CSIRTG_LIMIT', LIMIT)
+
+TIMEOUT = 300
+TIMEOUT = os.environ.get('CSIRTG_TIMEOUT', TIMEOUT)
+
+COLUMNS = "user,feed,indicator,firsttime,lasttime,count,comments,protocol,portlist,tags,description"
+COLUMNS = os.environ.get('CSIRTG_COLUMNS', COLUMNS)
+COLUMNS = COLUMNS.split(',')
+
+MAX_FIELD_SIZE = 30
+MAX_FIELD_SIZE = os.environ.get('CSIRTG_MAX_FIELD_SIZE', MAX_FIELD_SIZE)
