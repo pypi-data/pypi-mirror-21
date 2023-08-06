cc9c06f1b07e8b6ab7ec76ec2bee1ba72f458930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package eoddata-client/0.3.0/eoddata_client-0.3.0.tar.gz

---
+++ packages/eoddata-client/eoddata_client-0.3.0.tar.gz/eoddata_client-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='eoddata_client',
+    packages=['eoddata_client'],
+    version='0.3.0',
+    description='Client to get historical market data from EODData web service.',
+    author='Aleksey',
+    author_email='apologist.code@gmail.com',
+    url='https://github.com/apologist/eoddata-client',
+    license='Public Domain',
+    download_url='',
+    keywords=['market', 'data', 'trading', 'stocks', 'finance'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['requests', 'pandas'],
+)
