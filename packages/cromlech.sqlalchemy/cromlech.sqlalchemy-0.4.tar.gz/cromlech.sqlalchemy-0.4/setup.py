8b9ee98cd0c6d82dcda542fcec61fff26aa9c06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:38 +0000

    Add package cromlech.sqlalchemy/0.4/cromlech.sqlalchemy-0.4.tar.gz

---
+++ packages/cromlech.sqlalchemy/cromlech.sqlalchemy-0.4.tar.gz/cromlech.sqlalchemy-0.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4'
+
+install_requires = [
+    'setuptools',
+    'SQLAlchemy',
+    'transaction >= 1.2',
+    'zope.interface',
+    'zope.sqlalchemy >= 0.7',
+    ]
+
+tests_require = [
+    ]
+
+setup(name='cromlech.sqlalchemy',
+      version=version,
+      description="Cromlech Web Framework utility methods and components " +
+                  "for applications based on SQLAlchemy.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("src", 'cromlech', 'sqlalchemy',
+                                         "test_api.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='SQLAlchemy ORM Cromlech',
+      author='Dolmen Team',
+      author_email='dolmen@list.dolmen-project.org',
+      url='http://gitweb.dolmen-project.org',
+      license='ZPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['cromlech', ],
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=tests_require,
+      install_requires=install_requires,
+      extras_require={'test': tests_require},
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/cromlech.sqlalchemy/cromlech.sqlalchemy-0.4.tar.gz/cromlech.sqlalchemy-0.4/src/cromlech/sqlalchemy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .components import create_engine
+from .controlled import SQLAlchemySession, SharedSQLAlchemySession, get_session
