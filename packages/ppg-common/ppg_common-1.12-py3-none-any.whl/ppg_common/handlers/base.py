de44c4759b530ca055b9fe76018f30d9d7b9382b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:52 +0000

    Add package ppg-common/1.12/ppg_common-1.12-py3-none-any.whl

---
+++ packages/ppg-common/ppg_common-1.12-py3-none-any.whl/ppg_common/handlers/base.py
@@ -0,0 +1,33 @@
+from ppg_common.errors.bubbles import BubblesHttpError
+from ppg_common.errors.blossom import BlossomHTTPError
+from ppg_common.errors.buttercup import ButterCupHTTPException
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header('Content-Type', 'application/json')
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', 'x-session')
+        self.set_header('Access-Control-Allow-Methods',
+                        'GET,PUT,PATCH,POST,DELETE,OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+
+    def write_error(self, status_code, **kwargs):
+        exc_class, exc_obj, traceback = kwargs.get('exc_info')
+        if any([exc_class is err for err in [
+            BubblesHttpError,
+            BlossomHTTPError,
+            ButterCupHTTPException
+        ]]):
+            self.finish({
+                'message': exc_obj.log_message,
+                'code': exc_obj.custom_code
+            })
+        else:
+            self.finish({
+                'message': traceback,
+                'code': 'unknown'
+            })
