919a29f4ebc8506d0fbb7df130d28a54fff6695b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:24 +0000

    Add package singer-tools/0.3.1/singer-tools-0.3.1.tar.gz

---
+++ packages/singer-tools/singer-tools-0.3.1.tar.gz/singer-tools-0.3.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='singer-tools',
+      version='0.3.1',
+      description='Tools for working with Singer.io Taps and Targets',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      install_requires=[
+          'singer-python>=0.2.1',
+          'attrs==16.3.0',
+          'terminaltables==3.1.0'
+      ],
+      packages=['singertools'],
+      entry_points='''
+          [console_scripts]
+          singer-infer-schema=singertools.infer_schema:main
+          singer-check-tap=singertools.check_tap:main
+          singer-release=singertools.release:main
+      ''',
+      include_package_data=True,
+)
