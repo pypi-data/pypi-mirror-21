dcc72c20f4448ca2c5d9a77af9c21e87b26ebe82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:55 +0000

    Add package Lawes/1.9.2.4/Lawes-1.9.2.4.tar.gz

---
+++ packages/Lawes/Lawes-1.9.2.4.tar.gz/Lawes-1.9.2.4/setup.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '1.9.2.4'
+
+setup(
+    name='Lawes',
+    description='',
+    long_description='',
+    classifiers=[],
+    keywords='',
+    author='Lawes',
+    author_email='haiou_chen@sina.cn',
+    url='https://github.com/MrLawes/Lawes',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'pymongo>=2.8',
+    ],
+    version=VERSION,
+)
