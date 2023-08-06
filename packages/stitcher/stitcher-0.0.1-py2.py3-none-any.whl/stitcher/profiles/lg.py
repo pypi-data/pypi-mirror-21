9b4ab933ee706ba57fc0e22620b3c23c16d15ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:16 +0000

    Add package stitcher/0.0.1/stitcher-0.0.1-py2.py3-none-any.whl

---
+++ packages/stitcher/stitcher-0.0.1-py2.py3-none-any.whl/stitcher/profiles/lg.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+G3 = {
+    'header_height': 344,
+    'footer_height': 552,
+    'additional_message_gap': 8,
+    'reply_message_gap': 24
+}
---
+++ packages/stitcher/stitcher-0.0.1-py2.py3-none-any.whl/stitcher/profiles/nokia.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+LUMIA_925 = {
+    'mode': 'RGBA',
+    'header_height': 232,
+    'footer_height': 116,
+    'additional_message_gap': 2,
+    'reply_message_gap': 7
+}
