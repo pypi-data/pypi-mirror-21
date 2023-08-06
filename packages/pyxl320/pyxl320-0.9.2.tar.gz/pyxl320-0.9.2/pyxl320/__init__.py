ce3f98264c02b563df9a4f7a4640a2844c799cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:49 +0000

    Add package pyxl320/0.9.2/pyxl320-0.9.2.tar.gz

---
+++ packages/pyxl320/pyxl320-0.9.2.tar.gz/pyxl320-0.9.2/pyxl320/__init__.py
@@ -0,0 +1,14 @@
+
+
+__version__ = '0.9.2'
+__copyright__ = 'Copyright (c) 2016 Kevin Walchko'
+__license__ = 'MIT'
+__author__ = 'Kevin J. Walchko'
+
+
+from .ServoSerial import ServoSerial
+# from .DummySerial import DummySerial
+from . import utils
+# from utils import listSerialPorts
+from . import xl320
+from . import Packet
