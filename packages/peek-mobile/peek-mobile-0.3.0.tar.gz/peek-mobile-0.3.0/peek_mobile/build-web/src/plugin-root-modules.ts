518671d7468fe175c36b8d4b29aed233305052f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:58 +0000

    Add package peek-mobile/0.3.0/peek-mobile-0.3.0.tar.gz

---
+++ packages/peek-mobile/peek-mobile-0.3.0.tar.gz/peek-mobile-0.3.0/peek_mobile/build-web/src/plugin-root-modules.ts
@@ -0,0 +1,11 @@
+// This file is auto generated, the git version is blank and .gitignored
+import {PluginActiveTaskRootModule} from "@peek/peek_plugin_active_task/plugin-active-task-root.module";
+import {PluginNoopClientRootModule} from "@peek/peek_plugin_noop/_private/mobile/plugin-noop-root.module";
+import {PluginPofFieldSwitchingRootModule} from "@peek/peek_plugin_pof_field_switching/plugin-pof-field-switching-root.module";
+import {PluginUserRootModule} from "@peek/peek_plugin_user/plugin-user-root.module";
+export const pluginRootModules = [
+	PluginActiveTaskRootModule,
+	PluginNoopClientRootModule,
+	PluginPofFieldSwitchingRootModule,
+	PluginUserRootModule
+];
