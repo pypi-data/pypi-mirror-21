d0cdf33f373bcf09c7ce2a866608acd2625e2eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package pyweechat/0.2/pyweechat-0.2.zip

---
+++ packages/pyweechat/pyweechat-0.2.zip/pyweechat-0.2/pyweechat/__init__.py
@@ -0,0 +1,5 @@
+from .message import WeeChatMessage
+from .exceptions import WeeChatUnknownCommandException
+from .socket import WeeChatSocket
+from .buffer import WeeChatBuffer
+from .client import WeeChatClient
