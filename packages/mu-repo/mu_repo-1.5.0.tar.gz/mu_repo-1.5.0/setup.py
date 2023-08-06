450dc6509e36ff78041db71a381a1e5ab82f8553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:42 +0000

    Add package mu-repo/1.5.0/mu_repo-1.5.0.tar.gz

---
+++ packages/mu-repo/mu_repo-1.5.0.tar.gz/mu_repo-1.5.0/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='mu_repo',
+    version='1.5.0',
+    description = 'Tool to work with multiple git repositories',
+    author='Fabio Zadrozny',
+    url='http://fabioz.github.io/mu-repo',
+    # scripts=['mu'], -- entry_points:console_scripts should do what we want.
+    packages=['mu_repo'],
+    
+    entry_points = {
+        'console_scripts': [
+            'mu = mu_repo:main_entry_point',                  
+        ],
+    },
+)
+
+# Note: nice reference: https://jamie.curle.io/blog/my-first-experience-adding-package-pypi/
+# New version: change version and then:
+# git tag -a mu_repo_1_5_0
+# git push --tags
+# python setup.py sdist
+# python setup.py sdist upload
