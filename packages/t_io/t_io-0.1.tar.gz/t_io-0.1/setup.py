03d9db9acc5ad17be09ad79f73cffbc7a1d21eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:50 +0000

    Add package t_io/0.1/t_io-0.1.tar.gz

---
+++ packages/t_io/t_io-0.1.tar.gz/t_io-0.1/setup.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = 't_io',
+      version = '0.1',
+      description = 'T IO library',
+      url = 'https://gitlab.com/ddmello/lib/python/threads_io.git',
+      author = 'Darrin D\'Mello',
+      author_email = 'darrin.dmello@gmail.com',
+      license = 'MIT',
+      packages = ['t_io'],
+      zip_safe = True)
---
+++ packages/t_io/t_io-0.1.tar.gz/t_io-0.1/t_io/__init__.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/env python
+
+import errno
+import os
+
+def read_file(path, split_lines = False):
+    '''Reads the specified file and returns its contents as one block'''
+    with open(path, 'r') as file_handle:
+        if split_lines:
+            contents = file_handle.readlines()
+        else:
+            contents = file_handle.read()
+    return contents
+
+def write_file(contents, path, makedirs = False):
+    # TODO: check if contents is an array and simply branch and use writelines
+    if makedirs:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, 'w') as file_handle:
+        file_handle.write(contents)
+
+# Python 2 Implementation
+#def write_file_x(path, contents):
+#    '''Writes the specified contents to the specified file, iff the file does not exist'''
+#    file_handle_flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+#    file_handle = None
+#    try:
+#        file_handle = os.open(path, file_handle_flags)
+#        writable_file_handle = os.fdopen(file_handle, 'w')
+#        writable_file_handle.write(contents)
+#    finally:
+#        if file_handle:
+#            os.close(file_handle)
+
+# Python 3 Implementation
+def write_file_x(path, contents):
+    '''Writes the specified contents to the specified file, iff the file does not exist'''
+    with open(path, 'x') as file_handle:
+        file_handle.write(contents)
