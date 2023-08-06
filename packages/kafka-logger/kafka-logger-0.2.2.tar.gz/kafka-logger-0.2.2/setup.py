edd147fd6bc0a8f177c83943487c0e622addf177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:31 +0000

    Add package kafka-logger/0.2.2/kafka-logger-0.2.2.tar.gz

---
+++ packages/kafka-logger/kafka-logger-0.2.2.tar.gz/kafka-logger-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kafka-logger',
+    packages=find_packages(exclude=['build', 'docs', 'templates']),
+    version='0.2.2',
+    description='A simple Kafka logger in Python.',
+    author='Peijie Hu',
+    author_email='peijie@peijiehu.com',
+    license='MIT',
+    url='https://github.com/peijiehu/kafka-logger',
+    keywords=['python', 'kafka', 'logstash', 'logging'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
