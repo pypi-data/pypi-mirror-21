436add36a313586fc21b317479076feb2622d5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:42 +0000

    Add package stochasticdp/0.2/stochasticdp-0.2.tar.gz

---
+++ packages/stochasticdp/stochasticdp-0.2.tar.gz/stochasticdp-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import pypandoc
+
+long_description = pypandoc.convert('README.md', 'rst')
+
+setuptools.setup(
+    name='stochasticdp',
+    version='0.2',
+    description='''
+        A simple implementation of backwards induction for solving finite-horizon, finite-state stochastic
+        dynamic programs.
+    ''',
+    long_description=long_description,
+    url='https://github.com/nelsonuhan/stochasticdp',
+    author='Nelson Uhan',
+    author_email='nelson@uhan.me',
+    license='MIT',
+    packages=['stochasticdp'],
+)
