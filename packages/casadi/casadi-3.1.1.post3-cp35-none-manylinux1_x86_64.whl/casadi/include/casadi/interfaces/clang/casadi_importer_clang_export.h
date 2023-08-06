ef27b0e4310eebe992353e9a5943c1a4d7a32ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:36 +0000

    Add package casadi/3.1.1/casadi-3.1.1.post3-cp35-none-manylinux1_x86_64.whl

---
+++ packages/casadi/casadi-3.1.1.post3-cp35-none-manylinux1_x86_64.whl/casadi/include/casadi/interfaces/clang/casadi_importer_clang_export.h
@@ -0,0 +1,41 @@
+
+#ifndef CASADI_IMPORTER_CLANG_EXPORT_H
+#define CASADI_IMPORTER_CLANG_EXPORT_H
+
+#ifdef CASADI_IMPORTER_CLANG_STATIC_DEFINE
+#  define CASADI_IMPORTER_CLANG_EXPORT
+#  define CASADI_IMPORTER_CLANG_NO_EXPORT
+#else
+#  ifndef CASADI_IMPORTER_CLANG_EXPORT
+#    ifdef casadi_importer_clang_EXPORTS
+        /* We are building this library */
+#      define CASADI_IMPORTER_CLANG_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define CASADI_IMPORTER_CLANG_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef CASADI_IMPORTER_CLANG_NO_EXPORT
+#    define CASADI_IMPORTER_CLANG_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef CASADI_IMPORTER_CLANG_DEPRECATED
+#  define CASADI_IMPORTER_CLANG_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef CASADI_IMPORTER_CLANG_DEPRECATED_EXPORT
+#  define CASADI_IMPORTER_CLANG_DEPRECATED_EXPORT CASADI_IMPORTER_CLANG_EXPORT CASADI_IMPORTER_CLANG_DEPRECATED
+#endif
+
+#ifndef CASADI_IMPORTER_CLANG_DEPRECATED_NO_EXPORT
+#  define CASADI_IMPORTER_CLANG_DEPRECATED_NO_EXPORT CASADI_IMPORTER_CLANG_NO_EXPORT CASADI_IMPORTER_CLANG_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define CASADI_IMPORTER_CLANG_NO_DEPRECATED
+#endif
+
+#endif
