8b88f613dea009d4f1e23f0c123e797296b7f756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package filebrowser-s3/1.0.0/filebrowser_s3-1.0.0.tar.gz

---
+++ packages/filebrowser-s3/filebrowser_s3-1.0.0.tar.gz/filebrowser_s3-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="filebrowser_s3",
+	version="1.0.0",
+	description="An S3 fix for Mezzanine's media manager.",
+	author = 'Mozilla Foundation',
+	author_email = 'pomax@mozillafoundation.org',
+	maintainer="Pomax",
+	maintainer_email="pomax@mozillafoundation.org",
+	url = 'https://github.com/Pomax/filebrowser_s3',
+	download_url = 'https://github.com/Pomax/filebrowser_s3/archive/1.0.0.tar.gz',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	classifiers=[],
+)
