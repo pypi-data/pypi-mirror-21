022a4c506ee05e97cf0078a53924e0b20dc16428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:03 +0000

    Add package odoo9-addon-l10n-es-aeat-mod303/9.0.1.0.0.99.dev1/odoo9_addon_l10n_es_aeat_mod303-9.0.1.0.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo9-addon-l10n-es-aeat-mod303/odoo9_addon_l10n_es_aeat_mod303-9.0.1.0.0.99.dev1-py2-none-any.whl/odoo_addons/l10n_es_aeat_mod303/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2013 Alberto Martín Cortada (Guadaltech)
+# © 2014-2015 Pedro M. Baeza
+# © 2015 AvanzOSC - Ainara Galdona
+# © 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
+
+{
+    "name": "AEAT modelo 303",
+    "version": "9.0.1.0.0",
+    'category': "Accounting & Finance",
+    'author': "Guadaltech,"
+              "AvanzOSC,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    'website': "https://odoo-community.org/",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es",
+        "l10n_es_aeat",
+    ],
+    "data": [
+        "data/tax_code_map_mod303_data.xml",
+        "data/aeat_export_mod303_data.xml",
+        "views/mod303_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    'installable': True,
+}
