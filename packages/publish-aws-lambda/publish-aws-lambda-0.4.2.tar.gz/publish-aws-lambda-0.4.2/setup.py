f18e12d2d3364ab1d02a8e133d3260c380a6f515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:51 +0000

    Add package publish-aws-lambda/0.4.2/publish-aws-lambda-0.4.2.tar.gz

---
+++ packages/publish-aws-lambda/publish-aws-lambda-0.4.2.tar.gz/publish-aws-lambda-0.4.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+from publish_aws_lambda import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="publish-aws-lambda",
+    version=__version__,
+    description="Publish a Python module as a set of AWS lambda functions",
+    url="https://github.com/ophirh/publish-aws-lambda",
+    author="Ophir",
+    author_email="opensource@itculate.io",
+    license="MIT",
+    keywords=["aws", "lambda", "publish"],
+    packages=find_packages(),
+)
