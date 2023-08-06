ed6f6e69f77e360777ff55298ded1f4e06872eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package zaifapi/1.5.0/zaifapi-1.5.0.tar.gz

---
+++ packages/zaifapi/zaifapi-1.5.0.tar.gz/zaifapi-1.5.0/zaifapi/__init__.py
@@ -0,0 +1,4 @@
+from .impl import ZaifPrivateApi, ZaifPublicApi, ZaifPrivateTokenApi, ZaifPublicStreamApi
+from .token import ZaifTokenApi
+
+__all__ = ['ZaifPrivateApi', 'ZaifPublicApi', 'ZaifPrivateTokenApi', 'ZaifTokenApi', 'ZaifPublicStreamApi']
