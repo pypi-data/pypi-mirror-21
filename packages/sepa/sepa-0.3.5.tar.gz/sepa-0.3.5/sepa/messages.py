0f6f77dd4bc4780796d721d098b29b3a251e0be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:29 +0000

    Add package sepa/0.3.5/sepa-0.3.5.tar.gz

---
+++ packages/sepa/sepa-0.3.5.tar.gz/sepa-0.3.5/sepa/messages.py
@@ -0,0 +1,91 @@
+from .general import code_or_proprietary, group_header, party
+from .payment import payment_group_header, payment
+from .mandate import mandate
+
+def original_message(tag):
+    return {
+        '_self': tag,
+        '_sorting': ['MsgId', 'MsgNmId', 'CreDtTm'],
+        'message_id': 'MsgId',
+        'message_name_id': 'MsgNmId',
+        'creation_date_time': 'CreDtTm'
+    }
+
+customer_direct_debit_initiation = {
+    '_namespaces': {
+        None: 'urn:iso:std:iso:20022:tech:xsd:pain.008.001.07',
+        'xs': 'http://www.w3.org/2001/XMLSchema'
+    },
+    '_self': 'CstmrDrctDbtInitn',
+    '_sorting': ['GrpHdr', 'PmtInf', 'SplmtryData'],
+    'group_header': payment_group_header('GrpHdr'),
+    'payments': [payment('PmtInf')],
+    'supplementary_data': ['SplmtryData']
+}
+
+mandate_initiation_request = {
+    '_namespaces': {
+        None: 'urn:iso:std:iso:20022:tech:xsd:pain.009.001.05',
+        'xs': 'http://www.w3.org/2001/XMLSchema'
+    },
+    '_self': 'MndtInitnReq',
+    '_sorting': ['GrpHdr', 'Mndt', 'SplmtryData'],
+    'group_header': group_header('GrpHdr'),
+    'mandate': [mandate('Mndt')],
+    'supplementary_data': ['SplmtryData']
+}
+
+mandate_amendment_request = {
+    '_namespaces': {
+        None: 'urn:iso:std:iso:20022:tech:xsd:pain.010.001.05',
+        'xs': 'http://www.w3.org/2001/XMLSchema'
+    },
+    '_self': 'MndtAmdmntReq',
+    '_sorting': ['GrpHdr', 'UndrlygAmdmntDtls', 'SplmtryData'],
+    'group_header': group_header('GrpHdr'),
+    'amendment': [{
+        '_self': 'UndrlygAmdmntDtls',
+        'original_message': original_message('OrgnlMsgInf'),
+        'reason': {
+            '_self': 'AmdmntRsn',
+            'originator': party('Orgtr'),
+            'reason': code_or_proprietary('Rsn'),
+            'additional_information': ['AddtInf']
+        },
+        'mandate': mandate('Mndt'),
+        'original_mandate': {
+            '_self': 'OrgnlMsgInf',
+            'id': 'OrgnlMndtId',
+            'mandate': mandate(' OrgnMndt')
+        },
+        'supplementary_data': ['SplmtryData']
+    }],
+    'supplementary_data': ['SplmtryData']
+}
+
+mandate_cancellation_request = {
+    '_namespaces': {
+        None: 'urn:iso:std:iso:20022:tech:xsd:pain.011.001.05',
+        'xs': 'http://www.w3.org/2001/XMLSchema'
+    },
+    '_self': 'MndtCxlReq',
+    '_sorting': ['GrpHdr', 'UndrlygCxlDtls', 'SplmtryData'],
+    'group_header': group_header('GrpHdr'),
+    'cancellatiion': [{
+        '_self': 'UndrlygCxlDtls',
+        'original_message': original_message('OrgnlMsgInf'),
+        'reason': {
+            '_self': 'CxlRsn',
+            'originator': party('Orgtr'),
+            'reason': code_or_proprietary('Rsn'),
+            'additional_information': ['AddtInf']
+        },
+        'original_mandate': {
+            '_self': 'OrgnlMsgInf',
+            'id': 'OrgnlMndtId',
+            'mandate': mandate(' OrgnMndt')
+        },
+        'supplementary_data': ['SplmtryData']
+    }],
+    'supplementary_data': ['SplmtryData']
+}
---
+++ packages/sepa/sepa-0.3.5.tar.gz/sepa-0.3.5/tests/test.py
@@ -0,0 +1,94 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+from lxml import etree
+from sepa import builder, parser, validator, signer
+from .util import xml_compare
+
+data_object = {
+    'group_header': {
+        'message_id': '1234567890',
+        'creation_date_time': '2017-03-05 13:45',
+        'authorisation': {
+            'code': 'test123'
+        }
+    },
+    'mandate': [{
+        'id': '78904536',
+        'request_id': '9823701',
+        'authentication': [{
+            'date': '2017-03-05',
+            'channel': {
+                'code': 'ABC'
+            }
+        }]
+    }]
+}
+
+data_xml = ('<MndtInitnReq>'
+    '<Mndt>'
+        '<MndtId>78904536</MndtId>'
+        '<Authntcn>'
+            '<Dt>2017-03-05</Dt>'
+            '<Chanl>'
+                '<Cd>ABC</Cd>'
+            '</Chanl>'
+        '</Authntcn>'
+        '<MndtReqId>9823701</MndtReqId>'
+    '</Mndt>'
+    '<GrpHdr>'
+        '<CreDtTm>2017-03-05 13:45</CreDtTm>'
+        '<Authstn>'
+            '<Cd>test123</Cd>'
+        '</Authstn>'
+        '<MsgId>1234567890</MsgId>'
+    '</GrpHdr>'
+'</MndtInitnReq>')
+
+class TestSuite(unittest.TestSuite):
+    def test_builder(self):
+        # Build XML
+        data_out = builder.build(builder.mandate_initiation_request, data_object, document=False)
+
+        # Compare generated data to expected output
+        assert xml_compare(data_out, etree.fromstring(data_xml))
+
+    def test_validator(self):
+        # Build XML
+        data_out = builder.build(builder.mandate_initiation_request, data_object)
+
+        # TODO: remove debug print
+        print(etree.tostring(data_out))
+
+        # Validate using XML Schema Definition (XSD)
+        # assert not validator.validate_or_error(validator.mandate_initiation_request, data_out)
+
+    def test_signer(self):
+        # Build XML
+        data_out = builder.build(builder.mandate_initiation_request, data_object)
+
+        # Sign XML
+        with open(os.path.join(os.path.dirname(__file__), 'privkey3.pem'), 'rb') as f:
+            key = f.read()
+        with open(os.path.join(os.path.dirname(__file__), 'cert3.pem'), 'rb') as f:
+            cert = f.read()
+        data_signed = signer.sign(data_out, key=key, cert=cert)
+
+        print(etree.tostring(data_signed))
+
+        assert False
+
+        # Verify XML
+        # assert signer.verify(data_signed)
+
+    def test_parser(self):
+        # Parse XML
+        data_out = parser.parse(parser.mandate_initiation_request, etree.fromstring(data_xml))
+
+        # Compare generated data to expected output
+        assert data_out == data_object
+
+if __name__ == '__main__':
+    unittest.main()
