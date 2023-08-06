ae467ba555436abbb34bc6c529cb62d81c8caab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:31 +0000

    Add package pydomo/0.1.0/pydomo-0.1.0.tar.gz

---
+++ packages/pydomo/pydomo-0.1.0.tar.gz/pydomo-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='pydomo',
+    packages=['pydomo'],
+    version='0.1.0',
+    description='The official python3 Domo API SDK - Domo, Inc.',
+    author='Bobby Swingler',
+    author_email='bobby.swingler@domo.com',
+    url='https://github.com/domoinc/domo-python-sdk',
+    keywords=['domo', 'api', 'sdk']
+)
