051db8d468a9e4fc2cea964781b7e3aa3460edc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:58 +0000

    Add package easysvg/0.2.1/easysvg-0.2.1.tar.gz

---
+++ packages/easysvg/easysvg-0.2.1.tar.gz/easysvg-0.2.1/easysvg/__init__.py
@@ -0,0 +1,11 @@
+"""
+Simple package for drawing SVG images
+
+---
+
+Library URL: git@lemon.com:pylib/svglib
+"""
+
+from .svgrender import SvgGenerator, SvgPathGenerator, rotation_transform, render_error_svg
+
+__author__ = 'Stephen Brown (Little Fish Solutions LTD)'
