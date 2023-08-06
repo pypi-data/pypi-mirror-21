6b6743aa4e63dc8ac27b7f57cd8ba87fd1085aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:44 +0000

    Add package bmds/0.8.0/bmds-0.8.0.tar.gz

---
+++ packages/bmds/bmds-0.8.0.tar.gz/bmds-0.8.0/bmds/drunner.py
@@ -0,0 +1,79 @@
+import os
+from simple_settings import settings
+
+from . import utils, session
+
+
+class BatchDfileRunner(object):
+    """
+    Batch-execute a list of pre-created d-files.
+
+    Used to execute BMDS on a remote Windows server, if for example you're
+    running a library on a non-Windows OS.
+    """
+
+    def __init__(self, inputs):
+        self.tempfiles = utils.TempFileList()
+        self.inputs = inputs
+        self.outputs = []
+        self.execute()
+
+    def get_outfile(self, dfile, model_name):
+        outfile = dfile.replace('.(d)', '.out')
+        oo2 = outfile.replace('.out', '.002')
+
+        # if not exponential, exit early
+        if 'exponential' not in model_name.lower():
+            return outfile
+
+        # side-effect- cleanup other files created by exponential
+        if os.path.exists(outfile):
+            self.tempfiles.append(outfile)
+        if os.path.exists(oo2):
+            self.tempfiles.append(oo2)
+
+        # get exponential model prefix
+        prefix = model_name.split('-')[1]
+        path, fn = os.path.split(outfile)
+        outfile = os.path.join(path, prefix + fn)
+        return outfile
+
+    def execute(self):
+
+        for obj in self.inputs:
+
+            # get executable path
+            exe = session.BMDS.get_model(obj['bmds_version'],
+                                         obj['model_name']).get_exe_path()
+
+            # write dfile
+            dfile = self.tempfiles.get_tempfile(prefix='bmds-dfile-', suffix='.(d)')
+            with open(dfile, 'w') as f:
+                f.write(obj['dfile'])
+
+            output = {
+                'output_created': False,
+                'outfile': None,
+            }
+            try:
+                utils.RunBMDS(
+                    [exe, dfile],
+                    timeout=settings.BMDS_MODEL_TIMEOUT_SECONDS
+                ).call()
+                outfile = self.get_outfile(dfile, obj['model_name'])
+                oo2 = outfile.replace('.out', '.002')
+                if os.path.exists(outfile):
+                    self.tempfiles.append(outfile)
+                    output['output_created'] = True
+                    with open(outfile, 'r') as f:
+                        output['outfile'] = f.read()
+                if os.path.exists(oo2):
+                    self.tempfiles.append(oo2)
+            except Exception as e:
+                raise e
+            finally:
+                self.tempfiles.cleanup()
+
+            self.outputs.append(output)
+
+        return self.outputs
---
+++ packages/bmds/bmds-0.8.0.tar.gz/bmds-0.8.0/setup.py
@@ -0,0 +1,54 @@
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    regex = r"""^__version__ = '(.*)'$"""
+    with open('bmds/__init__.py', 'r') as f:
+        text = f.read()
+    return re.findall(regex, text, re.MULTILINE)[0]
+
+
+def get_readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+requirements = [
+    'six',
+    'requests',
+    'numpy',
+    'pandas',
+    'openpyxl',
+    'matplotlib',
+    'scipy',
+    'simple-settings',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='bmds',
+    version=get_version(),
+    description='Software development kit for US EPA\'s Benchmark dose modeling software (BMDS)',
+    long_description=get_readme(),
+    url='https://github.com/shapiromatron/bmds',
+    author='Andy Shapiro',
+    author_email='shapiromatron@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements,
+    tests_require=test_requirements,
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
