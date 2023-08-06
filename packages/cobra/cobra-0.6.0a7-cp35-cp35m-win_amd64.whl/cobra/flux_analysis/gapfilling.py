4fd200055a1a94f4d1b293894cca7365d12d3267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package cobra/0.6.0a7/cobra-0.6.0a7-cp35-cp35m-win_amd64.whl

---
+++ packages/cobra/cobra-0.6.0a7-cp35-cp35m-win_amd64.whl/cobra/flux_analysis/gapfilling.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from cobra.exceptions import DefunctError
+
+
+def growMatch():
+    raise DefunctError('growMatch', 'modelseed', 'http://modelseed.org/')
