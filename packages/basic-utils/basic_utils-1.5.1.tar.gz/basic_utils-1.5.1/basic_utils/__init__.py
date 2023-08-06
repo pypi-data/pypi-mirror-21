e24d5ff16d6cde777a226d6bfdcf33086f17a3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:30 +0000

    Add package basic-utils/1.5.1/basic_utils-1.5.1.tar.gz

---
+++ packages/basic-utils/basic_utils-1.5.1.tar.gz/basic_utils-1.5.1/basic_utils/__init__.py
@@ -0,0 +1,14 @@
+"""             __
+               / _) <( ** basic_utils *** )
+        .-^^^-/ /
+    __/       /
+    <__.|_|-|_|
+"""
+
+from .core import *  # noqa
+from .datastructures import *  # noqa
+from .dates import *  # noqa
+from .helpers import *  # noqa
+from .primitives import *  # noqa
+
+__version__ = '1.5.1'
---
+++ packages/basic-utils/basic_utils-1.5.1.tar.gz/basic_utils-1.5.1/setup.py
@@ -0,0 +1,33 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='basic_utils',
+    version='1.5.1',
+    description='A simple set of Python utils',
+    long_description=long_description,
+    url='https://github.com/Jackevansevo/basic-utils',
+    author='Jack Evans',
+    author_email='jack@evans.gb.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='utils utilities',
+    packages=['basic_utils'],
+)
