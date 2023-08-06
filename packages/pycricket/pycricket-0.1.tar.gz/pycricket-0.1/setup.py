d23c06e262a547401b490e7eedbb38bf611b7cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:34 +0000

    Add package pycricket/0.1/pycricket-0.1.tar.gz

---
+++ packages/pycricket/pycricket-0.1.tar.gz/pycricket-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+  name = 'pycricket',
+  packages = ['pycricket'], 
+  version = '0.1',
+  description = 'A library for fetching cricket scorecards of past cricket matches',
+  author = 'Shivam Mitra',
+  author_email = 'shivamm389@gmail.com',
+  license = 'GPLv2',
+  url = 'https://github.com/codophobia/cricket-scorecards-and-commentary-with-python', 
+  download_url = 'https://github.com/codophobia/cricket-scorecards-and-commentary-with-python/tarball/0.1', 
+  keywords = ['cricket', 'scorecards'], 
+  install_requires=[
+          'beautifulsoup4'
+      ],
+  classifiers = [],
+)
+
