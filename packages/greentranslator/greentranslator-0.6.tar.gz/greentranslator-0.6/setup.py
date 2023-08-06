2b35f339799b78bcac9c1798e2d89597d9e36e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:06 +0000

    Add package greentranslator/0.6/greentranslator-0.6.tar.gz

---
+++ packages/greentranslator/greentranslator-0.6.tar.gz/greentranslator-0.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup 
+setup(
+      name = 'greentranslator',
+      packages = [ 'greentranslator' ], # this must be the same as the name above
+      version = '0.6',
+      description = 'Green Team BioMedical Data Translator',
+      author = 'Steve Cox',
+      author_email = 'scox@renci.org',
+      url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator',
+      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.6.tar.gz',
+      keywords = [ 'biomedical', 'environmental', 'exposure', 'clinical' ],
+      classifiers = [ ],
+    )
