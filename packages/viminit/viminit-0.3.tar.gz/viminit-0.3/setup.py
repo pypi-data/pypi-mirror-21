1ab0902c31e717923529c9a825a9474a9ad3e73f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:30 +0000

    Add package viminit/0.3/viminit-0.3.tar.gz

---
+++ packages/viminit/viminit-0.3.tar.gz/viminit-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='viminit',
+      version='0.3',
+      description='Set up a vimlang plugin project directory.',
+      url='http://github.com/vmchale/viminit',
+      author='Vanessa McHale',
+      author_email='tmchale@wisc.edu',
+      license='BSD3',
+      packages=['viminit'],
+      scripts=['bin/viminit'],
+      install_requires=[
+          'gitpython',
+      ],
+      zip_safe=False)
---
+++ packages/viminit/viminit-0.3.tar.gz/viminit-0.3/viminit/__init__.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+
+
+def parser():
+    parser = argparse.ArgumentParser(description='Set up a vimscript project')
+    parser.add_argument('type', metavar='EXTENSION', type=str, default='syntax',
+            help='Type of extension, e.g. suite, plugin, syntax, ftplugin, colorscheme, etc.')
+    parser.add_argument('name', metavar='PROJECT', type=str,
+            help='Project name')
+    parser.add_argument('--license', metavar='LICENSE', type=str,
+            help='License to use, e.g. BSD3, MIT, GPL, etc.', default='BDS3')
+    parser.add_argument('-a', dest='autoload', action="store_true", default=False, 
+            help='Whether to make a dir for autoload')
+    parser.add_argument('--syntastic', dest='autoload', action="store_true", default=False, 
+            help='make files/folders for syntastic')
+    return parser.parse_args()
+
+
+def subdir(base, name):
+    if not os.path.exists(base + "/" + name):
+        os.makedirs(base + "/" + name)
+
+
+# Create documentation if necessary
+def docs(basedir, name):
+    subdir(basedir, "docs")
+    f = open(basedir + "/doc/" + name + ".txt", 'w+')
+
+
+def autload(basedir, name):
+    subdir(basedir, "autoload")
+
+
+# Create plugin dir if necessary. Place plugin files within.
+def plugin(basedir, name):
+    docs(basedir, name)
+    subdir(basedir, "plugin")
+    f = open(basedir + "/plugin/" + name + ".vim", 'w+')
+
+
+def colors(basedir):
+    subdir(basedir, "colors")
+
+
+def syntax(basedir, name):
+    subdir(basedir, "syntax")
+    f = open(basedir + "/syntax/" + name + ".vim", 'w+')
+
+# TODO make this fully working
+def syntastic(base, basedir, name):
+    subdir(basedir, "syntax_checkers")
+
+def ft_plugin(basedir, name):
+    docs(basedir)
+    subdir(basedir, "ftdetect")
+    subdir(basedir, "ftplugin")
+    syntax(basedir, name)
+    f1 = open(basedir + "/ftplugin/" + basedir + ".vim", 'w+')
+    f2 = open(basedir + "/ftdetect/" + basedir + ".vim", 'w+')
