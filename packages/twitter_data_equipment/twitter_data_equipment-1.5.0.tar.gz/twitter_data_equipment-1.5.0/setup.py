a986bf68a5569d7334f9213af9071e8d91ed73fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package twitter_data_equipment/1.5.0/twitter_data_equipment-1.5.0.tar.gz

---
+++ packages/twitter_data_equipment/twitter_data_equipment-1.5.0.tar.gz/twitter_data_equipment-1.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='twitter_data_equipment',
+      version='1.5.0',
+      description='A package that wraps tools for harvesting, hydrating, cleaning and visualize twitter data',
+      url='https://github.com/HolzmanoLagrene/FacharbeitLenzBaumannHS16FS17',
+      author='Lenz Baumann',
+      author_email='lnzbmnn@gmail.com',
+      license='MIT',
+      packages=['twitter_data_equipment'],
+      install_requires=['bs4', 'pandas', 'numpy', 'python-louvain', 'matplotlib', 'networkx', 'twarc'],
+      include_package_data=True,
+      zip_safe=False)
+
