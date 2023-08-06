323fd029affafde21e1719d8705739168390fd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:51 +0000

    Add package HierMat/0.1/HierMat-0.1.tar.gz

---
+++ packages/HierMat/HierMat-0.1.tar.gz/HierMat-0.1/setup.py
@@ -0,0 +1,46 @@
+"""
+HierMat
+=======
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this package.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HierMat.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='HierMat',
+    version='0.1',
+    packages=['HierMat'],
+    url='http://hierarchicalmatrices.readthedocs.io/en/latest/index.html',
+    download_url='https://github.com/maekke97/HierarchicalMatrices',
+    license='GNU GPL v3',
+    author='Markus Neumann',
+    author_email='markus.neumann@math.uzh.ch',
+    requires=['numpy', 'matplotlib'],
+    description='Framework for Hierarchical Matrices',
+    long_description='''This package is the result of my master thesis at the Institute of Mathematics, University of Zurich.
+It provides a framework for the concept of hierarchical matrices and is manly based on the book by W. Hackbusch.'''
+)
