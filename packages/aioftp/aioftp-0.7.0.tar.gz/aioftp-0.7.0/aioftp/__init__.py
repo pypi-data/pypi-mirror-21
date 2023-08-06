55914e8f787d84d183daabd2a24af8e958b6f83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:38 +0000

    Add package aioftp/0.7.0/aioftp-0.7.0.tar.gz

---
+++ packages/aioftp/aioftp-0.7.0.tar.gz/aioftp-0.7.0/aioftp/__init__.py
@@ -0,0 +1,20 @@
+"""ftp client/server for asyncio"""
+# flake8: noqa
+
+from .client import *
+from .server import *
+from .errors import *
+from .common import *
+from .pathio import *
+
+__version__ = "0.7.0"
+version = tuple(map(int, str.split(__version__, ".")))
+
+__all__ = (
+    client.__all__ +
+    server.__all__ +
+    errors.__all__ +
+    common.__all__ +
+    pathio.__all__ +
+    ("version", "__version__")
+)
