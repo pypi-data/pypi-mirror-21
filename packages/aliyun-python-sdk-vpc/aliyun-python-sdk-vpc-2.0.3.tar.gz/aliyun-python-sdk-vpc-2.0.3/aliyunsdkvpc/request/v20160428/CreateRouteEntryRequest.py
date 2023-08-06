393d6ec67764a35cadd3aff44070c9b03b64e4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:58 +0000

    Add package aliyun-python-sdk-vpc/2.0.3/aliyun-python-sdk-vpc-2.0.3.tar.gz

---
+++ packages/aliyun-python-sdk-vpc/aliyun-python-sdk-vpc-2.0.3.tar.gz/aliyun-python-sdk-vpc-2.0.3/aliyunsdkvpc/request/v20160428/CreateRouteEntryRequest.py
@@ -0,0 +1,151 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+
+class CreateRouteEntryRequest(RpcRequest):
+    def __init__(self):
+        RpcRequest.__init__(self, 'Vpc', '2016-04-28', 'CreateRouteEntry', 'vpc')
+
+    def get_OwnerId(self):
+        return self.get_query_params().get('OwnerId')
+
+    def set_OwnerId(self, OwnerId):
+        self.add_query_param('OwnerId', OwnerId)
+
+    def get_ResourceOwnerAccount(self):
+        return self.get_query_params().get('ResourceOwnerAccount')
+
+    def set_ResourceOwnerAccount(self, ResourceOwnerAccount):
+        self.add_query_param('ResourceOwnerAccount', ResourceOwnerAccount)
+
+    def get_ResourceOwnerId(self):
+        return self.get_query_params().get('ResourceOwnerId')
+
+    def set_ResourceOwnerId(self, ResourceOwnerId):
+        self.add_query_param('ResourceOwnerId', ResourceOwnerId)
+
+    def get_RouteTableId(self):
+        return self.get_query_params().get('RouteTableId')
+
+    def set_RouteTableId(self, RouteTableId):
+        self.add_query_param('RouteTableId', RouteTableId)
+
+    def get_DestinationCidrBlock(self):
+        return self.get_query_params().get('DestinationCidrBlock')
+
+    def set_DestinationCidrBlock(self, DestinationCidrBlock):
+        self.add_query_param('DestinationCidrBlock', DestinationCidrBlock)
+
+    def get_NextHopId(self):
+        return self.get_query_params().get('NextHopId')
+
+    def set_NextHopId(self, NextHopId):
+        self.add_query_param('NextHopId', NextHopId)
+
+    def get_ClientToken(self):
+        return self.get_query_params().get('ClientToken')
+
+    def set_ClientToken(self, ClientToken):
+        self.add_query_param('ClientToken', ClientToken)
+
+    def get_NextHopType(self):
+        return self.get_query_params().get('NextHopType')
+
+    def set_NextHopType(self, NextHopType):
+        self.add_query_param('NextHopType', NextHopType)
+
+    def get_OwnerAccount(self):
+        return self.get_query_params().get('OwnerAccount')
+
+    def set_OwnerAccount(self, OwnerAccount):
+        self.add_query_param('OwnerAccount', OwnerAccount)
+
+    def set_NextHopList_1_NextHopType(self, NextHopList_1_NextHopType):
+        self.add_query_param('NextHopList.1.NextHopType', NextHopList_1_NextHopType)
+
+    def get_NextHopList_1_NextHopType(self):
+        self.get_query_params().get('NextHopList.1.NextHopType')
+
+    def set_NextHopList_2_NextHopType(self, NextHopList_2_NextHopType):
+        self.add_query_param('NextHopList.2.NextHopType', NextHopList_2_NextHopType)
+
+    def get_NextHopList_2_NextHopType(self):
+        self.get_query_params().get('NextHopList.2.NextHopType')
+
+    def set_NextHopList_3_NextHopType(self, NextHopList_3_NextHopType):
+        self.add_query_param('NextHopList.3.NextHopType', NextHopList_3_NextHopType)
+
+    def get_NextHopList_3_NextHopType(self):
+        self.get_query_params().get('NextHopList.3.NextHopType')
+
+    def set_NextHopList_4_NextHopType(self, NextHopList_4_NextHopType):
+        self.add_query_param('NextHopList.4.NextHopType', NextHopList_4_NextHopType)
+
+    def get_NextHopList_4_NextHopType(self):
+        self.get_query_params().get('NextHopList.4.NextHopType')
+
+    def set_NextHopList_1_NextHopId(self, NextHopList_1_NextHopId):
+        self.add_query_param('NextHopList.1.NextHopId', NextHopList_1_NextHopId)
+
+    def get_NextHopList_1_NextHopId(self):
+        self.get_query_params().get('NextHopList.1.NextHopId')
+
+    def set_NextHopList_2_NextHopId(self, NextHopList_2_NextHopId):
+        self.add_query_param('NextHopList.2.NextHopId', NextHopList_2_NextHopId)
+
+    def get_NextHopList_2_NextHopId(self):
+        self.get_query_params().get('NextHopList.2.NextHopId')
+
+    def set_NextHopList_3_NextHopId(self, NextHopList_3_NextHopId):
+        self.add_query_param('NextHopList.3.NextHopId', NextHopList_3_NextHopId)
+
+    def get_NextHopList_3_NextHopId(self):
+        self.get_query_params().get('NextHopList.3.NextHopId')
+
+    def set_NextHopList_4_NextHopId(self, NextHopList_4_NextHopId):
+        self.add_query_param('NextHopList.4.NextHopId', NextHopList_4_NextHopId)
+
+    def get_NextHopList_4_NextHopId(self):
+        self.get_query_params().get('NextHopList.4.NextHopId')
+
+    def set_NextHopList_1_Weight(self, NextHopList_1_Weight):
+        self.add_query_param('NextHopList.1.Weight', NextHopList_1_Weight)
+
+    def get_NextHopList_1_Weight(self):
+        self.get_query_params().get('NextHopList.1.Weight')
+
+    def set_NextHopList_2_Weight(self, NextHopList_2_Weight):
+        self.add_query_param('NextHopList.2.Weight', NextHopList_2_Weight)
+
+    def get_NextHopList_2_Weight(self):
+        self.get_query_params().get('NextHopList.2.Weight')
+
+    def set_NextHopList_3_Weight(self, NextHopList_3_Weight):
+        self.add_query_param('NextHopList.3.Weight', NextHopList_3_Weight)
+
+    def get_NextHopList_3_Weight(self):
+        self.get_query_params().get('NextHopList.3.Weight')
+
+    def set_NextHopList_4_Weight(self, NextHopList_4_Weight):
+        self.add_query_param('NextHopList.4.Weight', NextHopList_4_Weight)
+
+    def get_NextHopList_4_Weight(self):
+        self.get_query_params().get('NextHopList.4.Weight')
---
+++ packages/aliyun-python-sdk-vpc/aliyun-python-sdk-vpc-2.0.3.tar.gz/aliyun-python-sdk-vpc-2.0.3/aliyunsdkvpc/request/v20160428/DescribeVirtualBorderRoutersRequest.py
@@ -0,0 +1,175 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class DescribeVirtualBorderRoutersRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Vpc', '2016-04-28', 'DescribeVirtualBorderRouters','vpc')
+
+	def get_OwnerId(self):
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self,OwnerId):
+		self.add_query_param('OwnerId',OwnerId)
+
+	def get_ResourceOwnerAccount(self):
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self,ResourceOwnerAccount):
+		self.add_query_param('ResourceOwnerAccount',ResourceOwnerAccount)
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def  set_Filter_1_Key(self, Filter_1_Key):
+		self.add_query_param('Filter.1.Key', Filter_1_Key)
+
+	def  get_Filter_1_Key(self):
+		self.get_query_params().get('Filter.1.Key')
+
+	def  set_Filter_2_Key(self, Filter_2_Key):
+		self.add_query_param('Filter.2.Key', Filter_2_Key)
+
+	def  get_Filter_2_Key(self):
+		self.get_query_params().get('Filter.2.Key')
+
+	def  set_Filter_3_Key(self, Filter_3_Key):
+		self.add_query_param('Filter.3.Key', Filter_3_Key)
+
+	def  get_Filter_3_Key(self):
+		self.get_query_params().get('Filter.3.Key')
+
+	def  set_Filter_4_Key(self, Filter_4_Key):
+		self.add_query_param('Filter.4.Key', Filter_4_Key)
+
+	def  get_Filter_4_Key(self):
+		self.get_query_params().get('Filter.4.Key')
+
+	def  set_Filter_1_Value_1(self, Filter_1_Value_1):
+		self.add_query_param('Filter.1.Value.1', Filter_1_Value_1)
+
+	def  get_Filter_1_Value_1(self):
+		self.get_query_params().get('Filter.1.Value.1')
+
+	def  set_Filter_1_Value_2(self, Filter_1_Value_2):
+		self.add_query_param('Filter.1.Value.2', Filter_1_Value_2)
+
+	def  get_Filter_1_Value_2(self):
+		self.get_query_params().get('Filter.1.Value.2')
+
+	def  set_Filter_1_Value_3(self, Filter_1_Value_3):
+		self.add_query_param('Filter.1.Value.3', Filter_1_Value_3)
+
+	def  get_Filter_1_Value_3(self):
+		self.get_query_params().get('Filter.1.Value.3')
+
+	def  set_Filter_1_Value_4(self, Filter_1_Value_4):
+		self.add_query_param('Filter.1.Value.4', Filter_1_Value_4)
+
+	def  get_Filter_1_Value_4(self):
+		self.get_query_params().get('Filter.1.Value.4')
+
+	def  set_Filter_2_Value_1(self, Filter_2_Value_1):
+		self.add_query_param('Filter.2.Value.1', Filter_2_Value_1)
+
+	def  get_Filter_2_Value_1(self):
+		self.get_query_params().get('Filter.2.Value.1')
+
+	def  set_Filter_2_Value_2(self, Filter_2_Value_2):
+		self.add_query_param('Filter.2.Value.2', Filter_2_Value_2)
+
+	def  get_Filter_2_Value_2(self):
+		self.get_query_params().get('Filter.2.Value.2')
+
+	def  set_Filter_2_Value_3(self, Filter_2_Value_3):
+		self.add_query_param('Filter.2.Value.3', Filter_2_Value_3)
+
+	def  get_Filter_2_Value_3(self):
+		self.get_query_params().get('Filter.2.Value.3')
+
+	def  set_Filter_2_Value_4(self, Filter_2_Value_4):
+		self.add_query_param('Filter.2.Value.4', Filter_2_Value_4)
+
+	def  get_Filter_2_Value_4(self):
+		self.get_query_params().get('Filter.2.Value.4')
+
+	def  set_Filter_3_Value_1(self, Filter_3_Value_1):
+		self.add_query_param('Filter.3.Value.1', Filter_3_Value_1)
+
+	def  get_Filter_3_Value_1(self):
+		self.get_query_params().get('Filter.3.Value.1')
+
+	def  set_Filter_3_Value_2(self, Filter_3_Value_2):
+		self.add_query_param('Filter.3.Value.2', Filter_3_Value_2)
+
+	def  get_Filter_3_Value_2(self):
+		self.get_query_params().get('Filter.3.Value.2')
+
+	def  set_Filter_3_Value_3(self, Filter_3_Value_3):
+		self.add_query_param('Filter.3.Value.3', Filter_3_Value_3)
+
+	def  get_Filter_3_Value_3(self):
+		self.get_query_params().get('Filter.3.Value.3')
+
+	def  set_Filter_3_Value_4(self, Filter_3_Value_4):
+		self.add_query_param('Filter.3.Value.4', Filter_3_Value_4)
+
+	def  get_Filter_3_Value_4(self):
+		self.get_query_params().get('Filter.3.Value.4')
+
+	def  set_Filter_4_Value_1(self, Filter_4_Value_1):
+		self.add_query_param('Filter.4.Value.1', Filter_4_Value_1)
+
+	def  get_Filter_4_Value_1(self):
+		self.get_query_params().get('Filter.4.Value.1')
+
+	def  set_Filter_4_Value_2(self, Filter_4_Value_2):
+		self.add_query_param('Filter.4.Value.2', Filter_4_Value_2)
+
+	def  get_Filter_4_Value_2(self):
+		self.get_query_params().get('Filter.4.Value.2')
+
+	def  set_Filter_4_Value_3(self, Filter_4_Value_3):
+		self.add_query_param('Filter.4.Value.3', Filter_4_Value_3)
+
+	def  get_Filter_4_Value_3(self):
+		self.get_query_params().get('Filter.4.Value.3')
+
+	def  set_Filter_4_Value_4(self, Filter_4_Value_4):
+		self.add_query_param('Filter.4.Value.4', Filter_4_Value_4)
+
+	def  get_Filter_4_Value_4(self):
+		self.get_query_params().get('Filter.4.Value.4')
+
