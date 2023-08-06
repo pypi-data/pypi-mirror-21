c279cd59cfff377a8e78336a2dd3b24a6804342a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:11 +0000

    Add package fortiosapi/0.5.1/fortiosapi-0.5.1.tar.gz

---
+++ packages/fortiosapi/fortiosapi-0.5.1.tar.gz/fortiosapi-0.5.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='fortiosapi',
+      version='0.5.1',
+      description='Python modules to interact with fortinet products configuration rest and ssh',
+      install_requires=['requests','paramiko'],
+      author='Nicolas Thomas',
+      author_email='nthomas@fortinet.com',
+      url='https://github.com/thomnico/fortiosapi',
+      packages=['fortiosapi'],
+      )
