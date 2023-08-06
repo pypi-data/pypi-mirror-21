422dc492170831261dec4657c002c211c6c01ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:05 +0000

    Add package boot2docker-sdk/0.1/boot2docker-sdk-0.1.0.1.tar.gz

---
+++ packages/boot2docker-sdk/boot2docker-sdk-0.1.0.1.tar.gz/boot2docker-sdk-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+     name='boot2docker-sdk',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     author='morganics',
+     author_email='ian@datagram.co'
+)
