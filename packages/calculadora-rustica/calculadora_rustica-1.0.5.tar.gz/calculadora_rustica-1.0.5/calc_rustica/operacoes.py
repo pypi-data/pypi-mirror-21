7332b3e1994d2f3fb1e58bbce880136eced565ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:31 +0000

    Add package calculadora-rustica/1.0.5/calculadora_rustica-1.0.5.tar.gz

---
+++ packages/calculadora-rustica/calculadora_rustica-1.0.5.tar.gz/calculadora_rustica-1.0.5/calc_rustica/operacoes.py
@@ -0,0 +1,16 @@
+# Esta é uma função para somar dois numeros inteiros
+def soma(x, y):
+    return (x+y)
+
+# Esta é uma função para subtrair dois numeros inteiros
+def subtracao(x, y):
+    return (x-y)
+
+# Esta é uma função para multiplicar dois numeros inteiros
+def multiplicacao(x, y):
+    return (x*y)
+
+# Esta é uma função para dividir dois numeros inteiros
+def divisao(x, y):
+    return (x/y)
+
