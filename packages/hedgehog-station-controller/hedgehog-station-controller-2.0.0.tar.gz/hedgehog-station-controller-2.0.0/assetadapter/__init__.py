88b47a624965150e08ef07e621f3f0b57c27d0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:01 +0000

    Add package hedgehog-station-controller/2.0.0/hedgehog-station-controller-2.0.0.tar.gz

---
+++ packages/hedgehog-station-controller/hedgehog-station-controller-2.0.0.tar.gz/hedgehog-station-controller-2.0.0/assetadapter/__init__.py
@@ -0,0 +1,6 @@
+from assetadapter.devicemanager import DeviceManager
+import logging
+
+logging.info('Start device-manager ...')
+dm = DeviceManager(visa_library='test.yml@sim')
+logging.info('Device-manager started.')
