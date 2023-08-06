b41f9747b92d3b8823f920e61f0422af4e10223d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:36 +0000

    Add package twosampyle/0.1.1/twosampyle-0.1.1.tar.gz

---
+++ packages/twosampyle/twosampyle-0.1.1.tar.gz/twosampyle-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='twosampyle',
+      version='0.1.1',
+      description='Python module for two-sample statistical tests',
+      url='https://github.com/jwilber/twosampyle',
+      author='Jared Wilber',
+      author_email='jdwlbr@gmail.com',
+      license='GNU3',
+      packages=['twosampyle'],
+      install_requires=[
+            'pandas',
+            'numpy',
+            'matplotlib'
+      ],
+      zip_safe=False)
