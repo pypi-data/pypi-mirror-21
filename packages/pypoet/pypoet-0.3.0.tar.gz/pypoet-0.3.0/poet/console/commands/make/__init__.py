adc1a217668307a6aa85bd873f777c51efd8bdd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:46 +0000

    Add package pypoet/0.3.0/pypoet-0.3.0.tar.gz

---
+++ packages/pypoet/pypoet-0.3.0.tar.gz/pypoet-0.3.0/poet/console/commands/make/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .requirements import MakeRequirementsCommand
+from .setup import MakeSetupCommand
---
+++ packages/pypoet/pypoet-0.3.0.tar.gz/pypoet-0.3.0/poet/lock.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from .package import PipDependency, Dependency
+from .poet import Poet
+
+
+class Lock(Poet):
+
+    def is_lock(self):
+        return True
+
+    def load(self):
+        root = self._config['root']
+        self._name = root['name']
+        self._version = root['version']
+
+        packages = self._config['package']
+
+        for package in packages:
+            dep = Dependency(
+                package['name'],
+                package['version'],
+                category=package['category'],
+                optional=package.get('optional')
+            )
+            pip_dep = PipDependency(
+                package['name'],
+                package['version'],
+                category=package['category'],
+                optional=package.get('optional'),
+                checksum=package.get('checksum')
+            )
+
+            if package['category'] == 'dev':
+                self._dev_dependencies.append(dep)
+                self._pip_dev_dependencies.append(pip_dep)
+            else:
+                self._dependencies.append(dep)
+                self._pip_dependencies.append(pip_dep)
+
+        self._features = self._config.get('features', {})
