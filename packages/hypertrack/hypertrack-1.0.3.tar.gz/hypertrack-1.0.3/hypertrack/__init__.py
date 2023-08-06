b9b30caa7a303ec6674ee2e593daf85605f51d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:04 +0000

    Add package hypertrack/1.0.3/hypertrack-1.0.3.tar.gz

---
+++ packages/hypertrack/hypertrack-1.0.3.tar.gz/hypertrack-1.0.3/hypertrack/__init__.py
@@ -0,0 +1,12 @@
+secret_key = None
+base_url = 'https://api.hypertrack.com/api/v1/'
+
+
+from hypertrack.resource import (
+    Action,
+    User,
+    Place,
+    Event
+)
+
+from hypertrack.version import VERSION
