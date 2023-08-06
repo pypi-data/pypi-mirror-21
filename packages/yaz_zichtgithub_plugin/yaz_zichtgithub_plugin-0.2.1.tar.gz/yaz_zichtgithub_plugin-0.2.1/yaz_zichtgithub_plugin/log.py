d5108535ab93ead5bb5d0802b1726d1caf60fa8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:02 +0000

    Add package yaz_zichtgithub_plugin/0.2.1/yaz_zichtgithub_plugin-0.2.1.tar.gz

---
+++ packages/yaz_zichtgithub_plugin/yaz_zichtgithub_plugin-0.2.1.tar.gz/yaz_zichtgithub_plugin-0.2.1/yaz_zichtgithub_plugin/log.py
@@ -0,0 +1,14 @@
+"""Logging configuration."""
+
+import logging
+
+__all__ = ["logger", "set_verbose"]
+
+# Name the logger after the package.
+logger = logging.getLogger(__package__)
+
+
+def set_verbose(verbose: bool):
+    """Set the logging level to INFO when VERBOSE"""
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
