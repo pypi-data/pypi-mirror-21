1f212c2e2e8b76ec0d9744a21416da513444ba31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:16 +0000

    Add package mltk/0.0.1/mltk-0.0.1.tar.gz

---
+++ packages/mltk/mltk-0.0.1.tar.gz/mltk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'mltk',
+  packages = ['mltk'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Machine learning toolkit for python',
+  author = 'Manan Shah',
+  author_email = 'manan.shah.777@gmail.com',
+  url = 'https://github.com/mananshah99/mltk', # use the URL to the github repo
+  keywords = ['ml', 'metrics', 'toolkit'], # arbitrary keywords
+  classifiers = [],
+)
