b5879d50b57c491a1729088a34073dd52885c628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:36 +0000

    Add package tableauserverclient/0.4.1/tableauserverclient-0.4.1.tar.gz

---
+++ packages/tableauserverclient/tableauserverclient-0.4.1.tar.gz/tableauserverclient-0.4.1/setup.py
@@ -0,0 +1,25 @@
+import versioneer
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='tableauserverclient',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Tableau',
+    author_email='github@tableau.com',
+    url='https://github.com/tableau/server-client-python',
+    packages=['tableauserverclient', 'tableauserverclient.models', 'tableauserverclient.server',
+              'tableauserverclient.server.endpoint'],
+    license='MIT',
+    description='A Python module for working with the Tableau Server REST API.',
+    test_suite='test',
+    install_requires=[
+        'requests>=2.11,<2.12.0a0'
+    ],
+    tests_require=[
+        'requests-mock>=1.0,<1.1a0'
+    ]
+)
