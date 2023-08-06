90c66de17156f4aac28a34d90fa71b553906ada8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:58 +0000

    Add package demosys-py/0.2.1/demosys-py-0.2.1.tar.gz

---
+++ packages/demosys-py/demosys-py-0.2.1.tar.gz/demosys-py-0.2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+def reqs_from_requirements_file():
+    reqs = parse_requirements('requirements.txt', session='hack')
+    return [str(r.req) for r in reqs]
+
+
+setup(
+    name="demosys-py",
+    version="0.2.1",
+    description="Modern OpenGL 4.1+ Prototype Framework inspired by Django",
+    long_description=open('README.rst').read(),
+    url="https://github.com/Contraz/demosys-py",
+    author="Einar Forselv",
+    author_email="eforselv@gmail.com",
+    maintainer="Einar Forselv",
+    maintainer_email="eforselv@gmail.com",
+    packages=['demosys'],
+    include_package_data=True,
+    keywords = ['opengl', 'framework', 'demoscene'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: MacOS X',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=reqs_from_requirements_file(),
+    entry_points={'console_scripts': [
+        'demosys-admin = demosys.core.management:execute_from_command_line',
+    ]},
+)
