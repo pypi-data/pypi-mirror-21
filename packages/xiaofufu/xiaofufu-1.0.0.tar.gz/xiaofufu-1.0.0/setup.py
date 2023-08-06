5a271740374f8269ad31e24fd08a503599c3553a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:17 +0000

    Add package xiaofufu/1.0.0/xiaofufu-1.0.0.tar.gz

---
+++ packages/xiaofufu/xiaofufu-1.0.0.tar.gz/xiaofufu-1.0.0/setup.py
@@ -0,0 +1,10 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(name = 'xiaofufu',
+      version = '1.0.0',
+      py_modules = ['xiaofufu'],
+      author = 'xiaofu',
+      author_email = '981430378@qq.com',
+      url = 'http://www.headfirstlabs.com',
+      description = 'A simple printer of nester lists',
+      )
