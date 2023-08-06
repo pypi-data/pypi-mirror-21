82cf2ed72ce52e336f44daa1717e93cad24a75a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package qbx/2017.5.2/qbx-2017.5.2.tar.gz

---
+++ packages/qbx/qbx-2017.5.2.tar.gz/qbx-2017.5.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='qbx',
+      author='Youze Tang',
+      url='https://github.com/qbtrade/qbx',
+      author_email='tangyouze@gmail.com',
+      packages=['qbx'],
+      version='2017.5.2',
+      install_requires=[
+      ],
+      zip_safe=False,
+      )
