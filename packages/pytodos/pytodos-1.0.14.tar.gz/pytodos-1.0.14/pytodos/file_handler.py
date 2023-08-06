56a39ea8ef0eae01eb092e438f27fa1be0b8e0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:43 +0000

    Add package pytodos/1.0.14/pytodos-1.0.14.tar.gz

---
+++ packages/pytodos/pytodos-1.0.14.tar.gz/pytodos-1.0.14/pytodos/file_handler.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+import pickle
+
+directory = os.path.dirname(os.path.abspath(__file__))
+FILE = '{directory}/data.txt'.format(directory=directory)
+
+
+def write_file(todos, file_path=FILE):
+    with open(file_path, 'w') as fp:
+        pickle.dump(todos, fp, protocol=2)
+
+
+def read_file(file_path=FILE):
+    todos = []
+    if not os.path.exists(file_path):
+        return todos
+    with open(file_path, 'r') as fp:
+        todos = pickle.load(fp)
+    return todos
