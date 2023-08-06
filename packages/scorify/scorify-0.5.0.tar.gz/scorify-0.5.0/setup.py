f2344d0cbded185b4656185dbada7d9be706558a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:05 +0000

    Add package scorify/0.5.0/scorify-0.5.0.tar.gz

---
+++ packages/scorify/scorify-0.5.0.tar.gz/scorify-0.5.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+
+
+def get_locals(filename):
+    l = {}
+    with open(filename, 'r') as f:
+        code = compile(f.read(), filename, 'exec')
+        exec(code, {}, l)
+    return l
+
+metadata = get_locals(os.path.join('scorify', '_metadata.py'))
+
+setup(
+    name="scorify",
+    version=metadata['version'],
+    author=metadata['author'],
+    author_email=metadata['author_email'],
+    license=metadata['license'],
+    url=metadata['url'],
+    packages=['scorify'],
+    entry_points={
+        'console_scripts': [
+            'score_data = scorify.scripts.score_data:main'
+        ]}
+    )
