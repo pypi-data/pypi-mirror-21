e781c1edfa743f1acc6864669564c7e4e53c7522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:37 +0000

    Add package py_lets_be_rational/1.0.1/py_lets_be_rational-1.0.1.tar.gz

---
+++ packages/py_lets_be_rational/py_lets_be_rational-1.0.1.tar.gz/py_lets_be_rational-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name='py_lets_be_rational',
+    version='1.0.1',
+    packages=['py_lets_be_rational'],
+    url='http://jaeckel.org',
+    license='MIT',
+    maintainer='vollib',
+    maintainer_email='vollib@gammoncap.com',
+    description='Pure python implementation of Peter Jaeckel\'s LetsBeRational.',
+    install_requires = [
+        'numpy'
+    ]
+)
