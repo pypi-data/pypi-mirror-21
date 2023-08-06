a130b896a1547b15eec54d04d5c5070f6bcc123f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:57 +0000

    Add package winger/0.0.1/winger-0.0.1.tar.gz

---
+++ packages/winger/winger-0.0.1.tar.gz/winger-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+# import winger
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+NAME = 'winger'
+
+
+DESCRIPTION = 'A Python3 module defines dict-like classes to handle Windows registry'
+
+
+URL = 'https://github.com/meng89/' + NAME
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name=NAME,
+      # version=winger.__version__,
+      version='0.0.1',
+      description=DESCRIPTION,
+      include_package_data=True,
+      author='Chen Meng',
+      author_email='ObserverChan@gmail.com',
+      license='LGPL v3.0',
+      url=URL,
+      py_modules=[NAME],
+      install_requires=requirements,
+      classifiers=CLASSIFIERS)
