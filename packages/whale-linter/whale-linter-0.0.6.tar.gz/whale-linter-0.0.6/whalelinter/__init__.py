d302ac2b28fa2589583417275d817ed40bf4cd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:04 +0000

    Add package whale-linter/0.0.6/whale-linter-0.0.6.tar.gz

---
+++ packages/whale-linter/whale-linter-0.0.6.tar.gz/whale-linter-0.0.6/whalelinter/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import argparse
+from pkg_resources import get_distribution, DistributionNotFound
+
+from whalelinter.app       import App
+from whalelinter.parser    import Parser
+from whalelinter.checker   import Checker
+from whalelinter.utils     import Collecter
+
+__all__ = ['run']
+
+
+def run():
+    dist = get_distribution('whale-linter')
+    __version__ = dist.version
+
+    parser = argparse.ArgumentParser(description='A simple non professional Dockerfile linter')
+    parser.add_argument('-i', '--ignore', default=None, help='Rule to ignore', action='append')
+    parser.add_argument('-v', '--version', help='Print version', action='version', version='%(prog)s {}'.format(__version__))
+    parser.add_argument('DOCKERFILE', help="The Dockerfile to lint (can be a file, a GitHub repo. or a direct URL)")
+    args = parser.parse_args()
+
+    print('\n{}\n'.format(args.DOCKERFILE))
+
+    if args.ignore is None:
+        args.ignore = []
+
+    App._collecter = Collecter(App._config.get('rules'), args.ignore)
+
+    parser  = Parser(args.DOCKERFILE)
+    checker = Checker(parser.structure)
+    checker.check()
+
+    App._collecter.display()
+
+if __name__ == '__main__':
+    run()
---
+++ packages/whale-linter/whale-linter-0.0.6.tar.gz/whale-linter-0.0.6/whalelinter/parser.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import shlex
+import operator
+import re
+import urllib.request
+import os
+
+from dockerfile_parse import DockerfileParser
+
+from whalelinter.app import App
+from whalelinter.utils import DockerfileCommand
+
+
+class Parser(object):
+    def __init__(self, filename):
+        self.dockerfileParser = DockerfileParser()
+
+        if self.is_url(filename) is not None:
+            response = urllib.request.urlopen(filename)
+            if self.is_content_type_plain_text(response):
+                self.dockerfileParser.content = response.read().decode('utf-8')
+            else:
+                print('ERROR: file format not supported\n')
+
+        elif os.path.isfile(filename):
+            self.dockerfileParser.content = open(filename, encoding='utf-8').read()
+
+        elif self.is_github_repo(filename):
+            filename = 'https://raw.githubusercontent.com/' + filename + '/master/Dockerfile'
+            self.dockerfileParser.content = urllib.request.urlopen(filename).read().decode('utf-8')
+
+        else:
+            print('ERROR: file format not supported\n')
+
+        self.TOKENS = App._config.get('all')
+        self.structure = self.dict_to_command_object(self.dockerfileParser.structure)
+
+    def is_github_repo(self, filename):
+        regex = re.compile(r'^[-_.0-9a-z]+/[-_.0-9a-z]+$', re.IGNORECASE)
+
+        return True if regex.match(filename) is not None else False
+
+    def is_url(self, filename):
+        regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        return regex.match(filename)
+
+    def is_content_type_plain_text(self, response):
+        content_type = response.getheader('Content-Type')
+        regex        = re.compile(r'text/plain')
+
+        return True if regex.search(content_type) is not None else False
+
+    def dict_to_command_object(self, lst):
+        commands = []
+
+        for element in lst:
+            command = DockerfileCommand()
+            command.instruction = element.get('instruction').upper()
+            command.arguments = element.get('value').split(' ')
+            command.line = element.get('startline') + 1
+            command._raw = element.get('content')
+            commands.append(command)
+
+        return commands
