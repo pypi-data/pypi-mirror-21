ef4c7dde6e6c0b86d6ea84be5b81d3673c29e503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:32 +0000

    Add package leehom_nester/1.3.0/leehom_nester-1.3.0.tar.gz

---
+++ packages/leehom_nester/leehom_nester-1.3.0.tar.gz/leehom_nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+       name         ='leehom_nester',
+       version      ='1.3.0',
+       py_modules   =['nester'],
+       author       ='hfpython',
+       author_email ='hfpyton@headfirstlabs.com',
+       url          ='http://www.headfirstlabs.com',
+       description  ='A simple printer of nested lists',
+      )
