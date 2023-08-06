3df4b32407ae11327519fb3406073485434ba7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:11 +0000

    Add package odoo9-addon-sale-medical-prescription/9.0.2.0.0.99.dev4/odoo9_addon_sale_medical_prescription-9.0.2.0.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo9-addon-sale-medical-prescription/odoo9_addon_sale_medical_prescription-9.0.2.0.0.99.dev4-py2-none-any.whl/odoo_addons/sale_medical_prescription/wizards/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_sale_line_temp
+from . import medical_sale_temp
+from . import medical_sale_wizard
