2740847c5c522eaf3bf3bfaa961eeb49ac03cb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:11 +0000

    Add package kervi-device-library/0.10.64/kervi-device-library-0.10.64.tar.gz

---
+++ packages/kervi-device-library/kervi-device-library-0.10.64.tar.gz/kervi-device-library-0.10.64/kervi_devices/platforms/common/sensors/cpu_temp.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2016, Tim Wentzlau
+# Licensed under MIT
+
+""" Module that defines core cpu sensors """
+
+from kervi.hal import SensorDeviceDriver
+import psutil
+from pyspectator.processor import Processor
+
+class CPUTempSensorDeviceDriver(SensorDeviceDriver):
+    """ Sensor that mesures cpu temp on host """
+    def __init__(self):
+        try:
+            self.cpu = Processor(monitoring_latency=1)
+        except :
+            self.cpu = None
+
+    def read_value(self):
+        try:
+            if self.cpu:
+                print(self.cpu.temperature)
+            return 0
+        except:
+            return 0
+
+    @property
+    def max(self):
+        return 150
+
+    @property
+    def min(self):
+        return 0
+
+    @property
+    def type(self):
+        return "temperature"
+
+    @property
+    def unit(self):
+        return "C"
