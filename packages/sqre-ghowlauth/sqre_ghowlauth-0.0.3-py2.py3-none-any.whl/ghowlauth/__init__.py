eb18f3dcfa6ca1194c028af3b085613c8b6da2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:43 +0000

    Add package sqre-ghowlauth/0.0.3/sqre_ghowlauth-0.0.3-py2.py3-none-any.whl

---
+++ packages/sqre-ghowlauth/sqre_ghowlauth-0.0.3-py2.py3-none-any.whl/ghowlauth/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from .ghowl import GHOWLAuthenticator, GHOWLLoginHandler, \
+    LocalGHOWLAuthenticator
+
+__all__ = ['GHOWLAuthenticator', 'GHOWLLoginHandler',
+           'LocalGHOWLAuthenticator']
