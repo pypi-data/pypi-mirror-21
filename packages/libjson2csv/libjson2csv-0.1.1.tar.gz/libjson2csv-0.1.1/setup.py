6f72dede9565290eedb3046433a0bd02939cff25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:33 +0000

    Add package libjson2csv/0.1.1/libjson2csv-0.1.1.tar.gz

---
+++ packages/libjson2csv/libjson2csv-0.1.1.tar.gz/libjson2csv-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name='libjson2csv',
+    version='0.1.1',
+    description='Converts nested json object to csv and csv back to json',
+    author='Nag Varun Chunduru',
+    author_email='cnv1989@gmail.com',
+    long_description=(
+        open('README.rst').read() + '\n\n' +
+        open('CHANGELOG.rst').read()),
+    url='https://github.com/cnv1989/libjson2csv',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/cnv1989/libjson2csv/archive/v0.1.1.tar.gz',
+    keywords=['json', 'csv', 'converter'],  # arbitrary keywords
+    classifiers=[],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+)
