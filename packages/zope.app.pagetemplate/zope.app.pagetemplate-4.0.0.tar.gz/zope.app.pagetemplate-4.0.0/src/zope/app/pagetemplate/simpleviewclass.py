622f82c99b56fe47097459889f0b6e879411ef84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:48 +0000

    Add package zope.app.pagetemplate/4.0.0/zope.app.pagetemplate-4.0.0.tar.gz

---
+++ packages/zope.app.pagetemplate/zope.app.pagetemplate-4.0.0.tar.gz/zope.app.pagetemplate-4.0.0/src/zope/app/pagetemplate/simpleviewclass.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Simple View Class
+
+"""
+__docformat__ = 'restructuredtext'
+
+# BBB
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
+from zope.browserpage.simpleviewclass import simple
+from zope.browserpage.simpleviewclass import SimpleViewClass
