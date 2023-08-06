ddd6fc12b3d0684cc7737112e3a733bfdea5b6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:25 +0000

    Add package pythymio/1.1/pythymio-1.1.tar.gz

---
+++ packages/pythymio/pythymio-1.1.tar.gz/pythymio-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pythymio',
+  packages = ['pythymio'], # this must be the same as the name above
+  version = '1.1',
+  description = 'A lib to interact with a Thymio II from python',
+  author = 'GhilesA',
+  author_email = 'g4l135@gmail.com',
+  url = 'https://github.com/GhilesA/RPiThymio', # use the URL to the github repo
+  download_url = 'https://github.com/GhilesA/RPiThymio/tarball/0.1', # I'll explain this in a second
+  keywords = ['Thymio', 'pythymio', 'LIDAR', 'Camera'], # arbitrary keywords
+  classifiers = [],
+)
