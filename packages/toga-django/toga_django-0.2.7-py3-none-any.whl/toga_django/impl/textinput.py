82a9af276a2fe70e54cda296866899c7d3825031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:18 +0000

    Add package toga-django/0.2.7/toga_django-0.2.7-py3-none-any.whl

---
+++ packages/toga-django/toga_django-0.2.7-py3-none-any.whl/toga_django/impl/textinput.py
@@ -0,0 +1,33 @@
+
+
+class TextInput:
+    def __init__(self, id, initial, placeholder, readonly, style=None):
+        self.id = id
+
+        self.initial = initial
+        self.placeholder = placeholder
+        self.readonly = readonly
+        self.style = style
+
+    def __html__(self):
+        return '<input id="toga:%s" class="toga TextInput" style="%s" data-toga-class="toga.TextInput" data-toga-parent="%s" data-toga-ports="%s" type="text" value="%s"%s%s>' % (
+            self.id,
+            self.style,
+            self.parent.id,
+            '',  #  self.ports,
+            self.initial,
+            ' placeholder="%s"' % self.placeholder if self.placeholder else '',
+            ' disabled' if self.readonly else '',
+        )
+
+    @property
+    def value(self):
+        return self.impl.value
+
+    @value.setter
+    def value(self, value):
+        print("value", value)
+        self.impl.value = value
+
+    def clear(self):
+        self.impl.value = ''
