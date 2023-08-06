1c17a0870efa8a9f0c912611ccd959a3358f81d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:11 +0000

    Add package pytimer/0.0.10/pytimer-0.0.10.tar.gz

---
+++ packages/pytimer/pytimer-0.0.10.tar.gz/pytimer-0.0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
+setuptools.setup(
+    name='pytimer',
+    version='0.0.10',
+    description='pytimer is an easy to use timer',
+    long_description='context based timer to easily time your code',
+    url='https://veltzer.github.io/pytimer',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='python timing time context manager utility',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+)
