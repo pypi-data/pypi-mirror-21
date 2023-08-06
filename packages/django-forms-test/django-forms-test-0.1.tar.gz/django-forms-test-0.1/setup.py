466ca6fe1dac58a12777f78cdcd128f1968f8d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package django-forms-test/0.1/django-forms-test-0.1.tar.gz

---
+++ packages/django-forms-test/django-forms-test-0.1.tar.gz/django-forms-test-0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    readme = open('README.md').read()
+
+VERSION = "0.1"
+URL = "https://github.com/Thyrst/django-forms-test"
+
+setup(
+    name="django-forms-test",
+    version=VERSION,
+
+    description="Module to simplify testing forms in Django framework",
+    long_description=readme,
+    url=URL,
+    download_url="%s/archive/%s.tar.gz" % (URL, VERSION),
+
+    author="Thyrst",
+    author_email="thyrst@seznam.cz",
+
+    license="MIT",
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    keywords="django forms unittest test",
+    py_modules=["django_forms_test"],
+)
