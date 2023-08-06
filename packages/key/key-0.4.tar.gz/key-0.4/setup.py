3c8f6ad3abaf7114d6d36e80e9ee873a3ac1b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:33 +0000

    Add package key/0.4/key-0.4.tar.gz

---
+++ packages/key/key-0.4.tar.gz/key-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name="key",
+  version="0.4",
+  description="Short getters for python",
+  url="https://github.com/awans/key",
+  author="Andrew Wansley",
+  author_email="andrew.wansley@gmail.com",
+  license="MIT",
+  packages=["k"]
+)
