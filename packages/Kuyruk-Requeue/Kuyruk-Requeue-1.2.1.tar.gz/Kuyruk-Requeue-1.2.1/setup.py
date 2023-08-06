9440c18b0a33c68e06f120423960f343f46cf130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:47 +0000

    Add package Kuyruk-Requeue/1.2.1/Kuyruk-Requeue-1.2.1.tar.gz

---
+++ packages/Kuyruk-Requeue/Kuyruk-Requeue-1.2.1.tar.gz/Kuyruk-Requeue-1.2.1/setup.py
@@ -0,0 +1,30 @@
+# coding=utf8
+from setuptools import setup
+
+setup(
+    name='Kuyruk-Requeue',
+    version="1.2.1",
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    keywords='kuyruk requeue',
+    url='https://github.com/cenkalti/kuyruk-requeue',
+    py_modules=["kuyruk_requeue"],
+    install_requires=[
+        'kuyruk>=8.0.0',
+        'redis>=2.10.3',
+    ],
+    entry_points={'kuyruk.config': 'requeue = kuyruk_requeue:CONFIG',
+                  'kuyruk.commands': 'requeue = kuyruk_requeue:command'},
+    description='Save failed tasks to Redis and requeue them.',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Object Brokering',
+        'Topic :: System :: Distributed Computing',
+    ],
+)
