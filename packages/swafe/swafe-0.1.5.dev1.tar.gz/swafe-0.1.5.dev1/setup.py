b1c065ccac4aa3742157f28f31f6fd686bd07a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:37 +0000

    Add package swafe/0.1.5.dev1/swafe-0.1.5.dev1.tar.gz

---
+++ packages/swafe/swafe-0.1.5.dev1.tar.gz/swafe-0.1.5.dev1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='swafe',
+      version='0.1.5.dev1',
+      description='A python library for orchestrating AWS SWF workflows',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7',
+          'Intended Audience :: Developers',
+      ],
+      keywords='aws-swf SWF',
+      url='https://github.com/ishuah/swafe',
+      author='Ishuah Kariuki',
+      author_email='kariuki@ishuah.com',
+      license='GNU GPLv3',
+      packages=['swafe'],
+      install_requires=[
+          'boto3',
+          'click'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'swafe = swafe.cli:run',
+          ]
+      })
