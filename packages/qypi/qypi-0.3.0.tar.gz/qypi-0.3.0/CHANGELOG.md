76178dfc6489c6a9db83adf10e1c19df3b4e9731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:26 +0000

    Add package qypi/0.3.0/qypi-0.3.0.tar.gz

---
+++ packages/qypi/qypi-0.3.0.tar.gz/qypi-0.3.0/CHANGELOG.md
@@ -0,0 +1,15 @@
+v0.3.0 (2017-04-14)
+-------------------
+- Gave `info`, `readme`, and `files` an `-A`/`--all-versions` option
+- Gave `info` a `--description` option for including (long) descriptions
+
+v0.2.0 (2017-04-03)
+-------------------
+- Gave `info`, `readme`, and `files` `--newest` and `--highest` options for
+  controlling how the latest version of a package is determined; `--highest` is
+  now always the default
+- Added a `--no-pre` option for negating `--pre`
+
+v0.1.0 (2017-04-01)
+-------------------
+Initial release
---
+++ packages/qypi/qypi-0.3.0.tar.gz/qypi-0.3.0/qypi/util.py
@@ -0,0 +1,106 @@
+import json
+import re
+import click
+
+def obj_option(*args, **kwargs):
+    """
+    Like `click.option`, but sets an attribute on ``ctx.obj`` instead of
+    creating a parameter
+    """
+    def callback(ctx, param, value):
+        setattr(ctx.obj, param.name, value)
+    return click.option(*args, callback=callback, expose_value=False, **kwargs)
+
+pre_opt = obj_option(
+    '--pre/--no-pre',
+    default=False,
+    help='Show prerelease versions',
+    show_default=True,
+)
+
+all_opt = obj_option(
+    '-A',
+    '--all-versions/--latest-version',
+    default=False,
+    help='Show all versions/only the latest version when no version is specified [default: latest]',
+)
+
+sort_opt = obj_option(
+    '--newest/--highest',
+    default=False,
+    help='Does "latest" mean "newest" or "highest"? [default: highest]',
+)
+
+def package_args(versioned=True):
+    if versioned:
+        def wrapper(f):
+            return all_opt(sort_opt(pre_opt(click.argument(
+                'packages',
+                nargs=-1,
+                callback=lambda ctx, param, value:
+                            ctx.obj.lookup_package_version(value),
+            )(f))))
+        return wrapper
+    else:
+        return click.argument(
+            'packages',
+            nargs=-1,
+            callback=lambda ctx, param, value: ctx.obj.lookup_package(value),
+        )
+
+def dumps(obj):
+    return json.dumps(obj, sort_keys=True, indent=4, ensure_ascii=False)
+
+def clean_pypi_dict(d):
+    return {
+        k: (None if v in ('', 'UNKNOWN') else v)
+        for k,v in d.items() if not k.startswith(('cheesecake', '_pypi'))
+    }
+
+class JSONLister:
+    def __init__(self):
+        self.first = True
+
+    def __enter__(self):
+        click.echo('[', nl=False)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self.first:
+            click.echo()
+        click.echo(']')
+        return False
+
+    def append(self, obj):
+        if self.first:
+            click.echo()
+            self.first = False
+        else:
+            click.echo(',')
+        click.echo(re.sub(r'^', '    ', dumps(obj), flags=re.M), nl=False)
+
+
+class JSONMapper:
+    def __init__(self):
+        self.first = True
+
+    def __enter__(self):
+        click.echo('{', nl=False)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self.first:
+            click.echo()
+        click.echo('}')
+        return False
+
+    def append(self, key, value):
+        if self.first:
+            click.echo()
+            self.first = False
+        else:
+            click.echo(',')
+        click.echo(
+            re.sub(r'^', '    ', json.dumps(key)+': '+dumps(value), flags=re.M),
+            nl=False,
+        )
