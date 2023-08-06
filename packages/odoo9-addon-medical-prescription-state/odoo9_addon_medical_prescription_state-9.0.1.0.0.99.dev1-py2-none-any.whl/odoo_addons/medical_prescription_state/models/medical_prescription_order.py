baecadfaef0669346ee77ae7f67f613ff9c0181c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:20 +0000

    Add package odoo9-addon-medical-prescription-state/9.0.1.0.0.99.dev1/odoo9_addon_medical_prescription_state-9.0.1.0.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-prescription-state/odoo9_addon_medical_prescription_state-9.0.1.0.0.99.dev1-py2-none-any.whl/odoo_addons/medical_prescription_state/models/medical_prescription_order.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class MedicalPrescriptionOrder(models.Model):
+    """ Add Kanban functionality to MedicalPrescriptionOrder """
+    _inherit = ['medical.prescription.order', 'base.kanban.abstract']
+    _name = 'medical.prescription.order'
