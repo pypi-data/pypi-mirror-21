74d97f40b430c2fb46f7d7b6b648627719923619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:37 +0000

    Add package Sahiti/0.1/Sahiti-0.1.tar.gz

---
+++ packages/Sahiti/Sahiti-0.1.tar.gz/Sahiti-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Sahiti',
+  packages = ['Sahiti'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Harshit',
+  author_email = 'hkk.nitc@gmail.com',
+  url = 'https://github.com/harkrish1/Sahiti', # use the URL to the github repo
+  download_url = 'https://github.com/harkrish1/Sahiti/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
