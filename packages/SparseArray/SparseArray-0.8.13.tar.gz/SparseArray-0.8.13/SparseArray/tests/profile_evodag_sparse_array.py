1dc7a69e762e1d3c373f839bbeaa62e12bfa2a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package SparseArray/0.8.13/SparseArray-0.8.13.tar.gz

---
+++ packages/SparseArray/SparseArray-0.8.13.tar.gz/SparseArray-0.8.13/SparseArray/tests/profile_evodag_sparse_array.py
@@ -0,0 +1,42 @@
+# Copyright 2016 Mario Graff Guerrero
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from EvoDAG.sparse_array import SparseArray
+from nose.tools import assert_almost_equals
+from random import random
+
+
+def random_lst(size=100, p=0.5):
+    lst = []
+    for i in range(size):
+        if random() < p:
+            lst.append(random())
+        else:
+            lst.append(0)
+    return lst
+
+
+def test_sum():
+    for p in [0.5]:
+        a = random_lst(size=100000, p=p)
+        b = random_lst(size=100000, p=p)
+        a[10] = 12.433
+        b[10] = -12.433
+        for i in range(100):
+            c = SparseArray.fromlist(a) + SparseArray.fromlist(b)
+            # c = SparseArray.fromlist(a).sin()
+
+
+test_sum()
