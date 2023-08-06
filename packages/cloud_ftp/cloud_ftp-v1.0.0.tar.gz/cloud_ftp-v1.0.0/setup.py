3f15e1583260ce2f9121452cbd7c75fa2dfb7044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:18 +0000

    Add package cloud_ftp/v1.0.0/cloud_ftp-v1.0.0.tar.gz

---
+++ packages/cloud_ftp/cloud_ftp-v1.0.0.tar.gz/cloud_ftp-v1.0.0/setup.py
@@ -0,0 +1,16 @@
+# Copyright 2017 Real Kinetic, LLC. All Rights Reserved.
+
+from distutils.core import setup
+
+setup(
+    name = 'cloud_ftp',
+    packages = ['cloud_ftp'],
+    version = 'v1.0.0',
+    description = 'Convenience wrapper for downloading FTP files on Google App Engine',
+    author = 'Real Kinetic',
+    author_email = 'dev@realkinetic.com',
+    url = 'https://github.com/RealKinetic/cloud-ftp',
+    download_url = 'https://github.com/RealKinetic/cloud-ftp/archive/v1.0.0.tar.gz',
+    keywords = ['app engine', 'ftp'],
+    classifiers = [],
+)
