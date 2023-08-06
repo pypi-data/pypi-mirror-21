9edc7f5df7c23b123ef8c9daf7fa7aaef4723116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:35 +0000

    Add package lingu_nester/1.2.0/lingu_nester-1.2.0.tar.gz

---
+++ packages/lingu_nester/lingu_nester-1.2.0.tar.gz/lingu_nester-1.2.0/nester.py
@@ -0,0 +1,11 @@
+"""Print every item of list """
+def print_nest_list(items,level=0):
+    """Judgement of item"""
+    for each_item in items:
+        if isinstance(each_item, list):
+            print_nest_list(each_item,level+1)
+        else:
+            for tap_stop in range(level):
+                print('\t',end='')
+            print(each_item)
+
