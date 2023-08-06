2cb37d0ef8e3b966c5052b7c4cef2523ddff71b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:53 +0000

    Add package angr/6.7.4.12/angr-6.7.4.12.tar.gz

---
+++ packages/angr/angr-6.7.4.12.tar.gz/angr-6.7.4.12/angr/__init__.py
@@ -0,0 +1,35 @@
+""" angr module """
+# pylint: disable=wildcard-import
+
+import logging
+logging.getLogger("angr").addHandler(logging.NullHandler())
+
+from .project import *
+from .path import *
+from .errors import *
+from .surveyor import *
+from .service import *
+from .analyses import *
+from .analysis import *
+from .tablespecs import *
+from . import surveyors
+from .blade import Blade
+from .simos import SimOS
+from .path_group import PathGroup
+from .surveyors.caller import Callable
+from .log import Loggers
+from . import knowledge
+from . import exploration_techniques
+
+import sys
+i = 0
+while True:
+    i += 1
+    module = sys._getframe(i).f_globals.get('__name__')
+    if module == '__main__' or module == '__console__':
+        loggers = Loggers()
+        break
+    elif module is not None and module.startswith('nose.'):
+        break
+
+del sys, i, module
