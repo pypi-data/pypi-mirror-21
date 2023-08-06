76cdccb99d9c6f7fc911d1a5a7ef9066284f3133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:27 +0000

    Add package infi.app-repo/1.0.24/infi.app_repo-1.0.24-py2-none-any.whl

---
+++ packages/infi.app-repo/infi.app_repo-1.0.24-py2-none-any.whl/infi/app_repo/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.24"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v1.0.24)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'aafd996809f7c6fffc20912251d14b380770f111'
+__git_head_subject__ = u'Finished Release v1.0.24'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-04-24 11:27:07'
