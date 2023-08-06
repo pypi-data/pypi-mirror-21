2e8172aad85d951562bdbbc5cf0a80e39d7f0187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:39 +0000

    Add package Sphinx/1.6b1/Sphinx-1.6b1-py2.py3-none-any.whl

---
+++ packages/Sphinx/Sphinx-1.6b1-py2.py3-none-any.whl/sphinx/util/websupport.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinx.util.websupport
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2007-2017 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from sphinxcontrib.websupport.utils import is_commentable  # NOQA
