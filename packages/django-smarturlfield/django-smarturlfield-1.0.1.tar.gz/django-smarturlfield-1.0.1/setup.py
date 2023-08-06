d7ab1eb568b0e28318faef37c9855a005364406d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:56 +0000

    Add package django-smarturlfield/1.0.1/django-smarturlfield-1.0.1.tar.gz

---
+++ packages/django-smarturlfield/django-smarturlfield-1.0.1.tar.gz/django-smarturlfield-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-smarturlfield',
+    version='1.0.1',
+    author='Maxim Oransky',
+    author_email='maxim.oransky@gmail.com',
+    description="Django model and form fields that performs URL normalization. For example it adds 'http://' prefix and forces punicode encoding.",
+    long_description=open('README.rst').read(),
+    url='https://github.com/shantilabs/django-smarturlfield',
+    packages=[
+        'smarturlfield'
+    ],
+    keywords=[
+        'django', 'django-fields', 'url'
+    ],
+)
