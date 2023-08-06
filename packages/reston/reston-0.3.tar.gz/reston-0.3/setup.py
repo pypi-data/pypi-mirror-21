1754565b2c2d32d4e766ce60b3677876287459f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:17 +0000

    Add package reston/0.3/reston-0.3.tar.gz

---
+++ packages/reston/reston-0.3.tar.gz/reston-0.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name='reston',
+    description='Reston is a full python tool to play with Android files.',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=['pyasn1', 'cryptography>=1.0', 'future', 'pygments'],
+    setup_requires=['setuptools'],
+
+)
