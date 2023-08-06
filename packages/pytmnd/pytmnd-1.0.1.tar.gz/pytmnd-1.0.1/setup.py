beeea43cef7b95d64afafa6f4be8c242791aeb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:45 +0000

    Add package pytmnd/1.0.1/pytmnd-1.0.1.tar.gz

---
+++ packages/pytmnd/pytmnd-1.0.1.tar.gz/pytmnd-1.0.1/setup.py
@@ -0,0 +1,25 @@
+"""
+PyPI setup file
+"""
+
+from setuptools import setup
+
+
+setup(
+    name = 'pytmnd',
+    packages = ['pytmnd'],
+    version = '1.0.1',
+    author='Matt Cotton',
+    author_email='matthewcotton.cs@gmail.com',
+    url='https://github.com/MattCCS/Pytmnd',
+
+    description='YTMND site-opener',
+    long_description=open("README.md").read(),
+    classifiers=["Programming Language :: Python :: 3"],
+
+    entry_points={
+        'console_scripts': [
+            'pytmnd=pytmnd.main:main',
+        ],
+    },
+)
