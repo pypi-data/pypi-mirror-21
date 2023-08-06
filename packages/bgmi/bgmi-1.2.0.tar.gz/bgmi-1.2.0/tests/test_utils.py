de9fc8ea531a815652a10f06115cd329091e6d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:36 +0000

    Add package bgmi/1.2.0/bgmi-1.2.0.tar.gz

---
+++ packages/bgmi/bgmi-1.2.0.tar.gz/bgmi-1.2.0/tests/test_utils.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import unittest
+
+
+class UtilsTest(unittest.TestCase):
+    def test_test_connection(self):
+        # Just for now..
+        # self.assertEqual(False, test_connection())
+        pass
