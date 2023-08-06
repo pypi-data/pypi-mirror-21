9efeeb6cd6bc9864f6049662fa51c1e2a921071b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:13 +0000

    Add package annict/0.6.1/annict-0.6.1-py3-none-any.whl

---
+++ packages/annict/annict-0.6.1-py3-none-any.whl/annict/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+python-annict
+~~~~~~~~~~~~~~~~~~~~~
+
+Annict API for Python.
+
+"""
+
+__title__ = 'python-annict'
+__version__ = '0.6.1'
+__author__ = 'Hiro Ashiya'
+__license__ = 'MIT'
+
+from .api import API  # noqa
