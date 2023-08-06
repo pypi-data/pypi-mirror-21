72fb6885a6ca1ef5cb198e4ae25232e5e7baed21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:31 +0000

    Add package cardinal-pythonlib/0.2.8/cardinal_pythonlib-0.2.8.tar.gz

---
+++ packages/cardinal-pythonlib/cardinal_pythonlib-0.2.8.tar.gz/cardinal_pythonlib-0.2.8/cardinal_pythonlib/version.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# cardinal_pythonlib/version.py
+# Copyright (c) Rudolf Cardinal (rudolf@pobox.com).
+# See LICENSE for details.
+
+
+VERSION = '0.2.8'
+# Use semantic versioning: http://semver.org/
+
+RECENT_VERSION_HISTORY = """
+
+- 0.2.7, 2017-04-28
+  Fixed bug in rnc_extract_text that was using get_file_contents() as a
+  converter when it wasn't accepting generic **kwargs; now it is.
+  
+- 0.2.8, 2017-04-28
+  Fixed DOCX table processing bug, in docx_process_table().
+
+"""
