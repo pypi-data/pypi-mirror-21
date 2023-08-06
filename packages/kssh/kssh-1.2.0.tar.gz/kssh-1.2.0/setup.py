312cd1db31f2fad229cc3bd4571a3e22f41f61cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:56 +0000

    Add package kssh/1.2.0/kssh-1.2.0.tar.gz

---
+++ packages/kssh/kssh-1.2.0.tar.gz/kssh-1.2.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kssh',
+    version='1.2.0',
+    description='kSSH is a simple utility for managing SSH hosts and tracking aliases in an SSH config file.',
+    long_description=long_description,
+    url='https://github.com/kris-nova/kssh',
+    author='Kris Nova',
+    author_email='kris@nivenly.com',
+    license='MIT',
+    packages=['kssh', ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: MacOS X',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='SSH, ssh, manager, management, wrapper, config, configuration, tool, utility, cli, command line, command, line',
+    entry_points={
+        'console_scripts': ['kssh=kssh.kssh:main', ],
+    },
+)
