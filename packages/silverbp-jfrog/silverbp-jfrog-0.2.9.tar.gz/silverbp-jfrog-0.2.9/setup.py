48eac7b63b43c04d65e5f59e4c62855da995a520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:23 +0000

    Add package silverbp-jfrog/0.2.9/silverbp-jfrog-0.2.9.tar.gz

---
+++ packages/silverbp-jfrog/silverbp-jfrog-0.2.9.tar.gz/silverbp-jfrog-0.2.9/setup.py
@@ -0,0 +1,15 @@
+"""
+Setup.py
+"""
+
+from setuptools import setup
+
+setup(name='silverbp-jfrog',
+      version='0.2.9',
+      description='Python API for interacting with JFrog Apis',
+      url='https://github.com/silverbp/pypi-jfrog',
+      author='Silver Blueprints LLC',
+      author_email='admin@silverblueprints.com',
+      license='MIT',
+      packages=['silverbp_jfrog'],
+      zip_safe=False)
