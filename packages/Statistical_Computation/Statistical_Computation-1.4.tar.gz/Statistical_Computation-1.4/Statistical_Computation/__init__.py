f6abdcda6a9a65eaa561e148a732204f6ef2fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package Statistical_Computation/1.4/Statistical_Computation-1.4.tar.gz

---
+++ packages/Statistical_Computation/Statistical_Computation-1.4.tar.gz/Statistical_Computation-1.4/Statistical_Computation/__init__.py
@@ -0,0 +1,1 @@
+from Statistical_Computation import *
---
+++ packages/Statistical_Computation/Statistical_Computation-1.4.tar.gz/Statistical_Computation-1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Statistical_Computation',
+    version='1.4',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Implementation of scalable k-means++',
+    install_requires=['numpy'],
+    url='https://github.com/susancherry/Statistical_Computation.git',
+    author='Susan Cherry',
+    author_email='susan.cherry@duke.edu'
+)
