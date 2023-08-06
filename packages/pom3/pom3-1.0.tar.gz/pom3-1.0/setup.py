b517005088c903bc0f6ec3bc69b4dd35d5424b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package pom3/1.0/pom3-1.0.tar.gz

---
+++ packages/pom3/pom3-1.0.tar.gz/pom3-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pom3',
+  packages = ['pom3'],
+  version = '1.0',
+  description = 'Simulation of agile requirements engineering.',
+  author = 'Joseph Krall',
+  author_email = 'kralljoe@gmail.com',
+  url = 'https://github.com/tanzairatier/pom3', 
+  download_url = 'https://github.com/tanzairatier/pom3/archive/1.0.tar.gz',
+  keywords = ['agile', 'requirements', 'engineering', 'simulation', 'jmoo'],
+  classifiers = [],
+)
