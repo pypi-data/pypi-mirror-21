4ee8787e6a80ebb159a3217948c834305dee975e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:05 +0000

    Add package emoji-list/0.50/emoji-list-0.50.tar.gz

---
+++ packages/emoji-list/emoji-list-0.50.tar.gz/emoji-list-0.50/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='emoji-list',
+      version='0.50',
+      description="Raw lists of emoji by type, stripped so you don't have to",
+      url='http://github.com/vincentmwong/emoji-list',
+      author='Vincent Wong',
+      author_email='vmwong1@gmail.com',
+      license='MIT',
+      packages=['emoji-list'],
+      zip_safe=False)
