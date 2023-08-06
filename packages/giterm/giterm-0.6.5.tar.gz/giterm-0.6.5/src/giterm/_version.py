a02b696c6cac4e4e08c47bc3dbd399a7efe0c83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:19 +0000

    Add package giterm/0.6.5/giterm-0.6.5.tar.gz

---
+++ packages/giterm/giterm-0.6.5.tar.gz/giterm-0.6.5/src/giterm/_version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.6.5'
+
+__version_text__ = '''giterm version {0} -
+    Copyright © 2015-2017 Tim Legrand -
+    License BSD 2-Clause License -
+    This is free software; see the source for copying conditions. There is NO
+    warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+    '''.format(__version__)
---
+++ packages/giterm/giterm-0.6.5.tar.gz/giterm-0.6.5/src/giterm/watch.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import sys
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from giterm.observer import Trigger
+
+
+# Prevent tracking '.goutputstream-*' files (known Ubuntu bug)
+GIT_BLACK_LIST = ['.goutputstream']
+GIT_WHITE_LIST = ['.gitignore', '.gitconfig', '.gitmodules']
+
+
+class FileChangedHandler(FileSystemEventHandler, Trigger):
+    def __init__(self, timeout_in_seconds=None, *args, **kwargs):
+        self.timeout = timeout_in_seconds if timeout_in_seconds else 1
+        self.last_call = time.time()
+        super(FileChangedHandler, self).__init__()
+        print('timeout: ' + str(self.timeout))
+
+    def on_any_event(self, event):
+        if time.time() - self.last_call < self.timeout:
+            return
+        path = event.src_path
+        if path.startswith('./'):
+            path = path[2:]
+        if path == '.git':
+            # Not interested by the '.git' folder itself, only its contents
+            return
+        for forbidden in GIT_BLACK_LIST:
+            if path.startswith(forbidden):
+                if path not in GIT_WHITE_LIST:
+                    return
+                else:
+                    break
+        message = event.event_type + ': ' + path
+        message = message[0].upper() + message[1:]
+        self.last_call = time.time()
+        self.action(message)
+
+    def action(self, msg):
+        if self.callbacks:
+            self.fire(content=msg)
+        else:
+            print(msg)
+
+
+class Watcher(object):
+    def __init__(self, path='.'):
+        super(Watcher, self).__init__()
+        self.path = path
+        self.event_handler = FileChangedHandler()
+        self.observer = Observer(timeout=0.4)
+        self.observer.schedule(self.event_handler, self.path, recursive=True)
+
+    def start(self):
+        self.observer.start()
+
+    def stop(self):
+        self.observer.stop()
+        self.observer.join()
+
+
+if __name__ == '__main__':
+    watchdir = sys.argv[1] if len(sys.argv) > 1 else '.'
+    w = Watcher()
+    w.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        w.stop()
