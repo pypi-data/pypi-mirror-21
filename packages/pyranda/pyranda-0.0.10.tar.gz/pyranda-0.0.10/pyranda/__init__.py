8236dbdd9fcebc9647474b057fc9c5a3bbc05bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:41 +0000

    Add package pyranda/0.0.10/pyranda-0.0.10.tar.gz

---
+++ packages/pyranda/pyranda-0.0.10.tar.gz/pyranda-0.0.10/pyranda/__init__.py
@@ -0,0 +1,6 @@
+from .okres import Okres
+from .okress import Okress
+from .kraj import Kraj
+from .krajs import Krajs, getRandomKraj, list_kraj
+from .krajs import Krajs, getRandomKraj, list_kraj
+from .osoba import Osoba
