ce253b80efb67615460a6619feaf64b936e07c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:37 +0000

    Add package ara/0.12.5/ara-0.12.5.tar.gz

---
+++ packages/ara/ara-0.12.5.tar.gz/ara-0.12.5/ara/views/__init__.py
@@ -0,0 +1,9 @@
+from .debug import debug
+from .file import file
+from .home import home
+from .host import host
+from .playbook import playbook
+from .reports import reports
+from .result import result
+
+# flake8: noqa
