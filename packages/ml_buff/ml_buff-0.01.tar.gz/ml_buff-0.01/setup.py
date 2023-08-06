fd26975eb38ec9a5849ce64b2430516a8d7a08df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:30 +0000

    Add package ml_buff/0.01/ml_buff-0.01.tar.gz

---
+++ packages/ml_buff/ml_buff-0.01.tar.gz/ml_buff-0.01/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='ml_buff',
+      version='0.01',
+      py_modules=['ml_buff.models', 'ml_buff.helpers'],
+      author='Bruno Tinen',
+      author_email='bruno.tinen@usp.br',
+      license='MIT',
+      )
