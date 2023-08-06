df31324dfacbffe8fd3d6ee4f43959dfb96b640f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:39 +0000

    Add package calculadora-rustica/1.0.0/calculadora_rustica-1.0.0-py2-none-any.whl

---
+++ packages/calculadora-rustica/calculadora_rustica-1.0.0-py2-none-any.whl/calc_rustica/operacoes.py
@@ -0,0 +1,16 @@
+
+# Esta é uma função para somar dois numeros inteiros
+def soma():
+    x1 = int(input('Primeiro valor: '))
+    x2 = int(input('Primeiro valor: '))
+    print('O resultado de ', x1, '+', x2, ' é: ', (x1+x2))
+
+# Esta é uma função para subtrair dois numeros inteiros
+def subtracao():
+    x1 = int(input('Primeiro valor: '))
+    x2 = int(input('Primeiro valor: '))
+    print('O resultado de ', x1, '-', x2, ' é: ', (x1-x2))
+
+
+def main():
+    soma()
