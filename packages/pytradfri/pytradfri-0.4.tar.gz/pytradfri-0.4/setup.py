1b9b92dbc0ce5570a117343fa2348ecd318d3ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:17 +0000

    Add package pytradfri/0.4/pytradfri-0.4.tar.gz

---
+++ packages/pytradfri/pytradfri-0.4.tar.gz/pytradfri-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+  name = 'pytradfri',
+  packages = ['pytradfri'],
+  version = '0.4',
+  description = 'A python library for communicating with the Tradfri Gateway',
+  author = 'balloob, ggravlingen',
+  author_email = 'no@email.com',
+  url = 'https://github.com/ggravlingen/pytradfri',
+  download_url = 'https://github.com/ggravlingen/pytradfri/archive/v0.4.zip'
+)
