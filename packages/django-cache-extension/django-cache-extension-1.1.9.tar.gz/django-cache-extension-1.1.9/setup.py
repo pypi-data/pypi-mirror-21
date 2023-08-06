7b73ebf2753992831b035daee0f41c7b9fc976ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:21 +0000

    Add package django-cache-extension/1.1.9/django-cache-extension-1.1.9.tar.gz

---
+++ packages/django-cache-extension/django-cache-extension-1.1.9.tar.gz/django-cache-extension-1.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='django-cache-extension',
+    version='1.1.9',
+    description='cache extension for django',
+    url='https://github.com/Beeblio/django-cache-extension',
+    author='Shanbay python dev group',
+    author_email='developer@shanbay.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=['cache_extension'],
+    package_data={'cache_extension': ['backends/*.py']},
+    install_requires=['django-redis', 'redis', 'hiredis'],
+)
