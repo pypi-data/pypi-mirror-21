88682589adb8bcaf8b0782e94e93e78883829cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package omkar-periodic-table/0.1/omkar_periodic_table-0.1.tar.gz

---
+++ packages/omkar-periodic-table/omkar_periodic_table-0.1.tar.gz/omkar_periodic_table-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(name='omkar_periodic_table',
+      version='0.1',
+      description='chemistry symbols and name',
+      url='',
+      author='omkar',
+      author_email='xyz@test.com',
+      license='MIT',
+      packages=['periodic_table'],
+      zip_safe=False)
