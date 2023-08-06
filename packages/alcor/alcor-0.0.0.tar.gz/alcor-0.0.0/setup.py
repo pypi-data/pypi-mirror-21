a0f9b7013a7fac18cc677d4931ce3c5b71b320d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:43 +0000

    Add package alcor/0.0.0/alcor-0.0.0.tar.gz

---
+++ packages/alcor/alcor-0.0.0.tar.gz/alcor-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='alcor',
+      version='0.0.0',
+      description='Library for astronomical researches.',
+      author='Georgy Skorobogatov, '
+             'Azat Ibrakov',
+      author_email='skorobogatov@phystech.edu, '
+                   'azatibrakov@gmail.com',
+      url='https://github.com/lycantropos/alcor',
+      download_url='https://github.com/lycantropos/alcor/archive/master.tar.gz',
+      keywords=['astronomy'])
