826bc5ea1bafa426cbadb23fb0270e50bb52c5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:53 +0000

    Add package cryptography-with-deps/0.0.1/cryptography-with-deps-0.0.1.tar.gz

---
+++ packages/cryptography-with-deps/cryptography-with-deps-0.0.1.tar.gz/cryptography-with-deps-0.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        # TODO(snormore): Assuming Ubuntu for now...
+        subprocess.check_call('sudo apt-get install build-essential libssl-dev libffi-dev'.split(), shell=True)
+        install.run(self)
+
+
+setup(
+    name='cryptography-with-deps',
+    version='0.0.1',
+    description='Cryptography package that also installs system dependencies.',
+    author='Steven Normore',
+    author_email='steven@dataup.io',
+    url='https://dataup.io/',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=[
+        'cryptography'
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
