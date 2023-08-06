c01e989b3f6f6a569c0fbae242c27634f34ee46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:50 +0000

    Add package publish-aws-lambda/0.4.1/publish-aws-lambda-0.4.1.tar.gz

---
+++ packages/publish-aws-lambda/publish-aws-lambda-0.4.1.tar.gz/publish-aws-lambda-0.4.1/publish_aws_lambda/__main__.py
@@ -0,0 +1,35 @@
+import logging
+import optparse
+import sys
+from publish_aws_lambda import plan, print_plan, publish
+
+logging.basicConfig(level=logging.INFO, format="%(message)s", datefmt="%Y-%m-%d %H:%M:%S", stream=sys.stdout)
+
+# Check the command line...
+parser = optparse.OptionParser(description="Publish auth Lambdas into AWS (code will upload to a S3 bucket)")
+
+parser.add_option("--dry-run", dest="dry_run", action="store_true", help="Plan but don't execute")
+parser.add_option("--dir", dest="dir", default=".", help="Root dir of project to upload")
+parser.add_option("--module", dest="modules", action="append", help="Python modules to upload as AWS lambda functions")
+parser.add_option("--bucket", dest="bucket", help="S3 bucket to upload code into")
+parser.add_option("--force-upload", dest="force", action="store_true", default=False, help="Force upload to S3")
+
+options, args = parser.parse_args()
+
+assert options.modules, "At least one python module has to be provided"
+assert options.bucket, "S3 bucket has to be provided"
+
+if options.dry_run:
+    p = plan(root_dir=options.dir,
+             modules=options.modules,
+             force=options.force)
+
+    print_plan(options.modules, *p)
+
+else:
+    publish(root_dir=options.dir,
+            modules=options.modules,
+            bucket=options.bucket,
+            force=options.force)
+
+    print("Done!")
