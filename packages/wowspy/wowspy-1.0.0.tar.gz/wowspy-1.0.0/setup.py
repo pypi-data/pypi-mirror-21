e7d1c966d8b663b97240338ae459771b440fe3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:34 +0000

    Add package wowspy/1.0.0/wowspy-1.0.0.tar.gz

---
+++ packages/wowspy/wowspy-1.0.0.tar.gz/wowspy-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='wowspy',
+    version='1.0.0',
+    packages=['wowspy'],
+    url='https://github.com/MaT1g3R/Warships.py',
+    license='MIT',
+    author='MaT1g3R',
+    author_email='982341777@qq.com',
+    description='A Python World of Warships API wrapper '
+)
