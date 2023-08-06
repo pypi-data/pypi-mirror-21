e7d3f5321f26ade52d9fa5dd5a9bef90e11cf15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:50 +0000

    Add package aquests/0.7.3.2/aquests-0.7.3.2.tar.gz

---
+++ packages/aquests/aquests-0.7.3.2.tar.gz/aquests-0.7.3.2/aquests/lib/rjapi.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from urllib.parse import quote
+
+def tostr (p):
+	if p:
+		return "?" + "&".join (["%s=%s" % (k, quote (str (v))) for k, v in p.items ()])	
+	return ""
+			
+class API:
+	API_SERVER = "http://127.0.0.1:5000"
+	REQ_TIMEOUT = 30.0
+	
+	def __init__ (self, server, timeout = 30.0):
+		self.API_SERVER = server
+		self.REQ_TIMEOUT = timeout
+		self.session = requests.Session ()
+			
+	def post (self, uri, d):
+		return self.session.post ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
+	
+	def put (self, uri, d):
+		return self.session.put ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
+	
+	def get (self, uri, p = {}):	
+		return self.session.get ("%s/%s%s" % (self.API_SERVER, uri, tostr (p)), timeout = self.REQ_TIMEOUT).json ()
+	
+	def delete (self, uri, p = {}):
+		return self.session.delete ("%s/%s%s" % (self.API_SERVER, uri, tostr (p)), timeout = self.REQ_TIMEOUT).json ()
+
