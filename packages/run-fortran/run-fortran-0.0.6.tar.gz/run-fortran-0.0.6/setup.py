24bc80a8815032231b8bd3340c9505bb3e3de83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:39 +0000

    Add package run-fortran/0.0.6/run-fortran-0.0.6.tar.gz

---
+++ packages/run-fortran/run-fortran-0.0.6.tar.gz/run-fortran-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='run-fortran',
+      version='0.0.6',
+      scripts=['run-fortran.py'],
+      description='Script for generating list of files paths '
+                  'in hierarchical order '
+                  'for correct "Fortran"-files compilation.',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url='https://github.com/lycantropos/run-fortran',
+      download_url='https://github.com/lycantropos/run-fortran/archive/'
+                   'master.tar.gz',
+      keywords=['fortran'],
+      install_requires=[
+          'click>=6.7',  # parsing command-line arguments
+      ])
