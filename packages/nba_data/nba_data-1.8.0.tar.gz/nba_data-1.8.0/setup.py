922745786ddffd6a0e0e35c69b01f3a3823bfdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:32 +0000

    Add package nba_data/1.8.0/nba_data-1.8.0.tar.gz

---
+++ packages/nba_data/nba_data-1.8.0.tar.gz/nba_data-1.8.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'nba_data',
+  packages = find_packages(exclude=['tests*']),
+  install_requires=['requests', 'enum34'],
+  version = '1.8.0',
+  description = 'A nba stats client',
+  author = 'Jae Bradley',
+  author_email = 'jae.b.bradley@gmail.com',
+  url = 'https://github.com/jaebradley/nba_data', # use the URL to the github repo
+  download_url = 'https://github.com/jaebradley/nba_data/tarball/1.1', # I'll explain this in a second
+  keywords = ['nba', 'nba_data'], # arbitrary keywords
+  classifiers = [],
+)
