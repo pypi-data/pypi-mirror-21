75b33f7881d37f5cf01eaf9cf0a44487de02165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package mosfit/0.5.9/mosfit-0.5.9.tar.gz

---
+++ packages/mosfit/mosfit-0.5.9.tar.gz/mosfit-0.5.9/mosfit/modules/outputs/lightcurve.py
@@ -0,0 +1,37 @@
+"""Definitions for the `LightCurve` class."""
+from collections import OrderedDict
+
+from mosfit.modules.outputs.output import Output
+
+
+# Important: Only define one ``Module`` class per file.
+
+
+class LightCurve(Output):
+    """Output a light curve to disk."""
+
+    _lc_keys = [
+        'magnitudes', 'e_magnitudes', 'model_observations',
+        'all_telescopes', 'all_bands', 'all_systems', 'all_instruments',
+        'all_bandsets', 'all_modes',
+        'all_times', 'all_frequencies', 'observed', 'all_band_indices',
+        'observation_types'
+    ]
+
+    def __init__(self, **kwargs):
+        """Initialize module."""
+        super(LightCurve, self).__init__(**kwargs)
+        self._dense_keys = self._lc_keys
+        self._n_times = kwargs.get('ntimes', 0)
+
+    def process(self, **kwargs):
+        """Process module."""
+        output = OrderedDict()
+        for key in sorted(kwargs.keys()):
+            if key in self._dense_keys:
+                continue
+            output[key] = kwargs[key]
+        for key in self._dense_keys:
+            output[key.replace('all_', '')] = kwargs[key]
+
+        return output
