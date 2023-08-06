990e994058f3d3c89ad40fcc62b993e332965853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:29 +0000

    Add package pydplace/0.1/pydplace-0.1.tar.gz

---
+++ packages/pydplace/pydplace-0.1.tar.gz/pydplace-0.1/README.md
@@ -0,0 +1,15 @@
+# dplace-data
+
+Data for [D-PLACE](https://d-place.org).
+
+Research that uses data from D-PLACE should cite both the original source(s) of
+the data and the paper by Kirby et al. in which D-PLACE was first presented
+(e.g., research using cultural data from the Binford Hunter-Gatherer dataset:
+"Binford (2001); Binford and Johnson (2006); Kirby et al. 2016)." The reference
+list should include the date data were accessed and URL for [D-PLACE](https://d-place.org),
+in addition to the full references for Binford (2001), Binford and Johnson (2006), 
+and Kirby et al. 2016.
+
+## Versions:
+
+* [Version 1.0](https://github.com/D-PLACE/dplace-data/releases/tag/v1.0) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.439199.svg)](https://doi.org/10.5281/zenodo.439199)
---
+++ packages/pydplace/pydplace-0.1.tar.gz/pydplace-0.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pydplace',
+    version='0.1',
+    license='CC-BY-4.0',
+    description='programmatic access to D-PLACE/dplace-data',
+    long_description='',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://d-place.shh.mpg.de',
+    keywords='data',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'clldutils>=1.9',
+        'attrs',
+        'fiona',
+        'shapely',
+        'pyglottolog>=0.3.2',
+        'python-nexus>=1.4.2',
+        'ete3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dplace=pydplace.cli:main',
+        ]
+    },
+    tests_require=[],
+    test_suite="pydplace"
+)
