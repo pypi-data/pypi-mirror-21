1584f8b15e84d2d4cb824efd3a434981255b4c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:34 +0000

    Add package we-get/1.0.0/we-get-1.0.0.tar.gz

---
+++ packages/we-get/we-get-1.0.0.tar.gz/we-get-1.0.0/setup.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) 2016 we-get developers (https://github.com/levisabah/we-get/)
+See the file 'LICENSE' for copying permission
+"""
+
+from setuptools import setup, find_packages
+version = '1.0.0'
+
+setup(
+    name='we-get',
+    version=version,
+    description='Search torrents from the command-line',
+    author='Levi Sabah',
+    author_email='x@levisabah.com',
+    license='MIT',
+    keywords=['command line', 'torrent', 'torrents', 'magnet'],
+    url='https://github.com/levisabah/we-get',
+    packages=find_packages(),
+    install_requires=['docopt', 'prompt_toolkit', 'colorama'],
+    include_package_data=True,
+    package_data={'we_get': ['txt/useragents.txt']},
+    entry_points={'console_scripts': ['we-get=we_get:main']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    
+    ],
+)
