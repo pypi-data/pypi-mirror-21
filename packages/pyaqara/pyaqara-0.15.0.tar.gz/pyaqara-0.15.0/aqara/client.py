bb7cf2b38b19ad94ce7ec5bcd3967af01de3a1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:56 +0000

    Add package pyaqara/0.15.0/pyaqara-0.15.0.tar.gz

---
+++ packages/pyaqara/pyaqara-0.15.0.tar.gz/pyaqara-0.15.0/aqara/client.py
@@ -0,0 +1,182 @@
+"""
+Aqara Client
+
+A client implementation to receive Aqara events and send control messages.
+
+Feature includes:
+- Discover gateways
+- Discover devices of a gateway
+- Subscribe to updates for devices
+- Read values from a device (async)
+- Send control command to a device (async)
+- Heartbeat
+
+"""
+import asyncio
+import json
+import logging
+
+from pydispatch import dispatcher
+from aqara.protocol import AqaraProtocol
+from aqara.gateway import AqaraGateway
+from aqara.const import (
+    LISTEN_IP, LISTEN_PORT,
+    AQARA_ENCRYPT_DUMMY_PASSWORD,
+    AQARA_EVENT_NEW_GATEWAY
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+def _extract_data(msg):
+    return json.loads(msg["data"])
+
+class AqaraClient(AqaraProtocol):
+    """Aqara Client implementation."""
+    def __init__(self, gw_secrets=None):
+        super().__init__()
+        self.transport = None
+        self._gw_secrets = {} if gw_secrets is None else gw_secrets
+        self._gateways = {}
+        self._device_to_gw = {}
+
+    @property
+    def gateways(self):
+        """property: gateways"""
+        return self._gateways
+
+    @asyncio.coroutine
+    def start(self, loop):
+        """Start listening on gateway events"""
+        listen = loop.create_datagram_endpoint(lambda: self, local_addr=(LISTEN_IP, LISTEN_PORT))
+        transport, _protocol = yield from listen
+        self.transport = transport
+        self.discover_gateways()
+        _LOGGER.info("started")
+
+    def stop(self):
+        """Stop listening to gateway events"""
+        if self.transport is None:
+            _LOGGER.info("not started")
+        else:
+            self.transport.close()
+            _LOGGER.info("stopped")
+
+    def discover_gateways(self):
+        """Ask all gateways to respond identity."""
+        _LOGGER.info('discovering gateways...')
+        discovery_msg = {"cmd": "whois"}
+        self.broadcast(discovery_msg)
+
+    def discover_devices(self, gw_addr):
+        """Ask a gateway to reply with the SID of all attached devices."""
+        discover_devices_msg = {"cmd": "get_id_list"}
+        self.unicast(gw_addr, discover_devices_msg)
+
+    def read_device(self, gw_addr, sid):
+        """Send a request to read device 'sid' on gateway 'gw_addr'"""
+        read_msg = {"cmd": "read", "sid": sid}
+        self.unicast(gw_addr, read_msg)
+
+    def write_device(self, gw_addr, model, sid, data, meta=None):
+        """Send a request to write 'data' to device 'sid' on gateway 'gw_addr'"""
+        write_msg = {
+            "cmd": "write",
+            "model": model,
+            "sid": sid,
+            "data": json.dumps(data)
+        }
+
+        if meta != None:
+            write_msg.update(meta)
+
+        self.unicast(gw_addr, write_msg)
+
+    def handle_message(self, msg, src_addr):
+        """Override: handle_message implementation"""
+        _LOGGER.debug("handle_message from %s", src_addr)
+
+        cmd = msg["cmd"]
+        sid = msg["sid"]
+
+        if cmd == "iam":
+            addr = msg["ip"]
+            self.on_gateway_discovered(sid, addr)
+        elif cmd == "get_id_list_ack":
+            sids = _extract_data(msg)
+            self.on_devices_discovered(sid, sids)
+        elif cmd == "read_ack":
+            model = msg["model"]
+            data = _extract_data(msg)
+            self.on_read_ack(model, sid, data)
+        elif cmd == "write_ack":
+            model = msg["model"]
+            data = _extract_data(msg)
+            self.on_write_ack(model, sid, data)
+        elif cmd == "report":
+            model = msg["model"]
+            data = _extract_data(msg)
+            self.on_report(model, sid, data)
+        elif cmd == "heartbeat":
+            model = msg["model"]
+            data = _extract_data(msg)
+            gw_token = None if "token" not in msg else msg["token"]
+            self.on_heartbeat(model, sid, data, gw_token)
+
+    def on_gateway_discovered(self, gw_sid, gw_addr):
+        """Called when a gateway is discovered"""
+        _LOGGER.info("discovered gateway at %s [%s]", gw_sid, gw_addr)
+        gw_secret = AQARA_ENCRYPT_DUMMY_PASSWORD \
+            if gw_sid not in self._gw_secrets \
+            else self._gw_secrets[gw_sid]
+        new_gateway = AqaraGateway(self, gw_sid, gw_addr, gw_secret)
+        self._gateways[gw_sid] = new_gateway
+        self._device_to_gw[gw_sid] = new_gateway
+        dispatcher.send(signal=AQARA_EVENT_NEW_GATEWAY, gateway=new_gateway, sender=self)
+
+    def on_devices_discovered(self, gw_sid, sids):
+        """Called when list of devices of gateway is returned."""
+        if gw_sid not in self._gateways:
+            _LOGGER.error("on_devices_discovered(): gateway %s not found", gw_sid)
+            return
+        gateway = self._gateways[gw_sid]
+        for sid in sids:
+            _LOGGER.info("found device %s on gateway %s", sid, gw_sid)
+            self._device_to_gw[sid] = gateway
+        gateway.on_devices_discovered(sids)
+
+    def on_read_ack(self, model, sid, data):
+        """Called when a gateway send back ACK for a read request."""
+        if sid not in self._device_to_gw:
+            _LOGGER.error("on_read_ack(): sid not found %s", sid)
+            return
+
+        self._device_to_gw[sid].on_read_ack(model, sid, data)
+
+    def on_write_ack(self, model, sid, data):
+        """Called when a gateway send back ACK for a write request."""
+        if sid not in self._device_to_gw:
+            _LOGGER.error("on_write_ack(): sid not found %s", sid)
+            return
+        self._device_to_gw[sid].on_write_ack(model, sid, data)
+
+    def on_report(self, model, sid, data):
+        """Called when a device sent a status report."""
+        if sid not in self._device_to_gw:
+            _LOGGER.warning("on_report(): sid not found %s", sid)
+            return
+        self._device_to_gw[sid].on_device_report(model, sid, data)
+
+    def on_heartbeat(self, model, sid, data, gw_token):
+        """Called when a heartbeat is received."""
+        if sid not in self._device_to_gw:
+            _LOGGER.warning("on_heartbeat(): sid not found %s", sid)
+            return
+        self._device_to_gw[sid].on_device_heartbeat(model, sid, data, gw_token)
+
+    def subscribe(self, handle_new_gateway):
+        """Subscribe to gateway events."""
+        dispatcher.connect(handle_new_gateway, signal=AQARA_EVENT_NEW_GATEWAY, sender=self)
+
+    def unsubscribe(self, handle_new_gateway):
+        """Unsubscribe from gateway events."""
+        dispatcher.disconnect(handle_new_gateway, signal=AQARA_EVENT_NEW_GATEWAY, sender=self)
---
+++ packages/pyaqara/pyaqara-0.15.0.tar.gz/pyaqara-0.15.0/aqara/gateway.py
@@ -0,0 +1,188 @@
+"""
+Aqara Gateway
+
+Gateway abstraction
+
+Features:
+- Call discovery
+- Persist list of sensors
+- Control gateway lights
+
+"""
+
+import json
+import binascii
+
+from Crypto.Cipher import AES
+from pydispatch import dispatcher
+from aqara.device import (create_device, AqaraBaseDevice)
+from aqara.const import (
+    AQARA_ENCRYPT_IV,
+    AQARA_DEVICE_GATEWAY,
+    AQARA_MID_STOP,
+    AQARA_EVENT_NEW_DEVICE
+)
+
+class AqaraGateway(AqaraBaseDevice):
+    """Aqara Gateway implementation."""
+    def __init__(self, client, sid, addr, secret):
+        super().__init__(AQARA_DEVICE_GATEWAY, self, sid)
+        self._client = client
+        self._addr = addr
+        self._cipher = AES.new(secret, AES.MODE_CBC, IV=AQARA_ENCRYPT_IV)
+        self._token = None
+        self._properties = {
+            "rgbw": 0,
+            "illumination": 0,
+            "proto_version": None,
+            "voltage": 0
+        }
+        self._devices = {}
+        self._devices[sid] = self
+
+    @property
+    def devices(self):
+        """property: devices"""
+        return self._devices
+
+    @property
+    def addr(self):
+        """property: addr"""
+        return self._addr
+
+    @property
+    def rgbw(self):
+        """property: rgbw"""
+        return self._properties["rgbw"]
+
+    @property
+    def illumination(self):
+        """property: illumination"""
+        return self._properties["illumination"]
+
+    @property
+    def proto_version(self):
+        """property: proto_version"""
+        return self._properties["proto_version"]
+
+    def connect(self):
+        """Start the gateway"""
+        self.discover_devices()
+        self.read_device(self._sid)
+
+    def discover_devices(self):
+        """discover devices attached to this gateway"""
+        self.log_info('discovering devices...')
+        self._client.discover_devices(self._addr)
+
+    def read_device(self, sid):
+        """force read the value of a device attached to this gateway"""
+        self._client.read_device(self._addr, sid)
+
+    def write_device(self, device, data, meta):
+        """write data to device"""
+        data["key"] = self._make_key()
+        self._client.write_device(self._addr, device.model, device.sid, data, meta)
+
+    def set_light(self, rgbw):
+        """Set gateway light (rgbw)"""
+        self._properties["rgbw"] = rgbw
+        data = {
+            "rgb": rgbw,
+        }
+        meta = {
+            "short_id": 0,
+            "key": 8
+        }
+        self.write_device(self, data, meta)
+
+    def play_ringtone(self, mid):
+        """Play ringtone id 'mid'"""
+        self._set_mid(mid)
+
+    def stop_ringtone(self):
+        """Stop playing ringtone"""
+        self._set_mid(AQARA_MID_STOP)
+
+    def on_devices_discovered(self, sids):
+        """Callback when devices are discovered"""
+        for sid in sids:
+            self._client.read_device(self._addr, sid)
+
+    def on_read_ack(self, model, sid, data):
+        """Callback on read_ack"""
+        self.log_debug("on_read_ack: [{}] {}: {}".format(model, sid, json.dumps(data)))
+        if model == "gateway" and sid == self.sid:
+            # handle read_ack for gateway itself
+            self.on_update(data)
+        else:
+            # handle read_ack for devices attached to this gateway
+            if sid not in self._devices:
+                new_device = create_device(self, model, sid)
+                self.log_info("added new device {} [{}]".format(sid, model))
+                self._devices[sid] = new_device
+                dispatcher.send(signal=AQARA_EVENT_NEW_DEVICE, device=new_device, sender=self)
+            self._try_update_device(model, sid, data)
+
+    def on_write_ack(self, model, sid, data):
+        """Callback on write_ack"""
+        self.log_debug("on_write_ack: {} [{}]: {}".format(sid, model, json.dumps(data)))
+
+    def on_device_report(self, model, sid, data):
+        """Callback on report"""
+        self.log_debug("on_report: {} [{}]: {}".format(sid, model, json.dumps(data)))
+        self._try_update_device(model, sid, data)
+
+    def on_device_heartbeat(self, model, sid, data, gw_token):
+        """Callback on heartbeat"""
+        self.log_debug("on_heartbeat: {} [{}]: {}".format(sid, model, json.dumps(data)))
+        if sid == self._sid:
+            # handle as gateway heartbeat
+            self._token = gw_token
+        else:
+            # handle as device heartbeat
+            self._try_update_device(model, sid, data)
+
+    def do_update(self, data):
+        if "rgb" in data:
+            self._properties["rgb"] = data["rgb"]
+        if "illumination" in data:
+            self._properties["illumination"] = data["illumination"]
+        if "proto_version" in data:
+            self._properties["proto_version"] = data["proto_version"]
+
+    def subscribe(self, handle_new_device):
+        """Subscribe to new device event."""
+        dispatcher.connect(handle_new_device, signal=AQARA_EVENT_NEW_DEVICE, sender=self)
+
+    def unsubscribe(self, handle_new_device):
+        """Unsubscribe from new device event."""
+        dispatcher.disconnect(handle_new_device, signal=AQARA_EVENT_NEW_DEVICE, sender=self)
+
+    def _try_update_device(self, model, sid, data):
+        """Update device data"""
+        if sid not in self._devices:
+            self.log_warning('unregistered device: {} [{}]'.format(model, sid))
+            return
+        self._devices[sid].on_update(data)
+
+
+    def _try_heartbeat_device(self, model, sid, data):
+        """Send heartbeat to device"""
+        if sid not in self._devices:
+            self.log_warning('unregistered device: {} [{}]'.format(model, sid))
+            return
+        self._devices[sid].on_heartbeat(data)
+
+    def _make_key(self):
+        return binascii.hexlify(self._cipher.encrypt(self._token)).decode("utf-8")
+
+    def _set_mid(self, mid):
+        data = {
+            "mid": mid,
+        }
+        meta = {
+            "short_id": 0,
+            "key": 8
+        }
+        self.write_device(self, data, meta)
