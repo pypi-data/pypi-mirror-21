b73764614db0949f7b6e199bc2fdc29eddea360a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:40 +0000

    Add package elasticmanager/0.2.6/elasticmanager-0.2.6.tar.gz

---
+++ packages/elasticmanager/elasticmanager-0.2.6.tar.gz/elasticmanager-0.2.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'elasticmanager',
+  packages = ['elasticmanager','elasticmanager.management','elasticmanager.management.commands'],
+  version = '0.2.6',
+  description = 'Django model manager like class for elasticsearch integration',
+  author = 'Adam Hopkins',
+  author_email = 'adam@optymizer.com',
+  url = 'https://github.com/ahopkins/elasticmanager',
+  download_url = 'https://github.com/ahopkins/elasticmanager/archive/master.zip',
+  keywords = ['django', 'elasticsearch',],
+  install_requires=[
+      'elasticsearch_dsl',
+  ],
+  classifiers = [],
+)
