db9cbc1cf4dc4c21a142ed5c1dc046768128a289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:27 +0000

    Add package pybot-adapter-slack/0.0.1/pybot-adapter-slack-0.0.1.tar.gz

---
+++ packages/pybot-adapter-slack/pybot-adapter-slack-0.0.1.tar.gz/pybot-adapter-slack-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pybot-adapter-slack',
+    version = '0.0.1',
+    description = "Slack adapter for pybot",
+    url = 'https://github.com/jcomo/pybot-adapter-slack',
+    author = 'Jonathan Como',
+    author_email = 'jonathan.como@gmail.com',
+    packages = find_packages(exclude=['docs', 'tests', 'scripts']),
+    install_requires = ['slackclient'],
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = 'python chat bot framework automation slack'
+)
