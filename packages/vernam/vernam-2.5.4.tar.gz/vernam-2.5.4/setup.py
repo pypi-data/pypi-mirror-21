9670e34eb249aafe7e456226dac34fdd9a945bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:32 +0000

    Add package vernam/2.5.4/vernam-2.5.4.tar.gz

---
+++ packages/vernam/vernam-2.5.4.tar.gz/vernam-2.5.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open( path.join(path.abspath(path.dirname(__file__)) , 'README.md' )) as f:
+    long_description = f.read()
+
+setup(
+    name = 'vernam',
+    version = '2.5.4',
+    packages = ["vernam"],
+    scripts=['vernam/__main__.py'],
+    entry_points = {
+        'console_scripts': ['vernam = vernam.__main__:main']
+    },
+
+    description = 'Vernam cipher',
+    long_description = long_description,
+
+    url = 'https://github.com/roysoup/Vernam',
+
+    author = 'Roy Siu',
+    author_email = '',
+
+    license = 'MIT Licence',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable'
+    ],
+    
+    install_requires = ['typing'],
+)
---
+++ packages/vernam/vernam-2.5.4.tar.gz/vernam-2.5.4/vernam/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from vernam import vernam, usage
+import sys
+
+def main():
+    try:
+        print( vernam( sys.argv[1], sys.argv[2] ) )
+    except (TypeError, IndexError):
+        usage()
+
+if __name__ =="__main__":
+    main()
