fbce89c5e734df4fa001837fa5c142af68d7cddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:50 +0000

    Add package falsy/2017.4.4.dev91/falsy-2017.4.4.dev91-py3-none-any.whl

---
+++ packages/falsy/falsy-2017.4.4.dev91-py3-none-any.whl/falsy/netboy/netboy.py
@@ -0,0 +1,38 @@
+import typing
+
+from falsy.netboy.fetch import net_boy
+from falsy.netboy.run import run
+
+
+class NetBoy:
+    class Exception(Exception):
+        pass
+
+    class Dict(typing.Dict[str, typing.Any]):
+        def __getattr__(self, name):
+            # type: (str) -> Any
+            try:
+                return self[name]
+            except KeyError:
+                raise NetBoy.Exception('netboy key error: ' + name)
+            except Exception:
+                raise NetBoy.Exception('netboy exception: ' + name)
+
+        def __setattr__(self, name, value):
+            # type: (str, Any) -> None
+            self[name] = value
+
+    def __init__(self, payload=None):
+        self.payload = payload
+
+    def run(self, payload=None):
+        if self.payload is None:
+            ress = run(net_boy(payload))
+        elif payload is None:
+            ress = run(net_boy(self.payload))
+        else:
+            ress = run(net_boy(self.payload + payload))
+        obj_ress = []
+        for v in ress:
+            obj_ress.append(NetBoy.Dict(v))
+        return obj_ress
