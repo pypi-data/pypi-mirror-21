33bbbaf9f4214873b8382b601e503d44fb7be860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:38 +0000

    Add package django-template-preview/0.1.5/django-template-preview-0.1.5.tar.gz

---
+++ packages/django-template-preview/django-template-preview-0.1.5.tar.gz/django-template-preview-0.1.5/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    README = f.read()
+
+
+setup(
+    name='django-template-preview',
+    version='0.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple Django app to preview rendered templates in your browser.',
+    long_description=README,
+    url='https://github.com/crolfe/django-template-preview/',
+    author='Colin Rolfe',
+    license='BSD License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
