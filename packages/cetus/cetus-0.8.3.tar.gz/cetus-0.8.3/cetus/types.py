874604134f119667b688735348dbd03b7253b4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:31 +0000

    Add package cetus/0.8.3/cetus-0.8.3.tar.gz

---
+++ packages/cetus/cetus-0.8.3.tar.gz/cetus-0.8.3/cetus/types.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+from datetime import datetime
+from typing import (Any,
+                    Union,
+                    MutableMapping,
+                    KT, VT,
+                    Generator,
+                    Tuple,
+                    List)
+
+from aiomysql.pool import Pool as MySQLConnectionPool
+from aiomysql.sa.connection import SAConnection as MySQLConnection
+from asyncpg.connection import Connection as PostgresConnection
+from asyncpg.pool import Pool as PostgresConnectionPool
+from asyncpg.transaction import Transaction as PostgresTransaction
+
+
+class OrderedDictType(OrderedDict, MutableMapping[KT, VT]):
+    pass
+
+
+StringGenerator = Generator[str, None, None]
+ColumnValueType = Union[int, bool,
+                        float, str,
+                        datetime, None]
+RecordType = Tuple[ColumnValueType, ...]
+
+FilterType = Tuple[str,
+                   Union[
+                       ColumnValueType,  # single value
+                       List[ColumnValueType],  # list of values
+                       Tuple[ColumnValueType,
+                             ColumnValueType]  # values range
+                   ]]
+FiltersType = Tuple[str, Any]
+OrderingType = Tuple[str, str]
+UpdatesType = OrderedDictType[str, ColumnValueType]
+
+MySQLConnectionType = MySQLConnection
+PostgresConnectionType = PostgresConnection
+ConnectionType = Union[MySQLConnectionType,
+                       PostgresConnectionType]
+TransactionType = Union[PostgresTransaction]
+ConnectionPoolType = Union[MySQLConnectionPool,
+                           PostgresConnectionPool]
