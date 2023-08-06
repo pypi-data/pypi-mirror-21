529e6680479d0bbe44e96e6d387b7313c543d811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package vtools/0.0.21/vtools-0.0.21.tar.gz

---
+++ packages/vtools/vtools-0.0.21.tar.gz/vtools-0.0.21/vtools/__init__.py
@@ -0,0 +1,5 @@
+from .vtools import memoized, Memorize, flatten, sanitize_id, Node, Tree
+
+__all__ = ( 'memoized', 'Memorize', 'flatten', 'sanitize_id', 'Node', 'Tree' )
+
+from vtools.vtools import memoized, Memorize, flatten, sanitize_id, Node, Tree
