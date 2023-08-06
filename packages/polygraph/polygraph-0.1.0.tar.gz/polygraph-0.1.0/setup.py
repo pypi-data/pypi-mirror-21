cdebc4fe1a973edec2c6309d59e7c9b4e620b779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:47 +0000

    Add package polygraph/0.1.0/polygraph-0.1.0.tar.gz

---
+++ packages/polygraph/polygraph-0.1.0.tar.gz/polygraph-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='polygraph',
+    version='0.1.0',
+    description='Python library for defining GraphQL schemas',
+    url='https://github.com/yen223/polygraph/',
+    author='Wei Yen, Lee',
+    author_email='hello@weiyen.net',
+    license='MIT',
+    install_requires=[
+        'graphql-core>=1.0.1',
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'ipython',
+            'autopep8',
+            'isort',
+            'twine==1.8.1',
+        ]
+    }
+)
