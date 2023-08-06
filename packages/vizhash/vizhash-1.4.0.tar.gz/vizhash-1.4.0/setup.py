50c69464e0d82a86d0b3623d7a7b9f7572c1594a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:06 +0000

    Add package vizhash/1.4.0/vizhash-1.4.0.tar.gz

---
+++ packages/vizhash/vizhash-1.4.0.tar.gz/vizhash-1.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+__version__ = '1.4.0'
+
+setup(
+    name='vizhash',
+    packages=['vizhash'],
+    version=__version__,
+    description=('Python Visual Hash,'
+                 'generate a visual random image associated with a string.'),
+    author='luxcem',
+    author_email='a@luxcem.fr',
+    url='https://github.com/luxcem/vizhash',
+    download_url='https://github.com/luxcem/vizhash/tarball/{}'.format(
+        __version__),
+    keywords='visual hash vizhash random maze identicon',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    install_requires=['Pillow'],
+    entry_points={
+        'console_scripts': [
+            'vizhash = vizhash.vizhash:entrypoint',
+        ],
+    },
+    tests_require=['Pillow', 'pytest', 'pytest-cov'])
