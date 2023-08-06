4e457bd88109327250b5b5b48daaf35292b9ed6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:59 +0000

    Add package apolloMusicPlayer/0.1/apolloMusicPlayer-0.1.tar.gz

---
+++ packages/apolloMusicPlayer/apolloMusicPlayer-0.1.tar.gz/apolloMusicPlayer-0.1/setup.py
@@ -0,0 +1,15 @@
+#from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+setup(
+  name = 'apolloMusicPlayer',
+  packages = ['apolloMusicPlayer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'The music player for Apollo',
+  author = 'Erik Beitel',
+  author_email = 'erik.beitel@gmail.com',
+  url = 'https://github.com/ebber/apolloMusicPlayer', # use the URL to the github repo
+  download_url = 'https://github.com/ebber/apolloMusicPlayer/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['audio', 'music player'], # arbitrary keywords
+  classifiers = [],
+)
