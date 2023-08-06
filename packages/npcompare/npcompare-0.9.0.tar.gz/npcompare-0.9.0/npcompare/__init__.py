22f13e47b78a4ea66da4f66ade9542bbdd7df677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:03 +0000

    Add package npcompare/0.9.0/npcompare-0.9.0.tar.gz

---
+++ packages/npcompare/npcompare-0.9.0.tar.gz/npcompare-0.9.0/npcompare/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from npcompare.compare import Compare
+from npcompare.fourierseries import fourierseries
+from npcompare.estimate import EstimateBFS
+
+__version__ = "0.9.0"
+
+__all__ = ["Compare", "EstimateBFS", "fourierseries"]
