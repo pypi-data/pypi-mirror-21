fdc6988f0dce790f08d6aa7b6e6462d0352e6d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:09 +0000

    Add package c2go/0.2/c2go-0.2.tar.gz

---
+++ packages/c2go/c2go-0.2.tar.gz/c2go-0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+import versioneer
+
+from setuptools import setup, find_packages
+from contextlib import suppress
+from setuptools.dist import Distribution
+
+with suppress(Exception):
+    Distribution({'setup_requires': ['setuptools-markdown']})
+
+setup(
+    name='c2go',
+    version=versioneer.get_version(),
+    description='c to go conversion',
+    author='Mars Galactic',
+    author_email='xoviat@users.noreply.github.com',
+    url='https://github.com/xoviat/c2go',
+    packages=find_packages(),
+    license=open(
+        os.path.join(os.path.dirname(__file__), 'LICENSE')).readline().strip(),
+    platforms='any',
+    classifiers=[],
+    install_requires=[],
+    cmdclass=versioneer.get_cmdclass(),
+    long_description_markdown_filename='README.md')
