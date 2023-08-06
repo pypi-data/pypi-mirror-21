2cdf65d616108f93490d92f26271fbe7389f5254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:56 +0000

    Add package embed/0.1.0/embed-0.1.0.tar.gz

---
+++ packages/embed/embed-0.1.0.tar.gz/embed-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name = 'embed',
+	version = '0.1.0',
+	description = 'embed',
+	author = 'Szabolcs Dombi',
+	author_email = 'cprogrammer1994@gmail.com',
+	license = 'MIT',
+)
