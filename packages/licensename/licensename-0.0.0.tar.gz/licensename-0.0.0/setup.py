2ca039948fbabc2185c4da8f39e6441cbff980dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:35 +0000

    Add package licensename/0.0.0/licensename-0.0.0.tar.gz

---
+++ packages/licensename/licensename-0.0.0.tar.gz/licensename-0.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for licensename.
+
+    This file was generated with PyScaffold 2.5.7, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+"""
+
+import sys
+from setuptools import setup, find_packages
+
+
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=sphinx)
+
+
+if __name__ == "__main__":
+    setup_package()
