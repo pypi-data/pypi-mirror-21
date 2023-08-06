6180adacd2f1e39b1c4be54ce06ddb8939f6bd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:56 +0000

    Add package paasta-tools/0.59.1/paasta-tools-0.59.1.tar.gz

---
+++ packages/paasta-tools/paasta-tools-0.59.1.tar.gz/paasta-tools-0.59.1/paasta_tools/native_mesos_scheduler.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# Without this, the import of mesos.interface breaks because paasta_tools.mesos exists
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import argparse
+import sys
+import time
+from time import sleep
+
+from paasta_tools.frameworks.native_scheduler import create_driver
+from paasta_tools.frameworks.native_scheduler import get_paasta_native_jobs_for_cluster
+from paasta_tools.frameworks.native_scheduler import NativeScheduler
+from paasta_tools.utils import compose_job_id
+from paasta_tools.utils import DEFAULT_SOA_DIR
+from paasta_tools.utils import load_system_paasta_config
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Runs native paasta mesos scheduler.')
+    parser.add_argument('-d', '--soa-dir', dest="soa_dir", metavar="SOA_DIR", default=DEFAULT_SOA_DIR)
+    parser.add_argument('--stay-alive-seconds', dest="stay_alive_seconds", type=int, default=300)
+    parser.add_argument('--periodic-interval', dest="periodic_interval", type=int, default=30)
+    parser.add_argument('--staging-timeout', dest="staging_timeout", type=float, default=60)
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
+
+    system_paasta_config = load_system_paasta_config()
+    cluster = system_paasta_config.get_cluster()
+
+    drivers = []
+    schedulers = []
+    for service, instance in get_paasta_native_jobs_for_cluster(cluster=cluster, soa_dir=args.soa_dir):
+        scheduler = NativeScheduler(
+            service_name=service,
+            instance_name=instance,
+            cluster=cluster,
+            staging_timeout=args.staging_timeout,
+            system_paasta_config=system_paasta_config,
+            soa_dir=args.soa_dir,
+        )
+        schedulers.append(scheduler)
+
+        driver = create_driver(
+            framework_name="paasta %s" % compose_job_id(service, instance),
+            scheduler=scheduler,
+            system_paasta_config=system_paasta_config
+        )
+        driver.start()
+        drivers.append(driver)
+
+    end_time = time.time() + args.stay_alive_seconds
+    while time.time() < end_time:
+        sleep(args.periodic_interval)
+        for scheduler, driver in zip(schedulers, drivers):
+            scheduler.periodic(driver)
+
+    return schedulers
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
