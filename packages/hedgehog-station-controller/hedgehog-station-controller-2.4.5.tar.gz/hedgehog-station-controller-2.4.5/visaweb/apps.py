74ca44fd93bffa6b89ce121322aa5ec759aa5edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:21 +0000

    Add package hedgehog-station-controller/2.4.5/hedgehog-station-controller-2.4.5.tar.gz

---
+++ packages/hedgehog-station-controller/hedgehog-station-controller-2.4.5.tar.gz/hedgehog-station-controller-2.4.5/visaweb/apps.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+import assetadapter
+
+
+class VisawebConfig(AppConfig):
+    name = 'visaweb'
+    verbose_name = "Visaweb Application"
+
+    def ready(self):
+        from visaweb.models import Asset
+        import visaweb
+
+        asset_list = Asset.objects.all()
+        mapping = {}
+        for a in asset_list:
+            mapping[a.alias] = a.address
+        visaweb.client.alias(mapping)
+        
