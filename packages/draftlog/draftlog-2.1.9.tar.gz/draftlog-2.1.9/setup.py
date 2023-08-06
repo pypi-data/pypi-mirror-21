a4f26818bf239a85c81e852f56727f06cf73f971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:15 +0000

    Add package draftlog/2.1.9/draftlog-2.1.9.tar.gz

---
+++ packages/draftlog/draftlog-2.1.9.tar.gz/draftlog-2.1.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name =         'draftlog',
+    version =      '2.1.9',
+    description =  'Create updatable log lines into the terminal.',
+    url =          'https://github.com/kepoorhampond/python-draftlog',
+    author =       'Kepoor Hampond',
+    author_email = 'kepoorh@gmail.com',
+    license =      'MIT',
+    packages =     ['draftlog'],
+    install_requires= [
+        'colorama'  # This package will ensure ANSI support for windows cmd
+    ],
+)
