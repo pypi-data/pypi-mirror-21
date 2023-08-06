a923d5e8cfb8a2e1bbebc6aac85296c7871d0c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:59 +0000

    Add package gigalixir/0.5.0/gigalixir-0.5.0.tar.gz

---
+++ packages/gigalixir/gigalixir-0.5.0.tar.gz/gigalixir-0.5.0/gigalixir/ssh_key.py
@@ -0,0 +1,38 @@
+import requests
+from . import auth
+import json
+import click
+
+def get(host):
+    r = requests.get('%s/api/ssh_keys' % host, headers = {
+        'Content-Type': 'application/json',
+    })
+    if r.status_code != 200:
+        if r.status_code == 401:
+            raise auth.AuthException()
+        raise Exception(r.text)
+    else:
+        data = json.loads(r.text)["data"]
+        click.echo(json.dumps(data, indent=2, sort_keys=True))
+
+def create(host, key):
+    r = requests.post('%s/api/ssh_keys' % host, headers = {
+        'Content-Type': 'application/json',
+    }, json = {
+        "ssh_key": key,
+    })
+    if r.status_code != 201:
+        if r.status_code == 401:
+            raise auth.AuthException()
+        raise Exception(r.text)
+
+def delete(host, key_id):
+    r = requests.delete('%s/api/ssh_keys' % (host), headers = {
+        'Content-Type': 'application/json',
+    }, json = {
+        "id": key_id
+    })
+    if r.status_code != 200:
+        if r.status_code == 401:
+            raise auth.AuthException()
+        raise Exception(r.text)
