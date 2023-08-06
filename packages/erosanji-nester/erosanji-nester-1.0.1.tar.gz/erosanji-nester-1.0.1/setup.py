eecf322ff794ec7b4e1686fe203386d8e43967cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:50 +0000

    Add package erosanji-nester/1.0.1/erosanji-nester-1.0.1.tar.gz

---
+++ packages/erosanji-nester/erosanji-nester-1.0.1.tar.gz/erosanji-nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+        name         = 'erosanji-nester',
+        version      = '1.0.1',
+        py_modules   = ['erosanji-nester'],
+        author       = 'Zheng Shi',
+        author_email = 'eromonstersanji@icloud.com',
+        url          = 'https://www.github.com/EroMonsterSanji',
+        description  = 'A simple printer of nested lists'
+)
