2630931d7ed1130f38aa7fa28d486e9b7fd69e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:35 +0000

    Add package mountwizzard/2.1.17/mountwizzard-2.1.17.zip

---
+++ packages/mountwizzard/mountwizzard-2.1.17.zip/mountwizzard-2.1.17/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='mountwizzard',
+    version='2.1.17',
+    packages=[
+        'mountwizzard',
+        'mountwizzard/support'
+    ],
+    install_requires=[
+        'PyQt5>=5.6',
+        'matplotlib>=1.5.3',
+        'pypiwin32>=219',
+        'pyfits>=3.4'
+    ],
+    include_package_data=True,
+    url='https://pypi.python.org/pypi/mountwizzard',
+    license='APL 2.0',
+    author='mw',
+    author_email='michael@wuertenberger.org',
+    description='tooling for a 10micron mount',
+)
