893aae66285614e63fc0ad0bf87f76b157f24361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package zoomascii/0.2/zoomascii-0.2.tar.gz

---
+++ packages/zoomascii/zoomascii-0.2.tar.gz/zoomascii-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup, Extension
+
+
+ext = Extension('zoomascii',
+                sources=['src/zoomascii.c', 'src/zoomascii.h'],
+#                extra_compile_args=["-O3"]
+                )
+
+setup(name         = "zoomascii",
+      version      = "0.2",
+      description  = "Faster versions of binascii functions.",
+      author       = "Sam Tregar",
+      author_email = 'sam@tregar.com',
+      platforms    = ["any"],
+      license      = "BSD",
+      url          = "http://github.com/samtregar/zoomascii",
+      packages     = find_packages(),
+      ext_modules  = [ext],
+      test_suite   = "tests",
+      )
---
+++ packages/zoomascii/zoomascii-0.2.tar.gz/zoomascii-0.2/tests/basic.py
@@ -0,0 +1,33 @@
+import unittest
+import zoomascii
+import binascii
+
+from os import listdir, path
+
+corpus = []
+data_dir = path.dirname(__file__) + '/../data'
+for fname in listdir(data_dir):
+    with open(data_dir + '/' + fname, 'rb') as fh:
+        data = ''.join(fh.readlines())
+        corpus.append(data)
+
+
+class BasicTests(unittest.TestCase):
+    def test_qp(self):
+        self.assertEqual(zoomascii.b2a_qp("dude"), 'dude')
+        self.assertEqual(zoomascii.b2a_qp("dude\t\r\n"), "dude=09\r\n")
+
+        # can't test for equality of output for zoomascii VS binascii
+        # - QP isn't a deterministic format and zoomascii will make
+        # different decisions about the data
+        for data in corpus:
+            zoom_encoded = zoomascii.b2a_qp(data)
+            self.assertEqual(binascii.a2b_qp(zoom_encoded),
+                             data)
+
+    def test_swapcase(self):
+        self.assertEqual(zoomascii.swapcase("dude"), 'DUDE')
+
+        for data in corpus:
+            self.assertEqual(zoomascii.swapcase(data), data.swapcase())
+            
