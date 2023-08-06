03bf7d16e2955305168ec0730f552998795d7d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package doodle/0.1/doodle-0.1.tar.gz

---
+++ packages/doodle/doodle-0.1.tar.gz/doodle-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='doodle',
+      version='0.1',
+      description='A library to segment objects in an image in a semi-automated fashion.',
+      url='https://github.com/pskeshu/doodle',
+      author='Kesavan Subburam',
+      author_email='pskesavan@tifrh.res.in',
+      license='MIT',
+      packages=['doodle'],
+      install_requires=['numpy>=1.12.1',
+                        'scikit-image>=0.13.0', 
+                        'matplotlib>=2.0.0'],
+      zip_safe=False)
