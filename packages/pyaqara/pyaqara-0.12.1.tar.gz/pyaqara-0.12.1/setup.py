96d4222237ec2826526568485561e108664a2d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package pyaqara/0.12.1/pyaqara-0.12.1.tar.gz

---
+++ packages/pyaqara/pyaqara-0.12.1.tar.gz/pyaqara-0.12.1/setup.py
@@ -0,0 +1,15 @@
+"""Setup file for aqara package."""
+from setuptools import setup, find_packages
+
+setup(name='pyaqara',
+ version='0.12.1',
+ description='Python API for interfacing with the Aqara gateway',
+ url='https://github.com/javefang/pyaqara',
+ download_url="https://github.com/javefang/pyaqara/tarball/0.12.1",
+ author='Xinghong Fang',
+ author_email= 'xinghong.fang@gmail.com',
+ license='MIT',
+ packages=['aqara'],
+ keywords = ['aqara', 'home', 'automation', 'sensor'],
+ install_requires=['pycrypto', 'PyDispatcher']
+)
