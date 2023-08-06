053309f90291f180cd936cf6b3c4b067724c56bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:01 +0000

    Add package pytodos/1.0.2/pytodos-1.0.2.tar.gz

---
+++ packages/pytodos/pytodos-1.0.2.tar.gz/pytodos-1.0.2/PyTodos/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from PyTodos import todo
+hello = 'x'
---
+++ packages/pytodos/pytodos-1.0.2.tar.gz/pytodos-1.0.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+entry_points = [
+    "t=PyTodos.todo:cli"
+]
+
+setup(
+    name="pytodos",
+    version='1.0.2',
+    description="",
+    long_description="",
+    author="chuanwu",
+    author_email="chuanwusun@gmail.com",
+    packages=['pytodos'],
+    url="https://github.com/chuanwu/PyToDos.py",
+    entry_points={"console_scripts": entry_points},
+    install_requires=[],
+
+)
