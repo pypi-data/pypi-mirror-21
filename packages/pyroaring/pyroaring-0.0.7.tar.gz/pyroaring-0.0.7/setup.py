0fd9e8ee9da72c1f638d9224fed9323ea5f02bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:33 +0000

    Add package pyroaring/0.0.7/pyroaring-0.0.7.tar.gz

---
+++ packages/pyroaring/pyroaring-0.0.7.tar.gz/pyroaring-0.0.7/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='pyroaring',
+    version='0.0.7',
+    description='Fast and lightweight set for unsigned 32 bits integers.',
+    url='https://github.com/Ezibenroc/PyRoaringBitMap',
+    author='Tom Cornebize',
+    author_email='tom.cornebize@gmail.com',
+    license='MIT',
+    packages=[
+        'pyroaring',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
