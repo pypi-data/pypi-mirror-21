4b5f6e1540be282c360749e83b67f2163c81abc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:55 +0000

    Add package portinus/0.9.0/portinus-0.9.0.tar.gz

---
+++ packages/portinus/portinus-0.9.0.tar.gz/portinus-0.9.0/portinus/monitor/cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import click
+import logging
+
+import portinus
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group()
+@click.option('-v', '--verbose', count=True, help="Enable more logging. More -v's for more logging")
+def task(verbose):
+    log_level = logging.WARNING
+    if verbose == 1:
+        log_level = logging.INFO
+    if verbose >= 2:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
+
+
+@task.command()
+@click.option('--name', required=True, help="The name of the service to remove")
+def check(name):
+    portinus.monitor.checker.run(name)
+
+
+if __name__ == "__main__":
+    task()
