ea74f4b744aa541a06fc8d495a7cfd39b74e0d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:42 +0000

    Add package kibitzr/3.0.2.dev2/kibitzr-3.0.2.dev2-py2.py3-none-any.whl

---
+++ packages/kibitzr/kibitzr-3.0.2.dev2-py2.py3-none-any.whl/kibitzr/notifier/__init__.py
@@ -0,0 +1,6 @@
+from .mailgun import post_mailgun  # noqa
+from .custom import post_python  # noqa
+from .bash import post_bash  # noqa
+from .gitter import post_gitter  # noqa
+from .slack import SlackSession  # noqa
+from .smtp import post_smtp  # noqa
