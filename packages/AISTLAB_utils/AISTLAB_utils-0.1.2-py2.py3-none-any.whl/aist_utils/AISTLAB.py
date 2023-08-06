8b8c6844cdcd9240f9661d93badfaeb21b34c63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:29 +0000

    Add package AISTLAB_utils/0.1.2/AISTLAB_utils-0.1.2-py2.py3-none-any.whl

---
+++ packages/AISTLAB_utils/AISTLAB_utils-0.1.2-py2.py3-none-any.whl/aist_utils/AISTLAB.py
@@ -0,0 +1,7 @@
+def about_aistlab():
+    """
+    WELCOME TO AISTLAB,4C103,XHU,CHENGDU,CHINA
+    WISH OUR FRIENDSHIP AND THE LABORATORY WILL LAST FOREVER.
+    WINXOS 2017-04-21
+    """
+    print(about_aistlab.__doc__)
---
+++ packages/AISTLAB_utils/AISTLAB_utils-0.1.2-py2.py3-none-any.whl/aist_utils/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# AISTLAB utils
+# Tools packages created by python.
+# winxos
+# since:2017-04-20
+
+__version__ = "0.1.2"
+__title__ = "AISTLAB_utils"
+__description__ = "Tools packages created by python."
+__long_description__ = open('README.rst').read()
+__url__ = "https://github.com/winxos/AIST_utils"
+__docsurl__ = ""
+__author__ = "winxos"
+__email__ = "winxos@hotmail.com"
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) AISTLAB"
