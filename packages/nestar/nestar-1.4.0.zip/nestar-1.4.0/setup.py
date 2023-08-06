bbcd012a200b2e694dfec3526b2c14a491017972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:15 +0000

    Add package nestar/1.4.0/nestar-1.4.0.zip

---
+++ packages/nestar/nestar-1.4.0.zip/nestar-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            =   'nestar',
+    version         =   '1.4.0',
+    py_modules      =   ['nestar'],
+    author          =   'Aska_Snr',
+    author_email    =   'samoah15@yahoo.com',
+    url             =   'http://www.headfirstlabs.com',
+    description     =   'A simple printer of nested list',
+    )
