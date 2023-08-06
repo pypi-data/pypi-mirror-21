03e00a6a064135f676d250b020045b976a390b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package django-include/0.1.0/django-include-0.1.0.tar.gz

---
+++ packages/django-include/django-include-0.1.0.tar.gz/django-include-0.1.0/include/expressions.py
@@ -0,0 +1,12 @@
+from django.contrib.postgres.fields import JSONField
+from django.db.models.expressions import Func
+
+
+class JSONBuildArray(Func):
+    function = 'JSON_BUILD_ARRAY'
+
+    def __init__(self, *args, **kwargs):
+        super(JSONBuildArray, self).__init__(*args, output_field=JSONField(), **kwargs)
+
+    def as_sqlite(self, compiler, connection, **extra_context):
+        return self.as_sql(compiler, connection, function='JSON_ARRAY', **extra_context)
