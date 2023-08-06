eb8c08a335c410f0e72f8d567a7382615a287280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:59 +0000

    Add package nerd/1.0.0/nerd-1.0.0.zip

---
+++ packages/nerd/nerd-1.0.0.zip/nerd-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name = 'nerd',
+version = '1.0.0',
+py_modules = ['nerd'],
+author = 'needssilens',
+author_email = 'needssilens@gmail.com',
+url = 'http://www.google.com',
+description = 'A simple printer of nested lists',)
