f18fe75cab2edb9b4f5ad950b5b1a8feffcca4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:48 +0000

    Add package cssdecl/0.0.0/cssdecl-0.0.0.tar.gz

---
+++ packages/cssdecl/cssdecl-0.0.0.tar.gz/cssdecl-0.0.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+from setuptools import setup
+
+DOCLINES = (__doc__ or '').split("\n")
+
+# Some boilerplate related to versioning taken from numpy
+
+MAJOR = 0
+MINOR = 0
+MICRO = 0
+ISRELEASED = True
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def get_version_info():
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+
+    return FULLVERSION, GIT_REVISION
+
+
+def setup_package():
+    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    try:
+        # See setup.cfg
+        setup(setup_requires=['pytest-runner'],
+              tests_require=['pytest>=2.7', 'pytest-cov~=2.4'],
+              version=get_version_info()[0])
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+    return
+
+
+if __name__ == '__main__':
+    setup_package()
