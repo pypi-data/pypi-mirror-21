cac28696658a19a68c70e857880699ea0fe5c854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:16 +0000

    Add package pyreqs/0.1.1/pyreqs-0.1.1.tar.gz

---
+++ packages/pyreqs/pyreqs-0.1.1.tar.gz/pyreqs-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 rmad17 <souravbasu17@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+from setuptools import setup
+
+setup(
+    name='pyreqs',
+    version='0.1.1',
+    description='Better manage python requirements file',
+    author='Sourav Basu',
+    author_email='souravbasu17@gmail.com',
+    url='https://github.com/rmad17/pyreqs',
+    py_modules=['pyreqs'],
+    install_requires=[
+        'click', 'sh'
+    ],
+    entry_points='''
+        [console_scripts]
+        pyreqs=pyreqs:pyreqs
+    ''',
+)
