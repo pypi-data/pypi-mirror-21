86646812f786a17ac5da774d8e1882eea14b0158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:21 +0000

    Add package dockerscript/0.2.0/dockerscript-0.2.0.tar.gz

---
+++ packages/dockerscript/dockerscript-0.2.0.tar.gz/dockerscript-0.2.0/dockerscript/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tool for generating Dockerfiles from Python
+"""
+
+from .image import Image
+from .operations import (
+    base_image,
+    copy,
+    environment,
+    label,
+    run,
+    shell,
+    workdir,
+)
