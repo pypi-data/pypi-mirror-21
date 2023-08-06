d20b59418a2c4ab27733bb7cfbe85e05261f7845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package nesterglebson/1.1.0/nesterglebson-1.1.0.zip

---
+++ packages/nesterglebson/nesterglebson-1.1.0.zip/nesterglebson-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nesterglebson',
+        version = '1.1.0',
+        py_modules = ['nesterglebson'],
+        author = 'Gleb_Grishakov',
+        author_email = 'glebson1988@yandex.ru',
+        url = 'http://web-gleb.ru',
+        description = 'A simple printer of nested lists',
+      )
