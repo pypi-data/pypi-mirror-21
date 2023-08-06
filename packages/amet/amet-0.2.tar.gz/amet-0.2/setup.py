eb3b9ca68b095d09fd2d188de034eb4d6d320333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package amet/0.2/amet-0.2.tar.gz

---
+++ packages/amet/amet-0.2.tar.gz/amet-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup (
+    name='amet',
+    packages=['amet'],
+    version='0.2',
+    description='A library to read complex configuration from environment variables.',
+    author='Mauro de Carvalho',
+    author_email='mauro.dec@gmail.com',
+    url='https://github.com/maurodec/amet',
+    download_url='https://github.com/maurodec/amet/archive/0.1.tar.gz',
+    keywords=['configuration', 'environment'],
+    classifiers=[],
+)
