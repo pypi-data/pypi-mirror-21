05e3b7c7fdb5c24845abb3d2f4191070b9189e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:40 +0000

    Add package log_progress/1.1/log_progress-1.1.tar.gz

---
+++ packages/log_progress/log_progress-1.1.tar.gz/log_progress-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='log_progress',    # This is the name of your PyPI-package.
+    version='1.1',                          # Update the version number for new releases
+    packages=['log_progress']
+)
