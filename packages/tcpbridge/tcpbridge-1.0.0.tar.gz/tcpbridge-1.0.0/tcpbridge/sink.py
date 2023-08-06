d9cf47e35389d858ef71b11f70a2902c2d1aa731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:03 +0000

    Add package tcpbridge/1.0.0/tcpbridge-1.0.0.tar.gz

---
+++ packages/tcpbridge/tcpbridge-1.0.0.tar.gz/tcpbridge-1.0.0/tcpbridge/sink.py
@@ -0,0 +1,88 @@
+#  Copyright (C) Emlid Limited - All Rights Reserved
+#  All information contained in this file is the property of Emlid Limited.
+#  The intellectual and technical concepts contained herein are proprietary to
+#  Emlid Limited and are protected by copyright law. Distribution of this
+#  information or reproduction of this material is strictly forbidden without
+#  written permission obtained from Emlid Limited.
+#  Written by Denis Chagin <denis.chagin@emlid.com>
+
+
+import abc
+import socket
+
+BUFFER_SIZE = 2048
+
+
+class SinkError(Exception):
+    pass
+
+
+class AbstractSink(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def read(self):
+        pass
+
+    @abc.abstractmethod
+    def write(self, data):
+        pass
+
+    @abc.abstractmethod
+    def close(self):
+        pass
+
+    @abc.abstractproperty
+    def fileno(self):
+        pass
+
+
+class SocketSink(AbstractSink):
+    def __init__(self, sock):
+        self.sock = sock
+
+    def read(self):
+        try:
+            data = self.sock.recv(BUFFER_SIZE)
+            if not data:
+                raise SinkError
+            return data
+        except socket.error as error:
+            raise SinkError(str(error))
+
+    def write(self, data):
+        _bytes = self.sock.send(data)
+        return _bytes
+
+    def close(self):
+        self.sock.close()
+
+    @property
+    def fileno(self):
+        return self.sock.fileno()
+
+
+class FileSink(AbstractSink):
+    def __init__(self, file_object):
+        self.file_object = file_object
+
+    def write(self, data):
+        self.file_object.write(data)
+        self.file_object.flush()
+
+    def read(self):
+        try:
+            data = self.file_object.read(BUFFER_SIZE)
+            return data
+        except ValueError as error:
+            raise SinkError(str(error))
+
+    def close(self):
+        try:
+            self.file_object.close()
+        except IOError:
+            pass
+
+    @property
+    def fileno(self):
+        return self.file_object
