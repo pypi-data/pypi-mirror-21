517af2bdaf4c17fb0fc7cd925f953a373e46c59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:20 +0000

    Add package hedgehog-station-controller/2.6.8/hedgehog-station-controller-2.6.8.tar.gz

---
+++ packages/hedgehog-station-controller/hedgehog-station-controller-2.6.8.tar.gz/hedgehog-station-controller-2.6.8/core/common.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+import yaml
+import datetime
+
+def setupLogging(logging_level, logfile):
+    root = logging.getLogger()
+    root.setLevel(logging_level)
+
+    logging.basicConfig(filename=logfile, level=logging_level)
+
+    # ch = logging.StreamHandler(sys.stdout)
+    # ch.setLevel(logging_level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # root.setFormatter(formatter)
+    # root.addHandler(ch)
+    return root;
+
+def loadConfiguration(externalizedFilename, defaults):
+    try:
+        with open(externalizedFilename, 'r') as ymlfile:
+            configuration = yaml.load(ymlfile)
+
+        logging.info('Configuration loaded.')
+    except IOError as e:
+        logging.warning(e)
+        try:
+            logging.info('Initialize new configuration ...')
+            with open(externalizedFilename, 'w') as ymlfile:
+                configuration = yaml.dump(defaults, ymlfile, default_flow_style=False)
+            logging.info('Default configuration created.')
+        except IOError:
+            raise EnvironmentError('Configuration file mq could not created.')
+    return configuration
+
+def datetime_handler(self, x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    else:
+        return None
+    # raise TypeError("Unknown type of" + x)
