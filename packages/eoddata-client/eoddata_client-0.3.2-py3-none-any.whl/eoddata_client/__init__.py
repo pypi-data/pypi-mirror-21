ea35d3b4ccb5ff850d3a7f7b821ff0f2ae835243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:22 +0000

    Add package eoddata-client/0.3.2/eoddata_client-0.3.2-py3-none-any.whl

---
+++ packages/eoddata-client/eoddata_client-0.3.2-py3-none-any.whl/eoddata_client/__init__.py
@@ -0,0 +1,10 @@
+from .eoddata_client import (
+    EodDataHttpClient,
+    PERIODS as eod_periods
+)
+
+from .business_entities import (
+    EodDataQuoteExtended,
+    EodDataQuoteCompact,
+    EodDataExchange
+)
